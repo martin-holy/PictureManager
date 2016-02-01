39d9052062fb00816a3ecc91e87de5cf1a9f9f37
Martin Holý <martin.holy@hotmail.com>
2016-02-01 14:18:46 +0000

    InputDialog

---
+++ PictureManagerWPF/Dialogs/InputDialog.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel;
+using System.Windows;
+
+namespace PictureManager.Dialogs {
+  /// <summary>
+  /// Interaction logic for InputDialog.xaml
+  /// </summary>
+  public partial class InputDialog: INotifyPropertyChanged {
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    public void OnPropertyChanged(string name) {
+      PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
+
+    private string _iconName = "appbar_bug";
+    private string _question;
+    private string _answer;
+
+    public string IconName { get { return _iconName;} set { _iconName = value; OnPropertyChanged("IconName"); } }
+    public string Question { get { return _question; } set { _question = value; OnPropertyChanged("Question"); } }
+    public string Answer { get { return _answer; } set { _answer = value; OnPropertyChanged("Answer"); } }
+
+    public InputDialog() {
+      InitializeComponent();
+    }
+
+    private void BtnDialogOk_OnClicktnDialogOk_Click(object sender, RoutedEventArgs e) {
+      DialogResult = true;
+    }
+  }
+}
