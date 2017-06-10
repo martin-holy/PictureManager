d9769a57e8c022b0018d9685f20cc1f25a6f2aa1
Martin Holý <martin.holy@hotmail.com>
2017-06-10 11:46:33 +0200

    cleaning sql scripts

--- PictureManagerWPF/SQL.sql
+++ PictureManagerWPF/SQL.sql
@@ -1,22 +1,30 @@
 ﻿--                                        promazani duplicit
 
 --select Path from Directories group by Path having count(Id) > 1
-
---select * from MediaItems as MI where not exists (select D.Id from Directories as D where D.Id = MI.DirectoryId)
-
---select * from MediaItemKeyword as MIK where not exists (select MI.Id from MediaItems as MI where MI.Id = MIK.MediaItemId)
-
---select * from MediaItemPerson as MIP where not exists (select MI.Id from MediaItems as MI where MI.Id = MIP.MediaItemId)
-
-
 --delete from Directories where Path in (select Path from Directories group by Path having count(Id) > 1)
 
---delete from MediaItems where Id in (select MI.Id from MediaItems as MI where not exists (select D.Id from Directories as D where D.Id = MI.DirectoryId))
-
---delete from MediaItemKeyword where Id in (select MIK.Id from MediaItemKeyword as MIK where not exists (select MI.Id from MediaItems as MI where MI.Id = MIK.MediaItemId))
-
---delete from MediaItemPerson where Id in (select MIP.Id from MediaItemPerson as MIP where not exists (select MI.Id from MediaItems as MI where MI.Id = MIP.MediaItemId))
 
 
 -- smazani nepouzitych Keywords
---delete from Keywords where Name not like "%/%" and Id not in (select KeywordId from MediaItemKeyword)
+select * from Keywords where Name not like "%/%" and Id not in (select KeywordId from MediaItemKeyword)
+delete from Keywords where Name not like "%/%" and Id not in (select KeywordId from MediaItemKeyword)
+
+-- smazani vazby MediaItemKeyword, kdyz neexistuje MediaItem
+select * from MediaItemKeyword where Id in (select MIK.Id from MediaItemKeyword as MIK where not exists (select MI.Id from MediaItems as MI where MI.Id = MIK.MediaItemId))
+delete from MediaItemKeyword where Id in (select MIK.Id from MediaItemKeyword as MIK where not exists (select MI.Id from MediaItems as MI where MI.Id = MIK.MediaItemId))
+
+-- smazani MediaItemKeyword, kdyz neexistuje Keyword
+select * from MediaItemKeyword where Id in (select MIK.Id from MediaItemKeyword as MIK where not exists (select K.Id from Keywords as K where K.Id = MIK.KeywordId))
+delete from MediaItemKeyword where Id in (select MIK.Id from MediaItemKeyword as MIK where not exists (select K.Id from Keywords as K where K.Id = MIK.KeywordId))
+
+-- smazani vazby MediaItemPerson, kdyz neexistuje MediaItem
+select * from MediaItemPerson where Id in (select MIP.Id from MediaItemPerson as MIP where not exists (select MI.Id from MediaItems as MI where MI.Id = MIP.MediaItemId))
+delete from MediaItemPerson where Id in (select MIP.Id from MediaItemPerson as MIP where not exists (select MI.Id from MediaItems as MI where MI.Id = MIP.MediaItemId))
+
+-- smazani vazby MediaItemPerson, kdyz neexistuje Person
+select * from MediaItemPerson where Id in (select MIP.Id from MediaItemPerson as MIP where not exists (select P.Id from People as P where P.Id = MIP.PersonId))
+delete from MediaItemPerson where Id in (select MIP.Id from MediaItemPerson as MIP where not exists (select P.Id from People as P where P.Id = MIP.PersonId))
+
+-- smazani slozek bez MediaItems
+select * from Directories where Id not in (select distinct DirectoryId from MediaItems)
+delete from Directories where Id not in (select distinct DirectoryId from MediaItems)
