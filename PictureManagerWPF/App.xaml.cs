153b1b13b828f079f0d9f2533c5cab189f44dd2e
martin-holy <martin.holy@hotmail.com>
2020-04-29 16:10:51 +0200

    Refactoring

--- PictureManagerWPF/App.xaml.cs
+++ PictureManagerWPF/App.xaml.cs
@@ -3,37 +3,30 @@ using System.Windows;
 using PictureManager.Dialogs;
 
 namespace PictureManager {
-  /// <summary>
-  /// Interaction logic for App.xaml
-  /// </summary>
   public partial class App {
     public static AppCore Core => (AppCore) Current.Properties[nameof(AppProperty.AppCore)];
     public static WMain WMain => (WMain) Current.Properties[nameof(AppProperty.WMain)];
 
-    private void App_Startup(object sender, StartupEventArgs e) {
-      AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
-    }
-
     protected override async void OnStartup(StartupEventArgs e) {
       base.OnStartup(e);
 
-      Current.Properties[nameof(AppProperty.AppCore)] = new AppCore();
-      
+      AppDomain.CurrentDomain.UnhandledException += delegate(object o, UnhandledExceptionEventArgs exArgs) {
+        var wError = new UnhandledErrorDialog {TbError = {Text = ((Exception) exArgs.ExceptionObject).ToString()}};
+        wError.ShowDialog();
+      };
+
       var splashScreen = new Views.SplashScreen();
       MainWindow = splashScreen;
       splashScreen.Show();
 
+      Current.Properties[nameof(AppProperty.AppCore)] = new AppCore();
       await Core.InitAsync(splashScreen.ProgressMessage);
 
       Current.Properties[nameof(AppProperty.WMain)] = new WMain();
       MainWindow = WMain;
       WMain.Show();
-      splashScreen.Close();
-    }
 
-    private static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e) {
-      var wError = new UnhandledErrorDialog { TbError = {Text = ((Exception) e.ExceptionObject).ToString()}};
-      wError.ShowDialog();
+      splashScreen.Close();
     }
   }
 }
