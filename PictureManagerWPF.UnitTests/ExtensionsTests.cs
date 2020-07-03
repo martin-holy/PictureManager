40b5eb131bc3c4be9bfec3f489b195cbfea9a389
martin-holy <martin.holy@hotmail.com>
2020-07-03 13:20:40 +0200

    add to list in order

---
+++ PictureManagerWPF.UnitTests/ExtensionsTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PictureManager.Domain;
+
+namespace PictureManagerWPF.UnitTests {
+  [TestClass]
+  public class ExtensionsTests {
+    [TestMethod]
+    public void AddInOrder() {
+      var list = new List<string>{"a", "b", "c", "d"};
+      list.AddInOrder("ba", (a, b) => string.Compare(a, b, StringComparison.OrdinalIgnoreCase) >= 0);
+
+      Assert.AreEqual(list.IndexOf("ba"), 2);
+    }
+  }
+}
