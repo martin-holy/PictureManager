edd04b45818322c95b1167e8bd5e1f19e87d3b3f
martin-holy <martin.holy@hotmail.com>
2021-05-01 13:30:58 +0100

    SlidePanel and SlidePanelsGrid Controls

---
+++ PictureManagerWPF/CustomControls/SlidePanel.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Animation;
+
+namespace PictureManager.CustomControls {
+  public class SlidePanel: Control, INotifyPropertyChanged {
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    public void OnPropertyChanged([CallerMemberName] string name = null) {
+      PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
+
+    public static readonly DependencyProperty IsOpenProperty = DependencyProperty.Register(
+      nameof(IsOpen), typeof(bool), typeof(SlidePanel), new PropertyMetadata(true));
+
+    public static readonly DependencyProperty ContentProperty = DependencyProperty.Register(
+      nameof(Content), typeof(FrameworkElement), typeof(SlidePanel));
+
+    public static readonly DependencyProperty PositionProperty = DependencyProperty.Register(
+      nameof(Position), typeof(Dock), typeof(SlidePanel));
+
+    private bool _isPinned = true;
+
+    public bool IsPinned {
+      get => _isPinned;
+      set {
+        _isPinned = value;
+        OnPropertyChanged();
+        OnIsPinnedChanged?.Invoke();
+      }
+    }
+
+    public bool IsOpen {
+      get => (bool) GetValue(IsOpenProperty);
+      set {
+        SetValue(IsOpenProperty, value);
+        if (value)
+          _sbOpen.Begin(this); 
+        else 
+          _sbClose.Begin(this);
+      }
+    }
+
+    public Dock Position {
+      get => (Dock)GetValue(PositionProperty);
+      set => SetValue(PositionProperty, value);
+    }
+
+    public FrameworkElement Content {
+      get => (FrameworkElement)GetValue(ContentProperty);
+      set => SetValue(ContentProperty, value);
+    }
+
+    public Action OnIsPinnedChanged;
+
+    private ThicknessAnimation _openAnimation;
+    private ThicknessAnimation _closeAnimation;
+    private Storyboard _sbOpen;
+    private Storyboard _sbClose;
+
+    static SlidePanel() {
+      DefaultStyleKeyProperty.OverrideMetadata(typeof(SlidePanel),
+        new FrameworkPropertyMetadata(typeof(SlidePanel)));
+    }
+
+    public override void OnApplyTemplate() {
+      base.OnApplyTemplate();
+
+      MouseLeave += delegate {
+        if (!IsPinned && IsOpen) IsOpen = false;
+      };
+
+      UpdateAnimation();
+
+      Storyboard.SetTargetProperty(_openAnimation, new PropertyPath(MarginProperty));
+      _sbOpen = new Storyboard();
+      _sbOpen.Children.Add(_openAnimation);
+
+      Storyboard.SetTargetProperty(_closeAnimation, new PropertyPath(MarginProperty));
+      _sbClose = new Storyboard();
+      _sbClose.Children.Add(_closeAnimation);
+    }
+
+    // creates new or update existing animation if panel width changes
+    public void UpdateAnimation() {
+      if (_openAnimation == null)
+        _openAnimation = new ThicknessAnimation {
+          Duration = new Duration(TimeSpan.FromMilliseconds(250)),
+          To = new Thickness(0, Margin.Top, 0, Margin.Bottom)
+        };
+
+      if (_closeAnimation == null)
+        _closeAnimation = new ThicknessAnimation {
+          Duration = new Duration(TimeSpan.FromMilliseconds(250)),
+          From = new Thickness(0, Margin.Top, 0, Margin.Bottom)
+        };
+
+      var th = new Thickness();
+      switch (Position) {
+        case Dock.Left:
+          th = new Thickness(Width * -1, Margin.Top, 0, Margin.Bottom);
+          break;
+        case Dock.Right:
+          th = new Thickness(0, Margin.Top, Width * -1, Margin.Bottom);
+          break;
+      }
+
+      _openAnimation.From = th;
+      _closeAnimation.To = th;
+    }
+  }
+}
---
+++ PictureManagerWPF/CustomControls/SlidePanelsGrid.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace PictureManager.CustomControls {
+  public class SlidePanelsGrid: Control {
+
+    public static readonly DependencyProperty ContentLeftProperty = DependencyProperty.Register(
+      nameof(ContentLeft), typeof(SlidePanel), typeof(SlidePanelsGrid));
+    public static readonly DependencyProperty ContentMiddleProperty = DependencyProperty.Register(
+      nameof(ContentMiddle), typeof(FrameworkElement), typeof(SlidePanelsGrid));
+    public static readonly DependencyProperty ContentRightProperty = DependencyProperty.Register(
+      nameof(ContentRight), typeof(SlidePanel), typeof(SlidePanelsGrid));
+    public static readonly DependencyProperty GridSplitterWidthProperty = DependencyProperty.Register(
+      nameof(GridSplitterWidth), typeof(int), typeof(SlidePanelsGrid), new PropertyMetadata(3));
+
+    public SlidePanel ContentLeft {
+      get => (SlidePanel)GetValue(ContentLeftProperty);
+      set => SetValue(ContentLeftProperty, value);
+    }
+
+    public FrameworkElement ContentMiddle {
+      get => (FrameworkElement)GetValue(ContentMiddleProperty);
+      set => SetValue(ContentMiddleProperty, value);
+    }
+
+    public SlidePanel ContentRight {
+      get => (SlidePanel)GetValue(ContentRightProperty);
+      set => SetValue(ContentRightProperty, value);
+    }
+
+    public int GridSplitterWidth {
+      get => (int) GetValue(GridSplitterWidthProperty);
+      set => SetValue(GridSplitterWidthProperty, value);
+    }
+
+    public Action OnContentLeftWidthChanged;
+    public Action OnContentRightWidthChanged;
+
+    private Grid _mainGrid;
+
+    static SlidePanelsGrid() {
+      DefaultStyleKeyProperty.OverrideMetadata(typeof(SlidePanelsGrid),
+        new FrameworkPropertyMetadata(typeof(SlidePanelsGrid)));
+    }
+
+    public override void OnApplyTemplate() {
+      base.OnApplyTemplate();
+
+      // open Flyout if is not open and mouse cursor is close to left or right edge of FlyoutsControl
+      MouseMove += delegate(object sender, MouseEventArgs e) {
+        var pos = e.GetPosition(this);
+        if (pos.X < 5 && ContentLeft != null && !ContentLeft.IsOpen)
+          ContentLeft.IsOpen = true;
+        if (pos.X > ActualWidth - 5 && ContentRight != null && !ContentRight.IsOpen)
+          ContentRight.IsOpen = true;
+      };
+
+      if (Template.FindName("PART_MainGrid", this) is Grid mainGrid) {
+        _mainGrid = mainGrid;
+      }
+
+      if (Template.FindName("PART_GridSplitterLeft", this) is GridSplitter gsLeft) {
+        gsLeft.DragDelta += delegate { UpdateContentLeftWidth(); };
+        gsLeft.DragCompleted += delegate { UpdateContentLeftWidth(); };
+      }
+
+      if (Template.FindName("PART_GridSplitterRight", this) is GridSplitter gsRight) {
+        gsRight.DragDelta += delegate { UpdateContentRightWidth(); };
+        gsRight.DragCompleted += delegate { UpdateContentRightWidth(); };
+      }
+
+      if (ContentLeft != null) {
+        _mainGrid.ColumnDefinitions[0].Width = new GridLength(ContentLeft.Width);
+        _mainGrid.ColumnDefinitions[1].Width = new GridLength(GridSplitterWidth);
+
+        ContentLeft.OnIsPinnedChanged += delegate {
+          _mainGrid.ColumnDefinitions[0].Width = new GridLength(ContentLeft.IsPinned ? ContentLeft.Width : 0);
+          _mainGrid.ColumnDefinitions[1].Width = new GridLength(ContentLeft.IsPinned ? GridSplitterWidth : 0);
+        };
+      }
+
+      if (ContentRight != null) {
+        _mainGrid.ColumnDefinitions[3].Width = new GridLength(GridSplitterWidth);
+        _mainGrid.ColumnDefinitions[4].Width = new GridLength(ContentRight.Width);
+
+        ContentRight.OnIsPinnedChanged += delegate {
+          _mainGrid.ColumnDefinitions[3].Width = new GridLength(ContentRight.IsPinned ? GridSplitterWidth : 0);
+          _mainGrid.ColumnDefinitions[4].Width = new GridLength(ContentRight.IsPinned ? ContentRight.Width : 0);
+        };
+      }
+    }
+
+    private void UpdateContentLeftWidth() {
+      if (ContentLeft == null) return;
+      ContentLeft.Width = _mainGrid.ColumnDefinitions[0].ActualWidth;
+      ContentLeft.UpdateAnimation();
+      OnContentLeftWidthChanged?.Invoke();
+    }
+
+    private void UpdateContentRightWidth() {
+      if (ContentRight == null) return;
+      ContentRight.Width = _mainGrid.ColumnDefinitions[4].ActualWidth;
+      ContentRight.UpdateAnimation();
+      OnContentRightWidthChanged?.Invoke();
+    }
+
+  }
+}
