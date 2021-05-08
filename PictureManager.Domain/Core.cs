4b432668850cd734dcf9174f8fadbfe2356522cf
martin-holy <martin.holy@hotmail.com>
2021-05-08 15:12:55 +0100

    new category MediaItemClips

--- PictureManager.Domain/Core.cs
+++ PictureManager.Domain/Core.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
@@ -25,6 +25,8 @@ namespace PictureManager.Domain {
     public GeoNames GeoNames { get; }
     public Viewers Viewers { get; }
     public CategoryGroups CategoryGroups { get; }
+
+    public MediaItemClipsCategory MediaItemClipsCategory { get; }
     #endregion
 
     public SimpleDB.SimpleDB Sdb { get; private set; }
@@ -57,6 +59,7 @@ namespace PictureManager.Domain {
       CategoryGroups = new CategoryGroups();
 
       MediaItems = new MediaItems();
+      MediaItemClipsCategory = new MediaItemClipsCategory();
       VideoClips = new VideoClips();
       VideoClipsGroups = new VideoClipsGroups();
     }
