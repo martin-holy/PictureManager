8561990f1f1a3985543e380a791c098894c83976
martin-holy <martin.holy@hotmail.com>
2021-06-05 18:00:53 +0100

    List<IRecord> All

--- SimpleDB/ITable.cs
+++ SimpleDB/ITable.cs
@@ -1,6 +1,9 @@
-﻿namespace SimpleDB {
+﻿using System.Collections.Generic;
+
+namespace SimpleDB {
   public interface ITable {
     TableHelper Helper { get; set; }
+    List<IRecord> All { get; }
     void NewFromCsv(string csv);
     void LinkReferences();
     void SaveToFile();
