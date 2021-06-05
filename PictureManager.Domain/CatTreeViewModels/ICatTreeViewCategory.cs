488c6c46529995719c9faadff252aced9a04e54c
martin-holy <martin.holy@hotmail.com>
2021-06-05 18:28:58 +0100

    ICatTreeViewCategory interface

--- PictureManager.Domain/CatTreeViewModels/ICatTreeViewCategory.cs
+++ PictureManager.Domain/CatTreeViewModels/ICatTreeViewCategory.cs
@@ -1,17 +1,32 @@
 namespace PictureManager.Domain.CatTreeViewModels {
-  public interface ICatTreeViewCategory: ICatTreeViewBaseItem {
+  public interface ICatTreeViewCategory: ICatTreeViewItem {
     Category Category { get; }
     bool CanHaveGroups { get; set; }
     bool CanHaveSubItems { get; set; }
-    bool CanModifyItems { get; set; }
+    bool CanCreateItems { get; set; }
+    bool CanRenameItems { get; set; }
+    bool CanDeleteItems { get; set; }
+    bool CanCopyItem { get; set; }
+    bool CanMoveItem { get; set; }
+
     IconName CategoryGroupIconName { get; }
-    string ValidateNewItemTitle(ICatTreeViewBaseItem root, string name);
-    void ItemCreate(ICatTreeViewBaseItem root, string name);
-    void ItemRename(ICatTreeViewBaseItem item, string name);
-    void ItemDelete(ICatTreeViewBaseItem item);
-    void ItemMove(ICatTreeViewBaseItem item, ICatTreeViewBaseItem dest, bool aboveDest);
-    string ValidateNewGroupTitle(ICatTreeViewBaseItem root, string name);
-    void GroupCreate(ICatTreeViewBaseItem root, string name);
+
+    bool CanDrop(object src, ICatTreeViewItem dest);
+    void OnDrop(object src, ICatTreeViewItem dest, bool aboveDest, bool copy);
+
+    bool CanCreateItem(ICatTreeViewItem item);
+    bool CanRenameItem(ICatTreeViewItem item);
+    bool CanDeleteItem(ICatTreeViewItem item);
+
+    string ValidateNewItemTitle(ICatTreeViewItem root, string name);
+    ICatTreeViewItem ItemCreate(ICatTreeViewItem root, string name);
+    void ItemRename(ICatTreeViewItem item, string name);
+    void ItemDelete(ICatTreeViewItem item);
+    void ItemCopy(ICatTreeViewItem item, ICatTreeViewItem dest);
+    void ItemMove(ICatTreeViewItem item, ICatTreeViewItem dest, bool aboveDest);
+
+    string ValidateNewGroupTitle(ICatTreeViewItem root, string name);
+    ICatTreeViewGroup GroupCreate(ICatTreeViewCategory cat, string name);
     void GroupRename(ICatTreeViewGroup group, string name);
     void GroupDelete(ICatTreeViewGroup group);
     void GroupMove(ICatTreeViewGroup group, ICatTreeViewGroup dest, bool aboveDest);
