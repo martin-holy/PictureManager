c7765d70088af1db2b877be61ca5b338b5659a14
martin-holy <martin.holy@hotmail.com>
2021-06-05 19:16:08 +0100

    removed

--- PictureManager.Domain/Models/ICategoryItem.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PictureManager.Domain.Models {
-  public interface ICategoryItem {
-    IconName CategoryGroupIconName { get; }
-    string ValidateNewItemTitle(BaseTreeViewItem root, string name);
-    void ItemCreate(BaseTreeViewItem root, string name);
-    void ItemRename(BaseTreeViewItem item, string name);
-    void ItemDelete(BaseTreeViewItem item);
-  }
-}
