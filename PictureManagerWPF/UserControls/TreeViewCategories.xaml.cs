ee58af2e3b4fc0137ad627fd0b362b683e283fe0
martin-holy <martin.holy@hotmail.com>
2021-06-05 18:14:28 +0100

    swich to CatTreeView

--- PictureManagerWPF/UserControls/TreeViewCategories.xaml.cs
+++ PictureManagerWPF/UserControls/TreeViewCategories.xaml.cs
@@ -1,138 +1,135 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Input;
-using System.Windows.Media;
-using MahApps.Metro.Controls;
 using PictureManager.Commands;
 using PictureManager.Domain;
+using PictureManager.Domain.CatTreeViewModels;
 using PictureManager.Domain.Models;
 using PictureManager.ViewModels;
 
 namespace PictureManager.UserControls {
   public partial class TreeViewCategories {
-    private Point _dragDropStartPosition;
-    private object _dragDropObject;
-
     public TreeView TreeView => TvCategories;
 
     public TreeViewCategories() {
       InitializeComponent();
+      TreeViewItemsEvents();
+    }
+
+    private static void TreeViewItemsEvents() {
+      CatTreeViewUtils.OnAfterItemRename += delegate(object sender, EventArgs args) {
+        if (sender is Folder folder) {
+          // reload if the folder was selected before
+          if (folder.IsSelected)
+            App.Core.TreeView_Select(folder, false, false, false);
+        }
+      };
+
+      CatTreeViewUtils.OnAfterItemDelete += delegate (object sender, EventArgs args) {
+        if (sender is Folder folder) {
+          // delete folder, subfolders and mediaItems from file system
+          if (Directory.Exists(folder.FullPath))
+            AppCore.FileOperationDelete(new List<string> { folder.FullPath }, true, false);
+        }
+      };
+
+      CatTreeViewUtils.OnAfterOnDrop += delegate(object sender, EventArgs args) {
+        var data = (object[]) sender;
+        var src = data[0];
+        var dest = data[1] as ICatTreeViewItem;
+        //var aboveDest = (bool) data[2];
+        var copy = (bool) data[3];
+
+        switch (src) {
+          case Folder srcData: { // Folder
+            var foMode = copy ? FileOperationMode.Copy : FileOperationMode.Move;
+
+            FoldersViewModel.CopyMove(foMode, srcData, (Folder) dest);
+            App.Core.Model.MediaItems.Helper.IsModified = true;
+            App.Core.Model.Folders.Helper.IsModified = true;
+            App.Core.Model.FolderKeywords.Load();
+
+            // reload last selected source if was moved
+            if (foMode == FileOperationMode.Move && srcData.IsSelected) {
+              var folder = ((Folder) dest)?.GetByPath(srcData.Title);
+              if (folder == null) return;
+              CatTreeViewUtils.ExpandTo(folder);
+              App.Core.TreeView_Select(folder, false, false, false);
+            }
+
+            break;
+          }
+          case string[] _: { // MediaItems
+            var foMode = copy ? FileOperationMode.Copy : FileOperationMode.Move;
+            App.Core.MediaItemsViewModel.CopyMove(
+              foMode, App.Core.Model.MediaItems.ThumbsGrid.FilteredItems.Where(x => x.IsSelected).ToList(), (Folder)dest);
+            App.Core.Model.MediaItems.Helper.IsModified = true;
+
+            break;
+          }
+        }
+
+        App.Core.Model.Sdb.SaveAllTables();
+        App.Core.Model.MarkUsedKeywordsAndPeople();
+      };
     }
 
     private void BtnNavCategory_OnClick(object sender, RoutedEventArgs e) {
-      var cat = (BaseTreeViewItem)((Button)sender).DataContext;
-      if (!(TvCategories.ItemContainerGenerator.ContainerFromItem(cat) is TreeViewItem tvi)) return;
-      var sv = TvCategories.FindChildren<ScrollViewer>(true).SingleOrDefault();
-      sv?.ScrollToBottom();
-      tvi.BringIntoView();
-      sv?.ScrollToHorizontalOffset(0);
+      TvCategories.ScrollTo((ICatTreeViewItem)((Button)sender).DataContext);
     }
 
-    public void TreeItemBringIntoView(BaseTreeViewItem item) {
-      // here I assume that item and all parents are expanded
-      // ScrollToBottom will cache TreeViewItem so it can be bring ed into view
-      var sv = TvCategories.FindChildren<ScrollViewer>(true).SingleOrDefault();
-      sv?.ScrollToBottom();
-      App.WMain.UpdateLayout();
-
-      // get all parents of item
-      var items = new List<BaseTreeViewItem>();
-      item.GetThisAndParentRecursive(ref items);
-
-      // add top parent for some cases
-      switch (item) {
-        case Folder _: {
-          items.Add(App.Core.Model.Folders);
-          App.Core.Model.Folders.IsExpanded = true;
-          break;
-        }
-        case GeoName _: {
-          items.Add(App.Core.Model.GeoNames);
-          App.Core.Model.GeoNames.IsExpanded = true;
-          break;
-        }
-      }
-
-      items.Reverse();
-
-      // get container for item
-      var elm = TvCategories as ItemsControl;
-      foreach (var i in items)
-        elm = elm?.ItemContainerGenerator.ContainerFromItem(i) as ItemsControl;
-
-      elm?.BringIntoView();
-      sv?.ScrollToHorizontalOffset(0);
-    }
-
-    //this is PreviewMouseRightButtonDown on StackPanel in TreeView
     private void AttachContextMenu(object sender, MouseButtonEventArgs e) {
       e.Handled = true;
-      var stackPanel = (StackPanel)sender;
-      if (stackPanel.ContextMenu != null) return;
 
-      var item = stackPanel.DataContext;
-      var menu = new ContextMenu { Tag = item };
-      var binding = new Binding("PlacementTarget") {
+      var b = Extensions.FindThisOrParent<Border>(e.OriginalSource as FrameworkElement, "Border");
+
+      if (b == null || b.ContextMenu?.Tag is bool) return;
+
+      var item = b.DataContext;
+      var menu = b.ContextMenu;
+      var firstItem = true;
+      var binding = new Binding(nameof(ContextMenu.PlacementTarget)) {
         RelativeSource = new RelativeSource(RelativeSourceMode.FindAncestor, typeof(ContextMenu), 1)
       };
 
+      if (menu == null) menu = new ContextMenu();
+      menu.Tag = true; // indicates that menu has been fully created
+
       void AddMenuItem(ICommand command) {
+        if (firstItem) {
+          if (menu.Items.Count > 0)
+            menu.Items.Add(new Separator());
+          firstItem = false;
+        }
+
         var menuItem = new MenuItem { Command = command, CommandParameter = item };
         menuItem.SetBinding(MenuItem.CommandTargetProperty, binding);
         menu.Items.Add(menuItem);
       }
 
-      if ((item as BaseTreeViewItem)?.GetTopParent() is BaseCategoryItem category) {
-
-        if (item is BaseCategoryItem && category.Category == Category.GeoNames)
+      if (CatTreeViewUtils.GetTopParent(item as ICatTreeViewItem) is ICatTreeViewCategory category) {
+        if ((item as ICatTreeViewCategory)?.Category == Category.GeoNames)
           AddMenuItem(TreeViewCommands.GeoNameNewCommand);
 
-        if (category.CanModifyItems) {
-          var cat = item as BaseCategoryItem;
-          var group = item as CategoryGroup;
-
-          if (cat != null || group != null || category.CanHaveSubItems) {
-            AddMenuItem(TreeViewCommands.TagItemNewCommand);
-            AddMenuItem(TreeViewCommands.TagItemDeleteNotUsedCommand);
-          }
-
-          if (item is BaseTreeViewTagItem && group == null || item is Viewer) {
-            AddMenuItem(TreeViewCommands.TagItemRenameCommand);
-            AddMenuItem(TreeViewCommands.TagItemDeleteCommand);
-          }
-
-          if (category.CanHaveGroups && cat != null)
-            AddMenuItem(TreeViewCommands.CategoryGroupNewCommand);
-
-          if (group != null) {
-            AddMenuItem(TreeViewCommands.CategoryGroupRenameCommand);
-            AddMenuItem(TreeViewCommands.CategoryGroupDeleteCommand);
-          }
-        }
+        if (category.CanDeleteItems && (category.CanHaveSubItems || item is ICatTreeViewCategory || item is ICatTreeViewGroup)
+            && (category.Category == Category.People || category.Category == Category.Keywords))
+          AddMenuItem(TreeViewCommands.TagItemDeleteNotUsedCommand);
       }
 
       switch (item) {
         case Folder folder: {
-            AddMenuItem(TreeViewCommands.FolderNewCommand);
-
-            if (folder.Parent != null) {
-              AddMenuItem(TreeViewCommands.FolderRenameCommand);
-              AddMenuItem(TreeViewCommands.FolderDeleteCommand);
+            if (!(folder.Parent is ICatTreeViewCategory))
               AddMenuItem(TreeViewCommands.FolderAddToFavoritesCommand);
-            }
 
             AddMenuItem(TreeViewCommands.FolderSetAsFolderKeywordCommand);
             AddMenuItem(MetadataCommands.Reload2Command);
             AddMenuItem(MediaItemsCommands.RebuildThumbnailsCommand);
-            break;
-          }
-        case FavoriteFolder _: {
-            AddMenuItem(TreeViewCommands.FolderRemoveFromFavoritesCommand);
             break;
           }
         case Viewer _: {
@@ -151,18 +148,16 @@ namespace PictureManager.UserControls {
             break;
           }
         case FolderKeywords _: {
-            AddMenuItem(Commands.WindowCommands.OpenFolderKeywordsListCommand);
-            break;
-          }
-        case BaseTreeViewItem bti: {
-            if (bti.Parent?.Parent is Viewer)
-              AddMenuItem(TreeViewCommands.ViewerRemoveFolderCommand);
+            AddMenuItem(WindowCommands.OpenFolderKeywordsListCommand);
             break;
           }
       }
 
-      if (menu.Items.Count > 0)
-        stackPanel.ContextMenu = menu;
+      if (menu.Items.Count > 0 && b.ContextMenu == null)
+        b.ContextMenu = menu;
+
+      if (b.ContextMenu?.Items.Count == 0)
+        b.ContextMenu = null;
     }
 
     private void TreeView_Select(object sender, MouseButtonEventArgs e) {
@@ -172,130 +167,12 @@ namespace PictureManager.UserControls {
        (Person, Keyword, GeoName)(filters) => MBL => or, MBL+ctrl => and, MBL+alt => hide
        (Rating)(filter) => MBL => OR between ratings, AND in files
        */
-      if (e.ChangedButton != MouseButton.Left) return;
-      App.Core.TreeView_Select(((StackPanel)sender).DataContext as BaseTreeViewItem,
+      e.Handled = true;
+      if (e.OriginalSource is ToggleButton) return;
+      App.Core.TreeView_Select(((TreeViewItem)sender).DataContext as ICatTreeViewItem,
         (Keyboard.Modifiers & ModifierKeys.Control) > 0,
         (Keyboard.Modifiers & ModifierKeys.Alt) > 0,
         (Keyboard.Modifiers & ModifierKeys.Shift) > 0);
-    }
-
-    private void TreeView_OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e) {
-      if (!(sender is StackPanel stackPanel)) return;
-      _dragDropObject = stackPanel.DataContext;
-      _dragDropStartPosition = e.GetPosition(null);
-    }
-
-    private void TreeView_OnMouseMove(object sender, MouseEventArgs e) {
-      if (!IsDragDropStarted(e)) return;
-      if (!(e.OriginalSource is StackPanel stackPanel) || _dragDropObject == null) return;
-      DragDrop.DoDragDrop(stackPanel, _dragDropObject, DragDropEffects.Move | DragDropEffects.Copy);
-    }
-
-    private bool IsDragDropStarted(MouseEventArgs e) {
-      if (e.LeftButton != MouseButtonState.Pressed) return false;
-      var diff = _dragDropStartPosition - e.GetPosition(null);
-      return Math.Abs(diff.X) > SystemParameters.MinimumHorizontalDragDistance ||
-             Math.Abs(diff.Y) > SystemParameters.MinimumVerticalDragDistance;
-    }
-
-    private void TreeView_OnDrop(object sender, DragEventArgs e) {
-      var panel = (StackPanel)sender;
-      if (!(panel.DataContext is BaseTreeViewItem destData)) return;
-
-      // MediaItems
-      if (e.Data.GetDataPresent(DataFormats.FileDrop)) {
-        var foMode = e.KeyStates == DragDropKeyStates.ControlKey ? FileOperationMode.Copy : FileOperationMode.Move;
-        App.Core.MediaItemsViewModel.CopyMove(
-          foMode, App.Core.Model.MediaItems.ThumbsGrid.FilteredItems.Where(x => x.IsSelected).ToList(), (Folder)destData);
-        App.Core.Model.MediaItems.Helper.IsModified = true;
-      }
-      // Folder
-      else if (e.Data.GetDataPresent(typeof(Folder))) {
-        var foMode = e.KeyStates == DragDropKeyStates.ControlKey ? FileOperationMode.Copy : FileOperationMode.Move;
-        var srcData = (Folder)e.Data.GetData(typeof(Folder));
-        if (srcData == null) return;
-
-        FoldersViewModel.CopyMove(foMode, srcData, (Folder)destData);
-        App.Core.Model.MediaItems.Helper.IsModified = true;
-        App.Core.Model.Folders.Helper.IsModified = true;
-        App.Core.Model.FolderKeywords.Load();
-
-        // reload last selected source if was moved
-        if (foMode == FileOperationMode.Move && srcData.IsSelected) {
-          var folder = ((Folder)destData).GetByPath(srcData.Title);
-          if (folder == null) return;
-          BaseTreeViewItem.ExpandTo(folder);
-          App.Core.TreeView_Select(folder, false, false, false);
-        }
-      }
-      // Keyword
-      else if (e.Data.GetDataPresent(typeof(Keyword))) {
-        var srcData = (Keyword)e.Data.GetData(typeof(Keyword));
-        if (srcData == null) return;
-        var dropOnTop = e.GetPosition(panel).Y < panel.ActualHeight / 2;
-        App.Core.Model.Keywords.ItemMove(srcData, destData, dropOnTop);
-      }
-      // Person
-      else if (e.Data.GetDataPresent(typeof(Person))) {
-        var srcData = (Person)e.Data.GetData(typeof(Person));
-        if (srcData == null) return;
-        App.Core.Model.People.ItemMove(srcData, destData);
-      }
-
-      App.Core.Model.Sdb.SaveAllTables();
-      App.Core.Model.MarkUsedKeywordsAndPeople();
-    }
-
-    private void TreeView_AllowDropCheck(object sender, DragEventArgs e) {
-      // scroll treeView when the mouse is near the top or bottom
-      var treeView = ((StackPanel)sender).TryFindParent<TreeView>();
-      if (treeView != null) {
-        var border = VisualTreeHelper.GetChild(treeView, 0);
-        if (VisualTreeHelper.GetChild(border, 0) is ScrollViewer scrollViewer) {
-          var pos = e.GetPosition(treeView);
-          if (pos.Y < 25) {
-            scrollViewer.ScrollToVerticalOffset(scrollViewer.VerticalOffset - 25);
-          }
-          else if (treeView.ActualHeight - pos.Y < 25) {
-            scrollViewer.ScrollToVerticalOffset(scrollViewer.VerticalOffset + 25);
-          }
-        }
-      }
-
-      // return if the data can be dropped
-      var dataContext = ((StackPanel)sender).DataContext;
-
-      // MediaItems
-      if (e.Data.GetDataPresent(DataFormats.FileDrop)) {
-        var dragged = ((string[])e.Data.GetData(DataFormats.FileDrop))?.OrderBy(x => x).ToArray();
-        var selected = App.Core.Model.MediaItems.ThumbsGrid.FilteredItems.Where(x => x.IsSelected).Select(p => p.FilePath).OrderBy(p => p).ToArray();
-
-        if (dragged != null && selected.SequenceEqual(dragged) &&
-            dataContext is Folder destData && destData.IsAccessible) return;
-      }
-      else // Folder
-      if (e.Data.GetDataPresent(typeof(Folder))) {
-        var srcData = (Folder)e.Data.GetData(typeof(Folder));
-
-        if (srcData != null && dataContext is Folder destData && !destData.HasThisParent(srcData) &&
-            srcData != destData && destData.IsAccessible && (Folder)srcData.Parent != destData) return;
-      }
-      else // Keyword
-      if (e.Data.GetDataPresent(typeof(Keyword))) {
-        if (dataContext is BaseTreeViewItem destData &&
-            (destData.GetTopParent() as BaseCategoryItem)?.Category == Category.Keywords) return;
-      }
-      else // Person
-      if (e.Data.GetDataPresent(typeof(Person))) {
-        var srcData = (Person)e.Data.GetData(typeof(Person));
-
-        if ((dataContext is BaseCategoryItem cat && cat.Category == Category.People) || 
-            (dataContext is CategoryGroup group && group.Category == Category.People && srcData?.Parent != group)) return;
-      }
-
-      // can't be dropped
-      e.Effects = DragDropEffects.None;
-      e.Handled = true;
     }
 
     private void MediaItemSize_OnDragCompleted(object sender, DragCompletedEventArgs e) {
