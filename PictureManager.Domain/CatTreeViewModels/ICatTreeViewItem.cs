8f9c2ef0ea4d630db7840664b7523a34c947013c
martin-holy <martin.holy@hotmail.com>
2021-06-05 18:27:36 +0100

    ICatTreeViewItem interface

---
+++ PictureManager.Domain/CatTreeViewModels/ICatTreeViewItem.cs
@@ -0,0 +1,18 @@
+using System.Collections.ObjectModel;
+
+namespace PictureManager.Domain.CatTreeViewModels {
+  public interface ICatTreeViewItem {
+    ICatTreeViewItem Parent { get; set; }
+    ObservableCollection<ICatTreeViewItem> Items { get; set; }
+    IconName IconName { get; set; }
+    string Title { get; set; }
+    bool IsExpanded { get; set; }
+    bool IsSelected { get; set; }
+    bool IsHidden { get; set; }
+    bool IsMarked { get; set; }
+    int PicCount { get; set; }
+    object Tag { get; set; }
+    string ToolTip { get; set; }
+    BackgroundBrush BackgroundBrush { get; set; }
+  }
+}
