29b9d517cd0e8430538ef1fe920a1a51ab387778
martin-holy <martin.holy@hotmail.com>
2021-06-05 18:33:13 +0100

    removed

--- PictureManager.Domain/Models/BaseTreeViewTagItem.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PictureManager.Domain.Models {
-  public class BaseTreeViewTagItem : BaseTreeViewItem {
-    private bool _isMarked;
-    private int _picCount;
-
-    public bool IsMarked { get => _isMarked; set { _isMarked = value; OnPropertyChanged(); } }
-    public int PicCount { get => _picCount; set { _picCount = value; OnPropertyChanged(); } }
-  }
-}
