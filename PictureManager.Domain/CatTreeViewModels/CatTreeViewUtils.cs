5345f89d870f88093905558ec592902e95f5a3bc
martin-holy <martin.holy@hotmail.com>
2021-06-15 20:28:11 +0100

    refactoring

--- PictureManager.Domain/CatTreeViewModels/CatTreeViewUtils.cs
+++ PictureManager.Domain/CatTreeViewModels/CatTreeViewUtils.cs
@@ -10,19 +10,19 @@ namespace PictureManager.Domain.CatTreeViewModels {
     public static EventHandler OnAfterSort;
 
     public static IconName GetCategoryGroupIconName(Category category) {
-      switch (category) {
-        case Category.FavoriteFolders: return IconName.FolderStar;
-        case Category.Folders: return IconName.Folder;
-        case Category.Ratings: return IconName.Star;
-        case Category.People: return IconName.PeopleMultiple;
-        case Category.FolderKeywords: return IconName.Folder;
-        case Category.Keywords: return IconName.TagLabel;
-        case Category.Filters: return IconName.Filter;
-        case Category.Viewers: return IconName.Eye;
-        case Category.SqlQueries: return IconName.DatabaseSql;
-        case Category.MediaItemClips: return IconName.MovieClapper;
-        default: return IconName.Bug;
-      }
+      return category switch {
+        Category.FavoriteFolders => IconName.FolderStar,
+        Category.Folders => IconName.Folder,
+        Category.Ratings => IconName.Star,
+        Category.People => IconName.PeopleMultiple,
+        Category.FolderKeywords => IconName.Folder,
+        Category.Keywords => IconName.TagLabel,
+        Category.Filters => IconName.Filter,
+        Category.Viewers => IconName.Eye,
+        Category.SqlQueries => IconName.DatabaseSql,
+        Category.MediaItemClips => IconName.MovieClapper,
+        _ => IconName.Bug
+      };
     }
 
     public static ICatTreeViewItem GetTopParent(ICatTreeViewItem item) {
