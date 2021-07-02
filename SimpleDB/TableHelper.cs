ee71d48f49574aeb87eb5fe1d2e7ff3a5adce1aa
martin-holy <martin.holy@hotmail.com>
2021-07-02 15:42:44 +0100

    Table Properties

--- SimpleDB/TableHelper.cs
+++ SimpleDB/TableHelper.cs
@@ -9,7 +9,10 @@ namespace SimpleDB {
     public ITable Table { get; set; }
     public bool IsModified { get; set; }
     public bool AutoLoad { get; set; }
+    public bool AreTablePropsModified { get; set; }
+    public Dictionary<string, string> TableProps { get; set; }
     private readonly string _tableFilePath;
+    private readonly string _tablePropsFilePath;
     private readonly ILogger _logger;
 
     public TableHelper(ITable table, int maxId, ILogger logger, bool autoLoad) {
@@ -19,6 +22,7 @@ namespace SimpleDB {
       MaxId = maxId;
       AutoLoad = autoLoad;
       _tableFilePath = Path.Combine("db", $"{Table.GetType().Name}.csv");
+      _tablePropsFilePath = Path.Combine("db", $"{Table.GetType().Name}_props.csv");
     }
 
     public int GetNextId() {
@@ -46,6 +50,38 @@ namespace SimpleDB {
           sw.WriteLine(item.ToCsv());
 
         IsModified = false;
+      }
+      catch (Exception ex) {
+        _logger.LogError(ex);
+      }
+    }
+
+    public void LoadPropsFromFile() {
+      if (!File.Exists(_tablePropsFilePath)) return;
+      try {
+        TableProps = new();
+        using var sr = new StreamReader(_tablePropsFilePath, Encoding.UTF8);
+        string line;
+        while ((line = sr.ReadLine()) != null) {
+          var prop = line.Split('|');
+          if (prop.Length != 2) throw new ArgumentException("Incorrect number of values.", line);
+          TableProps.Add(prop[0], prop[1]);
+        }
+      }
+      catch (Exception ex) {
+        _logger.LogError(ex);
+      }
+    }
+
+    public void SaveTablePropsToFile() {
+      Table.TablePropsToCsv();
+      if (TableProps == null) return;
+      try {
+        using var sw = new StreamWriter(_tablePropsFilePath, false, Encoding.UTF8, 65536);
+        foreach (var prop in TableProps)
+          sw.WriteLine($"{prop.Key}|{prop.Value}");
+
+        AreTablePropsModified = false;
       }
       catch (Exception ex) {
         _logger.LogError(ex);
