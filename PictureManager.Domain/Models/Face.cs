c1fd8bf047fde53d0ab40d0b0c0c1438c089c47e
martin-holy <martin.holy@hotmail.com>
2021-07-02 15:57:13 +0100

    IsSelected, SetPicture async

--- PictureManager.Domain/Models/Face.cs
+++ PictureManager.Domain/Models/Face.cs
@@ -1,11 +1,14 @@
-﻿using SimpleDB;
+﻿using PictureManager.Domain.Utils;
+using SimpleDB;
 using System.Windows;
 using System.Windows.Media.Imaging;
 
 namespace PictureManager.Domain.Models {
   public class Face : ObservableObject, IRecord {
-    private BitmapSource _picture;
+    private BitmapImage _picture;
+    private bool _isSelected;
 
+    #region DB Properties
     public string[] Csv { get; set; }
     public int Id { get; }
     public MediaItem MediaItem { get; set; }
@@ -13,7 +16,10 @@ namespace PictureManager.Domain.Models {
     public int PersonId { get; set; } // < 0 for unknown people, 0 for unknown, > 0 for known people
     public Int32Rect FaceBox { get; set; }
     public long AvgHash { get; set; }
-    public BitmapSource Picture { get => _picture; set { _picture = value; OnPropertyChanged(); } }
+    #endregion
+
+    public BitmapImage Picture { get => _picture; set { _picture = value; OnPropertyChanged(); } }
+    public bool IsSelected { get => _isSelected; set { _isSelected = value; OnPropertyChanged(); } }
 
     public Face(int id, int personId, Int32Rect faceBox, long avgHash) {
       Id = id;
@@ -26,6 +32,12 @@ namespace PictureManager.Domain.Models {
     public string ToCsv() {
       var faceBox = string.Join(",", FaceBox.X, FaceBox.Y, FaceBox.Width, FaceBox.Height);
       return string.Join("|", Id.ToString(), MediaItem.Id.ToString(), PersonId.ToString(), faceBox, AvgHash.ToString());
+    }
+
+    public async void SetPicture(int size) {
+      if (_picture != null) return;
+      var filePath = MediaItem.MediaType == MediaType.Image ? MediaItem.FilePath : MediaItem.FilePathCache;
+      Picture = await Imaging.GetCroppedBitmapImageAsync(filePath, FaceBox, size);
     }
   }
 }
