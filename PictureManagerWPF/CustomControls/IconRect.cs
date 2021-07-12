480f9061a1cb45e8dac9d1cd785e20dfd3b41dfd
martin-holy <martin.holy@hotmail.com>
2021-07-12 09:44:14 +0100

    IconFill property

--- PictureManagerWPF/CustomControls/IconRect.cs
+++ PictureManagerWPF/CustomControls/IconRect.cs
@@ -1,15 +1,22 @@
 ﻿using PictureManager.Domain;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace PictureManager.CustomControls {
   public class IconRect : Control {
     public static readonly DependencyProperty IconNameProperty = DependencyProperty.Register(nameof(IconName), typeof(IconName), typeof(IconRect));
+    public static readonly DependencyProperty IconFillProperty = DependencyProperty.Register(nameof(IconFill), typeof(Brush), typeof(IconRect));
     public static readonly DependencyProperty SizeProperty = DependencyProperty.Register(nameof(Size), typeof(double), typeof(IconRect), new PropertyMetadata(18.0));
 
     public IconName IconName {
       get => (IconName)GetValue(IconNameProperty);
       set => SetValue(IconNameProperty, value);
+    }
+
+    public Brush IconFill {
+      get => (Brush)GetValue(IconFillProperty);
+      set => SetValue(IconFillProperty, value);
     }
 
     public double Size {
