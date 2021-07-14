59c9ca1f1ed540dd707350771f9797670ee935fa
martin-holy <martin.holy@hotmail.com>
2021-07-14 09:10:04 +0100

    DataTypeConverter

--- PictureManagerWPF/Convertors.cs
+++ PictureManagerWPF/Convertors.cs
@@ -179,4 +179,12 @@ namespace PictureManager {
 
     public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => value;
   }
+
+  public class DataTypeConverter : IValueConverter {
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture) =>
+      value?.GetType() ?? Binding.DoNothing;
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) =>
+      throw new NotSupportedException();
+  }
 }
