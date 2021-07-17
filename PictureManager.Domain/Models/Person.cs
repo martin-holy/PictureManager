73e16d32c17ecdd076c55cc6bee14e4af21a8a5f
martin-holy <martin.holy@hotmail.com>
2021-07-17 15:47:32 +0100

    GetMediaItems

--- PictureManager.Domain/Models/Person.cs
+++ PictureManager.Domain/Models/Person.cs
@@ -1,14 +1,17 @@
 ﻿using PictureManager.Domain.CatTreeViewModels;
 using SimpleDB;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace PictureManager.Domain.Models {
   public sealed class Person : CatTreeViewItem, IRecord, ICatTreeViewTagItem {
+    private Face _face;
+
     public string[] Csv { get; set; }
     public int Id { get; }
     public List<MediaItem> MediaItems { get; } = new List<MediaItem>();
     public List<VideoClip> VideoClips { get; set; }
-    public Face Face { get; set; }
+    public Face Face { get => _face; set { _face = value; OnPropertyChanged(); } }
 
     public Person(int id, string name) {
       Id = id;
@@ -18,5 +21,9 @@ namespace PictureManager.Domain.Models {
 
     // ID|Name
     public string ToCsv() => string.Join("|", Id.ToString(), Title);
+
+    public MediaItem[] GetMediaItems() =>
+      Core.Instance.Faces.All.Cast<Face>().Where(x => x.PersonId == Id).Select(x => x.MediaItem)
+      .Concat(MediaItems).Distinct().OrderBy(x => x.FileName).ToArray();
   }
 }
