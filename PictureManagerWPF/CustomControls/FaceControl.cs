4550bd89204c8d5b20e09792221a675f74d68faa
martin-holy <martin.holy@hotmail.com>
2021-07-22 16:54:26 +0100

    FaceControl

---
+++ PictureManagerWPF/CustomControls/FaceControl.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace PictureManager.CustomControls {
+  public class FaceControl : Control {
+    public static readonly DependencyProperty IsCheckmarkVisibleProperty = DependencyProperty.Register(nameof(IsCheckmarkVisible), typeof(bool), typeof(FaceControl), new PropertyMetadata(false));
+
+    public bool IsCheckmarkVisible {
+      get => (bool)GetValue(IsCheckmarkVisibleProperty);
+      set => SetValue(IsCheckmarkVisibleProperty, value);
+    }
+
+    static FaceControl() {
+      DefaultStyleKeyProperty.OverrideMetadata(typeof(FaceControl), new FrameworkPropertyMetadata(typeof(FaceControl)));
+    }
+  }
+}
