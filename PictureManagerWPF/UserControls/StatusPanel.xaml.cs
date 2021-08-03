15380a9b6ef581e9ec75e55d2ae80d0efbf22779
martin-holy <martin.holy@hotmail.com>
2021-08-03 17:49:19 +0100

    AppInfo => StatusPanel

--- PictureManagerWPF/UserControls/StatusPanel.xaml.cs
+++ PictureManagerWPF/UserControls/StatusPanel.xaml.cs
@@ -1,8 +1,22 @@
-﻿using System.Windows;
+﻿using PictureManager.Domain;
+using PictureManager.Domain.CatTreeViewModels;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Windows;
 using System.Windows.Input;
 
 namespace PictureManager.UserControls {
-  public partial class StatusPanel {
+  public partial class StatusPanel : INotifyPropertyChanged {
+    public event PropertyChangedEventHandler PropertyChanged;
+    public void OnPropertyChanged([CallerMemberName] string name = null) =>
+      PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+
+    private readonly Dictionary<string, string> _dateFormats = new() { { "d", "d. " }, { "M", "MMMM " }, { "y", "yyyy" } };
+
     public static readonly DependencyProperty IsPinnedProperty = DependencyProperty.Register(nameof(IsPinned), typeof(bool), typeof(StatusPanel), new UIPropertyMetadata(true));
 
     public bool IsPinned {
@@ -10,12 +24,75 @@ namespace PictureManager.UserControls {
       set => SetValue(IsPinnedProperty, value);
     }
 
-    public StatusPanel() {
-      InitializeComponent();
+    public string FileSize {
+      get {
+        try {
+          var size = App.Core.MediaItems.Current == null
+            ? App.Core.MediaItems.ThumbsGrid?.SelectedItems.Sum(mi => new FileInfo(mi.FilePath).Length)
+            : new FileInfo(App.Core.MediaItems.Current.FilePath).Length;
+
+          return size == null ? string.Empty : Extensions.FileSizeToString((long)size);
+        }
+        catch {
+          return string.Empty;
+        }
+      }
     }
 
-    private void OnPreviewMouseLeftButtonUp(object sender, MouseButtonEventArgs e) {
-      IsPinned = !IsPinned;
+    public ObservableCollection<string> FilePath {
+      get {
+        var paths = new ObservableCollection<string>();
+        var mi = App.Core.MediaItems.Current;
+        if (mi == null) return paths;
+
+
+        if (App.Ui.AppInfo.AppMode == AppMode.Browser || mi.Folder.FolderKeyword == null) {
+          paths.Add(mi.FilePath);
+          return paths;
+        }
+
+        var fks = new List<ICatTreeViewItem>();
+        CatTreeViewUtils.GetThisAndParentRecursive(mi.Folder.FolderKeyword, ref fks);
+        fks.Reverse();
+        foreach (var fk in fks)
+          if (fk.Parent != null) {
+            var startIndex = fk.Title.FirstIndexOfLetter();
+
+            if (fk.Title.Length - 1 == startIndex) continue;
+
+            paths.Add(startIndex == 0 ? fk.Title : fk.Title[startIndex..]);
+          }
+
+        var fileName = string.IsNullOrEmpty(DateAndTime) ? mi.FileName : mi.FileName[15..];
+        paths.Add(fileName);
+
+        return paths;
+      }
     }
+
+    public string ZoomActualFormatted => App.WMain?.MediaViewer.FullImage.ZoomActualFormatted;
+    public string DateAndTime => Domain.Extensions.DateTimeFromString(App.Core.MediaItems.Current?.FileName, _dateFormats, "H:mm:ss");
+    public ObservableCollection<IconName> Rating { get; } = new();
+
+    public StatusPanel() {
+      InitializeComponent();
+
+      App.Core.MediaItems.PropertyChanged += (o, e) => {
+        if (e.PropertyName.Equals(nameof(App.Core.MediaItems.Current))) {
+          OnPropertyChanged(nameof(FileSize));
+          OnPropertyChanged(nameof(DateAndTime));
+          OnPropertyChanged(nameof(FilePath));
+          UpdateRating();
+        }
+      };
+    }
+
+    public void UpdateRating() {
+      Rating.Clear();
+      for (var i = 0; i < App.Core.MediaItems.Current?.Rating; i++)
+        Rating.Add(IconName.Star);
+    }
+
+    private void OnPreviewMouseLeftButtonUp(object sender, MouseButtonEventArgs e) => IsPinned = !IsPinned;
   }
 }
