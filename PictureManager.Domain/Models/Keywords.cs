6d887f7b12a21a03a1f67a7df4a6b221e373bd62
martin-holy <martin.holy@hotmail.com>
2021-08-26 17:50:18 +0100

    Toggle method

--- PictureManager.Domain/Models/Keywords.cs
+++ PictureManager.Domain/Models/Keywords.cs
@@ -142,5 +142,52 @@ namespace PictureManager.Domain.Models {
         Core.Instance.Sdb.SetModified<Keywords>();
       }
     }
+
+    /// <summary>
+    /// Toggle Keyword on Media Item
+    /// </summary>
+    /// <param name="k">Keyword</param>
+    /// <param name="mi">Media Item</param>
+    public static void Toggle(Keyword k, MediaItem mi) {
+      if (!k.IsMarked && mi.Keywords != null) {
+        mi.Keywords?.Remove(k);
+        k.MediaItems.Remove(mi);
+        if (mi.Keywords?.Count == 0)
+          mi.Keywords = null;
+        return;
+      }
+
+      if (mi.Keywords != null) {
+        // skip if any Parent of MediaItem Keywords is marked Keyword
+        var skip = false;
+        foreach (var miKeyword in mi.Keywords) {
+          var tmpMik = miKeyword;
+          while (tmpMik.Parent is Keyword parent) {
+            tmpMik = parent;
+            if (!parent.Id.Equals(k.Id)) continue;
+            skip = true;
+            break;
+          }
+        }
+
+        if (skip) return;
+
+        // remove possible redundant keywords 
+        // example: if marked keyword is "Weather/Sunny" keyword "Weather" is redundant
+        foreach (var miKeyword in mi.Keywords.ToArray()) {
+          var tmpMarkedK = k;
+          while (tmpMarkedK.Parent is Keyword parent) {
+            tmpMarkedK = parent;
+            if (!parent.Id.Equals(miKeyword.Id)) continue;
+            mi.Keywords.Remove(miKeyword);
+            miKeyword.MediaItems.Remove(mi);
+          }
+        }
+      }
+
+      mi.Keywords ??= new();
+      mi.Keywords.Add(k);
+      k.MediaItems.Add(mi);
+    }
   }
 }
