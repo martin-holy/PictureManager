79f05159f40dd0588ae5f9d4bddd2e95a6ff5ae1
martin-holy <martin.holy@hotmail.com>
2021-10-01 19:17:31 +0100

    IconToggleButton

---
+++ MH.UI.WPF/Controls/IconToggleButton.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+using System.Windows.Controls.Primitives;
+using System.Windows.Media;
+
+namespace MH.UI.WPF.Controls {
+  public class IconToggleButton : ToggleButton {
+    public static readonly DependencyProperty SourceProperty = Icon.SourceProperty.AddOwner(typeof(IconToggleButton));
+    public static readonly DependencyProperty FillProperty = Icon.FillProperty.AddOwner(typeof(IconToggleButton));
+    public static readonly DependencyProperty SizeProperty = Icon.SizeProperty.AddOwner(typeof(IconToggleButton));
+
+    public PathGeometry Source {
+      get => (PathGeometry)GetValue(SourceProperty);
+      set => SetValue(SourceProperty, value);
+    }
+
+    public Brush Fill {
+      get => (Brush)GetValue(FillProperty);
+      set => SetValue(FillProperty, value);
+    }
+
+    public double Size {
+      get => (double)GetValue(SizeProperty);
+      set => SetValue(SizeProperty, value);
+    }
+
+    static IconToggleButton() {
+      DefaultStyleKeyProperty.OverrideMetadata(typeof(IconToggleButton), new FrameworkPropertyMetadata(typeof(IconToggleButton)));
+    }
+  }
+}
