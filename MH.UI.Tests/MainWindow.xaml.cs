cdf4d9ae1ff86ce15a2ce05a2317f74f4cc61b3f
martin-holy <martin.holy@hotmail.com>
2021-10-01 16:55:14 +0100

    Test project

---
+++ MH.UI.Tests/MainWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+
+namespace MH.UI.Tests {
+  public partial class MainWindow : Window {
+    public List<object> TestList { get; } = new();
+
+    public MainWindow() {
+      InitializeComponent();
+
+      for (int i = 0; i < 1000; i++) {
+        TestList.Add(i);
+      }
+    }
+  }
+}
