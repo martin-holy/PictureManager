9da2f22ba626e92e09b45a1efa3c041c7d772e7a
martin-holy <martin.holy@hotmail.com>
2021-10-07 13:43:28 +0100

    SetTitle

--- PictureManager.Domain/Models/BaseCatTreeViewCategory.cs
+++ PictureManager.Domain/Models/BaseCatTreeViewCategory.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 
 namespace PictureManager.Domain.Models {
-  public class BaseCatTreeViewCategory : CatTreeViewCategory, ICatTreeViewCategory {
+  public class BaseCatTreeViewCategory : CatTreeViewCategory {
     public BaseCatTreeViewCategory(Category category) : base(category) { }
 
     public override ICatTreeViewItem ItemCreate(ICatTreeViewItem root, string name) => throw new System.NotImplementedException();
@@ -13,7 +13,7 @@ namespace PictureManager.Domain.Models {
     public override void ItemRename(ICatTreeViewItem item, string name) {
       if (CatTreeViewUtils.GetTopParent(item) is not ICatTreeViewCategory cat || cat is not ITable table) return;
 
-      item.Title = name;
+      cat.SetTitle(item, name);
 
       var idx = CatTreeViewUtils.SetItemInPlace(item.Parent, item);
       var allIdx = Core.GetAllIndexBasedOnTreeOrder(table.All, item.Parent, idx);
