2a217dba3e0d1734d20414a617c98135a3ea0f54
martin-holy <martin.holy@hotmail.com>
2021-10-07 13:44:50 +0100

    FavoriteFoldersM

---
+++ PictureManager.Domain/Models/FavoriteFoldersM.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using PictureManager.Domain.DataAdapters;
+using PictureManager.Domain.Extensions;
+using SimpleDB;
+
+namespace PictureManager.Domain.Models {
+  public sealed class FavoriteFoldersM {
+    public DataAdapter DataAdapter { get; }
+    public ObservableCollection<FavoriteFolderM> All { get; } = new();
+
+    public FavoriteFoldersM(Core core) {
+      DataAdapter = new FavoriteFoldersDataAdapter(core, this);
+    }
+
+    public void ItemCreate(Folder folder) {
+      var ff = new FavoriteFolderM(DataAdapter.GetNextId()) {
+        Title = folder.Title,
+        Folder = folder
+      };
+
+      All.SetInOrder(ff, (x) => x.Title);
+      DataAdapter.IsModified = true;
+    }
+
+    public void ItemMove(FavoriteFolderM item, FavoriteFolderM dest, bool aboveDest) {
+      All.Move(item, dest, aboveDest);
+      DataAdapter.IsModified = true;
+    }
+
+    public bool ItemCanRename(string name) =>
+      !All.Any(x => x.Title.Equals(name, StringComparison.OrdinalIgnoreCase));
+
+    public void ItemRename(FavoriteFolderM item, string name) {
+      item.Title = name;
+      All.SetInOrder(item, (x) => x.Title);
+      DataAdapter.IsModified = true;
+    }
+
+    public void ItemDelete(FavoriteFolderM item) {
+      All.Remove(item);
+      item.Folder = null;
+      DataAdapter.IsModified = true;
+    }
+  }
+}
