c7fbeda21404bd7a5a7f0b49208af269bfd0202a
martin-holy <martin.holy@hotmail.com>
2021-10-07 13:50:12 +0100

    FavoriteFolderTreeVM

---
+++ PictureManagerWPF/ViewModels/FavoriteFolderTreeVM.cs
@@ -0,0 +1,13 @@
+﻿using PictureManager.Domain.CatTreeViewModels;
+using PictureManager.Domain.Models;
+
+namespace PictureManager.ViewModels {
+  public sealed class FavoriteFolderTreeVM : CatTreeViewItem {
+    public FavoriteFolderM Model { get; set; }
+
+    public FavoriteFolderTreeVM(FavoriteFolderM model, ICatTreeViewItem parent) {
+      Model = model;
+      Parent = parent;
+    }
+  }
+}
