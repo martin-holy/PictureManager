2b07adffc432cff8300a0524114a0cc88124d7d1
martin-holy <martin.holy@hotmail.com>
2021-10-07 13:44:36 +0100

    FavoriteFolderM

---
+++ PictureManager.Domain/Models/FavoriteFolderM.cs
@@ -0,0 +1,18 @@
+﻿using PictureManager.Domain.Extensions;
+using SimpleDB;
+
+namespace PictureManager.Domain.Models {
+  public sealed class FavoriteFolderM : ObservableObject, IRecord {
+    private Folder _folder;
+    private string _title;
+
+    public int Id { get; }
+    public Folder Folder { get => _folder; set { _folder = value; OnPropertyChanged(); } }
+    public string Title { get => _title; set { _title = value; OnPropertyChanged(); } }
+    public string[] Csv { get; set; }
+
+    public FavoriteFolderM(int id) {
+      Id = id;
+    }
+  }
+}
