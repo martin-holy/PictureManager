ded629c1607f31fa3048df01ddc32dde27995735
martin-holy <martin.holy@hotmail.com>
2021-10-16 12:23:24 +0100

    CategoryGroupIconControl

---
+++ PictureManagerWPF/UserControls/CategoryGroupIconControl.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+using PictureManager.Domain;
+
+namespace PictureManager.UserControls {
+  public partial class CategoryGroupIconControl {
+
+    public static readonly DependencyProperty CategoryProperty =
+      DependencyProperty.Register(nameof(Category), typeof(Category), typeof(CategoryGroupIconControl));
+
+    public int Category {
+      get => (int)GetValue(CategoryProperty);
+      set => SetValue(CategoryProperty, value);
+    }
+
+    public CategoryGroupIconControl() {
+      InitializeComponent();
+    }
+  }
+}
