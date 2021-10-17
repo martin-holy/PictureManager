388cac03bd536ed6148ed29069168a66c758b3a9
martin-holy <martin.holy@hotmail.com>
2021-10-17 14:29:12 +0100

    TreeToModel and ToTreeBranch methods

--- PictureManagerWPF/ViewModels/Tree/KeywordsTreeVM.cs
+++ PictureManagerWPF/ViewModels/Tree/KeywordsTreeVM.cs
@@ -47,33 +47,32 @@ namespace PictureManager.ViewModels.Tree {
         baseVM => DU.Tree.GetDestItemCat(baseVM, baseVM.Model.Id, All, () => new(baseVM, parent), onItemsChanged));
     }
 
-    public override string GetTitle(ICatTreeViewItem item) => ((KeywordTreeVM)item).BaseVM.Model.Name;
+    public override string GetTitle(ICatTreeViewItem item) => TreeToModel(item).Name;
 
     public override ICatTreeViewItem ItemCreate(ICatTreeViewItem root, string name) {
-      var keywordM = BaseVM.Model.ItemCreate(ToModel(root), name);
+      var keywordM = BaseVM.Model.ItemCreate(ToTreeBranch(root), name);
 
       return All[keywordM.Id];
     }
 
     public override void ItemMove(ICatTreeViewItem item, ICatTreeViewItem dest, bool aboveDest) =>
-      BaseVM.Model.ItemMove(((KeywordTreeVM)item).BaseVM.Model, ToModel(dest), aboveDest);
+      BaseVM.Model.ItemMove(TreeToModel(item), ToTreeBranch(dest), aboveDest);
 
     public override void ItemRename(ICatTreeViewItem item, string name) =>
-      BaseVM.Model.ItemRename(((KeywordTreeVM)item).BaseVM.Model, name);
+      BaseVM.Model.ItemRename(TreeToModel(item), name);
 
     public override string ValidateNewItemTitle(ICatTreeViewItem root, string name) =>
-      KeywordsM.ItemCanRename(ToModel(root), name) ? null : $"{name} item already exists!";
+      KeywordsM.ItemCanRename(ToTreeBranch(root), name) ? null : $"{name} item already exists!";
 
     public override string ValidateNewGroupTitle(ICatTreeViewItem root, string name) =>
-      CategoryGroupsM.ItemCanRename(ToModel(root), name) ? null : $"{name} group already exists!";
+      CategoryGroupsM.ItemCanRename(ToTreeBranch(root), name) ? null : $"{name} group already exists!";
 
-    public override void ItemDelete(ICatTreeViewItem item) {
-      if (item is not KeywordTreeVM keywordTreeVM) return;
-      
-      BaseVM.Model.ItemDelete(keywordTreeVM.BaseVM.Model);
-    }
+    public override void ItemDelete(ICatTreeViewItem item) =>
+      BaseVM.Model.ItemDelete(TreeToModel(item));
 
-    private static ITreeBranch ToModel(ICatTreeViewItem item) =>
+    private static KeywordM TreeToModel(object item) => ((KeywordTreeVM)item).BaseVM.Model;
+
+    private static ITreeBranch ToTreeBranch(object item) =>
       item switch {
         KeywordTreeVM x => x.BaseVM.Model,
         KeywordsTreeVM x => x.BaseVM.Model,
