c163680836bb6c10e5c2e8b59e62efcd657df910
martin-holy <martin.holy@hotmail.com>
2021-10-26 14:30:28 +0100

    CatTreeViewMarginConverter with offset parameter

--- PictureManagerWPF/Convertors.cs
+++ PictureManagerWPF/Convertors.cs
@@ -175,14 +175,16 @@ namespace PictureManager {
     public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
       if (value is not ICatTreeViewItem tvi) return new Thickness(0.0);
 
-      var levels = 0;
+      var levels = 1.5;
       var parent = tvi.Parent;
       while (parent != null) {
         levels++;
         parent = parent.Parent;
       }
 
-      return new Thickness(Length * levels, 0.0, 0.0, 0.0);
+      var offset = parameter == null ? 0 : int.Parse((string)parameter);
+
+      return new Thickness((Length * levels) + offset, 0.0, 0.0, 0.0);
     }
 
     public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => value;
