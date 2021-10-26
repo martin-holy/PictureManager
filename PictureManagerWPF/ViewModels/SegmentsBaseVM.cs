264922638ba6651278f4031f7085c0fc6aaac4b7
martin-holy <martin.holy@hotmail.com>
2021-10-26 14:23:33 +0100

    SegmentsBaseVM

---
+++ PictureManagerWPF/ViewModels/SegmentsBaseVM.cs
@@ -0,0 +1,21 @@
+﻿using PictureManager.Commands;
+using PictureManager.Domain;
+using PictureManager.Domain.Models;
+
+namespace PictureManager.ViewModels {
+  public class SegmentsBaseVM {
+    private readonly Core _core;
+
+    public RelayCommand SetSegmentPictureCommand { get; }
+
+    public SegmentsBaseVM(Core core) {
+      _core = core;
+      SetSegmentPictureCommand = new(SetSegmentPicture);
+    }
+
+    private async void SetSegmentPicture(object parameter) {
+      if (parameter is Segment segment)
+        await segment.SetPictureAsync(_core.Segments.SegmentSize);
+    }
+  }
+}
