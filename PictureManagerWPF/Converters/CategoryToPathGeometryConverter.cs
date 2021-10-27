5289a7fe1324441c133557d70284dea3db444ca5
martin-holy <martin.holy@hotmail.com>
2021-10-27 12:11:35 +0100

    CategoryToPathGeometryConverter

---
+++ PictureManagerWPF/Converters/CategoryToPathGeometryConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using PictureManager.Domain;
+
+namespace PictureManager.Converters {
+  public class CategoryToPathGeometryConverter : IValueConverter {
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+      if (value == null) throw new ArgumentNullException(nameof(value));
+
+      var resourceName = value switch {
+        Category.FavoriteFolders => "IconFolderStar",
+        Category.Folders => "IconFolder",
+        Category.Ratings => "IconStar",
+        Category.People => "IconPeopleMultiple",
+        Category.FolderKeywords => "IconFolder",
+        Category.Keywords => "IconTagLabel",
+        Category.Viewers => "IconEye",
+        Category.MediaItemClips => "IconMovieClapper",
+        _ => "IconBug"
+      };
+
+      return Application.Current.FindResource(resourceName);
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) =>
+      throw new NotSupportedException();
+  }
+}
