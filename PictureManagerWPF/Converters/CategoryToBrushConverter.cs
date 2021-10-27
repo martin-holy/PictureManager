da10eb7b2f3c6a74f9a475b3d1d2d33a6b8809ef
martin-holy <martin.holy@hotmail.com>
2021-10-27 12:11:47 +0100

    CategoryToBrushConverter

---
+++ PictureManagerWPF/Converters/CategoryToBrushConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using PictureManager.Domain;
+
+namespace PictureManager.Converters {
+  public class CategoryToBrushConverter : IValueConverter {
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+      if (value == null) throw new ArgumentNullException(nameof(value));
+
+      var resourceName = value switch {
+        Category.Folders or
+        Category.FavoriteFolders or
+        Category.FolderKeywords => "ColorBrushFolder",
+        Category.People => "ColorBrushPeople",
+        Category.Keywords => "ColorBrushTag",
+        Category.Ratings or
+        Category.Viewers or
+        Category.MediaItemClips or
+        _ => "ColorBrushWhite"
+      };
+
+      return Application.Current.FindResource(resourceName);
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) =>
+      throw new NotSupportedException();
+  }
+}
