2e9ef342d81a40b42dc2900582901ab9f3ec429e
martin-holy <martin.holy@hotmail.com>
2021-10-28 15:36:53 +0100

    IconRect => Icon

--- PictureManagerWPF/UserControls/InfoPanel.xaml.cs
+++ PictureManagerWPF/UserControls/InfoPanel.xaml.cs
@@ -1,16 +1,29 @@
-﻿using PictureManager.Domain;
-using System.Collections.ObjectModel;
+﻿using System.Collections.ObjectModel;
 using System.Windows;
+using System.Windows.Media;
+using MH.UI.WPF.Controls;
 
 namespace PictureManager.UserControls {
   public partial class InfoPanel {
-    public static readonly DependencyProperty IconNameProperty = DependencyProperty.Register(nameof(IconName), typeof(IconName), typeof(InfoPanel));
+    public static readonly DependencyProperty IconSourceProperty = Icon.SourceProperty.AddOwner(typeof(InfoPanel));
+    public static readonly DependencyProperty IconFillProperty = Icon.FillProperty.AddOwner(typeof(InfoPanel));
+    public static readonly DependencyProperty IconSizeProperty = Icon.SizeProperty.AddOwner(typeof(InfoPanel));
     public static readonly DependencyProperty ItemProperty = DependencyProperty.Register(nameof(Item), typeof(string), typeof(InfoPanel));
     public static readonly DependencyProperty ItemsProperty = DependencyProperty.Register(nameof(Items), typeof(ObservableCollection<string>), typeof(InfoPanel));
 
-    public IconName IconName {
-      get => (IconName)GetValue(IconNameProperty);
-      set => SetValue(IconNameProperty, value);
+    public PathGeometry IconSource {
+      get => (PathGeometry)GetValue(IconSourceProperty);
+      set => SetValue(IconSourceProperty, value);
+    }
+
+    public Brush IconFill {
+      get => (Brush)GetValue(IconFillProperty);
+      set => SetValue(IconFillProperty, value);
+    }
+
+    public double IconSize {
+      get => (double)GetValue(IconSizeProperty);
+      set => SetValue(IconSizeProperty, value);
     }
 
     public string Item {
