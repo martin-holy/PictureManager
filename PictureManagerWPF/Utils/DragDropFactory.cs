d6f856013417178bcb86c1c1e68feabed297a102
martin-holy <martin.holy@hotmail.com>
2021-11-14 15:01:15 +0100

    DoDragDrop with format, source and data as static private fields

--- PictureManagerWPF/Utils/DragDropFactory.cs
+++ PictureManagerWPF/Utils/DragDropFactory.cs
@@ -6,28 +6,37 @@ namespace PictureManager.Utils {
   public static class DragDropFactory {
     private static Point _dragDropStartPosition;
     private static bool _isActive;
+    private static object _source;
+    private static object _data;
 
-    public static void SetDrag(FrameworkElement source, Func<MouseEventArgs, object> canDrag) {
-      source.PreviewMouseLeftButtonDown += (o, e) => {
+    public static void SetDrag(FrameworkElement source, Func<MouseEventArgs, object> canDrag, string dataFormat = null) {
+      source.PreviewMouseLeftButtonDown += (_, e) => {
         _dragDropStartPosition = e.GetPosition(source);
         _isActive = true;
       };
 
       source.MouseMove += (o, e) => {
         if (!_isActive || e.LeftButton != MouseButtonState.Pressed || !HasDragStarted(e)) return;
+        
         var data = canDrag.Invoke(e);
+        
         if (data == null) {
           _isActive = false;
+          _source = null;
+          _data = null;
           return;
         }
-        _ = DragDrop.DoDragDrop(source, new object[] { data, source }, DragDropEffects.All);
+
+        _source = source;
+        _data = data;
+        var dataObject = string.IsNullOrEmpty(dataFormat) ? data : new DataObject(dataFormat, data);
+        _ = DragDrop.DoDragDrop(source, dataObject, DragDropEffects.All);
       };
     }
 
     public static void SetDrop(FrameworkElement target, Func<DragEventArgs, object, object, DragDropEffects> canDrop, Action<DragEventArgs, object, object> doDrop) {
       void AllowDropCheck(object sender, DragEventArgs e) {
-        var (data, source) = GetData(e.Data);
-        e.Effects = canDrop(e, source, data);
+        e.Effects = canDrop(e, _source, _data);
         e.Handled = true;
       }
 
@@ -35,17 +44,13 @@ namespace PictureManager.Utils {
       target.DragEnter += AllowDropCheck;
       target.DragLeave += AllowDropCheck;
       target.DragOver += AllowDropCheck;
-      target.Drop += (o, e) => {
-        var (data, source) = GetData(e.Data);
-        if (data == null) return;
-        doDrop.Invoke(e, source, data);
+      target.Drop += (_, e) => {
+        if (_data == null) return;
+        doDrop.Invoke(e, _source, _data);
       };
     }
 
-    public static (object, object) GetData(IDataObject dragData) =>
-      dragData.GetData(typeof(object[])) is not object[] data || data.Length != 2 ? (null, null) : (data[0], data[1]);
-
-    public static bool HasDragStarted(MouseEventArgs e) {
+    private static bool HasDragStarted(MouseEventArgs e) {
       var diff = _dragDropStartPosition - e.GetPosition(null);
       return Math.Abs(diff.X) > SystemParameters.MinimumHorizontalDragDistance ||
              Math.Abs(diff.Y) > SystemParameters.MinimumVerticalDragDistance;
