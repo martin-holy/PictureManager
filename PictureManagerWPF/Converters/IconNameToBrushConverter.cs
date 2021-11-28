a31893782287f5d4e07c6823c1d19e1a5f86324f
martin-holy <martin.holy@hotmail.com>
2021-11-28 16:23:47 +0100

    Converters

---
+++ PictureManagerWPF/Converters/IconNameToBrushConverter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace PictureManager.Converters {
+  public class IconNameToBrushConverter : IValueConverter {
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+      if (value == null) throw new ArgumentNullException(nameof(value));
+
+      var resourceName = value switch {
+        "IconFolder" or
+        "IconFolderStar" or
+        "IconFolderLock" or
+        "IconFolderPuzzle" or
+        "IconFolderOpen" => "ColorBrushFolder",
+
+        "IconTag" or
+        "IconTagLabel" => "ColorBrushTag",
+
+        "IconPeople" or
+        "IconPeopleMultiple" => "ColorBrushPeople",
+
+        "IconDrive" or
+        "IconDriveError" or
+        "IconCd" => "ColorBrushDrive",
+
+        _ => "ColorBrushWhite"
+      };
+
+      try {
+        return Application.Current.FindResource(resourceName);
+      }
+      catch (Exception ex) {
+        App.Core.LogError(ex);
+        return null;
+      }
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) =>
+      throw new NotSupportedException();
+  }
+}
