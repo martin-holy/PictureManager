0dbccaeedb7f5e4ac817ce8739d3c6762e283724
martin-holy <martin.holy@hotmail.com>
2021-12-02 14:06:52 +0100

    EqualityConverter

---
+++ MH.UI.WPF/Converters/EqualityConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace MH.UI.WPF.Converters {
+  public class EqualityConverter : IMultiValueConverter {
+    public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture) {
+      if (values?.Length != 3 || values[0] is not int valA || values[1] is not string op || values[2] is not int valB)
+        throw new ArgumentException(null, nameof(values));
+
+      return op.ToLower() switch {
+        "eq" => valA == valB,
+        "ne" => valA != valB,
+        "gt" => valA > valB,
+        "ge" => valA >= valB,
+        "lt" => valA < valB,
+        "le" => valA <= valB,
+        _ => throw new NotSupportedException(op)
+      };
+    }
+
+    public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture) =>
+      throw new NotSupportedException();
+  }
+}
