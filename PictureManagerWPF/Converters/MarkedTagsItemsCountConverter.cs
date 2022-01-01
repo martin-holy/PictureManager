80fd2b323fa377aa391e2697c7441ee56cb34ddd
martin-holy <martin.holy@hotmail.com>
2022-01-01 21:22:20 +0100

    MarkedTagsItemsCountConverter

---
+++ PictureManagerWPF/Converters/MarkedTagsItemsCountConverter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace PictureManager.Converters {
+  public class MarkedTagsItemsCountConverter : IMultiValueConverter {
+    public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture) =>
+      values?.Length == 2 && values[0] is Dictionary<object, int> tags
+        ? tags.ContainsKey(values[1])
+          ? tags[values[1]].ToString()
+          : string.Empty
+        : string.Empty;
+
+    public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture) =>
+      throw new NotSupportedException();
+  }
+}
