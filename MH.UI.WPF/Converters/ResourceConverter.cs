4bdd8d241957ad8a857cd36adb5aefc8eea4558e
martin-holy <martin.holy@hotmail.com>
2022-01-07 17:27:23 +0100

    DictionaryConverter and ResourceConverter

---
+++ MH.UI.WPF/Converters/ResourceConverter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace MH.UI.WPF.Converters {
+  public class ResourceConverter : IValueConverter {
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture) =>
+      value == null
+        ? DependencyProperty.UnsetValue
+        : Application.Current.TryFindResource(value)
+          ?? DependencyProperty.UnsetValue;
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) =>
+      throw new NotSupportedException();
+  }
+}
