7b368200e04e24931cc0afb161c58da4b594752d
martin-holy <martin.holy@hotmail.com>
2022-01-09 13:36:44 +0100

    ResourceConverter and DictionaryConverter merged

--- MH.UI.WPF/Converters/ResourceConverter.cs
+++ MH.UI.WPF/Converters/ResourceConverter.cs
@@ -1,15 +1,22 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Globalization;
 using System.Windows;
 using System.Windows.Data;
 
 namespace MH.UI.WPF.Converters {
   public class ResourceConverter : IValueConverter {
-    public object Convert(object value, Type targetType, object parameter, CultureInfo culture) =>
-      value == null
-        ? DependencyProperty.UnsetValue
-        : Application.Current.TryFindResource(value)
-          ?? DependencyProperty.UnsetValue;
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+      if (value == null)
+        return DependencyProperty.UnsetValue;
+
+      if (parameter is Dictionary<object, object> dict)
+        if (!dict.TryGetValue(value, out value) || value == null)
+          return DependencyProperty.UnsetValue;
+
+      return Application.Current.TryFindResource(value)
+             ?? DependencyProperty.UnsetValue;
+    }
 
     public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) =>
       throw new NotSupportedException();
