9dce048288a6e00c2e59d0008cdea674b485c011
martin-holy <martin.holy@hotmail.com>
2022-01-13 17:01:31 +0100

    ObjectEventArgs

---
+++ PictureManagerWPF/EventsArgs/ObjectEventArgs.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace PictureManager.EventsArgs {
+  public class ObjectEventArgs : EventArgs {
+    public object Data { get; }
+
+    public ObjectEventArgs(object data) {
+      Data = data;
+    }
+  }
+}
