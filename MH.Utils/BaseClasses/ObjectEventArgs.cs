c6fbba772ed4fa24a1e13663341155418956e92d
martin-holy <martin.holy@hotmail.com>
2022-01-14 15:34:00 +0100

    ObjectEventArgs

---
+++ MH.Utils/BaseClasses/ObjectEventArgs.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace MH.Utils.BaseClasses {
+  public class ObjectEventArgs : EventArgs {
+    public object Data { get; }
+
+    public ObjectEventArgs(object data) {
+      Data = data;
+    }
+  }
+}
