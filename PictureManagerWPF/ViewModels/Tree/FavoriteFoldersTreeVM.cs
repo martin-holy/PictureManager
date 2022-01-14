e1aad5568f74adda16a2395f3f281f24c3e33f78
martin-holy <martin.holy@hotmail.com>
2022-01-14 15:34:59 +0100

    FavoriteFolders MVVM rewrite

--- PictureManagerWPF/ViewModels/Tree/FavoriteFoldersTreeVM.cs
+++ PictureManagerWPF/ViewModels/Tree/FavoriteFoldersTreeVM.cs
@@ -1,57 +1,58 @@
-﻿using System.Collections.Specialized;
-using System.Linq;
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
 using MH.UI.WPF.BaseClasses;
 using MH.UI.WPF.Interfaces;
+using MH.Utils.Interfaces;
 using PictureManager.Domain;
 using PictureManager.Domain.Models;
 
 namespace PictureManager.ViewModels.Tree {
   public sealed class FavoriteFoldersTreeVM : CatTreeViewCategoryBase {
-    public FavoriteFoldersM Model { get; }
+    private readonly FavoriteFoldersM _model;
+    private readonly Dictionary<int, FavoriteFolderTreeVM> _all = new();
 
-    public static RelayCommand<FolderTreeVM> AddToFavoritesCommand { get; } =
-      new(item => App.Core.FavoriteFoldersM.ItemCreate(item.Model), item => item != null);
+    public RelayCommand<FolderTreeVM> AddToFavoritesCommand { get; }
 
     public FavoriteFoldersTreeVM(FavoriteFoldersM model) : base(Category.FavoriteFolders, "Favorites") {
-      Model = model;
+      _model = model;
+      CanMoveItem = true;
 
-      Model.All.CollectionChanged += All_CollectionChanged;
-      SyncItems();
+      AddToFavoritesCommand = new(
+        item => _model.ItemCreate(_model, item.Model),
+        item => item != null);
+
+      _model.Items.CollectionChanged += ModelItems_CollectionChanged;
+      _model.FavoriteFolderDeletedEvent += (_, e) =>
+        _all.Remove(((FavoriteFolderM)e.Data).Id);
+
+      // load items
+      ModelItems_CollectionChanged(_model.Items, null);
     }
 
-    private void All_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e) {
-      SyncItems();
+    private void ModelItems_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e) {
+      SyncCollection((ObservableCollection<ITreeLeaf>)sender, Items, this, SyncCollection);
     }
 
-    private void SyncItems() {
-      foreach (var ffVM in Items.Cast<FavoriteFolderTreeVM>().Where(vm => !Model.All.Contains(vm.Model)).ToArray()) {
-        Items.Remove(ffVM);
-        ffVM.Model = null;
-      }
-
-      for (int i = 0; i < Model.All.Count; i++) {
-        var ffM = Model.All[i];
-        var ffVM = Items.Cast<FavoriteFolderTreeVM>().SingleOrDefault(vm => vm.Model == ffM);
-        
-        if (ffVM == null) {
-          Items.Insert(i, new FavoriteFolderTreeVM(ffM, this));
-          continue;
-        }
-
-        var ffVMIdx = Items.IndexOf(ffVM);
-        if (ffVMIdx != i)
-          Items.Move(ffVMIdx, i);
-      }
+    private void SyncCollection(ObservableCollection<ITreeLeaf> src, ObservableCollection<ITreeLeaf> dest, ITreeBranch parent, MH.Utils.Tree.OnItemsChanged onItemsChanged) {
+      MH.Utils.Tree.SyncCollection<FavoriteFolderM, FavoriteFolderTreeVM>(src, dest, parent,
+        (model, treeVM) => treeVM.Model.Equals(model),
+        model => MH.Utils.Tree.GetDestItem(model, model.Id, _all, () => new(model, parent), null));
     }
 
     protected override void ModelItemRename(ICatTreeViewItem item, string name) =>
-      Model.ItemRename((FavoriteFolderM)ToModel(item), name);
+      _model.ItemRename((FavoriteFolderM)ToModel(item), name);
 
     protected override void ModelItemDelete(ICatTreeViewItem item) =>
-      Model.ItemDelete((FavoriteFolderM)ToModel(item));
+      _model.ItemDelete((FavoriteFolderM)ToModel(item));
+
+    public override void ItemMove(ICatTreeViewItem item, ICatTreeViewItem dest, bool aboveDest) =>
+      _model.ItemMove((FavoriteFolderM)ToModel(item), (ITreeLeaf)ToModel(dest), aboveDest);
 
     protected override string ValidateNewItemName(ICatTreeViewItem root, string name) =>
-      Model.ItemCanRename(name) ? null : $"{name} item already exists!";
+      _model.ItemCanRename(name)
+        ? null
+        : $"{name} item already exists!";
 
     public override string GetTitle(object item) =>
       (item as FavoriteFolderTreeVM)?.Model.Title;
@@ -59,11 +60,8 @@ namespace PictureManager.ViewModels.Tree {
     private static object ToModel(object item) =>
       item switch {
         FavoriteFolderTreeVM x => x.Model,
-        FavoriteFoldersTreeVM x => x.Model,
+        FavoriteFoldersTreeVM x => x._model,
         _ => null
       };
-
-    /*public override void ItemMove(ICatTreeViewItem item, ICatTreeViewItem dest, bool aboveDest) =>
-      Model.ItemMove(((FavoriteFolderTreeVM)item).Model,((FavoriteFolderTreeVM)dest).Model, aboveDest);*/
   }
 }
