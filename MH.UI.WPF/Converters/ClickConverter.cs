26d6a00fb1718de9e4709906b6f271216735988f
martin-holy <martin.holy@hotmail.com>
2022-02-03 16:38:23 +0100

    - DataContext, + OriginalSource

--- MH.UI.WPF/Converters/ClickConverter.cs
+++ MH.UI.WPF/Converters/ClickConverter.cs
@@ -1,13 +1,14 @@
 ﻿using System;
 using System.Globalization;
-using System.Windows;
 using System.Windows.Data;
 using System.Windows.Input;
 
 namespace MH.UI.WPF.Converters {
   public class ClickConverter : IValueConverter {
     public object Convert(object value, Type targetType, object parameter, CultureInfo culture) =>
-      value is not MouseButtonEventArgs args ? null : new ClickEventArgs(args, parameter is true);
+      value is not MouseButtonEventArgs args
+        ? null
+        : new ClickEventArgs(args, parameter is true);
 
     public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) =>
       throw new NotSupportedException();
@@ -16,7 +17,7 @@ namespace MH.UI.WPF.Converters {
   public class ClickEventArgs {
     public int ClickCount { get; }
     public object Source { get; }
-    public object DataContext { get; set; }
+    public object OriginalSource { get; }
     public bool IsCtrlOn { get; }
     public bool IsAltOn { get; }
     public bool IsShiftOn { get; }
@@ -24,7 +25,7 @@ namespace MH.UI.WPF.Converters {
     public ClickEventArgs(MouseButtonEventArgs args, bool allButtons) {
       ClickCount = args.ClickCount;
       Source = args.Source;
-      DataContext = (args.Source as FrameworkElement)?.DataContext;
+      OriginalSource = args.OriginalSource;
       IsCtrlOn = (Keyboard.Modifiers & ModifierKeys.Control) > 0;
       IsAltOn = (Keyboard.Modifiers & ModifierKeys.Alt) > 0;
       IsShiftOn = (Keyboard.Modifiers & ModifierKeys.Shift) > 0;
