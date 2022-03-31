43e6ccec8e9a6feea853b11ba2bbe1b94f327065
martin-holy <martin.holy@hotmail.com>
2022-03-31 13:58:15 +0100

    class for grouped items

---
+++ MH.Utils/HelperClasses/ItemsGroup.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections.Generic;
+
+namespace MH.Utils.HelperClasses {
+  public class ItemsGroup {
+    public List<object> GroupInfo { get; } = new();
+    public List<object> Items { get; } = new();
+  }
+}
