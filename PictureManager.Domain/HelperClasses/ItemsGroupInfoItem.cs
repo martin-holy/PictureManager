a3ac144b29189f763f663b618c815e5ee4a73ce9
martin-holy <martin.holy@hotmail.com>
2022-03-31 14:02:12 +0100

    info class for grouped items

---
+++ PictureManager.Domain/HelperClasses/ItemsGroupInfoItem.cs
@@ -0,0 +1,13 @@
+﻿using MH.Utils.BaseClasses;
+
+namespace PictureManager.Domain.HelperClasses {
+  public class ItemsGroupInfoItem : ObservableObject {
+    private string _icon;
+    private string _title;
+    private string _toolTip;
+
+    public string Icon { get => _icon; set { _icon = value; OnPropertyChanged(); } }
+    public string Title { get => _title; set { _title = value; OnPropertyChanged(); } }
+    public string ToolTip { get => _toolTip; set { _toolTip = value; OnPropertyChanged(); } }
+  }
+}
