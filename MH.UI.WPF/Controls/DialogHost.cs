78b04ea03b8938211b6bab360fc96a7cbd598558
martin-holy <martin.holy@hotmail.com>
2022-04-04 17:15:28 +0100

    DialogHost, MessageDialogStyle

---
+++ MH.UI.WPF/Controls/DialogHost.cs
@@ -0,0 +1,31 @@
+﻿using MH.Utils.Interfaces;
+using System.Windows;
+
+namespace MH.UI.WPF.Controls {
+  public class DialogHost : Window {
+    static DialogHost() {
+      DefaultStyleKeyProperty.OverrideMetadata(typeof(DialogHost),
+        new FrameworkPropertyMetadata(typeof(DialogHost)));
+    }
+
+    private DialogHost(IDialog content) {
+      Content = content;
+      Owner = Application.Current.MainWindow;
+
+      // I had problem with setting this in XAML
+      WindowStartupLocation = WindowStartupLocation.CenterOwner;
+
+      content.PropertyChanged += (_, e) => {
+        if (nameof(content.Result).Equals(e.PropertyName))
+          Close();
+      };
+    }
+
+    public static int Show(IDialog content) {
+      var dh = new DialogHost(content);
+      dh.ShowDialog();
+
+      return content.Result;
+    }
+  }
+}
