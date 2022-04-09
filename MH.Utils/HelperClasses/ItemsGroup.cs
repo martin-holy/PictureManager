5207d84a107f2e8c1796b9c213eb5d4fdc687611
martin-holy <martin.holy@hotmail.com>
2022-04-09 16:09:14 +0100

    ItemsGroup List to ObservableCollection

--- MH.Utils/HelperClasses/ItemsGroup.cs
+++ MH.Utils/HelperClasses/ItemsGroup.cs
@@ -1,8 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections.ObjectModel;
 
 namespace MH.Utils.HelperClasses {
   public class ItemsGroup {
-    public List<object> GroupInfo { get; } = new();
-    public List<object> Items { get; } = new();
+    public ObservableCollection<object> Info { get; } = new();
+    public ObservableCollection<object> Items { get; } = new();
   }
 }
