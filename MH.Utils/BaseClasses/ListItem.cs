dba81aba86c8be8a9884f61c3488ade0efcf9da9
martin-holy <martin.holy@hotmail.com>
2022-04-17 19:05:11 +0100

    IsHidden renamed to IsVisible

--- MH.Utils/BaseClasses/ListItem.cs
+++ MH.Utils/BaseClasses/ListItem.cs
@@ -2,11 +2,11 @@
   public class ListItem<T> : ObservableObject {
     private T _content;
     private bool _isSelected;
-    private bool _isVisible = true;
+    private bool _isHidden = true;
 
     public T Content { get => _content; set { _content = value; OnPropertyChanged(); } }
     public bool IsSelected { get => _isSelected; set { _isSelected = value; OnPropertyChanged(); } }
-    public bool IsVisible { get => _isVisible; set { _isVisible = value; OnPropertyChanged(); } }
+    public bool IsHidden { get => _isHidden; set { _isHidden = value; OnPropertyChanged(); } }
 
     public ListItem(T content) {
       Content = content;
