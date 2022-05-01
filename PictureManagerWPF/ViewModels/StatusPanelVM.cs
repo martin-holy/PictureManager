66d11620773349ce97ef96a37a453b3736361ac1
martin-holy <martin.holy@hotmail.com>
2022-05-01 12:08:13 +0100

    PinCommand moved to model

--- PictureManagerWPF/ViewModels/StatusPanelVM.cs
+++
@@ -1,8 +0,0 @@
-﻿using MH.Utils.BaseClasses;
-
-namespace PictureManager.ViewModels {
-  public static class StatusPanelVM  {
-    public static RelayCommand<object> PinCommand { get; } = new(
-      () => App.Core.StatusPanelM.IsPinned = !App.Core.StatusPanelM.IsPinned);
-  }
-}
