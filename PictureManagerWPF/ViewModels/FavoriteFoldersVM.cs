e46ffd79c325742e1697aceff93c476214c46c62
martin-holy <martin.holy@hotmail.com>
2022-05-01 09:25:18 +0100

    AddToFavoritesCommand moved to model

--- PictureManagerWPF/ViewModels/FavoriteFoldersVM.cs
+++
@@ -1,9 +0,0 @@
-﻿using MH.Utils.BaseClasses;
-using PictureManager.Domain.Models;
-
-namespace PictureManager.ViewModels {
-  public static class FavoriteFoldersVM {
-    public static RelayCommand<FolderM> AddToFavoritesCommand { get; } = new(
-      item => App.Core.FavoriteFoldersM.ItemCreate(App.Core.FavoriteFoldersM, item));
-  }
-}
