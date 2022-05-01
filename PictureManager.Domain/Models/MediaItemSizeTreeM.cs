c102e04fdee2618442d04894de95c06fd3c3ab08
martin-holy <martin.holy@hotmail.com>
2022-05-01 10:05:29 +0100

    MediaItemSizeTree RangeChangedCommand moved to model

--- PictureManager.Domain/Models/MediaItemSizeTreeM.cs
+++ PictureManager.Domain/Models/MediaItemSizeTreeM.cs
@@ -1,5 +1,20 @@
 ﻿using MH.Utils.BaseClasses;
 
 namespace PictureManager.Domain.Models {
-  public sealed class MediaItemSizeTreeM : TreeItem { }
+  public sealed class MediaItemSizeTreeM : TreeItem {
+    private ThumbnailsGridM _currentGrid;
+
+    public ThumbnailsGridM CurrentGrid { get => _currentGrid; set { _currentGrid = value; OnPropertyChanged(); } }
+    public RelayCommand<object> RangeChangedCommand { get; }
+
+    public MediaItemSizeTreeM() {
+      RangeChangedCommand = new(
+        () => {
+            CurrentGrid.FilterSize.AllSizes = false;
+            _ = CurrentGrid.ReapplyFilter();
+          },
+        () => CurrentGrid != null
+      );
+    }
+  }
 }
