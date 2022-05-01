42080f43a8fb1255db685bc5da143793839c318c
martin-holy <martin.holy@hotmail.com>
2022-05-01 17:50:30 +0100

    RotationDialog MVVM rewrite

---
+++ PictureManager.Domain/Dialogs/RotationDialogM.cs
@@ -0,0 +1,18 @@
+﻿using MH.Utils.BaseClasses;
+using MH.Utils.Interfaces;
+
+namespace PictureManager.Domain.Dialogs {
+  public sealed class RotationDialogM : ObservableObject, IDialog {
+    private string _title;
+    private int _result = 1;
+
+    public string Title { get => _title; set { _title = value; OnPropertyChanged(); } }
+    public int Result { get => _result; set { _result = value; OnPropertyChanged(); } }
+    public RelayCommand<MediaOrientation> RotationCommand { get; }
+
+    public RotationDialogM() {
+      Title = "Rotation";
+      RotationCommand = new(mo => Result = (int)mo);
+    }
+  }
+}
