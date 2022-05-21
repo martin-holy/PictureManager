f375f39eb298f90199f4dc7b819eb6125def2fee
martin-holy <martin.holy@hotmail.com>
2022-05-21 13:07:58 +0100

    AllToBoolConverter and AllToVisibilityConverter

---
+++ MH.UI.WPF/Converters/AllToBoolConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace MH.UI.WPF.Converters {
+  public class AllToBoolConverter : IValueConverter {
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture) =>
+      AllToBool(value, parameter);
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) =>
+      throw new NotSupportedException();
+
+    public static bool AllToBool(object value, object parameter) {
+      if (value == null) return false;
+
+      if (parameter != null)
+        return value switch {
+          string s => s.Equals(parameter),
+          int i => i.Equals(int.Parse((string)parameter)),
+          _ => value.Equals(parameter),
+        };
+
+      return value switch {
+        string s => !string.IsNullOrEmpty(s),
+        bool b => b,
+        int i => i > 0,
+        Collection<string> c => c.Count > 0,
+        _ => true, // value != null
+      };
+    }
+  }
+}
---
+++ MH.UI.WPF/Converters/AllToVisibilityConverter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace MH.UI.WPF.Converters {
+  public class AllToVisibilityConverter : IValueConverter {
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture) =>
+      AllToBoolConverter.AllToBool(value, parameter)
+      ? Visibility.Visible
+      : Visibility.Collapsed;
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) =>
+      throw new NotSupportedException();
+  }
+}
