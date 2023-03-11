e3cc27004eba25f1226cf7c4a2c99a60ba5c8160
martin-holy <martin.holy@hotmail.com>
2023-03-11 14:32:23 +0100

    IconTextButton control

---
+++ MH.UI.WPF/Controls/IconTextButton.cs
@@ -0,0 +1,38 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace MH.UI.WPF.Controls {
+  public class IconTextButton : Button {
+    public static readonly DependencyProperty IconProperty =
+      DependencyProperty.Register(nameof(Icon), typeof(PathGeometry), typeof(IconTextButton));
+    public static readonly DependencyProperty TextProperty =
+      DependencyProperty.Register(nameof(Text), typeof(string), typeof(IconTextButton));
+
+    public PathGeometry Icon {
+      get => (PathGeometry)GetValue(IconProperty);
+      set => SetValue(IconProperty, value);
+    }
+
+    public string Text {
+      get => (string)GetValue(TextProperty);
+      set => SetValue(TextProperty, value);
+    }
+
+    static IconTextButton() {
+      DefaultStyleKeyProperty.OverrideMetadata(
+        typeof(IconTextButton),
+        new FrameworkPropertyMetadata(typeof(IconTextButton)));
+    }
+
+    public override void OnApplyTemplate() {
+      base.OnApplyTemplate();
+
+      if (Effect == null) return;
+      (Template.FindName("PART_Icon", this) as UIElement).Effect = Effect;
+      (Template.FindName("PART_Text", this) as UIElement).Effect = Effect;
+      Effect = null;
+    }
+  }
+}
