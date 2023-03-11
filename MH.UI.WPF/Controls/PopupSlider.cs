d4cc6105be09be9879744bb23ab5637576bc474d
martin-holy <martin.holy@hotmail.com>
2023-03-11 14:32:44 +0100

    PopupSlider control

---
+++ MH.UI.WPF/Controls/PopupSlider.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace MH.UI.WPF.Controls {
+  public class PopupSlider: Slider {
+    public static readonly DependencyProperty IconProperty =
+      DependencyProperty.Register(nameof(Icon), typeof(PathGeometry), typeof(PopupSlider));
+    
+    public PathGeometry Icon {
+      get => (PathGeometry)GetValue(IconProperty);
+      set => SetValue(IconProperty, value);
+    }
+
+    static PopupSlider() {
+      DefaultStyleKeyProperty.OverrideMetadata(
+        typeof(PopupSlider),
+        new FrameworkPropertyMetadata(typeof(PopupSlider)));
+    }
+
+    public override void OnApplyTemplate() {
+      base.OnApplyTemplate();
+
+      if (Effect == null) return;
+      (Template.FindName("PART_IconTextButton", this) as UIElement).Effect = Effect;
+      Effect = null;
+    }
+  }
+}
