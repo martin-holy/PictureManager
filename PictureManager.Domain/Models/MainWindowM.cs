383db1a31e867c53eed6a1837c2c71b2addfe5b5
martin-holy <martin.holy@hotmail.com>
2023-03-15 11:42:25 +0100

    ActiveLayout

--- PictureManager.Domain/Models/MainWindowM.cs
+++ PictureManager.Domain/Models/MainWindowM.cs
@@ -2,14 +2,17 @@
 
 namespace PictureManager.Domain.Models {
   public sealed class MainWindowM : ObservableObject {
+    private int _activeLayout;
     private bool _isFullScreen;
 
-    public bool IsFullScreenIsChanging { get; private set; }
+    public int ActiveLayout { get => _activeLayout; set { _activeLayout = value; OnPropertyChanged(); } }
+    public bool IsFullScreenIsChanging { get; set; }
     public bool IsFullScreen {
       get => _isFullScreen;
       set {
         IsFullScreenIsChanging = true;
         _isFullScreen = value;
+        ActiveLayout = value ? 1 : 0;
         OnPropertyChanged();
         IsFullScreenIsChanging = false;
       }
