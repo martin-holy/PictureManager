c803ecfab8a067f552b25b2360966b1ba6ddfe78
martin-holy <martin.holy@hotmail.com>
2023-03-15 11:43:07 +0100

    custom window

--- PictureManagerWPF/ViewModels/MainWindowVM.cs
+++ PictureManagerWPF/ViewModels/MainWindowVM.cs
@@ -5,14 +5,21 @@ using PictureManager.Domain;
 using PictureManager.Domain.Dialogs;
 using PictureManager.Domain.Models;
 using PictureManager.Properties;
+using System;
 using System.Windows;
 
 namespace PictureManager.ViewModels {
   public sealed class MainWindowVM : ObservableObject {
-    private readonly Core _core;
-    private readonly AppCore _coreVM;
+    private bool _isMaximized = true;
+    private bool _fixWindowSize = true;
 
+    public Core CoreM { get; }
+    public AppCore CoreVM { get; }
     public MainWindowM Model { get; }
+
+    public bool IsMaximized { get => _isMaximized; set { _isMaximized = value; OnPropertyChanged(); } }
+    public bool FixWindowSize { get => _fixWindowSize; set { _fixWindowSize = value; OnPropertyChanged(); } }
+    public bool CanOpenStatusPanel => CoreM.ThumbnailsGridsM.Current != null || CoreVM.MediaViewerVM.IsVisible;
 
     public RelayCommand<object> SwitchToBrowserCommand { get; }
     public RelayCommand<object> OpenSettingsCommand { get; }
@@ -22,10 +29,11 @@ namespace PictureManager.ViewModels {
     public RelayCommand<object> SaveDbCommand { get; }
     public RelayCommand<object> ClosingCommand { get; }
     public RelayCommand<object> LoadedCommand { get; }
+    public RelayCommand<Window> WindowStateChangedCommand { get; }
 
-    public MainWindowVM(Core core, AppCore coreVM, MainWindowM model) {
-      _core = core;
-      _coreVM = coreVM;
+    public MainWindowVM(Core coreM, AppCore coreVM, MainWindowM model) {
+      CoreM = coreM;
+      CoreVM = coreVM;
       Model = model;
 
       SwitchToBrowserCommand = new(() => Model.IsFullScreen = false);
@@ -34,10 +42,50 @@ namespace PictureManager.ViewModels {
       OpenLogCommand = new(OpenLog);
       TestButtonCommand = new(() => Tests.Run());
       SaveDbCommand = new(
-        () => _core.Sdb.SaveAllTables(),
-        () => _core.Sdb.Changes > 0);
+        () => CoreM.Sdb.SaveAllTables(),
+        () => CoreM.Sdb.Changes > 0);
       ClosingCommand = new(Closing);
       LoadedCommand = new(Loaded);
+      WindowStateChangedCommand = new(WindowStateChanged);
+
+      AttachEvents();
+    }
+
+    private void AttachEvents() {
+      Model.PropertyChanged += (_, e) => {
+        if (nameof(Model.IsFullScreen).Equals(e.PropertyName))
+          OnFullScreenChanged(App.MainWindowV, Model.IsFullScreen);
+      };
+    }
+
+    private static void OnFullScreenChanged(Window window, bool isFullScreen) {
+      // INFO for smoother transition from/to full screen
+      // Adjust the appearance and performance of Windows
+      // Disable => Animate windows when minimizing and maximizing
+
+      var top = window.Top;
+      var left = window.Left;
+      var width = window.Width;
+      var height = window.Height;
+
+      window.Top = 0;
+      window.Left = 0;
+      window.Width = SystemParameters.PrimaryScreenWidth;
+      window.Height = SystemParameters.PrimaryScreenHeight;
+      window.WindowState = WindowState.Normal;
+      window.ResizeMode = isFullScreen ? ResizeMode.NoResize : ResizeMode.CanResize;
+      window.WindowStyle = isFullScreen ? WindowStyle.None : WindowStyle.SingleBorderWindow;
+      window.WindowState = WindowState.Maximized;
+
+      window.Top = top;
+      window.Left = left;
+      window.Width = width;
+      window.Height = height;
+    }
+
+    private void WindowStateChanged(Window window) {
+      IsMaximized = window.WindowState == WindowState.Maximized;
+      FixWindowSize = window.WindowState == WindowState.Maximized && !Model.IsFullScreen;
     }
 
     private static void OpenSettings() {
@@ -58,25 +106,25 @@ namespace PictureManager.ViewModels {
     }
 
     private void Closing() {
-      if (_core.MediaItemsM.ModifiedItems.Count > 0 &&
+      if (CoreM.MediaItemsM.ModifiedItems.Count > 0 &&
           Core.DialogHostShow(new MessageDialog(
             "Metadata Edit",
             "Some Media Items are modified, do you want to save them?",
             Res.IconQuestion,
             true)) == 0) {
-        _core.MediaItemsM.SaveEdit();
+        CoreM.MediaItemsM.SaveEdit();
       }
 
-      if (_core.Sdb.Changes > 0 &&
+      if (CoreM.Sdb.Changes > 0 &&
           Core.DialogHostShow(new MessageDialog(
             "Database changes",
             "There are some changes in database, do you want to save them?",
             Res.IconQuestion,
             true)) == 0) {
-        _core.Sdb.SaveAllTables();
+        CoreM.Sdb.SaveAllTables();
       }
 
-      _core.Sdb.BackUp();
+      CoreM.Sdb.BackUp();
     }
 
     private void Loaded() {
@@ -87,9 +135,9 @@ namespace PictureManager.ViewModels {
         windowsDisplayScale = PresentationSource.FromVisual(Application.Current.MainWindow)
         ?.CompositionTarget?.TransformToDevice.M11 ?? 1.0;
 
-      _core.ThumbnailsGridsM.DefaultThumbScale = 1 / windowsDisplayScale;
-      _coreVM.SegmentsVM.SegmentUiSize = _core.SegmentsM.SegmentSize / windowsDisplayScale;
-      _core.MediaItemsM.OnPropertyChanged(nameof(_core.MediaItemsM.MediaItemsCount));
+      CoreM.ThumbnailsGridsM.DefaultThumbScale = 1 / windowsDisplayScale;
+      CoreVM.SegmentsVM.SegmentUiSize = CoreM.SegmentsM.SegmentSize / windowsDisplayScale;
+      CoreM.MediaItemsM.OnPropertyChanged(nameof(CoreM.MediaItemsM.MediaItemsCount));
     }
   }
 }
