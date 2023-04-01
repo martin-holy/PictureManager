4f46ba3ae947811a363fe939991463766b2f7d08
martin-holy <martin.holy@hotmail.com>
2023-04-01 12:29:09 +0200

    select moved to model

--- PictureManagerWPF/ViewModels/PeopleVM.cs
+++ PictureManagerWPF/ViewModels/PeopleVM.cs
@@ -1,8 +1,6 @@
-﻿using System.Linq;
-using System.Windows;
+﻿using System.Windows;
 using MH.UI.WPF.Controls;
 using MH.Utils.BaseClasses;
-using MH.Utils.EventsArgs;
 using PictureManager.Domain;
 using PictureManager.Domain.Models;
 
@@ -11,7 +9,6 @@ namespace PictureManager.ViewModels {
     private readonly Core _core;
 
     public PeopleM PeopleM { get; }
-    public RelayCommand<ClickEventArgs> SelectCommand { get; }
     public RelayCommand<SizeChangedEventArgs> PanelSizeChangedCommand { get; }
 
     public PeopleVM(Core core, PeopleM peopleM) {
@@ -19,18 +16,7 @@ namespace PictureManager.ViewModels {
       PeopleM = peopleM;
 
       PeopleM.MainTabsItem = new(this, "People");
-
-      SelectCommand = new(Select);
       PanelSizeChangedCommand = new(PanelSizeChanged);
-
-      // TODO do it just for loaded
-      foreach (var person in PeopleM.DataAdapter.All.Values)
-        person.UpdateDisplayKeywords();
-    }
-
-    private void Select(ClickEventArgs e) {
-      if (e.DataContext is SegmentM segmentM)
-        PeopleM.Select(null, segmentM.Person, e.IsCtrlOn, e.IsShiftOn);
     }
 
     private void PanelSizeChanged(SizeChangedEventArgs e) {
