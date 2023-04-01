0627fe47f706e56695e417649c02fb7c635a0323
martin-holy <martin.holy@hotmail.com>
2023-04-01 11:13:25 +0200

    GetKeyboardModifiers removed

--- PictureManagerWPF/Utils/InputUtils.cs
+++ PictureManagerWPF/Utils/InputUtils.cs
@@ -2,19 +2,6 @@
 
 namespace PictureManager.Utils {
   public static class InputUtils {
-    public static (bool isCtrlOn, bool isShiftOn) GetKeyboardModifiers(MouseButtonEventArgs e) {
-      var isCtrlOn = (Keyboard.Modifiers & ModifierKeys.Control) > 0;
-      var isShiftOn = (Keyboard.Modifiers & ModifierKeys.Shift) > 0;
-
-      // use middle and right button like CTRL + left button
-      if (e.ChangedButton is MouseButton.Middle or MouseButton.Right) {
-        isCtrlOn = true;
-        isShiftOn = false;
-      }
-
-      return new(isCtrlOn, isShiftOn);
-    }
-
     public static (bool isCtrlOn, bool isAltOn, bool isShiftOn) GetControlAltShiftModifiers() {
       var isCtrlOn = (Keyboard.Modifiers & ModifierKeys.Control) > 0;
       var isAltOn = (Keyboard.Modifiers & ModifierKeys.Alt) > 0;
