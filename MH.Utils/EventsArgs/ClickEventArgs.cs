74e9ea7a00d3b8686f796bb275dbc84a89cf4ed3
martin-holy <martin.holy@hotmail.com>
2023-04-01 11:12:22 +0200

    ClickEventArgs

---
+++ MH.Utils/EventsArgs/ClickEventArgs.cs
@@ -0,0 +1,12 @@
+﻿namespace MH.Utils.EventsArgs {
+  public class ClickEventArgs {
+    public object OriginalSource { get; set; }
+    public object DataContext { get; set; }
+    public int ClickCount { get; set; }
+    public bool IsCtrlOn { get; set; }
+    public bool IsAltOn { get; set; }
+    public bool IsShiftOn { get; set; }
+
+    public ClickEventArgs() { }
+  }
+}
