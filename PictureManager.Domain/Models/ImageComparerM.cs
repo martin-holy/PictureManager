7d47601b7c3e22dff6ad8dda6e05bd3a730be285
martin-holy <martin.holy@hotmail.com>
2023-04-02 16:56:42 +0200

    ImageComparer moved to model

--- PictureManager.Domain/Models/ImageComparerM.cs
+++ PictureManager.Domain/Models/ImageComparerM.cs
@@ -1,31 +1,26 @@
 ﻿using MH.Utils.BaseClasses;
 using MH.Utils.Dialogs;
-using PictureManager.Domain.Models;
-using PictureManager.Domain;
-using PictureManager.Utils;
+using PictureManager.Domain.Utils;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net.Sockets;
 using System.Threading.Tasks;
-using System.Windows;
 
-namespace PictureManager.ViewModels {
-  public sealed class ImageComparerVM : ObservableObject {
+namespace PictureManager.Domain.Models {
+  public sealed class ImageComparerM : ObservableObject {
     private readonly Dictionary<object, long> _avgHashes = new();
     private readonly Dictionary<object, long> _pHashes = new();
     private int _diff;
 
     public int Diff { get => _diff; set { _diff = value; OnPropertyChanged(); } }
-    public delegate long HashMethod(string srcPath, Int32Rect rect);
     public RelayCommand<ThumbnailsGridM> AverageHashCommand { get; }
     public RelayCommand<ThumbnailsGridM> PHashCommand { get; }
 
-    public ImageComparerVM() {
+    public ImageComparerM() {
       AverageHashCommand = new(async (tg) => await Compare(tg, _avgHashes, Imaging.GetAvgHash));
       PHashCommand = new(async (tg) => await Compare(tg, _pHashes, Imaging.GetPerceptualHash));
     }
 
-    public async Task Compare(ThumbnailsGridM thumbsGrid, Dictionary<object, long> hashes, HashMethod hashMethod) {
+    public async Task Compare(ThumbnailsGridM thumbsGrid, Dictionary<object, long> hashes, Imaging.ImageHashFunc hashMethod) {
       if (thumbsGrid == null) return;
 
       var items = thumbsGrid.Filter(thumbsGrid.LoadedItems);
@@ -43,7 +38,7 @@ namespace PictureManager.ViewModels {
       await thumbsGrid.ThumbsGridReloadItems();
     }
 
-    private static List<object> GetSimilar(MediaItemM[] items, int limit, Dictionary<object, long> hashes, HashMethod hashMethod) {
+    private static List<object> GetSimilar(MediaItemM[] items, int limit, Dictionary<object, long> hashes, Imaging.ImageHashFunc hashMethod) {
       // get hashes
       if (hashes.Count == 0)
         GetHashes(items, hashes, hashMethod);
@@ -52,7 +47,7 @@ namespace PictureManager.ViewModels {
       return Imaging.GetSimilarImages(hashes, limit);
     }
 
-    private static void GetHashes(MediaItemM[] items, Dictionary<object, long> hashes, HashMethod hashMethod) {
+    private static void GetHashes(MediaItemM[] items, Dictionary<object, long> hashes, Imaging.ImageHashFunc hashMethod) {
       var progress = new ProgressBarDialog("Computing Hashes ...", false, 1);
       progress.AddEvents(
         items,
@@ -60,7 +55,7 @@ namespace PictureManager.ViewModels {
         // action
         (mi) => {
           if (!hashes.ContainsKey(mi))
-            hashes.Add(mi, hashMethod(mi.FilePathCache, Int32Rect.Empty));
+            hashes.Add(mi, hashMethod(mi.FilePathCache));
         },
         mi => mi.FilePath,
         null);
