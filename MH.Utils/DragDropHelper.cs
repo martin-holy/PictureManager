fc96c39bc382b9d084631aefe1d1b4652e68f0e1
martin-holy <martin.holy@hotmail.com>
2023-04-04 08:28:51 +0200

    DragDropHelper

---
+++ MH.Utils/DragDropHelper.cs
@@ -0,0 +1,16 @@
+﻿namespace MH.Utils {
+  public enum DragDropEffects {
+    Scroll = int.MinValue,
+    All = -2147483645,
+    None = 0,
+    Copy = 1,
+    Move = 2,
+    Link = 4
+  }
+
+  public static class DragDropHelper {
+    public delegate object CanDragFunc(object source);
+    public delegate DragDropEffects CanDropFunc(object target, object data, bool haveSameOrigin);
+    public delegate void DoDropAction(object data, bool haveSameOrigin);
+  }
+}
