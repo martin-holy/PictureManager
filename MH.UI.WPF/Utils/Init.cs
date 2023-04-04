5f034f367c2198800ed2d3afd73aaec840210163
martin-holy <martin.holy@hotmail.com>
2023-04-04 12:58:46 +0200

    Keyboard modifiers

---
+++ MH.UI.WPF/Utils/Init.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Input;
+
+namespace MH.UI.WPF.Utils {
+  public static class Init {
+    public static void SetDelegates() {
+      MH.Utils.Keyboard.IsCtrlOn = () => (Keyboard.Modifiers & ModifierKeys.Control) > 0;
+      MH.Utils.Keyboard.IsAltOn = () => (Keyboard.Modifiers & ModifierKeys.Alt) > 0;
+      MH.Utils.Keyboard.IsShiftOn = () => (Keyboard.Modifiers & ModifierKeys.Shift) > 0;
+    }
+  }
+}
