acd8b0212531c9996ac0c9056436f9d15755727e
martin-holy <martin.holy@hotmail.com>
2023-04-04 12:58:17 +0200

    Keyboard modifiers

---
+++ MH.Utils/Keyboard.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace MH.Utils {
+  public static class Keyboard {
+    public static Func<bool> IsCtrlOn { get; set; }
+    public static Func<bool> IsAltOn { get; set; }
+    public static Func<bool> IsShiftOn { get; set; }
+  }
+}
