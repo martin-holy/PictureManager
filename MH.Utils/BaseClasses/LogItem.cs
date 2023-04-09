3be8a28d3e0abf95301bce6ba6bcc7d2051d4564
martin-holy <martin.holy@hotmail.com>
2023-04-09 08:28:57 +0200

    Log

---
+++ MH.Utils/BaseClasses/LogItem.cs
@@ -0,0 +1,11 @@
+﻿namespace MH.Utils.BaseClasses {
+  public class LogItem {
+    public string Title { get; set; }
+    public string Detail { get; set; }
+
+    public LogItem(string title, string detail) {
+      Title = title;
+      Detail = detail;
+    }
+  }
+}
---
+++ MH.Utils/Log.cs
@@ -0,0 +1,20 @@
+﻿using MH.Utils.BaseClasses;
+using System;
+using System.Collections.ObjectModel;
+
+namespace MH.Utils {
+  public static class Log {
+    public static ObservableCollection<LogItem> Items { get; } = new();
+
+    public static void Error(Exception ex) =>
+      Error(ex, string.Empty);
+
+    public static void Error(Exception ex, string msg) =>
+      Tasks.RunOnUiThread(() =>
+        Items.Add(new(
+          string.IsNullOrEmpty(msg)
+            ? ex.Message
+            : msg,
+          $"{msg}\n{ex.Message}\n{ex.StackTrace}")));
+  }
+}
