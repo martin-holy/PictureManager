5a6d6fb1b5ba1fae2f289764e469b145ce7e7321
martin-holy <martin.holy@hotmail.com>
2023-04-09 08:02:30 +0200

    RunOnUiThread methods

---
+++ MH.Utils/Tasks.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+using System;
+
+namespace MH.Utils {
+  public static class Tasks {
+    public static TaskScheduler UiTaskScheduler { get; set; }
+
+    public static void SetUiTaskScheduler() =>
+      UiTaskScheduler = TaskScheduler.FromCurrentSynchronizationContext();
+
+    public static Task RunOnUiThread(Action action) {
+      var task = new Task(action);
+      task.Start(UiTaskScheduler);
+      return task;
+    }
+
+    public static Task<T> RunOnUiThread<T>(Func<T> func) {
+      var task = new Task<T>(func);
+      task.Start(UiTaskScheduler);
+      return task;
+    }
+  }
+}
