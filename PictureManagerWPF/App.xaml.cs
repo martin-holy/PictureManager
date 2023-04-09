dc92cdf097afc9b8e192405ef0dfc809da11c8fa
martin-holy <martin.holy@hotmail.com>
2023-04-09 07:47:36 +0200

    MainWindowV removed from properties

--- PictureManagerWPF/App.xaml.cs
+++ PictureManagerWPF/App.xaml.cs
@@ -10,8 +10,7 @@ namespace PictureManager {
   public partial class App {
     public static Core Core => (Core)Current.Properties[nameof(AppProperty.Core)];
     public static AppCore Ui => (AppCore)Current.Properties[nameof(AppProperty.Ui)];
-    public static MainWindowV MainWindowV => (MainWindowV)Current.Properties[nameof(AppProperty.MainWindowV)];
-
+    
     protected override async void OnStartup(StartupEventArgs e) {
       base.OnStartup(e);
 
@@ -25,17 +24,16 @@ namespace PictureManager {
 
       var splashScreen = new SplashScreenV();
       MainWindow = splashScreen;
-      splashScreen.Show();
+      MainWindow.Show();
 
       await Core.Instance.InitAsync(splashScreen.ProgressMessage);
 
       Current.Properties[nameof(AppProperty.Core)] = Core.Instance;
       Current.Properties[nameof(AppProperty.Ui)] = new AppCore();
-      Current.Properties[nameof(AppProperty.MainWindowV)] = new MainWindowV();
 
       ShutdownMode = ShutdownMode.OnMainWindowClose;
-      MainWindow = MainWindowV;
-      MainWindowV.Show();
+      MainWindow = new MainWindowV();
+      MainWindow.Show();
 
       splashScreen.Close();
     }
