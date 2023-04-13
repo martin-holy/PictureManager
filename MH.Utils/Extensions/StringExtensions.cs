4ede833dab25c0e7c79fd304a7fdc8cace6569a9
martin-holy <martin.holy@hotmail.com>
2023-04-13 12:14:32 +0200

    TryParseDoubleUniversal

--- MH.Utils/Extensions/StringExtensions.cs
+++ MH.Utils/Extensions/StringExtensions.cs
@@ -1,6 +1,9 @@
-﻿namespace MH.Utils.Extensions {
+﻿using System.Linq;
+
+namespace MH.Utils.Extensions {
   public static class StringExtensions {
-    public static int IntParseOrDefault(this string s, int d) => int.TryParse(s, out var result) ? result : d;
+    public static int IntParseOrDefault(this string s, int d) =>
+      int.TryParse(s, out var result) ? result : d;
 
     public static int FirstIndexOfLetter(this string s) {
       var index = 0;
@@ -11,6 +14,23 @@
       }
 
       return index;
+    }
+
+    public static bool TryParseDoubleUniversal(this string s, out double result) {
+      result = 0.0;
+      if (string.IsNullOrEmpty(s)) return false;
+
+      var clean = new string(s.Where(x => char.IsDigit(x) || x == '.' || x == ',' || x == '-').ToArray());
+      var iOfSep = clean.LastIndexOfAny(new[] { ',', '.' });
+      var partA = clean.Substring(0, iOfSep).Replace(",", string.Empty).Replace(".", string.Empty);
+      var partB = clean.Substring(iOfSep + 1);
+      if (!int.TryParse(partA, out var intA)) return false;
+      if (!int.TryParse(partB, out var intB)) return false;
+      if (intA < 0) intB *= -1;
+      var dp = double.Parse("1".PadRight(partB.Length + 1, '0'));
+
+      result = intA + intB / dp;
+      return true;
     }
   }
 }
