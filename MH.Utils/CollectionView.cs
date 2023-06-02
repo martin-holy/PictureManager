fe992ef70b97b35a8b1caa023a04165da42b325d
martin-holy <martin.holy@hotmail.com>
2023-06-02 11:17:30 +0200

    CollectionView with grouping

---
+++ MH.Utils/CollectionView.cs
@@ -0,0 +1,138 @@
+﻿using MH.Utils.BaseClasses;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using MH.Utils.Dialogs;
+
+namespace MH.Utils {
+  public class CollectionView {
+    public CollectionViewGroup Root { get; set; }
+    public virtual int GetItemWidth(object item) => throw new NotImplementedException();
+    public virtual IEnumerable<object> GetGroupByItems(IEnumerable<object> source) => throw new NotImplementedException();
+    public virtual IOrderedEnumerable<CollectionViewGroup> GroupByItem(CollectionViewGroup group, ListItem<object> item) => throw new NotImplementedException();
+    public virtual void Select(IEnumerable<object> source, object item, bool isCtrlOn, bool isShiftOn) => throw new NotImplementedException();
+
+    public RelayCommand<CollectionViewGroup> OpenGroupByDialogCommand { get; }
+
+    public CollectionView() {
+      OpenGroupByDialogCommand = new(OpenGroupByDialog);
+    }
+
+    private void OpenGroupByDialog(CollectionViewGroup group) {
+      var dlg = new GroupByDialog("Chose items to group by", "IconGroup");
+      dlg.SetAvailable(GetGroupByItems(group.Source));
+
+      if (Dialog.Show(dlg) != 1) return;
+
+      GroupIt(group, dlg.Chosen.Cast<ListItem<object>>().ToList());
+    }
+
+    public void GroupIt(CollectionViewGroup group, List<ListItem<object>> items, int index = 0) {
+      if (index >= items.Count) return;
+
+      var item = items[index];
+      var groups = GroupByItem(group, item).ToArray();
+
+      if (groups.Length == 1 && string.IsNullOrEmpty(groups[0].Title))
+        return;
+
+      group.WrappedItems.Clear();
+
+      foreach (var g in groups) {
+        group.WrappedItems.Add(g);
+        GroupIt(g, items, index + 1);
+      }
+    }
+  }
+
+  public class CollectionViewRow {
+    public CollectionViewGroup Group { get; }
+    public ObservableCollection<object> Items { get; } = new();
+    public bool IsExpanded { get; set; } = true;
+
+    public CollectionViewRow(CollectionViewGroup group) {
+      Group = group;
+    }
+  }
+
+  public class CollectionViewGroup : ObservableObject {
+    private bool _isExpanded;
+
+    public CollectionView View { get; set; }
+    public CollectionViewGroup Parent { get; }
+    public ObservableCollection<object> Source { get; } = new();
+    public ObservableCollection<object> WrappedItems { get; } = new();
+    public double Width { get; set; }
+    public string Icon { get; }
+    public string Title { get; }
+
+    public bool IsExpanded {
+      get => _isExpanded;
+      set {
+        _isExpanded = value;
+        ExpandedChangedEvent(this, EventArgs.Empty);
+        OnPropertyChanged();
+      }
+    }
+
+    public RelayCommand<double> SizeChangedCommand { get; }
+    public event EventHandler ExpandedChangedEvent = delegate { };
+
+    public CollectionViewGroup(CollectionViewGroup parent, string icon, string title, IEnumerable<object> source) {
+      Parent = parent;
+      Icon = icon;
+      Title = title;
+
+      if (Parent != null)
+        View = Parent.View;
+
+      SizeChangedCommand = new(OnWidthChanged);
+      UpdateSource(source);
+    }
+
+    public void UpdateSource(IEnumerable<object> items) {
+      if (items == null) return;
+
+      Source.Clear();
+      foreach (var item in items)
+        Source.Add(item);
+    }
+
+    public void OnWidthChanged(double width) {
+      Width = width;
+      ReWrap();
+    }
+
+    public void ReWrap() {
+      if (WrappedItems.FirstOrDefault() is CollectionViewGroup || !(Width > 0)) return;
+
+      WrappedItems.Clear();
+
+      foreach (var item in Source)
+        AddItem(item);
+    }
+
+    private void AddItem(object item) {
+      CollectionViewRow row = null;
+
+      if (WrappedItems.Count > 0)
+        row = WrappedItems[^1] as CollectionViewRow;
+
+      if (row == null) {
+        row = new(this);
+        WrappedItems.Add(row);
+      }
+
+      var usedSpace = row.Items.Sum(x => View.GetItemWidth(x));
+      var itemWidth = View.GetItemWidth(item);
+
+      if (Width - usedSpace < itemWidth) {
+        row = new(this);
+        WrappedItems.Add(row);
+      }
+
+      row.Items.Add(item);
+    }
+  }
+}
---
+++ MH.Utils/Dialogs/GroupByDialog.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using MH.Utils.BaseClasses;
+using static MH.Utils.DragDropHelper;
+
+namespace MH.Utils.Dialogs {
+  public class GroupByDialog : Dialog {
+    public ObservableCollection<object> Available { get; } = new();
+    public ObservableCollection<object> Chosen { get; } = new();
+    public CanDropFunc CanDropFunc { get; }
+    public DoDropAction DoDropAction { get; }
+
+    public GroupByDialog(string title, string icon) : base(title, icon) {
+      Buttons = new DialogButton[] {
+        new("Ok", "IconCheckMark", YesOkCommand, true),
+        new("Cancel", "IconXCross", CloseCommand, false, true) };
+
+      CanDropFunc = CanDrop;
+      DoDropAction = DoDrop;
+    }
+
+    public void SetAvailable(IEnumerable<object> items) {
+      Available.Clear();
+      
+      foreach (var item in items)
+        Available.Add(item);
+    }
+
+    private DragDropEffects CanDrop(object target, object data, bool haveSameOrigin) {
+      if (!haveSameOrigin && !Chosen.Contains(data))
+        return DragDropEffects.Copy;
+      if (haveSameOrigin && data != target)
+        return DragDropEffects.Move;
+
+      return DragDropEffects.None;
+    }
+
+    private void DoDrop(object data, bool haveSameOrigin) {
+      if (!Chosen.Remove(data))
+        Chosen.Add(data);
+    }
+  }
+}
