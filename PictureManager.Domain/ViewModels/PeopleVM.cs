e7889e1821a0017e66fc46846f77a36a8d3ed11a
martin-holy <martin.holy@hotmail.com>
2023-06-04 18:06:05 +0200

    People view update

--- PictureManager.Domain/ViewModels/PeopleVM.cs
+++ PictureManager.Domain/ViewModels/PeopleVM.cs
@@ -1,9 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using MH.Utils;
+﻿using MH.Utils;
 using MH.Utils.BaseClasses;
 using PictureManager.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace PictureManager.Domain.ViewModels {
   public sealed class PeopleVM : CollectionView {
@@ -20,6 +20,9 @@ namespace PictureManager.Domain.ViewModels {
     public override int GetItemWidth(object item) =>
       (int)Core.Instance.SegmentsM.SegmentUiFullWidth;
 
+    public override void Select(IEnumerable<object> source, object item, bool isCtrlOn, bool isShiftOn) =>
+      _peopleM.Select(source.OfType<PersonM>().ToList(), (PersonM)item, isCtrlOn, isShiftOn);
+
     public override IEnumerable<object> GetGroupByItems(IEnumerable<object> source) =>
       new[] {
         new ListItem<object>(null, Res.IconPeopleMultiple, "Group"),
@@ -29,25 +32,35 @@ namespace PictureManager.Domain.ViewModels {
           .Cast<PersonM>()
           .Where(x => x.Keywords != null)
           .SelectMany(x => x.Keywords)
+          .SelectMany(x => x.GetThisAndParentRecursive())
           .Distinct()
           .OrderBy(x => x.FullName)
           .Select(x => new ListItem<object>(x, Res.IconTag, x.FullName)));
 
-    public override IOrderedEnumerable<CollectionViewGroup> GroupByItem(CollectionViewGroup group, ListItem<object> item) {
-      if (item.Name == "Group")
-        return GroupByGroup(group);
-      
-      if (item.Name == "Keywords")
-        return GroupByKeywords(group);
+    public override string ItemGroupBy(CollectionViewGroup group, object item) {
+      if (group.GroupBy.Name == "Group")
+        return ItemGroupByGroup(item as PersonM);
 
-      return GroupByKeyword(group, item.Content as KeywordM);
+      if (group.GroupBy.Name == "Keywords")
+        return ItemGroupByKeywords(item as PersonM);
+
+      return ItemGroupByKeyword(item as PersonM, group.GroupBy.Content as KeywordM);
     }
 
-    public override void Select(IEnumerable<object> source, object item, bool isCtrlOn, bool isShiftOn) =>
-      _peopleM.Select(source.OfType<PersonM>().ToList(), (PersonM)item, isCtrlOn, isShiftOn);
+    public override IOrderedEnumerable<CollectionViewGroup> SourceGroupBy(CollectionViewGroup group) {
+      if (group.GroupBy.Name == "Group")
+        return SourceGroupByGroup(group);
+
+      if (group.GroupBy.Name == "Keywords")
+        return SourceGroupByKeywords(group);
+
+      return SourceGroupByKeyword(group, group.GroupBy.Content as KeywordM);
+    }
 
     public void Reload() {
-      Root.UpdateSource(_peopleM.DataAdapter.All);
+      Root.UpdateSource(_peopleM.DataAdapter.All
+        .Where(x => x.Parent is not CategoryGroupM { IsHidden: true }));
+
       GroupIt(
         Root,
         new() {
@@ -57,43 +70,47 @@ namespace PictureManager.Domain.ViewModels {
       Root.IsExpanded = true;
     }
 
-    private static IOrderedEnumerable<CollectionViewGroup> GroupByKeyword(CollectionViewGroup group, KeywordM keyword) =>
+    private static string ItemGroupByGroup(PersonM item) =>
+      item.Parent == null
+        ? _unknown
+        : item.Parent is CategoryGroupM
+          ? item.Parent.Name
+          : string.Empty;
+
+    private static string ItemGroupByKeywords(PersonM item) =>
+      item.DisplayKeywords == null
+        ? string.Empty
+        : string.Join(", ", item.DisplayKeywords.Select(dk => dk.Name));
+
+    private static string ItemGroupByKeyword(PersonM item, KeywordM keyword) =>
+      item.Keywords?
+        .SelectMany(x => x.GetThisAndParentRecursive())
+        .Contains(keyword) == true
+          ? keyword.FullName
+          : string.Empty;
+
+    private static IOrderedEnumerable<CollectionViewGroup> SourceGroupByKeyword(CollectionViewGroup group, KeywordM keyword) =>
       group.Source
         .Cast<PersonM>()
         .OrderBy(x => x.Name)
-        .GroupBy(x => x.Keywords == null
-          ? string.Empty
-          : x.Keywords.Contains(keyword)
-            ? keyword.FullName
-            : string.Empty)
+        .GroupBy(x => ItemGroupByKeyword(x, keyword))
         .Select(x => new CollectionViewGroup(group, Res.IconTag, x.Key, x))
         .OrderBy(x => x.Title);
 
-    private static IOrderedEnumerable<CollectionViewGroup> GroupByKeywords(CollectionViewGroup group) =>
+    private static IOrderedEnumerable<CollectionViewGroup> SourceGroupByKeywords(CollectionViewGroup group) =>
       group.Source
         .Cast<PersonM>()
         .OrderBy(x => x.Name)
-        .GroupBy(x => x.DisplayKeywords == null
-          ? string.Empty
-          : string.Join(", ", x.DisplayKeywords.Select(dk => dk.Name)))
+        .GroupBy(ItemGroupByKeywords)
         .Select(x => new CollectionViewGroup(group, Res.IconTagLabel, x.Key, x))
         .OrderBy(x => x.Title);
 
-    private static IOrderedEnumerable<CollectionViewGroup> GroupByGroup(CollectionViewGroup group) =>
+    private static IOrderedEnumerable<CollectionViewGroup> SourceGroupByGroup(CollectionViewGroup group) =>
       group.Source
         .Cast<PersonM>()
         .OrderBy(x => x.Name)
-        .GroupBy(x => x.Parent)
-        .Where(x => x.Key is not CategoryGroupM { IsHidden: true })
-        .Select(x => new CollectionViewGroup(
-          group,
-          Res.IconPeopleMultiple,
-          x.Key == null
-            ? _unknown
-            : x.Key is CategoryGroupM
-              ? x.Key.Name
-              : string.Empty,
-          x))
+        .GroupBy(ItemGroupByGroup)
+        .Select(x => new CollectionViewGroup(group, Res.IconPeopleMultiple, x.Key, x))
         .OrderBy(x => x.Title, Comparer<string>.Create((a, b) => {
           if (_unknown.Equals(a)) return 1;
           if (_unknown.Equals(b)) return -1;
