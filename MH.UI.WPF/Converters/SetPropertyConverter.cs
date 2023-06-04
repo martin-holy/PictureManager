ea5dcbe265c5b990044bc9c055b56b4fb84c10bf
martin-holy <martin.holy@hotmail.com>
2023-06-04 18:03:19 +0200

    SetPropertyConverter

---
+++ MH.UI.WPF/Converters/SetPropertyConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace MH.UI.WPF.Converters {
+  public class SetPropertyConverter : MarkupExtension, IMultiValueConverter {
+    private static readonly SetPropertyConverter _converter = null;
+
+    public override object ProvideValue(IServiceProvider serviceProvider) =>
+      _converter ?? new SetPropertyConverter();
+
+    public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture) {
+      if (values.Length == 2 && parameter is string propName)
+        values[0]?.GetType().GetProperty(propName)?.SetValue(values[0], values[1]);
+
+      return Binding.DoNothing;
+    }
+
+    public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture) =>
+      throw new NotSupportedException();
+  }
+}
