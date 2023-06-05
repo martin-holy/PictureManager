e41e6b0253fc1cf5afbac37c143a14b4b5af965c
martin-holy <martin.holy@hotmail.com>
2023-06-05 17:39:56 +0200

    KeyDataTemplateSelector

---
+++ MH.UI.WPF/Utils/KeyDataTemplateSelector.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace MH.UI.WPF.Utils {
+  public class KeyDataTemplateSelector : DataTemplateSelector {
+    public object[] Keys { get; set; }
+
+    public override DataTemplate SelectTemplate(object item, DependencyObject container) {
+      if (item == null || container is not FrameworkElement containerElement)
+        return base.SelectTemplate(item, container);
+
+      var type = item.GetType();
+      var typeName = string.Join('.', type.Namespace, type.Name);
+
+      if (!Keys.Contains(typeName))
+        return base.SelectTemplate(item, container);
+
+      var template = containerElement.TryFindResource(typeName) as DataTemplate;
+
+      return template ?? base.SelectTemplate(item, container);
+    }
+  }
+}
