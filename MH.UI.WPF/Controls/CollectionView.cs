eddbafbc048334bfc1a912173a4a33f6f2ef27a5
martin-holy <martin.holy@hotmail.com>
2023-06-06 14:39:00 +0200

    CollectionView Scroll to top item after reload

--- MH.UI.WPF/Controls/CollectionView.cs
+++ MH.UI.WPF/Controls/CollectionView.cs
@@ -1,13 +1,17 @@
 ﻿using MH.UI.WPF.Utils;
 using MH.Utils;
 using MH.Utils.BaseClasses;
+using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
+using System.Windows.Media;
 using Keyboard = System.Windows.Input.Keyboard;
 
 namespace MH.UI.WPF.Controls {
   public class CollectionView : TreeView {
+    private double _verticalOffset;
+
     public ScrollViewer ScrollViewer { get; set; }
 
     public static readonly DependencyProperty ViewProperty = DependencyProperty.Register(
@@ -41,7 +45,28 @@ namespace MH.UI.WPF.Controls {
     public override void OnApplyTemplate() {
       base.OnApplyTemplate();
 
+      ScrollViewer = (ScrollViewer)Template.FindName("PART_ScrollViewer", this);
+
+      ScrollViewer.ScrollChanged += (_, _) => {
+        SetTopItem();
+      };
+
+      LayoutUpdated += (_, _) => {
+        if (_verticalOffset > 0) {
+          ScrollViewer.ScrollToHorizontalOffset(0);
+          ScrollViewer.ScrollToVerticalOffset(_verticalOffset);
+          _verticalOffset = 0;
+        }
+      };
+
       ItemsSource = new[] { View.ObjectRoot };
+
+      View.PropertyChanged += (_, e) => {
+        if (nameof(View.ScrollToItem).Equals(e.PropertyName) && View.ScrollToItem != null) {
+          ScrollTo(View.ScrollToItem);
+          View.ScrollToItem = null;
+        }
+      };
     }
 
     private void SelectItem(MouseButtonEventArgs e) {
@@ -63,6 +88,32 @@ namespace MH.UI.WPF.Controls {
       }
 
       View.Select(row, item, isCtrlOn, isShiftOn);
+    }
+
+    private void SetTopItem() {
+      VisualTreeHelper.HitTest(this, null, e => {
+        if (e.VisualHit is FrameworkElement elm && View.SetTopItem(elm.DataContext))
+          return HitTestResultBehavior.Stop;
+
+        return HitTestResultBehavior.Continue;
+      }, new PointHitTestParameters(new(10, 10)));
+    }
+
+    private void ScrollTo(List<object> items) {
+      var parent = this as ItemsControl;
+
+      foreach (var item in items) {
+        var index = parent.Items.IndexOf(item);
+        if (index < 0) break;
+        var panel = parent.GetChildOfType<VirtualizingStackPanel>();
+        if (panel == null) break;
+        panel.BringIndexIntoViewPublic(index);
+        if (parent.ItemContainerGenerator.ContainerFromIndex(index) is not TreeViewItem tvi) break;
+        parent = tvi;
+      }
+
+      _verticalOffset = ScrollViewer.VerticalOffset
+        + parent.TransformToVisual(ScrollViewer).Transform(new(0, 0)).Y;
     }
   }
 }
