3d4c57f83fbdcda29940300973a922e1051dc44a
martin-holy <martin.holy@hotmail.com>
2023-06-25 11:05:33 +0200

    ExtObservableCollection

---
+++ MH.Utils/Extensions/ExtObservableCollection.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+
+namespace MH.Utils.Extensions {
+  public class ExtObservableCollection<T> : ObservableCollection<T> {
+    public ExtObservableCollection() { }
+
+    public ExtObservableCollection(IEnumerable<T> items) : base(items) { }
+
+    public void Execute(Action<IList<T>> itemsAction) {
+      itemsAction(Items);
+      OnPropertyChanged(new("Count"));
+      OnPropertyChanged(new("Item[]"));
+      OnCollectionChanged(new(NotifyCollectionChangedAction.Reset));
+    }
+  }
+}
