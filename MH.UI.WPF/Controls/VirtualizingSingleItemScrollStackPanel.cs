c6a47b1b9366308f1900c7c366e1184d2a4ba95a
martin-holy <martin.holy@hotmail.com>
2023-07-28 12:02:21 +0200

    VirtualizingSingleItemScrollStackPanel

---
+++ MH.UI.WPF/Controls/VirtualizingSingleItemScrollStackPanel.cs
@@ -0,0 +1,8 @@
+﻿using System.Windows.Controls;
+
+namespace MH.UI.WPF.Controls {
+  public class VirtualizingSingleItemScrollStackPanel : VirtualizingStackPanel {
+    public override void MouseWheelDown() => LineDown();
+    public override void MouseWheelUp() => LineUp();
+  }
+}
