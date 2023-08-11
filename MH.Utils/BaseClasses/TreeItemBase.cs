3496f8c70811ca920db338617617c14560ef3870
martin-holy <martin.holy@hotmail.com>
2023-08-11 19:31:36 +0200

    TreeItemBase class

---
+++ MH.Utils/BaseClasses/TreeItemBase.cs
@@ -0,0 +1,30 @@
+﻿using MH.Utils.Extensions;
+using MH.Utils.Interfaces;
+
+namespace MH.Utils.BaseClasses {
+  public class TreeItemBase<TP, TI, TL> : ObservableObject, ITreeItemBase<TP, TI, TL>
+    where TP : ITreeItemBase<TP, TI, TL>
+    where TI : ITreeItemBase<TP, TI, TL> {
+
+    private TP _parent;
+    private bool _isExpanded;
+
+    public TP Parent { get => _parent; set { _parent = value; OnPropertyChanged(); } }
+    public ExtObservableCollection<TI> Items { get; set; } = new();
+    public ExtObservableCollection<TL> Leaves { get; set; } = new();
+    public bool IsExpanded {
+      get => _isExpanded;
+      set {
+        _isExpanded = value;
+        OnIsExpandedChanged(value);
+        OnPropertyChanged();
+      }
+    }
+
+    public TreeItemBase(TP parent) {
+      Parent = parent;
+    }
+
+    public virtual void OnIsExpandedChanged(bool value) { }
+  }
+}
---
+++ MH.Utils/Interfaces/ITreeItemBase.cs
@@ -0,0 +1,13 @@
+﻿using MH.Utils.Extensions;
+
+namespace MH.Utils.Interfaces {
+  public interface ITreeItemBase<TP, TI, TL>
+    where TP : ITreeItemBase<TP, TI, TL>
+    where TI : ITreeItemBase<TP, TI, TL> {
+
+    public TP Parent { get; set; }
+    public ExtObservableCollection<TI> Items { get; set; }
+    public ExtObservableCollection<TL> Leaves { get; set; }
+    public bool IsExpanded { get; set; }
+  }
+}
