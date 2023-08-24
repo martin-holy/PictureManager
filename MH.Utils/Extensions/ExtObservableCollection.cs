3f66b1eb1b8a6e655c5a0014e7cbb21ec5af8f9e
martin-holy <martin.holy@hotmail.com>
2023-08-24 11:37:25 +0200

    ExtObservableCollection update

--- MH.Utils/Extensions/ExtObservableCollection.cs
+++ MH.Utils/Extensions/ExtObservableCollection.cs
@@ -9,11 +9,37 @@ namespace MH.Utils.Extensions {
 
     public ExtObservableCollection(IEnumerable<T> items) : base(items) { }
 
-    public void Execute(Action<IList<T>> itemsAction, NotifyCollectionChangedAction changedAction = NotifyCollectionChangedAction.Reset) {
+    public void Execute(Action<IList<T>> itemsAction) {
       itemsAction(Items);
+      NotifyChange(NotifyCollectionChangedAction.Reset, null);
+    }
+
+    public void AddItems(IList<T> items, Action<T> itemAction) {
+      foreach (var item in items) {
+        itemAction?.Invoke(item);
+        Items.Add(item);
+      }
+
+      NotifyChange(NotifyCollectionChangedAction.Add, items);
+    }
+
+    public void RemoveItems(IList<T> items, Action<T> itemAction) {
+      foreach (var item in items) {
+        itemAction?.Invoke(item);
+        Items.Remove(item);
+      }
+
+      NotifyChange(NotifyCollectionChangedAction.Remove, items);
+    }
+
+    private void NotifyChange(NotifyCollectionChangedAction action, IList<T> items) {
       OnPropertyChanged(new("Count"));
       OnPropertyChanged(new("Item[]"));
-      OnCollectionChanged(new(NotifyCollectionChangedAction.Reset));
+
+      if (items == null)
+        OnCollectionChanged(new(NotifyCollectionChangedAction.Reset));
+      else
+        OnCollectionChanged(new(action, items));
     }
   }
 }
