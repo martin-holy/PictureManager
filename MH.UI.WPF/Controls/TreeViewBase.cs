5efcb936f2d8d21c889614ded705dd7246bead46
martin-holy <martin.holy@hotmail.com>
2023-08-29 12:31:23 +0200

    TreeView selection and templates refactoring

--- MH.UI.WPF/Controls/TreeViewBase.cs
+++ MH.UI.WPF/Controls/TreeViewBase.cs
@@ -1,13 +1,9 @@
 ﻿using MH.UI.Interfaces;
 using MH.UI.WPF.Utils;
-using MH.Utils.BaseClasses;
 using System;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Controls.Primitives;
-using System.Windows.Input;
 using System.Windows.Media;
-using Keyboard = System.Windows.Input.Keyboard;
 
 namespace MH.UI.WPF.Controls {
   public class TreeViewBase : TreeView {
@@ -20,23 +16,9 @@ namespace MH.UI.WPF.Controls {
     public static readonly DependencyProperty TreeViewProperty = DependencyProperty.Register(
       nameof(TreeView), typeof(ITreeView), typeof(TreeViewBase));
 
-    public static readonly DependencyProperty SelectActionProperty = DependencyProperty.Register(
-      nameof(SelectAction), typeof(Action<object, bool, bool>), typeof(TreeViewBase));
-
     public ITreeView TreeView {
       get => (ITreeView)GetValue(TreeViewProperty);
       set => SetValue(TreeViewProperty, value);
-    }
-
-    public Action<object, bool, bool> SelectAction {
-      get => (Action<object, bool, bool>)GetValue(SelectActionProperty);
-      set => SetValue(SelectActionProperty, value);
-    }
-
-    public RelayCommand<MouseButtonEventArgs> SelectItemCommand { get; }
-
-    public TreeViewBase() {
-      SelectItemCommand = new(SelectItem);
     }
 
     public override void OnApplyTemplate() {
@@ -62,6 +44,9 @@ namespace MH.UI.WPF.Controls {
             case nameof(TreeView.ScrollToTop): ScrollToTop(); break;
           }
         };
+
+        SelectedItemChanged += (_, e) =>
+          TreeView.OnTreeItemSelected(e.NewValue);
       }
 
       LayoutUpdated += (_, _) => {
@@ -85,17 +70,6 @@ namespace MH.UI.WPF.Controls {
         if (TreeView != null && e.PreviousSize is not { Width: 0, Height: 0 })
           TreeView.IsSizeChanging = true;
       };
-    }
-
-    private void SelectItem(MouseButtonEventArgs e) {
-      if (e.Source is ToggleButton
-          || (e.OriginalSource as FrameworkElement)
-              ?.FindTopTemplatedParent() is not TreeViewItem tvi) return;
-
-      var isCtrlOn = (Keyboard.Modifiers & ModifierKeys.Control) > 0;
-      var isShiftOn = (Keyboard.Modifiers & ModifierKeys.Shift) > 0;
-
-      SelectAction(tvi.DataContext, isCtrlOn, isShiftOn);
     }
 
     private void ScrollToItems() {
