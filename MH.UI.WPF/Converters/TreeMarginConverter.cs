67ccae6a48f4929cfaeefa880a5bd3450851ea47
martin-holy <martin.holy@hotmail.com>
2023-08-29 13:07:06 +0200

    TreeMarginConverter refactoring

--- MH.UI.WPF/Converters/TreeMarginConverter.cs
+++ MH.UI.WPF/Converters/TreeMarginConverter.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MH.Utils;
+using MH.Utils.Interfaces;
+using System;
 using System.Globalization;
 using System.Windows;
 using System.Windows.Data;
@@ -7,28 +9,18 @@ using System.Windows.Markup;
 namespace MH.UI.WPF.Converters {
   public class TreeMarginConverter : MarkupExtension, IValueConverter {
     private static readonly TreeMarginConverter _converter = null;
-    private const string _parentPropName = "Parent";
 
     public override object ProvideValue(IServiceProvider serviceProvider) =>
       _converter ?? new TreeMarginConverter();
 
     public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+      var level = value is ITreeItem ti ? ti.GetLevel() : 0;
       var length = int.TryParse(parameter as string, out var l) ? l : 0;
-      var level = 0;
-      var parent = GetParent(value);
-
-      while (parent != null) {
-        level++;
-        parent = GetParent(parent);
-      }
 
       return new Thickness(length * level, 0.0, 0.0, 0.0);
     }
 
     public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) =>
       throw new NotSupportedException();
-
-    private static object GetParent(object o) =>
-      o?.GetType().GetProperty(_parentPropName)?.GetValue(o, null);
   }
 }
