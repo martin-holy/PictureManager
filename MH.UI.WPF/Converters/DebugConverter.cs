65d4710d8df0195a251f9290a09e85889fbeb0d3
martin-holy <martin.holy@hotmail.com>
2023-09-01 12:30:27 +0200

    DebugConverter refactoring

--- MH.UI.WPF/Converters/DebugConverter.cs
+++ MH.UI.WPF/Converters/DebugConverter.cs
@@ -1,15 +1,20 @@
 ﻿using System;
 using System.Globalization;
 using System.Windows.Data;
+using System.Windows.Markup;
 
 namespace MH.UI.WPF.Converters {
-  public class DebugConverter : IValueConverter {
+  public class DebugConverter : MarkupExtension, IValueConverter {
+    private static readonly DebugConverter _converter = null;
+
+    public override object ProvideValue(IServiceProvider serviceProvider) =>
+      _converter ?? new DebugConverter();
+
     public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
       return value;
     }
 
-    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
-      return value;
-    }
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) =>
+      throw new NotSupportedException();
   }
 }
