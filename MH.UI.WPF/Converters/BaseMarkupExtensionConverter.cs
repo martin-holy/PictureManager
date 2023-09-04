10966ec943523c6386f15337e603992245b2bd66
martin-holy <martin.holy@hotmail.com>
2023-09-04 14:15:08 +0200

    BaseMarkupExtensionConverter

---
+++ MH.UI.WPF/Converters/BaseMarkupExtensionConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace MH.UI.WPF.Converters {
+  public class BaseMarkupExtensionConverter : MarkupExtension, IValueConverter {
+    private static readonly IValueConverter _converter = null;
+
+    public override object ProvideValue(IServiceProvider serviceProvider) =>
+      _converter ?? Activator.CreateInstance(GetType());
+
+    public virtual object Convert(object value, Type targetType, object parameter, CultureInfo culture) =>
+      Convert(value, parameter);
+
+    public virtual object Convert(object value, object parameter) =>
+      throw new NotImplementedException();
+
+    public virtual object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) =>
+      throw new NotSupportedException();
+  }
+}
