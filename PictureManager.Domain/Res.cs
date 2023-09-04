c6576117b63765186fb1e3f2a7342c35416c649b
martin-holy <martin.holy@hotmail.com>
2023-09-04 15:06:43 +0200

    TypeToIcon removed

--- PictureManager.Domain/Res.cs
+++ PictureManager.Domain/Res.cs
@@ -1,11 +1,5 @@
-﻿using MH.UI.Controls;
-using MH.Utils.BaseClasses;
-using MH.Utils.Interfaces;
-using PictureManager.Domain.CollectionViews;
-using PictureManager.Domain.DataViews;
+﻿using MH.Utils.Interfaces;
 using PictureManager.Domain.Models;
-using PictureManager.Domain.TreeCategories;
-using System;
 using System.Collections.Generic;
 
 namespace PictureManager.Domain {
@@ -52,30 +46,6 @@ namespace PictureManager.Domain {
     public const string IconCheckMark = "IconCheckMark";
     public const string IconXCross = "IconXCross";
 
-    public static readonly Dictionary<Type, Tuple<string, string>> TypeToIconAndIconColorDic = new() {
-      { typeof(CollectionViewPeople), new(IconPeopleMultiple, ColorBrushPeople) },
-      { typeof(CollectionViewSegments), new(IconSegment, ColorBrushWhite) },
-      { typeof(DriveM), new(IconDrive, ColorBrushDrive) },
-      { typeof(FavoriteFolderM), new(IconFolder, ColorBrushFolder) },
-      { typeof(FolderM), new(IconFolder, ColorBrushFolder) },
-      { typeof(FolderKeywordsM), new(IconFolderPuzzle, ColorBrushFolder) },
-      { typeof(GeoNameM), new(IconLocationCheckin, ColorBrushWhite) },
-      { typeof(GeoNamesM), new(IconLocationCheckin, ColorBrushWhite) },
-      { typeof(MediaItemsView), new(IconImageMultiple, ColorBrushWhite) },
-      { typeof(PeopleM), new(IconPeopleMultiple, ColorBrushPeople) },
-      { typeof(PeopleToolsTabM), new(IconPeopleMultiple, ColorBrushPeople) },
-      { typeof(PeopleView), new(IconPeopleMultiple, ColorBrushPeople) },
-      { typeof(PersonDetail), new(IconSegment, ColorBrushWhite) },
-      { typeof(PersonM), new(IconPeople, ColorBrushPeople) },
-      { typeof(SegmentsDrawerM), new(IconSegment, ColorBrushWhite) },
-      { typeof(SegmentsView), new(IconSegment, ColorBrushWhite) },
-      { typeof(KeywordM), new(IconTag, ColorBrushTag) },
-      { typeof(KeywordsM), new(IconTagLabel, ColorBrushTag) },
-      { typeof(VideoClipsTreeCategory), new(IconMovieClapper, ColorBrushWhite) },
-      { typeof(ViewerM), new(IconEye, ColorBrushWhite) },
-      { typeof(ViewersM), new(IconEye, ColorBrushWhite) }
-    };
-
     public static readonly Dictionary<string, string> IconToIconColorDic = new() {
       { _default, ColorBrushWhite },
       { IconFolder, ColorBrushFolder },
@@ -91,11 +61,6 @@ namespace PictureManager.Domain {
       { IconDriveError, ColorBrushDrive },
       { IconCd, ColorBrushDrive }
     };
-
-    public static string IconToIconColor(string icon) =>
-      IconToIconColorDic.TryGetValue(icon, out var color)
-        ? color
-        : IconToIconColorDic[_default];
 
     public static string CategoryToIconName(Category category) =>
       category switch {
@@ -113,37 +78,8 @@ namespace PictureManager.Domain {
     public static string TypeToGroupByDialogTemplateKey(object o) =>
       o switch {
         PersonM => "PM.Views.List.PersonV",
-        IIconText => "PM.Views.List.IconTextV",
+        IListItem => "PM.Views.List.ListItemV",
         _ => null
       };
-
-    public static string TypeToIcon(object o, object parameter = null) =>
-      TypeToIconAndIconColor(o, parameter).Item1;
-    
-
-    public static string TypeToIconColor(object o, object parameter = null) =>
-      TypeToIconAndIconColor(o, parameter).Item2;
-
-    public static Tuple<string, string> TypeToIconAndIconColor(object o, object parameter = null) {
-      if (TypeToIconAndIconColorDic.TryGetValue(o.GetType(), out var icon))
-        return icon;
-
-      // TODO other types
-      switch (o) {
-        case CategoryGroupM g:
-          var gIcon = CategoryToIconName(g.Category);
-          return new(gIcon, IconToIconColor(gIcon));
-        case CollectionViewGroup<MediaItemM> g:
-          return TypeToIconAndIconColor(g.GroupedBy?.Data ?? g.View);
-        case CollectionViewGroup<PersonM> g:
-          return TypeToIconAndIconColor(g.GroupedBy?.Data ?? g.View);
-        case CollectionViewGroup<SegmentM> g:
-          return TypeToIconAndIconColor(g.GroupedBy?.Data ?? g.View);
-        case IconText it:
-          return new(it.IconName, IconToIconColor(it.IconName));
-        default:
-          return new(IconBug, ColorBrushWhite);
-      }
-    }
   }
 }
