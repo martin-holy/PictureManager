671906243d884c2dfa252e7f34cfa72914a25aea
martin-holy <martin.holy@hotmail.com>
2023-09-07 16:45:51 +0200

    CachedResourceDictionary

---
+++ MH.UI.WPF/Utils/CachedResourceDictionary.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+
+namespace MH.UI.WPF.Utils {
+  public class CachedResourceDictionary : ResourceDictionary {
+    private static readonly Dictionary<Uri, WeakReference> _cache = new();
+    private Uri _source;
+
+    public new Uri Source {
+      get => _source;
+      set {
+        _source = value;
+
+        if (!_cache.TryGetValue(_source, out var weakReference))
+          AddToCache();
+        else if (weakReference is { IsAlive: true })
+          MergedDictionaries.Add((ResourceDictionary)weakReference.Target);
+        else
+          AddToCache();
+      }
+    }
+
+    private void AddToCache() {
+      base.Source = _source;
+
+      if (_cache.ContainsKey(_source))
+        _cache.Remove(_source);
+
+      _cache.Add(_source, new(this, false));
+    }
+  }
+}
