a7056bdee24e8ac603b394f4c09019c1970dd3dc
martin-holy <martin.holy@hotmail.com>
2023-09-07 13:47:36 +0200

    GenericTypeExtension

---
+++ MH.UI.WPF/MarkupExtensions/GenericTypeExtension.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows.Markup;
+
+namespace MH.UI.WPF.MarkupExtensions {
+  public class GenericTypeExtension : MarkupExtension {
+    public Type BaseType { get; set; }
+    public Type[] InnerTypes { get; set; }
+
+    public GenericTypeExtension() { }
+
+    public GenericTypeExtension(Type baseType, Type[] innerTypes) {
+      BaseType = baseType;
+      InnerTypes = innerTypes;
+    }
+
+    public override object ProvideValue(IServiceProvider serviceProvider) =>
+      BaseType.MakeGenericType(InnerTypes);
+  }
+}
