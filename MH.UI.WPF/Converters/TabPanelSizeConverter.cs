45d35144ed79c91289d0206d2f21df84129da959
martin-holy <martin.holy@hotmail.com>
2023-09-07 13:54:27 +0200

    TabControl

---
+++ MH.UI.WPF/Converters/TabPanelSizeConverter.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows.Controls;
+using System.Windows.Data;
+using TabControl = MH.UI.Controls.TabControl;
+
+namespace MH.UI.WPF.Converters {
+  public class TabPanelSizeConverter : BaseMarkupExtensionMultiConverter {
+    public override object Convert(object[] values, object parameter) {
+      if (values.Length == 4
+          && values[0] is TabControl control
+          && values[1] is Dock placement
+          && values[2] is double width
+          && values[3] is double height)
+        control.TabMaxSize = placement is Dock.Top or Dock.Bottom
+          ? width
+          : height;
+
+      return Binding.DoNothing;
+    }
+  }
+}
