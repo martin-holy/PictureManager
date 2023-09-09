35da7ed0f6b4e51810dcf2f8595e7fdb474de22e
martin-holy <martin.holy@hotmail.com>
2023-09-09 14:09:59 +0200

    SegmentThumbnailSourceConverter refactoring

--- PictureManagerWPF/Converters/SegmentThumbnailSourceConverter.cs
+++ PictureManagerWPF/Converters/SegmentThumbnailSourceConverter.cs
@@ -1,4 +1,5 @@
-﻿using MH.UI.WPF.Utils;
+﻿using MH.UI.WPF.Converters;
+using MH.UI.WPF.Utils;
 using MH.Utils;
 using PictureManager.Domain;
 using PictureManager.Domain.Models;
@@ -6,107 +7,102 @@ using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Data;
 using System.Windows.Media.Imaging;
 
-namespace PictureManager.Converters {
-  public class SegmentThumbnailSourceConverter : IMultiValueConverter {
-    public static SegmentM IgnoreImageCacheSegment { get; set; }
+namespace PictureManager.Converters; 
 
-    private static bool _isRunning;
-    private static readonly List<SegmentM> _segmentsQueue = new();
+public class SegmentThumbnailSourceConverter : BaseMarkupExtensionMultiConverter {
+  public static SegmentM IgnoreImageCacheSegment { get; set; }
 
-    public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture) {
-      try {
-        if (values?.Length != 2 || values[1] is not SegmentM segment) return null;
+  private static bool _isRunning;
+  private static readonly List<SegmentM> _segmentsQueue = new();
 
-        if (!File.Exists(segment.FilePathCache)) {
-          if (!_segmentsQueue.Contains(segment))
-            _segmentsQueue.Add(segment);
+  public override object Convert(object[] values, object parameter) {
+    try {
+      if (values is not [_, SegmentM segment]) return null;
 
-          if (!_isRunning)
-            StartQueue();
+      if (!File.Exists(segment.FilePathCache)) {
+        if (!_segmentsQueue.Contains(segment))
+          _segmentsQueue.Add(segment);
 
-          return null;
-        }
+        if (!_isRunning)
+          StartQueue();
 
-        var src = new BitmapImage();
-        src.BeginInit();
-        src.CacheOption = BitmapCacheOption.OnLoad;
-        src.UriSource = new(segment.FilePathCache);
-        src.Rotation = Utils.Imaging.MediaOrientation2Rotation((MediaOrientation)segment.MediaItem.Orientation);
-
-        if (segment.Equals(IgnoreImageCacheSegment)) {
-          IgnoreImageCacheSegment = null;
-          src.CreateOptions = BitmapCreateOptions.IgnoreImageCache;
-        }
-
-        src.EndInit();
-
-        return src;
-      }
-      catch (Exception ex) {
-        Debug.WriteLine(ex);
         return null;
       }
-    }
 
-    public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture) =>
-      throw new NotSupportedException();
+      var src = new BitmapImage();
+      src.BeginInit();
+      src.CacheOption = BitmapCacheOption.OnLoad;
+      src.UriSource = new(segment.FilePathCache);
+      src.Rotation = Utils.Imaging.MediaOrientation2Rotation((MediaOrientation)segment.MediaItem.Orientation);
 
-    private static async void StartQueue() {
-      if (_segmentsQueue.Count == 0) {
-        _isRunning = false;
-        return;
+      if (segment.Equals(IgnoreImageCacheSegment)) {
+        IgnoreImageCacheSegment = null;
+        src.CreateOptions = BitmapCreateOptions.IgnoreImageCache;
       }
 
-      _isRunning = true;
-      var segments = _segmentsQueue.ToArray();
+      src.EndInit();
 
-      await Task.WhenAll(
-        from partition in Partitioner.Create(segments).GetPartitions(Environment.ProcessorCount)
-        select Task.Run(() => {
-          using (partition)
-            while (partition.MoveNext()) {
-              var segment = partition.Current;
-              CreateThumbnail(segment);
-              segment?.OnPropertyChanged(nameof(segment.FilePathCache));
-            }
-          return Task.CompletedTask;
-        }));
+      return src;
+    }
+    catch (Exception ex) {
+      Debug.WriteLine(ex);
+      return null;
+    }
+  }
 
-      foreach (var segment in segments)
-        _segmentsQueue.Remove(segment);
-
+  private static async void StartQueue() {
+    if (_segmentsQueue.Count == 0) {
       _isRunning = false;
-      StartQueue();
+      return;
     }
 
-    public static void CreateThumbnail(SegmentM segment) {
-      var filePath = segment.MediaItem.MediaType == MediaType.Image
-        ? segment.MediaItem.FilePath
-        : segment.MediaItem.FilePathCache;
-      var rect = new Int32Rect(
-        (int)segment.X,
-        (int)segment.Y,
-        (int)segment.Size,
-        (int)segment.Size);
+    _isRunning = true;
+    var segments = _segmentsQueue.ToArray();
 
-      try {
-        Imaging.GetCroppedBitmapSource(filePath, rect, SegmentsM.SegmentSize)
-          ?.SaveAsJpg(80, segment.FilePathCache);
+    await Task.WhenAll(
+      from partition in Partitioner.Create(segments).GetPartitions(Environment.ProcessorCount)
+      select Task.Run(() => {
+        using (partition)
+          while (partition.MoveNext()) {
+            var segment = partition.Current;
+            CreateThumbnail(segment);
+            segment?.OnPropertyChanged(nameof(segment.FilePathCache));
+          }
+        return Task.CompletedTask;
+      }));
 
-        IgnoreImageCacheSegment = segment;
-        segment.OnPropertyChanged(nameof(segment.FilePathCache));
-      }
-      catch (Exception ex) {
-        Log.Error(ex, filePath);
-      }
+    foreach (var segment in segments)
+      _segmentsQueue.Remove(segment);
+
+    _isRunning = false;
+    StartQueue();
+  }
+
+  public static void CreateThumbnail(SegmentM segment) {
+    var filePath = segment.MediaItem.MediaType == MediaType.Image
+      ? segment.MediaItem.FilePath
+      : segment.MediaItem.FilePathCache;
+    var rect = new Int32Rect(
+      (int)segment.X,
+      (int)segment.Y,
+      (int)segment.Size,
+      (int)segment.Size);
+
+    try {
+      Imaging.GetCroppedBitmapSource(filePath, rect, SegmentsM.SegmentSize)
+        ?.SaveAsJpg(80, segment.FilePathCache);
+
+      IgnoreImageCacheSegment = segment;
+      segment.OnPropertyChanged(nameof(segment.FilePathCache));
+    }
+    catch (Exception ex) {
+      Log.Error(ex, filePath);
     }
   }
 }
