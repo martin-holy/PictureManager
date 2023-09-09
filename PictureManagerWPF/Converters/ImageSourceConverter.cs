400ddef1858120ccbd611c0196f936f5e8554cdc
martin-holy <martin.holy@hotmail.com>
2023-09-09 13:42:13 +0200

    ImageSourceConverter refactoring

--- PictureManagerWPF/Converters/ImageSourceConverter.cs
+++ PictureManagerWPF/Converters/ImageSourceConverter.cs
@@ -1,35 +1,33 @@
-﻿using System;
-using System.Globalization;
+﻿using MH.UI.WPF.Converters;
+using MH.Utils;
+using System;
 using System.IO;
 using System.Net.Cache;
-using System.Windows.Data;
 using System.Windows.Media.Imaging;
 
-namespace PictureManager.Converters {
-  public class ImageSourceConverter : IValueConverter {
-    public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
-      try {
-        if (value is not string filePath || !File.Exists(filePath)) return null;
+namespace PictureManager.Converters;
 
-        var src = new BitmapImage();
-        src.BeginInit();
+public class ImageSourceConverter : BaseMarkupExtensionConverter {
+  public override object Convert(object value, object parameter) {
+    try {
+      if (value is not string filePath || !File.Exists(filePath)) return null;
 
-        if ("IgnoreImageCache".Equals(parameter as string, StringComparison.Ordinal)) {
-          src.UriCachePolicy = new(RequestCacheLevel.BypassCache);
-          src.CreateOptions = BitmapCreateOptions.IgnoreImageCache;
-        }
+      var src = new BitmapImage();
+      src.BeginInit();
 
-        src.CacheOption = BitmapCacheOption.OnLoad;
-        src.UriSource = new(filePath);
-        src.EndInit();
-        return src;
+      if ("IgnoreImageCache".Equals(parameter as string, StringComparison.Ordinal)) {
+        src.UriCachePolicy = new(RequestCacheLevel.BypassCache);
+        src.CreateOptions = BitmapCreateOptions.IgnoreImageCache;
       }
-      catch (Exception ex) {
-        Console.WriteLine(ex);
-        return null;
-      }
+
+      src.CacheOption = BitmapCacheOption.OnLoad;
+      src.UriSource = new(filePath);
+      src.EndInit();
+      return src;
     }
-
-    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => value;
+    catch (Exception ex) {
+      Log.Error(ex);
+      return null;
+    }
   }
 }
