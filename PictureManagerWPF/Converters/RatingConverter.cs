3a4344081749d891e6253852d0b7171029048993
martin-holy <martin.holy@hotmail.com>
2023-09-09 13:31:22 +0200

    Rating view and converter refactoring

--- PictureManagerWPF/Converters/RatingConverter.cs
+++ PictureManagerWPF/Converters/RatingConverter.cs
@@ -1,26 +1,17 @@
-﻿using System;
-using System.Globalization;
+﻿using MH.UI.WPF.Converters;
 using System.Windows.Data;
-using System.Windows.Markup;
 using System.Windows.Media;
 
-namespace PictureManager.Converters {
-  public class RatingConverter : MarkupExtension, IValueConverter {
-    private static RatingConverter _converter = null;
+namespace PictureManager.Converters; 
 
-    public override object ProvideValue(IServiceProvider serviceProvider) =>
-      _converter ?? new RatingConverter();
+public class RatingConverter : BaseMarkupExtensionConverter {
+  private static readonly SolidColorBrush _white = new(Color.FromRgb(255, 255, 255));
+  private static readonly SolidColorBrush _gray = new(Color.FromRgb(104, 104, 104));
 
-    public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
-      if (value is not int v || !int.TryParse(parameter as string, out int p))
-        return Binding.DoNothing;
-
-      return p < v
-        ? new SolidColorBrush(Color.FromRgb(255, 255, 255))
-        : new SolidColorBrush(Color.FromRgb(104, 104, 104));
-    }
-
-    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) =>
-      throw new NotSupportedException();
-  }
+  public override object Convert(object value, object parameter) =>
+    value is not int v || !int.TryParse(parameter as string, out int p)
+      ? Binding.DoNothing
+      : p < v
+        ? _white
+        : _gray;
 }
