a08f62d46ba982c354571238b61cd66c80342a95
martin-holy <martin.holy@hotmail.com>
2023-09-14 14:47:40 +0200

    hide mouse timer refactoring

--- PictureManagerWPF/Views/MainWindowV.xaml.cs
+++ PictureManagerWPF/Views/MainWindowV.xaml.cs
@@ -1,45 +1,10 @@
-﻿using System;
-using System.Windows.Input;
-using System.Windows.Threading;
+﻿using PictureManager.ViewModels;
 
-namespace PictureManager.Views {
-  public partial class MainWindowV {
-    private bool _isMouseHidden;
-    private DispatcherTimer _mouseHideTimer;
-    private TimeSpan _mouseHideTimerInterval = TimeSpan.FromMilliseconds(3000);
-    private DateTime _mouseLastMove;
+namespace PictureManager.Views;
 
-    public MainWindowV() {
-      InitializeComponent();
-      SetUpMouseHideTimer();
-    }
-
-    private void SetUpMouseHideTimer() {
-      _mouseHideTimer = new() { Interval = _mouseHideTimerInterval };
-      _mouseHideTimer.Tick += delegate {
-        if (_isMouseHidden || !IsActive || DateTime.Now - _mouseLastMove < _mouseHideTimerInterval)
-          return;
-        
-        _isMouseHidden = true;
-        Mouse.OverrideCursor = Cursors.None;
-      };
-
-      MouseMove += delegate {
-        _mouseLastMove = DateTime.Now;
-        if (_isMouseHidden) {
-          _isMouseHidden = false;
-          Mouse.OverrideCursor = null;
-        }
-      };
-
-      App.Core.MediaViewerM.PropertyChanged += (_, e) => {
-        if (nameof(App.Core.MediaViewerM.IsVisible).Equals(e.PropertyName)) {
-          if (App.Core.MediaViewerM.IsVisible)
-            _mouseHideTimer.Start();
-          else
-            _mouseHideTimer.Stop();
-        }
-      };
-    }
+public partial class MainWindowV {
+  public MainWindowV() {
+    InitializeComponent();
+    HideMouseTimer.Init(this, App.Core.MediaViewerM.PresentationPanel);
   }
 }
