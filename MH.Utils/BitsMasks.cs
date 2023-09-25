2efb7ab6593cc156eda23879fddddc7c347a0108
martin-holy <martin.holy@hotmail.com>
2023-09-25 11:28:27 +0200

    BitMasks readonly

--- MH.Utils/BitsMasks.cs
+++ MH.Utils/BitsMasks.cs
@@ -1,9 +1,9 @@
-﻿namespace MH.Utils {
-  public static class BitsMasks {
-    public static int IsExpanded = 1;
-    public static int IsSelected = 2;
-    public static int IsHidden = 4;
-    public static int IsIconHidden = 8;
-    public static int IsNameHidden = 16;
-  }
+﻿namespace MH.Utils; 
+
+public static class BitsMasks {
+  public static readonly int IsExpanded = 1;
+  public static readonly int IsSelected = 2;
+  public static readonly int IsHidden = 4;
+  public static readonly int IsIconHidden = 8;
+  public static readonly int IsNameHidden = 16;
 }
