40bd406678a7fc61b885d83082a6733355489b6d
martin-holy <martin.holy@hotmail.com>
2023-09-25 11:22:39 +0200

    typo in class name

--- MH.Utils/EventsArgs/TreeItemDroppedEventArgs.cs
+++ MH.Utils/EventsArgs/TreeItemDroppedEventArgs.cs
@@ -2,13 +2,13 @@
 using System;
 
 namespace MH.Utils.EventsArgs {
-  public class TreeItemDoppedEventArgs : EventArgs {
+  public class TreeItemDroppedEventArgs : EventArgs {
     public object Data { get; }
     public ITreeItem Dest { get; }
     public bool AboveDest { get; }
     public bool Copy { get; }
 
-    public TreeItemDoppedEventArgs(object data, ITreeItem dest, bool aboveDest, bool copy) {
+    public TreeItemDroppedEventArgs(object data, ITreeItem dest, bool aboveDest, bool copy) {
       Data = data;
       Dest = dest;
       AboveDest = aboveDest;
