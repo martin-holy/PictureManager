3c5107c7f40046b60d19ff8fe4e3306b27eb771b
martin-holy <martin.holy@hotmail.com>
2023-10-05 14:54:52 +0200

    DatabaseMigration formating

--- PictureManager.Domain/Database/DatabaseMigration.cs
+++ PictureManager.Domain/Database/DatabaseMigration.cs
@@ -1,129 +1,126 @@
-﻿/* migration from version 0 to 1
-FavoriteFolders
-  old => ID|Folder
-  new => ID|Folder|Title
-Keywords
-  old => ID|Name|Parent|Index
-  new => ID|Name|Parent
-People
-  old => ID|Name
-  new => ID|Name|Segments|Keywords
-Viewers
-  old => ID|Name|IncludedFolders|ExcludedFolders|IsDefault
-  new => ID|Name|IncludedFolders|ExcludedFolders|ExcludedCategoryGroups|ExcludedKeywords|IsDefault
-*/
-
-/* migration from version 1 to 2
- FavoriteFolders, Folders, MediaItems, Segments, VideoClips and VideoClipsGroups
- are stored in separate files for each drive
- */
-
-/* migration from version 2 to 3
-Segments
-  old => ID|MediaItemId|PersonId|SegmentBox(centerX,centerY,radius)|Keywords
-  new => ID|MediaItemId|PersonId|SegmentBox(left,top,size)|Keywords
- */
-
-/* migration from version 3 to 4
-Folders
-  old => ID|Name|Parent|IsFolderKeyword
-  new => ID|Name|Parent
-FolderKeywords
-  new => ID
- */
-
-using MH.Utils;
+﻿using MH.Utils;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
 
-namespace PictureManager.Domain.Database {
-  public static class DatabaseMigration {
-    public static void Resolver(int oldVersion, int newVersion) {
-      // 0 => 1
-      if (oldVersion < 1) {
-        SimpleDB.MigrateFile(
-          Core.Db.FavoriteFolders.TableFilePath,
-          record => $"{record}|Favorite folder name");
+namespace PictureManager.Domain.Database; 
 
-         SimpleDB.MigrateFile(
-           Core.Db.Keywords.TableFilePath,
-          record => record[..record.LastIndexOf('|')]);
+public static class DatabaseMigration {
+  public static void Resolver(int oldVersion, int newVersion) {
+    if (oldVersion < 1) From0To1();
+    if (oldVersion < 2) From1To2();
+    if (oldVersion < 3) From2To3();
+    if (oldVersion < 4) From3To4();
+  }
 
-        SimpleDB.MigrateFile(
-          Core.Db.People.TableFilePath,
-          record => $"{record}||");
+  /// <summary>
+  /// FavoriteFolders
+  ///   old => ID|Folder
+  ///   new => ID|Folder|Title
+  /// Keywords
+  ///   old => ID|Name|Parent|Index
+  ///   new => ID|Name|Parent
+  /// People
+  ///   old => ID|Name
+  ///   new => ID|Name|Segments|Keywords
+  /// Viewers
+  ///   old => ID|Name|IncludedFolders|ExcludedFolders|IsDefault
+  ///   new => ID|Name|IncludedFolders|ExcludedFolders|ExcludedCategoryGroups|ExcludedKeywords|IsDefault
+  /// </summary>
+  private static void From0To1() {
+    SimpleDB.MigrateFile(
+      Core.Db.FavoriteFolders.TableFilePath,
+      record => $"{record}|Favorite folder name");
 
-        SimpleDB.MigrateFile(
-          Core.Db.Viewers.TableFilePath,
-          record => {
-            var lio = record.LastIndexOf('|');
-            return $"{record[..lio]}||{record[lio..]}";
-          });
-      }
+    SimpleDB.MigrateFile(
+      Core.Db.Keywords.TableFilePath,
+      record => record[..record.LastIndexOf('|')]);
 
-      // 1 => 2
-      if (oldVersion < 2) {
-        Core.Db.FavoriteFolders.IsModified = true;
-        Core.Db.Folders.IsModified = true;
-        Core.Db.MediaItems.IsModified = true;
-        Core.Db.Segments.IsModified = true;
-        Core.Db.VideoClips.IsModified = true;
-        Core.Db.VideoClipsGroups.IsModified = true;
-      }
+    SimpleDB.MigrateFile(
+      Core.Db.People.TableFilePath,
+      record => $"{record}||");
 
-      // 2 => 3
-      if (oldVersion < 3) {
-        var files = Directory.GetFiles("db")
-          .Where(x => x.StartsWith("db" + Path.DirectorySeparatorChar + "Segments."));
+    SimpleDB.MigrateFile(
+      Core.Db.Viewers.TableFilePath,
+      record => {
+        var lio = record.LastIndexOf('|');
+        return $"{record[..lio]}||{record[lio..]}";
+      });
+  }
 
-        foreach (var file in files) {
-          SimpleDB.MigrateFile(
-            file,
-            record => {
-              var values = record.Split('|');
-              var oldRect = values[3].Split(',');
-              var x = int.Parse(oldRect[0]);
-              var y = int.Parse(oldRect[1]);
-              var r = int.Parse(oldRect[2]);
-              var newRect = string.Join(',', x - r, y - r, r * 2);
+  /// <summary>
+  /// FavoriteFolders, Folders, MediaItems, Segments, VideoClips and VideoClipsGroups
+  /// are stored in separate files for each drive
+  /// </summary>
+  private static void From1To2() {
+    Core.Db.FavoriteFolders.IsModified = true;
+    Core.Db.Folders.IsModified = true;
+    Core.Db.MediaItems.IsModified = true;
+    Core.Db.Segments.IsModified = true;
+    Core.Db.VideoClips.IsModified = true;
+    Core.Db.VideoClipsGroups.IsModified = true;
+  }
 
-              return string.Join('|', values[0], values[1], values[2], newRect, values[4]);
-            });
-        }
-      }
+  /// <summary>
+  /// Segments
+  ///   old => ID|MediaItemId|PersonId|SegmentBox(centerX,centerY,radius)|Keywords
+  ///   new => ID|MediaItemId|PersonId|SegmentBox(left,top,size)|Keywords
+  /// </summary>
+  private static void From2To3() {
+    var files = Directory.GetFiles("db")
+      .Where(x => x.StartsWith("db" + Path.DirectorySeparatorChar + "Segments."));
 
-      // 3 => 4
-      if (oldVersion < 4) {
-        var fks = new List<string>();
-        var files = Directory.GetFiles("db")
-          .Where(x => x.StartsWith("db" + Path.DirectorySeparatorChar + "Folders."));
+    foreach (var file in files) {
+      SimpleDB.MigrateFile(
+        file,
+        record => {
+          var values = record.Split('|');
+          var oldRect = values[3].Split(',');
+          var x = int.Parse(oldRect[0]);
+          var y = int.Parse(oldRect[1]);
+          var r = int.Parse(oldRect[2]);
+          var newRect = string.Join(',', x - r, y - r, r * 2);
 
-        foreach (var file in files) {
-          fks.Clear();
+          return string.Join('|', values[0], values[1], values[2], newRect, values[4]);
+        });
+    }
+  }
 
-          SimpleDB.MigrateFile(
-            file,
-            record => {
-              var values = record.Split('|');
+  /// <summary>
+  /// Folders
+  ///   old => ID|Name|Parent|IsFolderKeyword
+  ///   new => ID|Name|Parent
+  /// FolderKeywords
+  ///   new => ID
+  /// </summary>
+  private static void From3To4() {
+    var fks = new List<string>();
+    var files = Directory.GetFiles("db")
+      .Where(x => x.StartsWith("db" + Path.DirectorySeparatorChar + "Folders."));
 
-              if (values[3] == "1")
-                fks.Add(values[0]);
+    foreach (var file in files) {
+      fks.Clear();
 
-              return string.Join('|', values[0], values[1], values[2]);
-            });
+      SimpleDB.MigrateFile(
+        file,
+        record => {
+          var values = record.Split('|');
 
-          if (fks.Count == 0) continue;
+          if (values[3] == "1")
+            fks.Add(values[0]);
 
-          using var sw = new StreamWriter(file.Replace("Folders", "FolderKeywords"), false, Encoding.UTF8, 65536);
+          return string.Join('|', values[0], values[1], values[2]);
+        });
+
+      if (fks.Count == 0) continue;
+
+      using var sw = new StreamWriter(file.Replace("Folders", "FolderKeywords"), false, Encoding.UTF8, 65536);
           
-          foreach (var fk in fks)
-            sw.WriteLine(fk);
+      foreach (var fk in fks)
+        sw.WriteLine(fk);
 
-          sw.Close();
-        }
-      }
+      sw.Close();
     }
   }
 }
