4b9fa80ae5f052a8dcf6b055771237cff4513031
martin-holy <martin.holy@hotmail.com>
2023-10-08 15:01:29 +0200

    IEnumerable EmptyIfNull method

---
+++ MH.Utils/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace MH.Utils.Extensions; 
+
+public static class EnumerableExtensions {
+  public static IEnumerable<T> EmptyIfNull<T>(this IEnumerable<T> items) =>
+    items ?? Enumerable.Empty<T>();
+}
