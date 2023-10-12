f0cf46db3711c531f80c7455d3e92891400d32c4
martin-holy <martin.holy@hotmail.com>
2023-10-12 18:14:30 +0200

    MediaItemsFilter refactoring and GeoNames added

--- PictureManager.Domain/Models/MediaItemsFilterM.cs
+++ PictureManager.Domain/Models/MediaItemsFilterM.cs
@@ -1,151 +1,140 @@
 ﻿using MH.Utils.BaseClasses;
+using PictureManager.Domain.Extensions;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 
-namespace PictureManager.Domain.Models {
-  public sealed class MediaItemsFilterM : ObservableObject {
-    private bool _showImages = true;
-    private bool _showVideos = true;
+namespace PictureManager.Domain.Models;
 
-    public bool ShowImages { get => _showImages; set { _showImages = value; OnFilterChanged(); OnPropertyChanged(); } }
-    public bool ShowVideos { get => _showVideos; set { _showVideos = value; OnFilterChanged(); OnPropertyChanged(); } }
-    public ObservableCollection<object> FilterAnd { get; } = new();
-    public ObservableCollection<object> FilterOr { get; } = new();
-    public ObservableCollection<object> FilterNot { get; } = new();
-    public SelectionRange Height { get; } = new();
-    public SelectionRange Width { get; } = new();
-    public SelectionRange Size { get; } = new();
+public sealed class MediaItemsFilterM : ObservableObject {
+  private bool _showImages = true;
+  private bool _showVideos = true;
 
-    public event EventHandler FilterChangedEventHandler = delegate { };
+  public bool ShowImages { get => _showImages; set { _showImages = value; OnFilterChanged(); OnPropertyChanged(); } }
+  public bool ShowVideos { get => _showVideos; set { _showVideos = value; OnFilterChanged(); OnPropertyChanged(); } }
+  public ObservableCollection<object> FilterAnd { get; } = new();
+  public ObservableCollection<object> FilterOr { get; } = new();
+  public ObservableCollection<object> FilterNot { get; } = new();
+  public SelectionRange Height { get; } = new();
+  public SelectionRange Width { get; } = new();
+  public SelectionRange Size { get; } = new();
 
-    public RelayCommand<object> SetAndCommand { get; }
-    public RelayCommand<object> SetOrCommand { get; }
-    public RelayCommand<object> SetNotCommand { get; }
-    public RelayCommand<object> ClearCommand { get; }
-    public RelayCommand<object> SizeChangedCommand { get; }
+  public event EventHandler FilterChangedEventHandler = delegate { };
 
-    public MediaItemsFilterM() {
-      SetAndCommand = new(item => Set(item, DisplayFilter.And));
-      SetOrCommand = new(item => Set(item, DisplayFilter.Or));
-      SetNotCommand = new(item => Set(item, DisplayFilter.Not));
-      ClearCommand = new(Clear);
-      SizeChangedCommand = new(OnFilterChanged);
+  public RelayCommand<object> SetAndCommand { get; }
+  public RelayCommand<object> SetOrCommand { get; }
+  public RelayCommand<object> SetNotCommand { get; }
+  public RelayCommand<object> ClearCommand { get; }
+  public RelayCommand<object> SizeChangedCommand { get; }
+
+  public MediaItemsFilterM() {
+    SetAndCommand = new(item => Set(item, DisplayFilter.And));
+    SetOrCommand = new(item => Set(item, DisplayFilter.Or));
+    SetNotCommand = new(item => Set(item, DisplayFilter.Not));
+    ClearCommand = new(Clear);
+    SizeChangedCommand = new(OnFilterChanged);
+  }
+
+  private void OnFilterChanged() {
+    FilterChangedEventHandler(this, EventArgs.Empty);
+  }
+
+  private void Clear() {
+    FilterAnd.Clear();
+    FilterOr.Clear();
+    FilterNot.Clear();
+
+    Size.CoerceValues(true);
+    Height.CoerceValues(true);
+    Width.CoerceValues(true);
+
+    OnFilterChanged();
+  }
+
+  public void Set(object item, DisplayFilter displayFilter) {
+    if (FilterAnd.Contains(item) || FilterOr.Contains(item) || FilterNot.Contains(item))
+      displayFilter = DisplayFilter.None;
+
+    switch (displayFilter) {
+      case DisplayFilter.None:
+        FilterAnd.Remove(item);
+        FilterOr.Remove(item);
+        FilterNot.Remove(item);
+        break;
+      case DisplayFilter.And:
+        FilterAnd.Add(item);
+        break;
+      case DisplayFilter.Or:
+        FilterOr.Add(item);
+        break;
+      case DisplayFilter.Not:
+        FilterNot.Add(item);
+        break;
+      default:
+        throw new ArgumentOutOfRangeException(nameof(displayFilter), displayFilter, null);
     }
 
-    private void OnFilterChanged() {
-      FilterChangedEventHandler(this, EventArgs.Empty);
-    }
+    OnFilterChanged();
+  }
 
-    private void Clear() {
-      FilterAnd.Clear();
-      FilterOr.Clear();
-      FilterNot.Clear();
+  public bool Filter(MediaItemM mi) {
+    // Media Type
+    if (!ShowImages && mi.MediaType == MediaType.Image) return false;
+    if (!ShowVideos && mi.MediaType == MediaType.Video) return false;
 
-      Size.CoerceValues(true);
-      Height.CoerceValues(true);
-      Width.CoerceValues(true);
+    // GeoNames
+    if (!Filter(mi.GetGeoNames().ToArray())) return false;
 
-      OnFilterChanged();
-    }
+    //Ratings
+    var chosenRatings = FilterOr.OfType<RatingM>().Select(x => x.Value).ToArray();
+    if (chosenRatings.Any() && !chosenRatings.Contains(mi.Rating)) return false;
 
-    public void Set(object item, DisplayFilter displayFilter) {
-      if (FilterAnd.Contains(item) || FilterOr.Contains(item) || FilterNot.Contains(item))
-        displayFilter = DisplayFilter.None;
-
-      switch (displayFilter) {
-        case DisplayFilter.None:
-          FilterAnd.Remove(item);
-          FilterOr.Remove(item);
-          FilterNot.Remove(item);
-          break;
-        case DisplayFilter.And:
-          FilterAnd.Add(item);
-          break;
-        case DisplayFilter.Or:
-          FilterOr.Add(item);
-          break;
-        case DisplayFilter.Not:
-          FilterNot.Add(item);
-          break;
-        default:
-          throw new ArgumentOutOfRangeException(nameof(displayFilter), displayFilter, null);
-      }
-
-      OnFilterChanged();
-    }
-
-    public bool Filter(MediaItemM mi) {
-      // Media Type
-      if (!ShowImages && mi.MediaType == MediaType.Image) return false;
-      if (!ShowVideos && mi.MediaType == MediaType.Video) return false;
-
-      // TODO GeoNames
-
-      //Ratings
-      var chosenRatings = FilterOr.OfType<RatingM>().Select(x => x.Value);
-      if (chosenRatings.Any() && !chosenRatings.Contains(mi.Rating)) return false;
-
-      // MediaItemSizes
-      if ((!Width.MaxSelection() && !Width.Fits(mi.Width))
+    // MediaItemSizes
+    if ((!Width.MaxSelection() && !Width.Fits(mi.Width))
         || (!Height.MaxSelection() && !Height.Fits(mi.Height))
         || (!Size.MaxSelection() && !Size.Fits((mi.Width * mi.Height) / 1000000.0)))
-        return false;
+      return false;
 
-      // People
-      var miPeople = new List<PersonM>();
-      if (mi.People != null)
-        miPeople.AddRange(mi.People);
-      if (mi.Segments != null)
-        miPeople.AddRange(mi.Segments.Where(x => x.Person != null).Select(x => x.Person));
+    // People
+    var miPeople = mi.GetPeople().ToArray();
+    if (!Filter(miPeople)) return false;
 
-      var notPeople = FilterNot.OfType<PersonM>();
-      if (notPeople.Any() && miPeople.Any() && notPeople.Any(fp => miPeople.Any(p => p == fp))) return false;
-      var andPeople = FilterAnd.OfType<PersonM>();
-      if (andPeople.Any() && (!miPeople.Any() || !andPeople.All(fp => miPeople.Any(p => p == fp)))) return false;
-      var orPeople = FilterOr.OfType<PersonM>();
-      if (orPeople.Any() && (!miPeople.Any() || !orPeople.Any(fp => miPeople.Any(p => p == fp)))) return false;
+    // Keywords
+    return Filter(mi.GetKeywords().Concat(miPeople.GetKeywords()).Distinct().ToArray());
+  }
 
-      // Keywords
-      var miKeywords = new List<KeywordM>();
-      miKeywords.AddRange(miPeople.Where(x => x.Keywords != null).SelectMany(x => x.Keywords));
-      if (mi.Keywords != null)
-        miKeywords.AddRange(mi.Keywords);
-      if (mi.Segments != null)
-        miKeywords.AddRange(mi.Segments.Where(x => x.Keywords != null).SelectMany(x => x.Keywords));
+  private bool Filter<T>(T[] miI) where T : class {
+    var notI = FilterNot.OfType<T>().ToArray();
+    if (notI.Any() && miI.Any() && notI.Any(fx => miI.Any(x => ReferenceEquals(x, fx)))) return false;
+    var andI = FilterAnd.OfType<T>().ToArray();
+    if (andI.Any() && (!miI.Any() || !andI.All(fx => miI.Any(x => ReferenceEquals(x, fx))))) return false;
+    var orI = FilterOr.OfType<T>().ToArray();
+    if (orI.Any() && (!miI.Any() || !orI.Any(fx => miI.Any(x => ReferenceEquals(x, fx))))) return false;
 
-      var notKeywords = FilterNot.OfType<KeywordM>();
-      if (notKeywords.Any() && miKeywords.Any() && notKeywords.Any(fk => miKeywords.Any(mik => mik.FullName.StartsWith(fk.FullName)))) return false;
-      var andKeywords = FilterAnd.OfType<KeywordM>();
-      if (andKeywords.Any() && (!miKeywords.Any() || !andKeywords.All(fk => miKeywords.Any(mik => mik.FullName.StartsWith(fk.FullName))))) return false;
-      var orKeywords = FilterOr.OfType<KeywordM>();
-      if (orKeywords.Any() && (!miKeywords.Any() || !orKeywords.Any(fk => miKeywords.Any(mik => mik.FullName.StartsWith(fk.FullName))))) return false;
+    return true;
+  }
 
-      return true;
+  public void UpdateSizeRanges(IList<MediaItemM> limit, bool maxSelection) {
+    var zeroItems = !limit.Any();
+
+    if (zeroItems) {
+      Size.Zero();
+      Height.Zero();
+      Width.Zero();
     }
+    else {
+      Size.Min = Math.Round(limit.Min(x => x.Width * x.Height) / 1000000.0, 1);
+      Size.Max = Math.Round((limit.Max(x => x.Width * x.Height) + 100000) / 1000000.0, 1);
+      Size.CoerceValues(maxSelection);
 
-    public void UpdateSizeRanges(IEnumerable<MediaItemM> limit, bool maxSelection) {
-      var zeroItems = !limit.Any();
+      Height.Min = limit.Min(x => x.Height);
+      Height.Max = limit.Max(x => x.Height);
+      Height.CoerceValues(maxSelection);
 
-      if (zeroItems) {
-        Size.Zero();
-        Height.Zero();
-        Width.Zero();
-      }
-      else {
-        Size.Min = Math.Round(limit.Min(x => x.Width * x.Height) / 1000000.0, 1);
-        Size.Max = Math.Round((limit.Max(x => x.Width * x.Height) + 100000) / 1000000.0, 1);
-        Size.CoerceValues(maxSelection);
-
-        Height.Min = limit.Min(x => x.Height);
-        Height.Max = limit.Max(x => x.Height);
-        Height.CoerceValues(maxSelection);
-
-        Width.Min = limit.Min(x => x.Width);
-        Width.Max = limit.Max(x => x.Width);
-        Width.CoerceValues(maxSelection);
-      }
+      Width.Min = limit.Min(x => x.Width);
+      Width.Max = limit.Max(x => x.Width);
+      Width.CoerceValues(maxSelection);
     }
   }
 }
