72386bbe83671f9be6b840c2319ef9a7ce7bcfdd
martin-holy <martin.holy@hotmail.com>
2023-10-15 13:47:07 +0200

    CollectionView Item selection template

---
+++ MH.UI.WPF/Converters/CollectionViewItemSizeConverter.cs
@@ -0,0 +1,12 @@
+﻿using MH.UI.Interfaces;
+using MH.Utils.Interfaces;
+using System.Windows.Data;
+
+namespace MH.UI.WPF.Converters;
+
+public class CollectionViewItemSizeConverter : BaseMarkupExtensionMultiConverter {
+  public override object Convert(object[] values, object parameter) =>
+    values is [{ } item, ITreeItem { Parent: ICollectionViewGroup g }] && parameter is bool getWidth
+      ? (double)g.GetItemSize(item, getWidth)
+      : Binding.DoNothing;
+}
