890c9b84ae2216efa2f3943b22e50724042130e7
martin-holy <martin.holy@hotmail.com>
2023-10-21 11:16:02 +0200

    SelectionEventArgs class

---
+++ MH.Utils/BaseClasses/SelectionEventArgs.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace MH.Utils.BaseClasses; 
+
+public class SelectionEventArgs<T> {
+  public List<T> Items { get; }
+  public T Item { get; }
+  public bool IsCtrlOn { get; }
+  public bool IsShiftOn { get; }
+
+  public SelectionEventArgs(List<T> items, T item, bool isCtrlOn, bool isShiftOn) {
+    Items = items;
+    Item = item;
+    IsCtrlOn = isCtrlOn;
+    IsShiftOn = isShiftOn;
+  }
+}
