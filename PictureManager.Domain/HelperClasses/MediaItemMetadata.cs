35b4c4a8972c803da9b43ff9aad4ad86ecf81069
martin-holy <martin.holy@hotmail.com>
2023-11-05 13:24:48 +0100

    read/write segments to files with keywords

--- PictureManager.Domain/HelperClasses/MediaItemMetadata.cs
+++ PictureManager.Domain/HelperClasses/MediaItemMetadata.cs
@@ -1,7 +1,11 @@
-﻿using PictureManager.Domain.Models;
+﻿using MH.Utils.Extensions;
+using PictureManager.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 
-namespace PictureManager.Domain.HelperClasses; 
+namespace PictureManager.Domain.HelperClasses;
 
 public class MediaItemMetadata {
   public MediaItemM MediaItem { get; }
@@ -9,30 +13,90 @@ public class MediaItemMetadata {
   public string[] People { get; set; }
   public string[] Keywords { get; set; }
   public string GeoName { get; set; }
+  public List<Tuple<string, List<Tuple<string, string[]>>>> PeopleSegmentsKeywords { get; set; }
 
   public MediaItemMetadata(MediaItemM mediaItem) {
     MediaItem = mediaItem;
   }
 
   public void FindRefs() {
+    FindPeople();
+    FindKeywords();
+    FindGeoName();
+  }
+
+  public void FindPeople() {
+    var oldSegments = MediaItem.Segments?.ToList();
     MediaItem.People = null;
-    if (People != null) {
-      MediaItem.People = new(People.Length);
-      foreach (var person in People)
-        MediaItem.People.Add(Core.Db.People.GetPerson(person, true));
+    MediaItem.Segments = null;
+    if (PeopleSegmentsKeywords == null) {
+      Core.Db.Segments.ItemsDelete(oldSegments);
+      return;
     }
 
-    MediaItem.Keywords = null;
-    if (Keywords != null) {
-      MediaItem.Keywords = new();
-      foreach (var k in Keywords.OrderByDescending(x => x).Distinct()) {
-        var keyword = Core.KeywordsM.GetByFullPath(k.Replace('|', ' '));
-        if (keyword != null)
-          MediaItem.Keywords.Add(keyword);
+    MediaItem.People = new(PeopleSegmentsKeywords.Count);
+    foreach (var psk in PeopleSegmentsKeywords) {
+      var person = Core.Db.People.GetPerson(psk.Item1, true);
+
+      // segments
+      if (!psk.Item2.Any()) {
+        MediaItem.People.Add(person);
+        continue;
+      }
+
+      MediaItem.Segments ??= new();
+        
+      foreach (var c in psk.Item2) {
+        var d = c.Item1.Split(",").Select(x => double.TryParse(x, CultureInfo.InvariantCulture, out var v) ? v : 0).ToArray();
+        var x = (int)Math.Round(d[0] * MediaItem.Width);
+        var y = (int)Math.Round(d[1] * MediaItem.Height);
+        var s = (int)Math.Round(d[2] * MediaItem.Width);
+
+        var segment = RecycleSegment(x, y, s, MediaItem, person, ref oldSegments);
+        if (c.Item2 == null)
+          segment.Keywords = null;
+        else {
+          segment.Keywords = new();
+          foreach (var k in c.Item2) {
+            var keyword = Core.Db.Keywords.GetByFullPath(k);
+            if (keyword != null)
+              segment.Keywords.Add(keyword);
+          }
+        }
       }
     }
 
-    FindGeoName();
+    Core.Db.Segments.ItemsDelete(oldSegments?.Except(MediaItem.Segments.EmptyIfNull()).ToList());
+  }
+
+  private static SegmentM RecycleSegment(int x, int y, int s, MediaItemM mi, PersonM person, ref List<SegmentM> bin) {
+    SegmentM segment;
+    if (bin?.Any() == true) {
+      segment = bin[^1];
+      bin.RemoveAt(bin.Count - 1);
+      segment.X = x;
+      segment.Y = y;
+      segment.Size = s;
+      mi.Segments.Add(segment);
+      Core.Db.Segments.IsModified = true;
+    }
+    else
+      segment = Core.Db.Segments.ItemCreate(x, y, s, mi);
+
+    segment.Person = person;
+
+    return segment;
+  }
+
+  public void FindKeywords() {
+    MediaItem.Keywords = null;
+    if (Keywords == null) return;
+    MediaItem.Keywords = new();
+    foreach (var k in Keywords.OrderByDescending(x => x).Distinct()) {
+      var keyword = Core.Db.Keywords.GetByFullPath(k.Replace('|', ' '));
+      if (keyword != null)
+        MediaItem.Keywords.Add(keyword);
+    }
   }
 
   public void FindGeoName() {
