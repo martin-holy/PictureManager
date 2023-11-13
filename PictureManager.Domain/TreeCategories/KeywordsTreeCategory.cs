7707fc4aa4e59b2f7d6e1aff79652c580b857cc8
martin-holy <martin.holy@hotmail.com>
2023-11-13 10:28:11 +0100

    KeywordsTreeCategory CanDrop update

--- PictureManager.Domain/TreeCategories/KeywordsTreeCategory.cs
+++ PictureManager.Domain/TreeCategories/KeywordsTreeCategory.cs
@@ -1,10 +1,13 @@
 ﻿using MH.UI.BaseClasses;
 using MH.Utils.BaseClasses;
+using MH.Utils.Interfaces;
 using PictureManager.Domain.Database;
 using PictureManager.Domain.Dialogs;
 using PictureManager.Domain.Models;
+using System;
+using System.Linq;
 
-namespace PictureManager.Domain.TreeCategories; 
+namespace PictureManager.Domain.TreeCategories;
 
 public sealed class KeywordsTreeCategory : TreeCategory<KeywordM, CategoryGroupM> {
   public CategoryGroupM AutoAddedGroup { get; set; }
@@ -22,4 +25,10 @@ public sealed class KeywordsTreeCategory : TreeCategory<KeywordM, CategoryGroupM
 
   public override void OnItemSelected(object o) =>
     ToggleDialogM.ToggleKeyword(o as KeywordM);
+
+  public override bool CanDrop(object src, ITreeItem dest) =>
+    base.CanDrop(src, dest) && src is ITreeItem srcItem &&
+    (!(dest is ITreeGroup ? dest : dest.Parent).Items
+       .Any(x => x.Name.Equals(srcItem.Name, StringComparison.OrdinalIgnoreCase)) ||
+     (dest is not ITreeGroup && ReferenceEquals(srcItem.Parent, dest.Parent)));
 }
