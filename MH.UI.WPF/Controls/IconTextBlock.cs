1a453c48736786eb6d0108aea674eb1d35cfb387
martin-holy <martin.holy@hotmail.com>
2023-11-17 17:11:07 +0100

    IconTextBlock control

---
+++ MH.UI.WPF/Controls/IconTextBlock.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace MH.UI.WPF.Controls; 
+
+public class IconTextBlock : Control {
+  static IconTextBlock() {
+    DefaultStyleKeyProperty.OverrideMetadata(
+      typeof(IconTextBlock),
+      new FrameworkPropertyMetadata(typeof(IconTextBlock)));
+  }
+}
