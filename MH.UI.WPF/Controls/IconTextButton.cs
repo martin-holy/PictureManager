bd3e19c20cee45a0df5ada1d2fa4e30a71d460c6
martin-holy <martin.holy@hotmail.com>
2023-11-17 17:12:43 +0100

    IconTextButton and PopupSlider using IconTextBlock

--- MH.UI.WPF/Controls/IconTextButton.cs
+++ MH.UI.WPF/Controls/IconTextButton.cs
@@ -1,28 +1,12 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Media;
 
-namespace MH.UI.WPF.Controls {
-  public class IconTextButton : Button {
-    public static readonly DependencyProperty IconProperty =
-      DependencyProperty.Register(nameof(Icon), typeof(PathGeometry), typeof(IconTextButton));
-    public static readonly DependencyProperty TextProperty =
-      DependencyProperty.Register(nameof(Text), typeof(string), typeof(IconTextButton));
+namespace MH.UI.WPF.Controls; 
 
-    public PathGeometry Icon {
-      get => (PathGeometry)GetValue(IconProperty);
-      set => SetValue(IconProperty, value);
-    }
-
-    public string Text {
-      get => (string)GetValue(TextProperty);
-      set => SetValue(TextProperty, value);
-    }
-
-    static IconTextButton() {
-      DefaultStyleKeyProperty.OverrideMetadata(
-        typeof(IconTextButton),
-        new FrameworkPropertyMetadata(typeof(IconTextButton)));
-    }
+public class IconTextButton : Button {
+  static IconTextButton() {
+    DefaultStyleKeyProperty.OverrideMetadata(
+      typeof(IconTextButton),
+      new FrameworkPropertyMetadata(typeof(IconTextButton)));
   }
 }
