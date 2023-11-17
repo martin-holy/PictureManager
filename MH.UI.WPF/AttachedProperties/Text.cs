fda1ecf7b596d0748aff6388faa0a462a60dc170
martin-holy <martin.holy@hotmail.com>
2023-11-17 17:10:30 +0100

    Text.Text attached property

---
+++ MH.UI.WPF/AttachedProperties/Text.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace MH.UI.WPF.AttachedProperties; 
+
+public static class Text {
+  public static readonly DependencyProperty TextProperty = DependencyProperty.RegisterAttached(
+    "Text", typeof(string), typeof(Text));
+
+  public static string GetText(DependencyObject d) => (string)d.GetValue(TextProperty);
+  public static void SetText(DependencyObject d, string value) => d.SetValue(TextProperty, value);
+}
