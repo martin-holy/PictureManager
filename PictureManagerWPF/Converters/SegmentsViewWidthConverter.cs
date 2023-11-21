b4e928fbd932b9897873fcc10b678a3aa89ccd91
martin-holy <martin.holy@hotmail.com>
2023-11-21 17:34:24 +0100

    Calculated width for PeopleView in SegmentsView and for ToolsTabs

---
+++ PictureManagerWPF/Converters/SegmentsViewWidthConverter.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Data;
+using MH.UI.WPF.Converters;
+
+namespace PictureManager.Converters; 
+
+public class SegmentsViewWidthConverter : BaseMarkupExtensionConverter {
+  public override object Convert(object value, object parameter) =>
+    value is int size && int.TryParse(parameter as string, out var count) ?
+      (size + 1) * count + 15 + 2 + 1 //(size + 1) * count + ScrollBar + Margin + ToBeSure
+      : Binding.DoNothing;
+}
