cbf6837c5adf7c2dbc19c46ad38529636971397a
martin-holy <martin.holy@hotmail.com>
2023-11-24 17:14:46 +0100

    What is new information

---
+++ PictureManager.Domain/Models/WhatIsNewM.cs
@@ -0,0 +1,26 @@
+﻿using MH.Utils.BaseClasses;
+using System;
+using System.IO;
+using System.Text;
+
+namespace PictureManager.Domain.Models;
+
+public class WhatIsNewM {
+  private static WhatIsNewM _inst;
+
+  public static string Content { get; set; }
+
+  public static RelayCommand<object> OpenCommand { get; } = new(Open);
+
+  private static void Open() {
+    try {
+      _inst ??= new();
+      using var sr = new StreamReader("WhatIsNew.txt", Encoding.UTF8);
+      Content = sr.ReadToEnd();
+      Core.MainTabs.Activate(Res.IconInformation, "What's new", _inst);
+    }
+    catch (Exception) {
+      // ignored
+    }
+  }
+}
