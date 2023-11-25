5cdb4c1eef869d80bf5c38d08e238eb9f31f029e
martin-holy <martin.holy@hotmail.com>
2023-11-25 12:40:36 +0100

    [C] VideoClip TimeStart, TimeEnd and Duration calculated using TimeFormatConverter

--- PictureManager.Domain/Models/VideoClipM.cs
+++ PictureManager.Domain/Models/VideoClipM.cs
@@ -2,88 +2,82 @@
 using System;
 using System.Collections.Generic;
 
-namespace PictureManager.Domain.Models {
-  public sealed class VideoClipM : TreeItem, IEquatable<VideoClipM> {
-    #region IEquatable implementation
-    public bool Equals(VideoClipM other) => Id == other?.Id;
-    public override bool Equals(object obj) => Equals(obj as VideoClipM);
-    public override int GetHashCode() => Id;
-    public static bool operator ==(VideoClipM a, VideoClipM b) => a?.Equals(b) ?? b is null;
-    public static bool operator !=(VideoClipM a, VideoClipM b) => !(a == b);
-    #endregion
+namespace PictureManager.Domain.Models; 
 
-    private string _title;
-    private int _timeStart;
-    private int _timeEnd;
-    private double _volume;
-    private double _speed;
+public sealed class VideoClipM : TreeItem, IEquatable<VideoClipM> {
+  #region IEquatable implementation
+  public bool Equals(VideoClipM other) => Id == other?.Id;
+  public override bool Equals(object obj) => Equals(obj as VideoClipM);
+  public override int GetHashCode() => Id;
+  public static bool operator ==(VideoClipM a, VideoClipM b) => a?.Equals(b) ?? b is null;
+  public static bool operator !=(VideoClipM a, VideoClipM b) => !(a == b);
+  #endregion
 
-    // DB Fields
-    public int Id { get; }
-    public MediaItemM MediaItem { get; set; }
-    public int TimeStart { get => _timeStart; set { _timeStart = value; OnPropertyChanged(); } }
-    public int TimeEnd { get => _timeEnd; set { _timeEnd = value; OnPropertyChanged(); } }
-    public string Title { get => _title; set { _title = value; OnPropertyChanged(); } }
-    public double Volume { get => _volume; set { _volume = value; OnPropertyChanged(); OnPropertyChanged(nameof(VolumePercent)); } }
-    public double Speed { get => _speed; set { _speed = value; OnPropertyChanged(); } }
-    public int? Rating { get; set; }
-    public string Comment { get; set; }
-    public List<PersonM> People { get; set; }
-    public List<KeywordM> Keywords { get; set; }
+  private string _title;
+  private int _timeStart;
+  private int _timeEnd;
+  private double _volume;
+  private double _speed;
 
-    public string TimeStartStr => FormatPosition(TimeStart);
-    public string TimeEndStr => FormatPosition(TimeEnd);
-    public string DurationStr => FormatDuration(TimeEnd - TimeStart);
-    public int VolumePercent => (int)(Volume * 100);
-    public string ThumbPath => GetThumbPath();
+  // DB Fields
+  public int Id { get; }
+  public MediaItemM MediaItem { get; set; }
+  public int TimeStart { get => _timeStart; set { _timeStart = value; OnPropertyChanged(); OnPropertyChanged(nameof(Duration)); } }
+  public int TimeEnd { get => _timeEnd; set { _timeEnd = value; OnPropertyChanged(); OnPropertyChanged(nameof(Duration)); } }
+  public string Title { get => _title; set { _title = value; OnPropertyChanged(); } }
+  public double Volume { get => _volume; set { _volume = value; OnPropertyChanged(); OnPropertyChanged(nameof(VolumePercent)); } }
+  public double Speed { get => _speed; set { _speed = value; OnPropertyChanged(); } }
+  public int? Rating { get; set; }
+  public string Comment { get; set; }
+  public List<PersonM> People { get; set; }
+  public List<KeywordM> Keywords { get; set; }
 
-    public VideoClipM(int id, MediaItemM mediaItem) {
-      Id = id;
-      MediaItem = mediaItem;
-    }
+  public int Duration => TimeEnd - TimeStart;
+  public int VolumePercent => (int)(Volume * 100);
+  public string ThumbPath => GetThumbPath();
 
-    private static string FormatPosition(int ms) =>
-      TimeSpan.FromMilliseconds(ms).ToString(
+  public VideoClipM(int id, MediaItemM mediaItem) {
+    Id = id;
+    MediaItem = mediaItem;
+  }
+
+  public static string FormatPosition(int ms) =>
+    TimeSpan.FromMilliseconds(ms).ToString(
+      ms >= 60 * 60 * 1000
+        ? @"h\:mm\:ss\.fff"
+        : @"m\:ss\.fff");
+
+  public static string FormatDuration(int ms) =>
+    ms < 0
+      ? string.Empty
+      : TimeSpan.FromMilliseconds(ms).ToString(
         ms >= 60 * 60 * 1000
-          ? @"h\:mm\:ss\.fff"
-          : @"m\:ss\.fff");
+          ? @"h\:mm\:ss\.f"
+          : ms >= 60 * 1000
+            ? @"m\:ss\.f"
+            : @"s\.f\s");
 
-    private static string FormatDuration(int ms) =>
-      ms < 0
-        ? string.Empty
-        : TimeSpan.FromMilliseconds(ms).ToString(
-          ms >= 60 * 60 * 1000
-            ? @"h\:mm\:ss\.f"
-            : ms >= 60 * 1000
-              ? @"m\:ss\.f"
-              : @"s\.f\s");
+  private string GetThumbPath() {
+    var fpc = MediaItem.FilePathCache;
+    return $"{fpc[..fpc.LastIndexOf('.')]}_clip_{GetHashCode().ToString()}.jpg";
+  }
 
-    private string GetThumbPath() {
-      var fpc = MediaItem.FilePathCache;
-      return $"{fpc[..fpc.LastIndexOf('.')]}_clip_{GetHashCode().ToString()}.jpg";
+  public void SetMarker(bool start, int ms, double volume, double speed) {
+    if (start) {
+      TimeStart = ms;
+      if (ms > TimeEnd)
+        TimeEnd = 0;
     }
-
-    public void SetMarker(bool start, int ms, double volume, double speed) {
-      if (start) {
+    else {
+      if (ms < TimeStart) {
+        TimeEnd = TimeStart;
         TimeStart = ms;
-        if (ms > TimeEnd)
-          TimeEnd = 0;
       }
-      else {
-        if (ms < TimeStart) {
-          TimeEnd = TimeStart;
-          TimeStart = ms;
-        }
-        else
-          TimeEnd = ms;
-      }
-
-      Volume = volume;
-      Speed = speed;
-
-      OnPropertyChanged(nameof(TimeStartStr));
-      OnPropertyChanged(nameof(TimeEndStr));
-      OnPropertyChanged(nameof(DurationStr));
+      else
+        TimeEnd = ms;
     }
+
+    Volume = volume;
+    Speed = speed;
   }
 }
