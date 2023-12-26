fd6a2f6601c3351a5507f3939cecbcee7cbb446d
martin-holy <martin.holy@hotmail.com>
2023-12-26 10:49:47 +0100

    [N] TimeFormatConverter for MediaPlayer

---
+++ MH.UI.WPF/Converters/TimeFormatConverter.cs
@@ -0,0 +1,18 @@
+﻿using MH.UI.Controls;
+using System.Windows.Data;
+
+namespace MH.UI.WPF.Converters;
+
+public sealed class TimeFormatConverter : BaseMarkupExtensionConverter {
+  private const string _position = "position";
+  private const string _duration = "duration";
+
+  public override object Convert(object value, object parameter) =>
+    value is not int ms || parameter is null
+      ? Binding.DoNothing
+      : parameter switch {
+        _position => MediaPlayer.FormatPosition(ms),
+        _duration => MediaPlayer.FormatDuration(ms),
+        _ => Binding.DoNothing
+      } ;
+}
