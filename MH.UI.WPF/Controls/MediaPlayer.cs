21f13917f7c398b6d2034943d110f407804d6f5c
martin-holy <martin.holy@hotmail.com>
2023-12-26 10:54:43 +0100

    [N] MediaPlayer

--- MH.UI.WPF/Controls/MediaPlayer.cs
+++ MH.UI.WPF/Controls/MediaPlayer.cs
@@ -1,25 +1,54 @@
-﻿using System;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
+using UIC = MH.UI.Controls;
 
-namespace MH.UI.WPF.Controls {
-  public class MediaPlayer : MediaElement {
-    public static readonly DependencyProperty SpeedProperty =
-      DependencyProperty.Register(nameof(Speed), typeof(double), typeof(MediaPlayer),
-        new((o, e) => (o as MediaPlayer)?.SpeedChanged()));
+namespace MH.UI.WPF.Controls;
 
-    public double Speed {
-      get => (double)GetValue(SpeedProperty);
-      set => SetValue(SpeedProperty, value);
-    }
+public class MediaPlayer : MediaElement {
+  private bool _modelSet;
 
-    static MediaPlayer() {
-      DefaultStyleKeyProperty.OverrideMetadata(
-        typeof(MediaPlayer),
-        new FrameworkPropertyMetadata(typeof(MediaPlayer)));
-    }
+  static MediaPlayer() {
+    DefaultStyleKeyProperty.OverrideMetadata(
+      typeof(MediaPlayer),
+      new FrameworkPropertyMetadata(typeof(MediaPlayer)));
+  }
 
-    public void SpeedChanged() =>
-      SpeedRatio = Speed;
+  public MediaPlayer() {
+    LoadedBehavior = MediaState.Manual;
+    UnloadedBehavior = MediaState.Stop;
+    ScrubbingEnabled = true;
+    Stretch = Stretch.Uniform;
+    StretchDirection = StretchDirection.Both;
+  }
+
+  public void SetModel(UIC.MediaPlayer model) {
+    if (_modelSet) return;
+    _modelSet = true;
+
+    model.PlayAction = Play;
+    model.PauseAction = Pause;
+    model.SetPositionAction = ms => Position = new(0, 0, 0, 0, ms);
+    model.GetPositionFunc = () => Position.TotalMilliseconds;
+    model.SetSource = s => Source = string.IsNullOrEmpty(s) ? null : new(s);
+
+    model.PropertyChanged += (_, e) => {
+      switch (e.PropertyName) {
+        case nameof(model.Speed): SpeedRatio = model.Speed; break;
+        case nameof(model.Volume): Volume = model.Volume; break;
+        case nameof(model.IsMuted): IsMuted = model.IsMuted; break;
+      }
+    };
+
+    MediaOpened += delegate {
+      if (HasVideo)
+        model.OnMediaOpened(NaturalDuration.HasTimeSpan
+          ? (int)NaturalDuration.TimeSpan.TotalMilliseconds
+          : 0);
+    };
+
+    MediaEnded += delegate {
+      model.OnMediaEnded();
+    };
   }
 }
