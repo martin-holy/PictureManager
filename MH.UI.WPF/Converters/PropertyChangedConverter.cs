f94d95c03ce1857bd6346ab6f3f3735422ac464f
martin-holy <martin.holy@hotmail.com>
2023-12-26 10:50:24 +0100

    [N] PropertyChangedConverter

---
+++ MH.UI.WPF/Converters/PropertyChangedConverter.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace MH.UI.WPF.Converters;
+
+public class PropertyChangedConverter : BaseMarkupExtensionConverter {
+  public override object Convert(object value, object parameter) {
+    if (value is not RoutedPropertyChangedEventArgs<double> e) return null;
+
+    return new MH.Utils.EventsArgs.PropertyChangedEventArgs<double>(e.OldValue, e.NewValue) {
+      IsSourceDesired = parameter?.Equals(e.OriginalSource.GetType().FullName) == true,
+      OriginalSource = e.OriginalSource,
+      DataContext = (e.OriginalSource as FrameworkElement)?.DataContext,
+      IsCtrlOn = (Keyboard.Modifiers & ModifierKeys.Control) > 0,
+      IsAltOn = (Keyboard.Modifiers & ModifierKeys.Alt) > 0,
+      IsShiftOn = (Keyboard.Modifiers & ModifierKeys.Shift) > 0
+    };
+  }
+}
