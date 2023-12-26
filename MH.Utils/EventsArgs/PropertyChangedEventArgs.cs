ef74aa62cf3353b68c118552f0948fe99dee5cb0
martin-holy <martin.holy@hotmail.com>
2023-12-26 10:31:18 +0100

    [N] PropertyChangedEventArgs

---
+++ MH.Utils/EventsArgs/PropertyChangedEventArgs.cs
@@ -0,0 +1,11 @@
+﻿namespace MH.Utils.EventsArgs;
+
+public class PropertyChangedEventArgs<T> : RoutedEventArgs {
+  public T NewValue { get; }
+  public T OldValue { get; }
+
+  public PropertyChangedEventArgs(T oldValue, T newValue) {
+    OldValue = oldValue;
+    NewValue = newValue;
+  }
+}
