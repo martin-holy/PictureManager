d7b3c0efc44470c18f08666ebd796f5f86d24fe0
martin-holy <martin.holy@hotmail.com>
2023-12-26 10:34:48 +0100

    [N] EnumerableExtensions ToHashCodes and ToCsv methods

--- MH.Utils/Extensions/EnumerableExtensions.cs
+++ MH.Utils/Extensions/EnumerableExtensions.cs
@@ -6,4 +6,12 @@ namespace MH.Utils.Extensions;
 public static class EnumerableExtensions {
   public static IEnumerable<T> EmptyIfNull<T>(this IEnumerable<T> items) =>
     items ?? Enumerable.Empty<T>();
+
+  public static IEnumerable<int> ToHashCodes<T>(this IEnumerable<T> items) =>
+    items?.Select(x => x.GetHashCode());
+
+  public static string ToCsv<T>(this IEnumerable<T> items, string separator = ",") =>
+    items == null
+      ? string.Empty
+      : string.Join(separator, items.Select(x => x.ToString()));
 }
