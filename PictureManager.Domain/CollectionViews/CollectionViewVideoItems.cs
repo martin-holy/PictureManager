bf741c7fec7262aebc2f127862d80e01d2768629
martin-holy <martin.holy@hotmail.com>
2023-12-26 12:27:36 +0100

    [N] CollectionView for VideoClips and VideoImages

---
+++ PictureManager.Domain/CollectionViews/CollectionViewVideoItems.cs
@@ -0,0 +1,35 @@
+﻿using MH.UI.Controls;
+using MH.Utils;
+using MH.Utils.BaseClasses;
+using PictureManager.Domain.Models.MediaItems;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PictureManager.Domain.CollectionViews;
+
+public class CollectionViewVideoItems : CollectionView<VideoItemM> {
+  public Selecting<VideoItemM> Selected { get; } = new();
+
+  public override int GetItemSize(VideoItemM item, bool getWidth) =>
+    (getWidth ? item.ThumbWidth : item.ThumbHeight) / 3;
+
+  public override IEnumerable<CollectionViewGroupByItem<VideoItemM>> GetGroupByItems(IEnumerable<VideoItemM> source) {
+    var src = source.ToArray();
+    var top = new List<CollectionViewGroupByItem<VideoItemM>>();
+    top.AddRange(GroupByItems.GetFoldersFromMediaItems(src));
+    top.Add(GroupByItems.GetDatesInGroupFromMediaItems(src));
+    top.Add(GroupByItems.GetKeywordsInGroupFromMediaItems(src));
+    top.Add(GroupByItems.GetPeopleInGroupFromMediaItems(src));
+
+    return top;
+  }
+
+  public override int SortCompare(VideoItemM itemA, VideoItemM itemB) =>
+    itemA.TimeStart - itemB.TimeStart;
+
+  public override void OnItemSelected(SelectionEventArgs<VideoItemM> e) {
+    Selected.Select(e.Items, e.Item, e.IsCtrlOn, e.IsShiftOn);
+    Core.VideosM.MediaPlayer.SetCurrent(e.Item);
+    Core.VideoClipsM.Current = e.Item as VideoClipM;
+  }
+}
