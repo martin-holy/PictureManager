9cbffc15f7256db852b4091f9e8a97387f4b51fd
martin-holy <martin.holy@hotmail.com>
2023-12-26 10:33:24 +0100

    [U] Log.Error method

--- MH.Utils/Log.cs
+++ MH.Utils/Log.cs
@@ -2,19 +2,24 @@
 using System;
 using System.Collections.ObjectModel;
 
-namespace MH.Utils {
-  public static class Log {
-    public static ObservableCollection<LogItem> Items { get; } = new();
+namespace MH.Utils;
 
-    public static void Error(Exception ex) =>
-      Error(ex, string.Empty);
+public static class Log {
+  public static ObservableCollection<LogItem> Items { get; } = new();
 
-    public static void Error(Exception ex, string msg) =>
-      Tasks.RunOnUiThread(() =>
-        Items.Add(new(
-          string.IsNullOrEmpty(msg)
-            ? ex.Message
-            : msg,
-          $"{msg}\n{ex.Message}\n{ex.StackTrace}")));
-  }
+  public static void Error(Exception ex) =>
+    Error(ex, string.Empty);
+
+  public static void Error(Exception ex, string msg) =>
+    Add(new(
+      string.IsNullOrEmpty(msg)
+        ? ex.Message
+        : msg,
+      $"{msg}\n{ex.Message}\n{ex.StackTrace}"));
+
+  public static void Error(string msg, string detail) =>
+    Add(new(msg, detail));
+
+  private static void Add(LogItem item) =>
+    Tasks.RunOnUiThread(() => Items.Add(item));
 }
