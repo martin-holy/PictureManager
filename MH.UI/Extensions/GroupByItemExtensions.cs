c7345eee9e12449b40f282431b4cd445e3fdee6d
martin-holy <martin.holy@hotmail.com>
2023-12-27 17:03:17 +0100

    [C] GroupByItemExtensions refactoring

--- MH.UI/Extensions/GroupByItemExtensions.cs
+++ MH.UI/Extensions/GroupByItemExtensions.cs
@@ -8,24 +8,19 @@ namespace MH.UI.Extensions;
 
 public static class GroupByItemExtensions {
   public static CollectionViewGroupByItem<T> InGroup<T>(
-    this IEnumerable<CollectionViewGroupByItem<T>> items, IListItem item, Func<T, object, bool> groupByFunc) =>
-    new(item, items, groupByFunc) { IsGroup = true };
+    this IEnumerable<CollectionViewGroupByItem<T>> items, IListItem item, Func<T, object, bool> groupByFunc, bool isGroup = true) =>
+    new(item, items, groupByFunc) { IsGroup = isGroup };
 
-  public static IEnumerable<CollectionViewGroupByItem<TWhat>> InGroups<TWhat, TBy>(
+  public static IEnumerable<CollectionViewGroupByItem<TWhat>> GroupByParent<TWhat, TBy>(
     this IEnumerable<CollectionViewGroupByItem<TWhat>> items, Func<TWhat, object, bool> groupByFunc)
-    where TBy : class, IListItem {
-    var groups = items.GroupBy(x => (x.Data as ITreeItem)?.Parent as TBy).ToArray();
-    return groups
-      .Where(x => x.Key != null)
-      .OrderBy(x => x.Key.Name)
-      .Select(x => {
-        var group = new CollectionViewGroupByItem<TWhat>(x.Key, groupByFunc);
-        group.Items.AddItems(x.Cast<ITreeItem>().ToArray(), item => item.Parent = group);
-
-        return group;
-      })
-      .Concat(groups.Where(x => x.Key == null).SelectMany(x => x));
-  }
+    where TBy : class, IListItem =>
+    items
+      .GroupBy(x => (x.Data as ITreeItem)?.Parent as TBy)
+      .OrderBy(x => x.Key == null)
+      .ThenBy(x => x.Key?.Name)
+      .SelectMany(x => x.Key == null
+        ? x.Select(y => y)
+        : new[] { x.InGroup(x.Key, groupByFunc, false) });
 
   public static IEnumerable<CollectionViewGroupByItem<TWhat>> ToGroupByItems<TWhat, TBy>(
     this IEnumerable<TBy> items, Func<TWhat, object, bool> groupByFunc)
