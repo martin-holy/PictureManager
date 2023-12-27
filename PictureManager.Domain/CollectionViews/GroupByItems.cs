6317400445375a1f3e9270d0889adc1b92999c26
martin-holy <martin.holy@hotmail.com>
2023-12-27 17:07:49 +0100

    [C] GroupByItems methods refactoring

--- PictureManager.Domain/CollectionViews/GroupByItems.cs
+++ PictureManager.Domain/CollectionViews/GroupByItems.cs
@@ -21,6 +21,11 @@ public static class GroupByItems {
   private static readonly Dictionary<string, string> _dateFormats =
     new() { { "d", "d. " }, { "M", "MMMM " }, { "y", "yyyy" } };
 
+  public static CollectionViewGroupByItem<T> GetDatesInGroupFromMediaItems<T>(IEnumerable<T> items) where T : MediaItemM =>
+    items
+      .GetDatesFromMediaItems()
+      .InGroup(_dateGroup, GroupMediaItemByDate);
+
   public static IEnumerable<CollectionViewGroupByItem<T>> GetDatesFromMediaItems<T>(this IEnumerable<T> items) where T : MediaItemM {
     var dates = items
       .Where(x => x.FileName.Length > 8)
@@ -39,155 +44,108 @@ public static class GroupByItems {
     }
   }
 
-  public static IEnumerable<CollectionViewGroupByItem<T>> GetFoldersFromMediaItems<T>(IEnumerable<T> items) where T : MediaItemM =>
+  public static IEnumerable<CollectionViewGroupByItem<T>> GetFoldersFromMediaItems<T>(IEnumerable<T> items)
+    where T : MediaItemM =>
+    items.GetFolders().ToGroupByItemsAsTree<T>(GroupByFolder);
+
+  public static IEnumerable<CollectionViewGroupByItem<SegmentM>> GetFoldersFromSegments(IEnumerable<SegmentM> items) =>
+    items.GetFolders().ToGroupByItemsAsTree<SegmentM>(GroupByFolder);
+
+  public static IEnumerable<CollectionViewGroupByItem<T>> ToGroupByItemsAsTree<T>(this IEnumerable<FolderM> items, Func<T, object, bool> func)
+    where T : class =>
+    items.ToGroupByItems(func).AsTree<CollectionViewGroupByItem<T>, FolderM, string>(x => x.FullPath);
+
+  public static IEnumerable<CollectionViewGroupByItem<SegmentM>> GetPeopleFromSegments(IEnumerable<SegmentM> items) =>
     items
-      .GetFolders()
-      .ToGroupByItems<T, FolderM>(GroupMediaItemByFolder)
-      .AsTree<CollectionViewGroupByItem<T>, FolderM, string>(x => x.FullPath);
-
-  public static List<CollectionViewGroupByItem<SegmentM>> GetFoldersFromSegments(IList<SegmentM> segments) =>
-    CollectionViewGroupByItem<SegmentM>.BuildTree<SegmentM, FolderM, string>(
-      segments.GetFolders(),
-      x => new(x, GroupSegmentByFolder),
-      x => x.FullPath);
-
-  public static List<CollectionViewGroupByItem<SegmentM>> GetKeywordsFromSegments(IEnumerable<SegmentM> segments) =>
-    CollectionViewGroupByItem<SegmentM>.BuildTree<SegmentM, KeywordM, string>(
-      segments.GetKeywords(),
-      x => new(x, GroupSegmentByKeyword),
-      x => x.FullName);
-
-  public static List<CollectionViewGroupByItem<SegmentM>> GetPeopleFromSegments(IEnumerable<SegmentM> segments) =>
-    segments
       .GetPeople()
       .OrderBy(x => x.Name)
-      .Select(x => new CollectionViewGroupByItem<SegmentM>(x, GroupSegmentByPerson))
-      .ToList();
-
-  public static List<CollectionViewGroupByItem<PersonM>> GetKeywordsFromPeople(IEnumerable<PersonM> people) =>
-    CollectionViewGroupByItem<PersonM>.BuildTree<PersonM, KeywordM, string>(
-      people.GetKeywords(),
-      x => new(x, GroupPersonByKeyword),
-      x => x.FullName);
-
-  public static CollectionViewGroupByItem<T> GetDatesInGroupFromMediaItems<T>(IEnumerable<T> items) where T : MediaItemM =>
-    items
-      .GetDatesFromMediaItems()
-      .InGroup(_dateGroup, GroupMediaItemByDate);
+      .ToGroupByItems<SegmentM, PersonM>(GroupByPerson);
 
   public static CollectionViewGroupByItem<T> GetKeywordsInGroupFromMediaItems<T>(IEnumerable<T> items) where T : MediaItemM =>
+    items.GetKeywords().ToGroupedGroupByItemsInGroup<T>(GroupByKeyword);
+
+  public static CollectionViewGroupByItem<PersonM> GetKeywordsInGroupFromPeople(IEnumerable<PersonM> items) =>
+    items.GetKeywords().ToGroupedGroupByItemsInGroup<PersonM>(GroupByKeyword);
+
+  public static CollectionViewGroupByItem<SegmentM> GetKeywordsInGroupFromSegments(IEnumerable<SegmentM> items) =>
+    items.GetKeywords().ToGroupedGroupByItemsInGroup<SegmentM>(GroupByKeyword);
+
+  private static CollectionViewGroupByItem<T> ToGroupedGroupByItemsInGroup<T>(this IEnumerable<KeywordM> items, Func<T, object, bool> func)
+    where T : class =>
     items
-      .GetKeywords()
-      .ToGroupByItems<T, KeywordM>(GroupMediaItemByKeyword)
+      .ToGroupByItems(func)
       .AsTree<CollectionViewGroupByItem<T>, KeywordM, string>(x => x.FullName)
-      .InGroups<T, CategoryGroupM>(GroupMediaItemByKeyword)
-      .InGroup(Core.KeywordsM.TreeCategory, GroupMediaItemByKeyword);
-
-  public static CollectionViewGroupByItem<PersonM> GetKeywordsInGroupFromPeople(IEnumerable<PersonM> people) {
-    var group = new CollectionViewGroupByItem<PersonM>(
-      Core.KeywordsM.TreeCategory, GroupPersonByKeyword) { IsGroup = true };
-    group.AddItems(GetItemsInGroups(GetKeywordsFromPeople(people), GroupPersonByKeyword));
-
-    return group;
-  }
-
-  public static CollectionViewGroupByItem<SegmentM> GetKeywordsInGroupFromSegments(IEnumerable<SegmentM> segments) {
-    var group = new CollectionViewGroupByItem<SegmentM>(
-      Core.KeywordsM.TreeCategory, GroupSegmentByKeyword) { IsGroup = true };
-    group.AddItems(GetItemsInGroups(GetKeywordsFromSegments(segments), GroupSegmentByKeyword));
-
-    return group;
-  }
+      .GroupByParent<T, CategoryGroupM>(func)
+      .InGroup(Core.KeywordsM.TreeCategory, func);
 
   public static CollectionViewGroupByItem<T> GetPeopleInGroupFromMediaItems<T>(IEnumerable<T> items) where T : MediaItemM =>
+    items.GetPeople().ToGroupedGroupByItemsInGroup<T>(GroupByPerson);
+
+  public static CollectionViewGroupByItem<SegmentM> GetPeopleInGroupFromSegments(IEnumerable<SegmentM> items) =>
+    items.GetPeople().ToGroupedGroupByItemsInGroup<SegmentM>(GroupByPerson);
+
+  private static CollectionViewGroupByItem<T> ToGroupedGroupByItemsInGroup<T>(this IEnumerable<PersonM> items, Func<T, object, bool> func)
+    where T : class =>
     items
-      .GetPeople()
       .OrderBy(x => x.Name)
-      .Select(x => new CollectionViewGroupByItem<T>(x, GroupMediaItemByPerson))
-      .InGroups<T, CategoryGroupM>(GroupMediaItemByPerson)
-      .InGroup(Core.PeopleM.TreeCategory, GroupMediaItemByPerson);
+      .ToGroupByItems(func)
+      .GroupByParent<T, CategoryGroupM>(func)
+      .InGroup(Core.PeopleM.TreeCategory, func);
 
-  public static CollectionViewGroupByItem<SegmentM> GetPeopleInGroupFromSegments(IEnumerable<SegmentM> segments) {
-    var group = new CollectionViewGroupByItem<SegmentM>(
-      Core.PeopleM.TreeCategory, GroupSegmentByPerson) { IsGroup = true };
-    group.AddItems(GetItemsInGroups(GetPeopleFromSegments(segments), GroupSegmentByPerson));
-
-    return group;
-  }
-
-  public static List<CollectionViewGroupByItem<T>> GetItemsInGroups<T>(
-    List<CollectionViewGroupByItem<T>> items, Func<T, object, bool> itemGroupBy) {
-
-    var groups = items.GroupBy(x => (x.Data as ITreeItem)?.Parent as CategoryGroupM).ToArray();
-    return groups
-      .Where(x => x.Key != null)
-      .OrderBy(x => x.Key.Name)
-      .Select(x => {
-        var group = new CollectionViewGroupByItem<T>(x.Key, itemGroupBy);
-        group.Items.AddItems(x.Cast<ITreeItem>().ToArray(), item => item.Parent = group);
-
-        return group;
-      })
-      .Concat(groups.Where(x => x.Key == null).SelectMany(x => x))
-      .ToList();
-  }
-
-  public static CollectionViewGroupByItem<PersonM> GetPeopleGroupsInGroupFromPeople(IEnumerable<PersonM> people) {
-    var group = new CollectionViewGroupByItem<PersonM>(
-      _peopleGroupsGroup, GroupPersonByGroup) { IsGroup = true };
-    var groupItems = people
+  public static CollectionViewGroupByItem<PersonM> GetPeopleGroupsInGroupFromPeople(IEnumerable<PersonM> people) =>
+    people
       .GroupBy(x => x.Parent)
       .Select(x => x.Key)
       .OrderBy(x => x.Name)
-      .Select(x => new CollectionViewGroupByItem<PersonM>(x, GroupPersonByGroup));
-
-    group.AddItems(groupItems);
-
-    return group;
-  }
+      .ToGroupByItems<PersonM, ITreeItem>(GroupPersonByGroup)
+      .InGroup(_peopleGroupsGroup, GroupPersonByGroup);
 
   public static bool GroupPersonByGroup(PersonM item, object parameter) =>
-    ReferenceEquals(parameter, _peopleGroupsGroup)
-    || ReferenceEquals(parameter, item.Parent);
+    ReferenceEquals(parameter, _peopleGroupsGroup) ||
+    ReferenceEquals(parameter, item.Parent);
 
   private static bool GroupMediaItemByDate(MediaItemM item, object parameter) =>
-    ReferenceEquals(parameter, _dateGroup)
-    || (item.FileName.Length > 7
-        && parameter is DateM date
-        && string.Equals(item.FileName[..8], date.Raw, StringComparison.Ordinal));
+    ReferenceEquals(parameter, _dateGroup) ||
+    (item.FileName.Length > 7 && 
+     parameter is DateM date && 
+     string.Equals(item.FileName[..8], date.Raw, StringComparison.Ordinal));
 
   private static bool GroupByFolder(FolderM folder, object parameter) =>
     parameter is FolderM f && folder.GetThisAndParents().Contains(f);
 
-  private static bool GroupMediaItemByFolder(MediaItemM item, object parameter) =>
+  private static bool GroupByFolder(MediaItemM item, object parameter) =>
     GroupByFolder(item.Folder, parameter);
 
-  private static bool GroupSegmentByFolder(SegmentM item, object parameter) =>
+  private static bool GroupByFolder(SegmentM item, object parameter) =>
     GroupByFolder(item.MediaItem.Folder, parameter);
 
   private static bool GroupByPerson(PersonM person, object parameter) =>
-    ReferenceEquals(parameter, person) || ReferenceEquals(parameter, person?.Parent);
+    ReferenceEquals(parameter, person) ||
+    ReferenceEquals(parameter, person?.Parent);
 
-  private static bool GroupMediaItemByPerson(MediaItemM item, object parameter) =>
-    ReferenceEquals(parameter, Core.PeopleM.TreeCategory)
-    || item.People?.Any(x => GroupByPerson(x, parameter)) == true
-    || item.Segments?.Any(x => GroupByPerson(x.Person, parameter)) == true;
+  private static bool GroupByPerson(IEnumerable<PersonM> items, object parameter) =>
+    ReferenceEquals(parameter, Core.PeopleM.TreeCategory) ||
+    items?.Any(x => GroupByPerson(x, parameter)) == true;
 
-  private static bool GroupSegmentByPerson(SegmentM item, object parameter) =>
-    ReferenceEquals(parameter, Core.PeopleM.TreeCategory)
-    || GroupByPerson(item.Person, parameter);
+  private static bool GroupByPerson(this MediaItemM item, object parameter) =>
+    GroupByPerson(item.People, parameter) ||
+    GroupByPerson(item.Segments?.GetPeople(), parameter);
 
-  private static bool GroupByKeyword(IEnumerable<KeywordM> keywords, object parameter) =>
-    ReferenceEquals(parameter, Core.KeywordsM.TreeCategory)
-    || keywords?.SelectMany(x => x.GetThisAndParents<ITreeItem>()).Contains(parameter) == true;
+  private static bool GroupByPerson(this SegmentM item, object parameter) =>
+    ReferenceEquals(parameter, Core.PeopleM.TreeCategory) || 
+    GroupByPerson(item.Person, parameter);
 
-  private static bool GroupMediaItemByKeyword(MediaItemM item, object parameter) =>
-    GroupByKeyword(item.Keywords, parameter)
-    || item.Segments?.Any(x => GroupByKeyword(x.Keywords, parameter)) == true;
+  private static bool GroupByKeyword(IEnumerable<KeywordM> items, object parameter) =>
+    ReferenceEquals(parameter, Core.KeywordsM.TreeCategory) ||
+    items?.SelectMany(x => x.GetThisAndParents<ITreeItem>()).Contains(parameter) == true;
 
-  private static bool GroupPersonByKeyword(PersonM item, object parameter) =>
+  private static bool GroupByKeyword(this MediaItemM item, object parameter) =>
+    GroupByKeyword(item.Keywords, parameter) ||
+    GroupByKeyword(item.Segments?.GetKeywords(), parameter);
+
+  private static bool GroupByKeyword(this PersonM item, object parameter) =>
     GroupByKeyword(item.Keywords, parameter);
 
-  private static bool GroupSegmentByKeyword(SegmentM item, object parameter) =>
+  private static bool GroupByKeyword(this SegmentM item, object parameter) =>
     GroupByKeyword(item.Keywords, parameter);
 }
