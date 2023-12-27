ba86b91a8d1a86884a4edb5b5acd1a3a0b8918b0
martin-holy <martin.holy@hotmail.com>
2023-12-27 17:02:51 +0100

    [C] CollectionViewGroupByItem.BuildTree method removed

--- MH.UI/Controls/CollectionViewGroupByItem.cs
+++ MH.UI/Controls/CollectionViewGroupByItem.cs
@@ -24,27 +24,6 @@ public class CollectionViewGroupByItem<T> : TreeItem {
   public bool Fit(T item) =>
     _fit(item, Data);
 
-  public static List<CollectionViewGroupByItem<TItem>> BuildTree<TItem, TGroup, TSort>(
-    IEnumerable<TGroup> source,
-    Func<TGroup, CollectionViewGroupByItem<TItem>> getGroupByItem,
-    Func<TGroup, TSort> orderBy) where TGroup : class, ITreeItem {
-
-    var root = new List<CollectionViewGroupByItem<TItem>>();
-    var all = source.ToDictionary(x => x, getGroupByItem);
-
-    foreach (var item in all.OrderBy(x => orderBy(x.Key))) {
-      if (item.Key.Parent is not TGroup parent) {
-        root.Add(item.Value);
-        continue;
-      }
-
-      item.Value.Parent = all[parent];
-      item.Value.Parent.Items.Add(item.Value);
-    }
-
-    return root;
-  }
-
   // TODO remove items as well
   public void Update(CollectionViewGroupByItem<T>[] items) {
     var newItems = Tree.FindItem(items, x => ReferenceEquals(x.Data, Data))?.Items.ToArray();
