ea9ef858384cf0e2f3592e7e34431b70e64b5fd5
martin-holy <martin.holy@hotmail.com>
2024-01-07 12:48:58 +0100

    [B] ToggleDialog set Rating icon and text

--- PictureManager.Domain/Models/RatingTreeM.cs
+++ PictureManager.Domain/Models/RatingTreeM.cs
@@ -1,11 +1,11 @@
 ﻿using MH.Utils.BaseClasses;
 
-namespace PictureManager.Domain.Models {
-  public sealed class RatingTreeM : TreeItem {
-    public RatingM Rating { get; }
+namespace PictureManager.Domain.Models;
 
-    public RatingTreeM(int value) {
-      Rating = new(value);
-    }
+public sealed class RatingTreeM : TreeItem {
+  public RatingM Rating { get; }
+
+  public RatingTreeM(int value) : base(Res.IconStar, value.ToString()) {
+    Rating = new(value);
   }
 }
