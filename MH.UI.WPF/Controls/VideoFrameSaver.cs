21b2ec58f327ffc297c08da5e587344409ac08c3
martin-holy <martin.holy@hotmail.com>
2024-01-20 14:33:10 +0100

    [U] VideoFrameSaver

--- MH.UI.WPF/Controls/VideoFrameSaver.cs
+++ MH.UI.WPF/Controls/VideoFrameSaver.cs
@@ -1,40 +1,31 @@
-﻿using MH.UI.WPF.Utils;
+﻿using MH.UI.HelperClasses;
+using MH.UI.Interfaces;
+using MH.UI.WPF.Extensions;
 using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
+using System.Windows.Media.Imaging;
 using System.Windows.Threading;
 
 namespace MH.UI.WPF.Controls;
 
-public class VideoFrameSaver : MediaElement {
+/* Not visible parts of the VideoFrameSaver are filed with black color.
+   But there is not problem with hiding VideoFrameSaver behind other controls. */
+
+public class VideoFrameSaver : MediaElement, IVideoFrameSaver {
   private int _idxVideo;
   private int _idxFrame;
   private long _hash;
-  private KeyValuePair<string, KeyValuePair<int, string>[]> _currentVideo;
-  private KeyValuePair<int, string> _currentFrame;
+  private VfsVideo[] _videos;
+  private VfsVideo _video;
+  private VfsFrame _frame;
   private readonly DispatcherTimer _positionTimer;
   private readonly Stopwatch _timeOut = new();
-
-  public static readonly DependencyProperty VideosProperty = DependencyProperty.Register(
-    nameof(Videos), typeof(List<KeyValuePair<string, KeyValuePair<int, string>[]>>), typeof(VideoFrameSaver),
-    new((o, _) => (o as VideoFrameSaver)?.Save()));
-
-  public static readonly DependencyProperty SizeProperty = DependencyProperty.Register(
-    nameof(Size), typeof(int), typeof(VideoFrameSaver));
-
-  public static readonly DependencyProperty QualityProperty = DependencyProperty.Register(
-    nameof(Quality), typeof(int), typeof(VideoFrameSaver));
-
-  public static readonly DependencyProperty FinishedProperty = DependencyProperty.Register(
-    nameof(Finished), typeof(Action), typeof(VideoFrameSaver));
-
-  public List<KeyValuePair<string, KeyValuePair<int, string>[]>> Videos { get => (List<KeyValuePair<string, KeyValuePair<int, string>[]>>)GetValue(VideosProperty); set => SetValue(VideosProperty, value); }
-  public int Size { get => (int)GetValue(SizeProperty); set => SetValue(SizeProperty, value); }
-  public int Quality { get => (int)GetValue(QualityProperty); set => SetValue(QualityProperty, value); }
-  public Action Finished { get => (Action)GetValue(FinishedProperty); set => SetValue(FinishedProperty, value); }
+  private readonly RotateTransform _rotateTransform = new();
+  private Action _onFinishedAction;
+  private Action<VfsFrame> _onSaveAction;
 
   static VideoFrameSaver() {
     DefaultStyleKeyProperty.OverrideMetadata(
@@ -48,61 +39,90 @@ public class VideoFrameSaver : MediaElement {
     Stretch = Stretch.Uniform;
     StretchDirection = StretchDirection.Both;
     ScrubbingEnabled = true;
+    LayoutTransform = _rotateTransform;
     MediaOpened += delegate { OnMediaOpened(); };
     _positionTimer = new() { Interval = TimeSpan.FromMilliseconds(1) };
     _positionTimer.Tick += delegate { OnPositionTimerTick(); };
   }
 
-  public void Save() {
+  public void Save(VfsVideo[] videos, Action<VfsFrame> onSaveAction, Action onFinishedAction) {
+    _videos = videos;
+    _onSaveAction = onSaveAction;
+    _onFinishedAction = onFinishedAction;
     _idxVideo = -1;
+    MaxWidth = ((FrameworkElement)Parent).ActualWidth;
+    MaxHeight = ((FrameworkElement)Parent).ActualHeight;
     NextVideo();
   }
 
   private void NextVideo() {
-    if (_idxVideo + 1 > Videos.Count - 1) {
+    if (_idxVideo + 1 > _videos.Length - 1) {
       Source = null;
-      Finished?.Invoke();
+      _onFinishedAction?.Invoke();
       return;
     }
 
-    _currentVideo = Videos[++_idxVideo];
-    Source = new(_currentVideo.Key);
+    _video = _videos[++_idxVideo];
+    _rotateTransform.Angle = _video.Rotation;
+    Source = new(_video.FilePath);
     Play();
     Stop();
   }
 
   private void OnMediaOpened() {
+    if (NaturalVideoWidth + NaturalVideoHeight == 0) {
+      NextVideo();
+      return;
+    }
+
     _hash = 0;
     _idxFrame = -1;
-    NextFrame();
+    Width = NaturalVideoWidth;
+    Height = NaturalVideoHeight;
+    Dispatcher.BeginInvoke(DispatcherPriority.Loaded, NextFrame);
   }
 
   private void NextFrame() {
-    if (_idxFrame + 1 > _currentVideo.Value.Length - 1) {
+    if (_idxFrame + 1 > _video.Frames.Count - 1) {
       _timeOut.Stop();
       NextVideo();
       return;
     }
 
-    _currentFrame = _currentVideo.Value[++_idxFrame];
-    Position = new(0, 0, 0, 0, _currentFrame.Key);
+    var oldPos = _frame?.Position ?? 0;
+    _frame = _video.Frames[++_idxFrame];
+    if (_frame.Position == oldPos) {
+      SaveFrame();
+      NextFrame();
+      return;
+    }
+
+    Position = new(0, 0, 0, 0, _frame.Position);
     _timeOut.Reset();
     _timeOut.Start();
     _positionTimer.Start();
   }
 
   private void OnPositionTimerTick() {
-    var hash = GetHash();
-
+    var hash = this.ToBitmap().GetAvgHash();
     if (MH.Utils.Imaging.CompareHashes(_hash, hash) == 0 && _timeOut.ElapsedMilliseconds < 1000)
       return;
 
     _positionTimer.Stop();
-    Imaging.CreateThumbnailFromVisual(this, _currentFrame.Value, Size, Quality);
     _hash = hash;
+    SaveFrame();
     NextFrame();
   }
 
-  private long GetHash() =>
-    Imaging.GetBitmapAvgHash(Imaging.VisualToBitmapSource(this));
+  private void SaveFrame() {
+    Crop(this.ToBitmap()).Resize(_frame.Size).SaveAsJpeg(_frame.FilePath, _frame.Quality);
+    _onSaveAction?.Invoke(_frame);
+  }
+
+  private BitmapSource Crop(BitmapSource bmp) {
+    var rect = new Int32Rect(_frame.X, _frame.Y, _frame.Width, _frame.Height);
+    if (!rect.HasArea) return bmp;
+    if (ActualWidth >= Width && ActualHeight >= Height) return bmp.Crop(rect);
+    return bmp.Crop(rect.Scale(ActualWidth / Width));
+  }
 }
