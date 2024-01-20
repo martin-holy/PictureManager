a60ce284319d3993faceeb0433e8adf8c108bceb
martin-holy <martin.holy@hotmail.com>
2024-01-20 14:28:17 +0100

    [N] BitmapMetadataExtensions

---
+++ MH.UI.WPF/Extensions/BitmapMetadataExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Media.Imaging;
+using System;
+
+namespace MH.UI.WPF.Extensions;
+
+public static class BitmapMetadataExtensions {
+  public static T GetQuery<T>(this BitmapMetadata bm, string query, T value = default) {
+    try {
+      if (bm.GetQuery(query) is T t) return t;
+      return value;
+    }
+    catch (Exception) {
+      return value;
+    }
+  }
+}
