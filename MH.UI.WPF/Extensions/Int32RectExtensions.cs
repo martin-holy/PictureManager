e2f5e65226664afe8437610c243cf30179d055b2
martin-holy <martin.holy@hotmail.com>
2024-01-20 14:29:11 +0100

    [N] Int32RectExtensions

---
+++ MH.UI.WPF/Extensions/Int32RectExtensions.cs
@@ -0,0 +1,8 @@
+﻿using System.Windows;
+
+namespace MH.UI.WPF.Extensions;
+
+public static class Int32RectExtensions {
+  public static Int32Rect Scale(this Int32Rect rect, double scale) =>
+    new((int)(rect.X * scale), (int)(rect.Y * scale), (int)(rect.Width * scale), (int)(rect.Height * scale));
+}
