6291610824a88aff2ab0676f04bf7dce7322512d
martin-holy <martin.holy@hotmail.com>
2024-01-21 23:15:20 +0100

    [N] BaseMultiConverter

---
+++ MH.UI.WPF/Converters/BaseMultiConverter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace MH.UI.WPF.Converters;
+
+public class BaseMultiConverter : IMultiValueConverter {
+  public virtual object Convert(object[] values, Type targetType, object parameter, CultureInfo culture) =>
+    Convert(values, parameter);
+
+  public virtual object Convert(object[] values, object parameter) =>
+    throw new NotImplementedException();
+
+  public virtual object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture) =>
+    throw new NotSupportedException();
+}
