2a3ddc4120b837a083b2ffcab4388670116d5c1e
martin-holy <martin.holy@hotmail.com>
2024-01-27 15:46:26 +0100

    [N] OrientationExtensions ToAngle, SwapRotateIf and ToInt

---
+++ MH.Utils/Extensions/OrientationExtensions.cs
@@ -0,0 +1,23 @@
+﻿namespace MH.Utils.Extensions;
+
+public static class OrientationExtensions {
+  public static int ToAngle(this Orientation orientation) =>
+    orientation switch {
+      Orientation.Rotate90 => 90,
+      Orientation.Rotate180 => 180,
+      Orientation.Rotate270 => 270,
+      _ => 0,
+    };
+
+  public static Orientation SwapRotateIf(this Orientation orientation, bool swap) =>
+    swap
+      ? orientation switch {
+        Orientation.Rotate270 => Orientation.Rotate90,
+        Orientation.Rotate90 => Orientation.Rotate270,
+        _ => orientation
+      }
+      : orientation;
+
+  public static int ToInt(this Orientation orientation) =>
+    (int)orientation;
+}
