fb59d9c538c12f188a38ff0a5efc1bd2ac4cdb07
martin-holy <martin.holy@hotmail.com>
2024-01-27 15:45:34 +0100

    [N] OrientationExtensions ToRotation

---
+++ MH.UI.WPF/Extensions/OrientationExtensions.cs
@@ -0,0 +1,14 @@
+﻿using MH.Utils;
+using System.Windows.Media.Imaging;
+
+namespace MH.UI.WPF.Extensions;
+
+public static class OrientationExtensions {
+  public static Rotation ToRotation(this Orientation orientation) =>
+    orientation switch {
+      Orientation.Rotate90 => Rotation.Rotate270,
+      Orientation.Rotate180 => Rotation.Rotate180,
+      Orientation.Rotate270 => Rotation.Rotate90,
+      _ => Rotation.Rotate0
+    };
+}
