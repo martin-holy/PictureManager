b7f62b9f58512c6beeea42a82489a174fe9d44d0
martin-holy <martin.holy@hotmail.com>
2024-01-27 17:03:51 +0100

    [C] RebuildVideoThumbnailsCommand removed

--- PictureManager.Domain/Models/MediaItems/MediaItemsM.cs
+++ PictureManager.Domain/Models/MediaItems/MediaItemsM.cs
@@ -33,7 +33,6 @@ public sealed class MediaItemsM : ObservableObject {
   public RelayCommand<object> CommentCommand { get; }
   public RelayCommand<object> ReloadMetadataCommand { get; }
   public RelayCommand<FolderM> ReloadMetadataInFolderCommand { get; }
-  public RelayCommand<object> RebuildVideoThumbnailsCommand { get; }
 
   public MediaItemsM(MediaItemsDA da) {
     _da = da;
@@ -49,10 +48,6 @@ public sealed class MediaItemsM : ObservableObject {
     ReloadMetadataInFolderCommand = new(
       x => ReloadMetadata(x.GetMediaItems(Keyboard.IsShiftOn()).ToList()),
       x => x != null);
-
-    RebuildVideoThumbnailsCommand = new(
-      () => Core.VideoThumbsM.Create(GetActive(), true),
-      () => GetActive().Any());
   }
 
   public void OnMetadataChanged(MediaItemM[] items) {
