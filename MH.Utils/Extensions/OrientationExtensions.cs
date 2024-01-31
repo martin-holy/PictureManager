cbca8f586b2a3d91066abcab6fb769822aa38ef7
martin-holy <martin.holy@hotmail.com>
2024-01-31 12:27:56 +0100

    [N] OrientationExtensions Rotate method

--- MH.Utils/Extensions/OrientationExtensions.cs
+++ MH.Utils/Extensions/OrientationExtensions.cs
@@ -9,6 +9,19 @@ public static class OrientationExtensions {
       _ => 0,
     };
 
+  public static Orientation Rotate(this Orientation orientation, Orientation rotation) {
+    var angle = orientation.ToAngle() + rotation.ToAngle();
+    if (angle >= 360) angle -= 360;
+
+    return angle switch {
+      0 => Orientation.Normal,
+      90 => Orientation.Rotate90,
+      180 => Orientation.Rotate180,
+      270 => Orientation.Rotate270,
+      _ => Orientation.Normal
+    };
+  }
+
   public static Orientation SwapRotateIf(this Orientation orientation, bool swap) =>
     swap
       ? orientation switch {
