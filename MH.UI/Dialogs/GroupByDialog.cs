972e49bf3d39aee9e70cc38e3b48c0bb27c2aa92
martin-holy <martin.holy@hotmail.com>
2024-02-02 16:32:52 +0100

    [C] DialogButton and RelayCommand

--- MH.UI/Dialogs/GroupByDialog.cs
+++ MH.UI/Dialogs/GroupByDialog.cs
@@ -5,53 +5,53 @@ using MH.Utils.Interfaces;
 using System.Collections.Generic;
 using System.Linq;
 
-namespace MH.UI.Dialogs {
-  public class GroupByDialog<T> : Dialog where T : class, ISelectable {
-    private bool _isRecursive;
-    private bool _isGroupBy = true;
-    private bool _isThenBy;
+namespace MH.UI.Dialogs;
 
-    public TreeView<GroupByItem<T>> TreeView { get; } = new() { ShowTreeItemSelection = true };
-    public bool IsRecursive { get => _isRecursive; set { _isRecursive = value; OnPropertyChanged(); } }
-    public bool IsGroupBy { get => _isGroupBy; set { _isGroupBy = value; OnPropertyChanged(); } }
-    public bool IsThenBy { get => _isThenBy; set { _isThenBy = value; OnPropertyChanged(); } }
+public class GroupByDialog<T> : Dialog where T : class, ISelectable {
+  private bool _isRecursive;
+  private bool _isGroupBy = true;
+  private bool _isThenBy;
 
-    public GroupByDialog() : base("Chose items for grouping", "IconGroup") {
-      Buttons = new DialogButton[] {
-        new("Ok", "IconCheckMark", YesOkCommand, true),
-        new("Cancel", "IconXCross", CloseCommand, false, true) };
-    }
+  public TreeView<GroupByItem<T>> TreeView { get; } = new() { ShowTreeItemSelection = true };
+  public bool IsRecursive { get => _isRecursive; set { _isRecursive = value; OnPropertyChanged(); } }
+  public bool IsGroupBy { get => _isGroupBy; set { _isGroupBy = value; OnPropertyChanged(); } }
+  public bool IsThenBy { get => _isThenBy; set { _isThenBy = value; OnPropertyChanged(); } }
 
-    public bool Open(CollectionViewGroup<T> group, IEnumerable<GroupByItem<T>> items) {
-      IsRecursive = group.IsRecursive;
-      IsGroupBy = group.IsGroupBy;
-      IsThenBy = group.IsThenBy;
-      TreeView.RootHolder.Clear();
-      TreeView.SelectedTreeItems.DeselectAll();
+  public GroupByDialog() : base("Chose items for grouping", "IconGroup") {
+    Buttons = new DialogButton[] {
+      new(OkCommand, true),
+      new(CancelCommand, false, true) };
+  }
 
-      foreach (var item in items)
-        TreeView.RootHolder.Add(item);
+  public bool Open(CollectionViewGroup<T> group, IEnumerable<GroupByItem<T>> items) {
+    IsRecursive = group.IsRecursive;
+    IsGroupBy = group.IsGroupBy;
+    IsThenBy = group.IsThenBy;
+    TreeView.RootHolder.Clear();
+    TreeView.SelectedTreeItems.DeselectAll();
 
-      if (Show(this) != 1) return false;
+    foreach (var item in items)
+      TreeView.RootHolder.Add(item);
 
-      group.IsGroupingRoot = true;
+    if (Show(this) != 1) return false;
 
-      if (TreeView.SelectedTreeItems.Items.Count == 0) {
-        group.GroupByItems = null;
-        group.Items.Clear();
-        group.ReWrap();
-        return true;
-      }
+    group.IsGroupingRoot = true;
 
-      group.IsRecursive = IsRecursive;
-      group.IsGroupBy = IsGroupBy;
-      group.IsThenBy = IsThenBy;
-      group.GroupByItems = TreeView.SelectedTreeItems.Items.ToArray();
-      group.GroupIt();
-      group.View.RemoveEmptyGroups(group, null);
-      group.IsExpanded = true;
-
+    if (TreeView.SelectedTreeItems.Items.Count == 0) {
+      group.GroupByItems = null;
+      group.Items.Clear();
+      group.ReWrap();
       return true;
     }
+
+    group.IsRecursive = IsRecursive;
+    group.IsGroupBy = IsGroupBy;
+    group.IsThenBy = IsThenBy;
+    group.GroupByItems = TreeView.SelectedTreeItems.Items.ToArray();
+    group.GroupIt();
+    group.View.RemoveEmptyGroups(group, null);
+    group.IsExpanded = true;
+
+    return true;
   }
 }
