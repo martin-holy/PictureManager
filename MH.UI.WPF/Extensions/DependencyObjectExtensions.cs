37ac450096fc1dc6b1df4aa7913a7da43e316329
martin-holy <martin.holy@hotmail.com>
2024-02-03 15:10:30 +0100

    [N] DependencyObjectExtensions HasAttachedProperty

---
+++ MH.UI.WPF/Extensions/DependencyObjectExtensions.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel;
+using System.Windows;
+
+namespace MH.UI.WPF.Extensions;
+
+public static class DependencyObjectExtensions {
+  public static bool HasAttachedProperty(this DependencyObject obj, DependencyProperty property) =>
+    (TypeDescriptor.GetProperties(obj).Find(property.Name, false) as DependencyPropertyDescriptor)
+    ?.DependencyProperty == property;
+}
