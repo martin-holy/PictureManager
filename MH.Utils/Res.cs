4e28360a719c93f31829495e1903da6d3c2010dc
martin-holy <martin.holy@hotmail.com>
2024-02-03 22:44:28 +0100

    [N] Res icons

--- MH.Utils/Res.cs
+++ MH.Utils/Res.cs
@@ -2,5 +2,18 @@
 
 public static class Res {
   public const string IconCheckMark = "IconCheckMark";
+  public const string IconChevronDown = "IconChevronDown";
+  public const string IconChevronLeft = "IconChevronLeft";
+  public const string IconChevronRight = "IconChevronRight";
+  public const string IconImage = "IconImage";
+  public const string IconMovieClapper = "IconMovieClapper";
+  public const string IconPause = "IconPause";
+  public const string IconPlay = "IconPlay";
+  public const string IconTimelineShiftBeginning = "IconTimelineShift.Beginning";
+  public const string IconTimelineShiftEnd = "IconTimelineShift.End";
+  public const string IconTimelineShiftLargeBack = "IconTimelineShift.LargeBack";
+  public const string IconTimelineShiftLargeForward = "IconTimelineShift.LargeForward";
+  public const string IconTimelineShiftSmallBack = "IconTimelineShift.SmallBack";
+  public const string IconTimelineShiftSmallForward = "IconTimelineShift.SmallForward";
   public const string IconXCross = "IconXCross";
 }
