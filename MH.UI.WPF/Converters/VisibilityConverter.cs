92f4dff01cfbe3c1a1277675d7cb793225702957
martin-holy <martin.holy@hotmail.com>
2024-02-09 22:14:42 +0100

    [N] VisibilityConverter ToHidden

--- MH.UI.WPF/Converters/VisibilityConverter.cs
+++ MH.UI.WPF/Converters/VisibilityConverter.cs
@@ -6,11 +6,17 @@ namespace MH.UI.WPF.Converters;
 public class VisibilityConverter : BaseConverter {
   private static readonly object _lock = new();
   private static VisibilityConverter _allToCollapsed;
+  private static VisibilityConverter _allToHidden;
   private static VisibilityConverter _allToVisible;
 
   public static VisibilityConverter AllToCollapsed { get {
     lock (_lock) {
       return _allToCollapsed ??= new() { ToCollapsed = true };
+    } } }
+
+  public static VisibilityConverter AllToHidden { get {
+    lock (_lock) {
+      return _allToHidden ??= new() { ToHidden = true };
     } } }
 
   public static VisibilityConverter AllToVisible { get {
@@ -19,15 +25,18 @@ public class VisibilityConverter : BaseConverter {
     } } }
 
   public bool ToCollapsed { get; init; }
+  public bool ToHidden { get; init; }
   public bool ToVisible { get; init; }
 
   public override object Convert(object value, object parameter) {
     if (AllToBoolConverter.AllToBool(value, parameter)) {
       if (ToCollapsed) return Visibility.Collapsed;
+      if (ToHidden) return Visibility.Hidden;
       if (ToVisible) return Visibility.Visible;
     }
     else {
       if (ToCollapsed) return Visibility.Visible;
+      if (ToHidden) return Visibility.Visible;
       if (ToVisible) return Visibility.Collapsed;
     }
 
