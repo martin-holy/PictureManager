276d058472e09ec0324de88c1f2e0bde6fbe2e1d
martin-holy <martin.holy@hotmail.com>
2024-02-11 16:27:42 +0100

    [U] SelectionRange

--- MH.Utils/BaseClasses/SelectionRange.cs
+++ MH.Utils/BaseClasses/SelectionRange.cs
@@ -1,43 +1,48 @@
-﻿namespace MH.Utils.BaseClasses {
-  public class SelectionRange : ObservableObject {
-    private double _min;
-    private double _max;
-    private double _start;
-    private double _end;
+﻿using System;
 
-    public double Min { get => _min; set { _min = value; OnPropertyChanged(); } }
-    public double Max { get => _max; set { _max = value; OnPropertyChanged(); } }
-    public double Start { get => _start; set { _start = value; OnPropertyChanged(); } }
-    public double End { get => _end; set { _end = value; OnPropertyChanged(); } }
+namespace MH.Utils.BaseClasses;
 
-    public SelectionRange() { }
+public class SelectionRange : ObservableObject {
+  private double _min;
+  private double _max;
+  private double _start;
+  private double _end;
 
-    public void CoerceValues(bool maxSelection) {
-      if (maxSelection) {
-        Start = Min;
-        End = Max;
+  public double Min { get => _min; set { _min = value; OnPropertyChanged(); OnPropertyChanged(nameof(IsOnMin)); } }
+  public double Max { get => _max; set { _max = value; OnPropertyChanged(); OnPropertyChanged(nameof(IsOnMax)); } }
+  public double Start { get => _start; set { _start = value; OnPropertyChanged(); OnPropertyChanged(nameof(IsOnMin)); } }
+  public double End { get => _end; set { _end = value; OnPropertyChanged(); OnPropertyChanged(nameof(IsOnMax)); } }
+  public bool IsOnMin => Min == Start;
+  public bool IsOnMax => Max == End;
+  public bool IsFullRange => IsOnMin && IsOnMax;
 
-        return;
-      }
+  public event EventHandler ChangedEvent = delegate { };
 
-      if (Start < Min) Start = Min;
-      if (Start > Max) Start = Max;
-      if (End < Min) End = Min;
-      if (End > Max) End = Max;
-      if (End == 0) End = Max;
+  public void CoerceValues(bool maxSelection) {
+    if (maxSelection) {
+      Start = Min;
+      End = Max;
+
+      return;
     }
 
-    public bool Fits(double value) =>
-      value >= Start && value <= End;
-
-    public bool MaxSelection() =>
-      Min == Start && End == Max;
-
-    public void Zero() {
-      Min = 0;
-      Max = 0;
-      Start = 0;
-      End = 0;
-    }
+    if (Start < Min) Start = Min;
+    if (Start > Max) Start = Max;
+    if (End < Min) End = Min;
+    if (End > Max) End = Max;
+    if (End == 0) End = Max;
   }
+
+  public bool Fits(double value) =>
+    value >= Start && value <= End;
+
+  public void Zero() {
+    Start = 0;
+    End = 0;
+    Min = 0;
+    Max = 0;
+  }
+
+  public void RaiseChangedEvent() =>
+    ChangedEvent(this, EventArgs.Empty);
 }
