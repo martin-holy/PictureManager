fabf5836535858e188e516806a2cbab25586c3b4
martin-holy <martin.holy@hotmail.com>
2024-02-11 16:27:02 +0100

    [N] DoubleExtensions RoundTo

---
+++ MH.Utils/Extensions/DoubleExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MH.Utils.Extensions;
+
+public static class DoubleExtensions {
+  public static double RoundTo(this double value, double maxDigits) {
+    var precision = 0;
+    while (maxDigits * Math.Pow(10, precision) != Math.Round(maxDigits * Math.Pow(10, precision)))
+      precision++;
+
+    return Math.Round(Math.Round(value / maxDigits, 0) * maxDigits, precision);
+  }
+}
