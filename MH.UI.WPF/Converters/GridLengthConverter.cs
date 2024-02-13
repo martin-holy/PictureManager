abd7efb44218b034d9ab0b97bef23c56d06ee82d
martin-holy <martin.holy@hotmail.com>
2024-02-13 17:15:41 +0100

    [N] GridLengthConverter

---
+++ MH.UI.WPF/Converters/GridLengthConverter.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace MH.UI.WPF.Converters;
+
+public class GridLengthConverter : BaseConverter {
+  private static readonly object _lock = new();
+  private static GridLengthConverter _inst;
+  public static GridLengthConverter Inst { get { lock (_lock) { return _inst ??= new(); } } }
+
+  public override object Convert(object value, object parameter) =>
+    new GridLength((double)value);
+
+  public override object ConvertBack(object value, object parameter) =>
+    ((GridLength)value).Value;
+}
