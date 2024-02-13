b48480237f22d1610ed73d1d3d5ff575604c4666
martin-holy <martin.holy@hotmail.com>
2024-02-13 17:12:25 +0100

    [N] ObservableObject SetIfVary method

--- MH.Utils/BaseClasses/ObservableObject.cs
+++ MH.Utils/BaseClasses/ObservableObject.cs
@@ -1,9 +1,18 @@
-﻿using System.ComponentModel;
+﻿using System.Collections.Generic;
+using System.ComponentModel;
 using System.Runtime.CompilerServices;
 
-namespace MH.Utils.BaseClasses {
-  public class ObservableObject : INotifyPropertyChanged {
-    public event PropertyChangedEventHandler PropertyChanged = delegate { };
-    public void OnPropertyChanged([CallerMemberName] string name = null) => PropertyChanged.Invoke(this, new(name));
+namespace MH.Utils.BaseClasses;
+
+public class ObservableObject : INotifyPropertyChanged {
+  public event PropertyChangedEventHandler PropertyChanged = delegate { };
+  public void OnPropertyChanged([CallerMemberName] string name = null) =>
+    PropertyChanged.Invoke(this, new(name));
+
+  protected bool SetIfVary<T>(ref T field, T value, [CallerMemberName] string name = null) {
+    if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+    field = value;
+    OnPropertyChanged(name);
+    return true;
   }
 }
