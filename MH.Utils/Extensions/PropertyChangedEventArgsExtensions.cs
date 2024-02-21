a3c331ab677b47b04d3f2a9e51c9ae13b55922db
martin-holy <martin.holy@hotmail.com>
2024-02-21 23:56:22 +0100

    [N] PropertyChangedEventArgsExtensions Is method

---
+++ MH.Utils/Extensions/PropertyChangedEventArgsExtensions.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace MH.Utils.Extensions;
+
+public static class PropertyChangedEventArgsExtensions {
+  public static bool Is(this PropertyChangedEventArgs e, string name) =>
+    name.Equals(e.PropertyName, StringComparison.Ordinal);
+}
