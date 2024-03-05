ee288633c8feaea3dda8e8599a24882900f3a44d
martin-holy <martin.holy@hotmail.com>
2024-03-05 17:25:26 +0100

    [C] CopyMove rewrite

--- PictureManager.Domain/Dialogs/FileOperationCollisionDialogM.cs
+++ PictureManager.Domain/Dialogs/FileOperationCollisionDialogM.cs
@@ -1,102 +1,124 @@
 ﻿using MH.Utils;
 using MH.Utils.BaseClasses;
 using MH.Utils.Dialogs;
-using PictureManager.Domain.HelperClasses;
+using MH.Utils.Extensions;
 using PictureManager.Domain.Models;
 using PictureManager.Domain.Models.MediaItems;
-using PictureManager.Domain.Services;
+using PictureManager.Domain.Utils;
 using System.IO;
 using System.Linq;
 
-namespace PictureManager.Domain.Dialogs {
-    public sealed class FileOperationCollisionDialogM : Dialog {
-    private string _error;
-    private string _fileName;
-    private FileInfo _srcFileInfo;
-    private FileInfo _destFileInfo;
-    private RealMediaItemM _srcMediaItem;
-    private RealMediaItemM _destMediaItem;
+namespace PictureManager.Domain.Dialogs;
 
-    public string Error { get => _error; set { _error = value; OnPropertyChanged(); } }
-    public string FileName { get => _fileName; set { _fileName = value; OnPropertyChanged(); } }
-    public FileInfo SrcFileInfo { get => _srcFileInfo; set { _srcFileInfo = value; OnPropertyChanged(); } }
-    public FileInfo DestFileInfo { get => _destFileInfo; set { _destFileInfo = value; OnPropertyChanged(); } }
-    public RealMediaItemM SrcMediaItem { get => _srcMediaItem; set { _srcMediaItem = value; OnPropertyChanged(); } }
-    public RealMediaItemM DestMediaItem { get => _destMediaItem; set { _destMediaItem = value; OnPropertyChanged(); } }
+public sealed class FileOperationCollisionDialogM : Dialog {
+  private readonly FolderM _destFolder;
 
-    public FileOperationCollisionDialogM(string srcFilePath, string destFilePath) : base("The destination already has a file with this name", Res.IconImageMultiple) {
-      SrcFileInfo = new(srcFilePath);
-      DestFileInfo = new(destFilePath);
-      SrcMediaItem = GetMediaItem(srcFilePath);
-      DestMediaItem = GetMediaItem(destFilePath);
-      FileName = SrcFileInfo.Name;
+  private string _error;
+  private string _fileName;
+  private RealMediaItemM _srcMediaItem;
+  private RealMediaItemM _destMediaItem;
+  private string _srcPath;
+  private string _destPath;
+  private string _srcSize;
+  private string _destSize;
+  private string _srcLastWrite;
+  private string _destLastWrite;
 
-      Buttons = new DialogButton[] {
-        new(new (Rename, null, "Rename")),
-        new(new (() => Result = (int)CollisionResult.Replace, null, "Replace")),
-        new(new (() => Result = (int)CollisionResult.Skip, null, "Skip")) }; 
+  public string Error { get => _error; set { _error = value; OnPropertyChanged(); } }
+  public string FileName { get => _fileName; set { _fileName = value; OnPropertyChanged(); } }
+  public RealMediaItemM SrcMediaItem { get => _srcMediaItem; set { _srcMediaItem = value; OnPropertyChanged(); } }
+  public RealMediaItemM DestMediaItem { get => _destMediaItem; set { _destMediaItem = value; OnPropertyChanged(); } }
+  public string SrcPath { get => _srcPath; set { _srcPath = value; OnPropertyChanged(); } }
+  public string DestPath { get => _destPath; set { _destPath = value; OnPropertyChanged(); } }
+  public string SrcSize { get => _srcSize; set { _srcSize = value; OnPropertyChanged(); } }
+  public string DestSize { get => _destSize; set { _destSize = value; OnPropertyChanged(); } }
+  public string SrcLastWrite { get => _srcLastWrite; set { _srcLastWrite = value; OnPropertyChanged(); } }
+  public string DestLastWrite { get => _destLastWrite; set { _destLastWrite = value; OnPropertyChanged(); } }
+
+  public FileOperationCollisionDialogM(FolderM src, FolderM dest, RealMediaItemM srcMi, string fileName) : base("The destination already has a file with this name", Res.IconImageMultiple) {
+    _destFolder = dest;
+    SetInfo(IOExtensions.PathCombine(src.FullPath, fileName), true);
+    SetInfo(IOExtensions.PathCombine(dest.FullPath, fileName), false);
+    SrcMediaItem = srcMi;
+    DestMediaItem = GetMediaItem(dest, fileName);
+    FileName = fileName;
+
+    Buttons = new DialogButton[] {
+      new(new (Rename, null, "Rename")),
+      new(new (() => Result = (int)CollisionResult.Replace, null, "Replace")),
+      new(new (() => Result = (int)CollisionResult.Skip, null, "Skip")) }; 
+  }
+
+  public static CollisionResult Open(FolderM src, FolderM dest, RealMediaItemM srcMi, ref string fileName, ref RealMediaItemM replacedMi) {
+    var result = CollisionResult.Skip;
+    var outFileName = fileName;
+    var outReplacedMi = replacedMi;
+
+    InitThumb(srcMi);
+
+    Tasks.RunOnUiThread(() => {
+      var cd = new FileOperationCollisionDialogM(src, dest, srcMi, outFileName);
+      result = (CollisionResult)Show(cd);
+      outFileName = cd.FileName;
+      outReplacedMi = cd.DestMediaItem;
+    }).GetAwaiter().GetResult();
+
+    fileName = outFileName;
+    replacedMi = outReplacedMi;
+
+    return result;
+  }
+
+  private static void InitThumb(RealMediaItemM mi) {
+    if (mi == null) return;
+    mi.SetInfoBox();
+    mi.SetThumbSize();
+  }
+
+  private static RealMediaItemM GetMediaItem(FolderM folder, string fileName) {
+    var mi = folder.MediaItems.GetByFileName(fileName)
+             ?? CopyMoveU.CreateMediaItemAndReadMetadata(folder, fileName);
+
+    InitThumb(mi);
+    return mi;
+  }
+
+  private void Rename() {
+    Error = string.Empty;
+
+    if (string.IsNullOrEmpty(FileName)) {
+      Error = "New file name is empty!";
+      return;
     }
 
-    public static CollisionResult Open(string srcFilePath, string destFilePath, ref string fileName) {
-      var result = CollisionResult.Skip;
-      var outFileName = fileName;
-
-      Tasks.RunOnUiThread(() => {
-        var cd = new FileOperationCollisionDialogM(srcFilePath, destFilePath);
-        result = (CollisionResult)Show(cd);
-        outFileName = cd.FileName;
-      }).GetAwaiter().GetResult();
-
-      fileName = outFileName;
-
-      return result == 0 ? CollisionResult.Skip : result;
+    if (Path.GetInvalidFileNameChars().Any(FileName.Contains)) {
+      Error = "New file name contains incorrect character(s)!";
+      return;
     }
 
-    private RealMediaItemM GetMediaItem(string filePath) {
-      var mi = GetMediaItemByPath(filePath);
-      mi.SetInfoBox();
-      mi.SetThumbSize();
-      return mi;
+    var newFilePath = Path.Combine(_destFolder.FullPath, FileName);
+    if (File.Exists(newFilePath)) {
+      SetInfo(newFilePath, false);
+      DestMediaItem = GetMediaItem(_destFolder, FileName);
+      return;
     }
 
-    private RealMediaItemM GetMediaItemByPath(string path) {
-      var lioSep = path.LastIndexOf(Path.DirectorySeparatorChar);
-      var folderPath = path[..lioSep];
-      var fileName = path[(lioSep + 1)..];
-      var folder = Tree.GetByPath(Core.R.Folder.Tree, folderPath, Path.DirectorySeparatorChar) as FolderM;
-      var mi = folder?.MediaItems.GetByFileName(fileName);
+    Result = (int)CollisionResult.Rename;
+  }
 
-      if (mi != null) return mi;
-
-      mi = Core.R.MediaItem.ItemCreate(folder, fileName);
-      var mim = new MediaItemMetadata(mi);
-      MediaItemS.ReadMetadata(mim, false);
-      if (mim.Success) mim.FindRefs().Wait();
-
-      return mi;
+  private void SetInfo(string path, bool src) {
+    var fi = new FileInfo(path);
+    var size = fi.Length.ToString("0 B");
+    var lastWrite = fi.LastWriteTime.ToString("G");
+    if (src) {
+      SrcPath = fi.FullName;
+      SrcSize = size;
+      SrcLastWrite = lastWrite;
     }
-
-    private void Rename() {
-      Error = string.Empty;
-
-      if (string.IsNullOrEmpty(FileName)) {
-        Error = "New file name is empty!";
-        return;
-      }
-
-      if (Path.GetInvalidFileNameChars().Any(FileName.Contains)) {
-        Error = "New file name contains incorrect character(s)!";
-        return;
-      }
-
-      var newFilePath = Path.Combine(DestFileInfo.DirectoryName, FileName);
-      if (File.Exists(newFilePath)) {
-        DestFileInfo = new(newFilePath);
-        DestMediaItem = GetMediaItem(newFilePath);
-        return;
-      }
-
-      Result = (int)CollisionResult.Rename;
+    else {
+      DestPath = fi.FullName;
+      DestSize = size;
+      DestLastWrite = lastWrite;
     }
   }
 }
