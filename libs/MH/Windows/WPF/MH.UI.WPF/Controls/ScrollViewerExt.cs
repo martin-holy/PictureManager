3e9ada819b5e2f7b783953ea8dc2c9d820bde728
martin-holy <martin.holy@hotmail.com>
2024-03-12 22:10:22 +0100

    [N] ScrollViewExt with ScrollBar VetricalTopContent

---
+++ libs/MH/Windows/WPF/MH.UI.WPF/Controls/ScrollViewerExt.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace MH.UI.WPF.Controls;
+
+public class ScrollViewerExt : ScrollViewer {
+  public static readonly DependencyProperty VerticalTopContentProperty = DependencyProperty.Register(
+    nameof(VerticalTopContent), typeof(object), typeof(ScrollViewerExt));
+
+  public object VerticalTopContent {
+    get => GetValue(VerticalTopContentProperty);
+    set => SetValue(VerticalTopContentProperty, value);
+  }
+}
