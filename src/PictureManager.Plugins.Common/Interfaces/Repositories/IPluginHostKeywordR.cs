6a0c489fc8edc6acfe1cdaaefbc22d3271c93047
martin-holy <martin.holy@hotmail.com>
2024-03-18 16:20:35 +0100

    [C] MovieManager update

---
+++ src/PictureManager.Plugins.Common/Interfaces/Repositories/IPluginHostKeywordR.cs
@@ -0,0 +1,9 @@
+﻿using MH.Utils.Interfaces;
+using PictureManager.Plugins.Common.Interfaces.Models;
+using System.Collections.Generic;
+
+namespace PictureManager.Plugins.Common.Interfaces.Repositories;
+
+public interface IPluginHostKeywordR {
+  public List<IPluginHostKeywordM> Link(string csv, IDataAdapter seeker);
+}
---
+++ src/PictureManager.Plugins.Common/Interfaces/Repositories/IPluginHostPersonR.cs
@@ -0,0 +1,9 @@
+﻿using MH.Utils.Interfaces;
+using PictureManager.Plugins.Common.Interfaces.Models;
+using System.Collections.Generic;
+
+namespace PictureManager.Plugins.Common.Interfaces.Repositories;
+
+public interface IPluginHostPersonR {
+  public List<IPluginHostPersonM> Link(string csv, IDataAdapter seeker);
+}
