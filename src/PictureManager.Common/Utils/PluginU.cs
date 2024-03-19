24b3ad94e8f6236b1d3c58f55b410607a68f59af
martin-holy <martin.holy@hotmail.com>
2024-03-19 18:46:39 +0100

    [U] LoadPlugins and MainMenu commands

---
+++ src/PictureManager.Common/Utils/PluginU.cs
@@ -0,0 +1,46 @@
+﻿using MH.Utils;
+using PictureManager.Plugins.Common.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Loader;
+
+namespace PictureManager.Common.Utils;
+
+public static class PluginU {
+  public static IPluginCore GetPluginCore(string pluginName) {
+    try {
+      var asm = LoadAssembly(Path.GetFullPath($"plugins\\{pluginName}\\{pluginName}.Common.dll"));
+      if (GetFirstTypeWithInterface(asm.GetTypes(), nameof(IPluginCore)) is not { } type) return null;
+      return Activator.CreateInstance(type) as IPluginCore;
+    }
+    catch (Exception ex) {
+      Log.Error(ex);
+      return null;
+    }
+  }
+
+  private static Type GetFirstTypeWithInterface(IEnumerable<Type> types, string name) =>
+    types.FirstOrDefault(type => type.GetInterface(name) is not null);
+
+  private static Assembly LoadAssembly(string path) {
+    var loadContext = new PluginLoadContext(path);
+    return loadContext.LoadFromAssemblyName(AssemblyName.GetAssemblyName(path));
+  }
+}
+
+public class PluginLoadContext(string pluginPath) : AssemblyLoadContext {
+  private readonly AssemblyDependencyResolver _resolver = new(pluginPath);
+
+  protected override Assembly Load(AssemblyName name) {
+    var path = _resolver.ResolveAssemblyToPath(name);
+    return path == null ? null : LoadFromAssemblyPath(path);
+  }
+
+  protected override IntPtr LoadUnmanagedDll(string name) {
+    var path = _resolver.ResolveUnmanagedDllToPath(name);
+    return path == null ? IntPtr.Zero : LoadUnmanagedDllFromPath(path);
+  }
+}
---
+++ src/PictureManager.Plugins.Common/Interfaces/ViewModels/IPluginCoreVM.cs
@@ -0,0 +1,10 @@
+﻿using MH.Utils.BaseClasses;
+using System.Collections.Generic;
+
+namespace PictureManager.Plugins.Common.Interfaces.ViewModels;
+
+public interface IPluginCoreVM {
+  public string PluginIcon { get; }
+  public string PluginTitle { get; }
+  public List<RelayCommand> MainMenuCommands { get; }
+}
