656940b4b9dd439f0db6b2a21b95b5a5a58570d9
martin-holy <martin.holy@hotmail.com>
2024-03-20 22:28:58 +0100

    [U] PluginsMenu

---
+++ src/PictureManager.Common/HelperClasses/GenericTypes.cs
@@ -0,0 +1,9 @@
+﻿using PictureManager.Plugins.Common.Interfaces;
+using System;
+using System.Collections.Generic;
+
+namespace PictureManager.Common.HelperClasses;
+
+public static class GenericTypes {
+  public static Type ListOfIPluginCore => typeof(List<IPluginCore>);
+}
