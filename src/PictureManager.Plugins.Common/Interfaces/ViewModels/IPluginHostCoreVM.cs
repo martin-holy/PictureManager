7d2472deeacfac54e2ac2de932b8cf8f1f1245d3
martin-holy <martin.holy@hotmail.com>
2024-03-21 12:42:43 +0100

    [C] MovieManager update

--- src/PictureManager.Plugins.Common/Interfaces/ViewModels/IPluginHostCoreVM.cs
+++ src/PictureManager.Plugins.Common/Interfaces/ViewModels/IPluginHostCoreVM.cs
@@ -4,4 +4,5 @@ namespace PictureManager.Plugins.Common.Interfaces.ViewModels;
 
 public interface IPluginHostCoreVM {
   public TabControl MainTabs { get; }
+  public TabControl ToolsTabs { get; }
 }
