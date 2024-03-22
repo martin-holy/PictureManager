b7089d5e6d744d86577103d6daf068bd756feb2c
martin-holy <martin.holy@hotmail.com>
2024-03-22 16:46:24 +0100

    [U] PopupSlider

---
+++ libs/MH/Windows/WPF/MH.UI.WPF/Controls/PopupSlider.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+
+namespace MH.UI.WPF.Controls;
+
+public class PopupSlider : Slider {
+  public static readonly DependencyProperty ContentProperty = DependencyProperty.Register(
+    nameof(Content), typeof(Button), typeof(PopupSlider));
+
+  public Button Content {
+    get => (Button)GetValue(ContentProperty);
+    set => SetValue(ContentProperty, value);
+  }
+
+  public override void OnApplyTemplate() {
+    base.OnApplyTemplate();
+
+    if (Content == null || GetTemplateChild("PART_Popup") is not Popup popup) return;
+
+    Content.Click += delegate { popup.IsOpen = true; };
+    popup.PreviewMouseUp += delegate { popup.IsOpen = false; };
+    popup.CustomPopupPlacementCallback += (size, targetSize, _) => {
+      var x = targetSize.Width / 2 - size.Width / 2;
+      return new[] { new CustomPopupPlacement(new(x, targetSize.Height), PopupPrimaryAxis.Vertical) };
+    };
+  }
+}
