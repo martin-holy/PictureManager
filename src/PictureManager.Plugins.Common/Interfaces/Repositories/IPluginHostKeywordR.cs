137bb701596739ab84ceb73fa7465ff5f6916487
martin-holy <martin.holy@hotmail.com>
2024-03-23 13:37:33 +0100

    [U] Movie keywords import

---
+++ src/PictureManager.Plugins.Common/Interfaces/Repositories/IPluginHostKeywordR.cs
@@ -0,0 +1,10 @@
+﻿using MH.Utils.Interfaces;
+using PictureManager.Plugins.Common.Interfaces.Models;
+using System.Collections.Generic;
+
+namespace PictureManager.Plugins.Common.Interfaces.Repositories;
+
+public interface IPluginHostKeywordR : IPluginHostR<IPluginHostKeywordM> {
+  public IPluginHostKeywordM GetByFullPath(string fullPath, IEnumerable<ITreeItem> src = null, ITreeItem rootForNew = null);
+  public ITreeItem GetCategoryGroup(string name);
+}
--- src/PictureManager.Plugins.Common/Interfaces/Repositories/IPluginHostR.cs
+++ src/PictureManager.Plugins.Common/Interfaces/Repositories/IPluginHostR.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 namespace PictureManager.Plugins.Common.Interfaces.Repositories;
 
-public interface IPluginHostRepository<T> {
+public interface IPluginHostR<T> {
   public T GetById(string id, bool nullable = false);
   public List<T> Link(string csv, IDataAdapter seeker);
 }
