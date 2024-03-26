a0226d8b05dbd0e4162e11cf177cb77eb9fe1ac6
martin-holy <martin.holy@hotmail.com>
2024-03-26 23:15:43 +0100

    [N] MovieManager.Plugins.IMDbAPIdev

---
+++ plugins/MovieManager/plugins/src/MovieManager.Plugins.IMDbAPIdev/Core.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace MovieManager.Plugins.IMDbAPIdev;
+
+public class Core {
+  public void GetMovieById(string imdbId) {
+    var query = $$"""
+                  query titleById {
+                    title(id: "{{imdbId}}") {
+                      id
+                      type
+                      start_year
+                      plot
+                      genres
+                      rating {
+                        aggregate_rating
+                        votes_count
+                      }
+                    }
+                  }
+                  """;
+
+    ExecuteGraphQLQuery(query).ContinueWith(response => {
+
+    });
+  }
+
+  private static async Task<string> ExecuteGraphQLQuery(string query) {
+    var httpClient = new HttpClient();
+    var request = new HttpRequestMessage {
+      RequestUri = new("https://graph.imdbapi.dev/v1"),
+      Method = HttpMethod.Post,
+      Content = new StringContent(JsonSerializer.Serialize(new { query }), Encoding.UTF8, "application/json")
+    };
+
+    var httpResponse = await httpClient.SendAsync(request);
+
+    if (!httpResponse.IsSuccessStatusCode) {
+      throw new Exception($"GraphQL request failed with status code {httpResponse.StatusCode}");
+    }
+
+    var responseContent = await httpResponse.Content.ReadAsStringAsync();
+    var jsonDocument = JsonDocument.Parse(responseContent);
+    var responseData = jsonDocument.RootElement.GetProperty("data").ToString();
+
+    return responseData;
+  }
+}
