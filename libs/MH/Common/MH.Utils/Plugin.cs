5c1087cc858dd60c2c85433c142aaa79fa1db34f
martin-holy <martin.holy@hotmail.com>
2024-03-29 17:04:41 +0100

    [U] Plugin

--- libs/MH/Common/MH.Utils/Plugin.cs
+++ libs/MH/Common/MH.Utils/Plugin.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Runtime.Loader;
@@ -7,19 +6,17 @@ using System.Runtime.Loader;
 namespace MH.Utils;
 
 public static class Plugin {
-  public static T Load<T>(string path) where T : class {
-    var asm = LoadAssembly(path);
-    if (GetFirstTypeWithInterface(asm.GetTypes(), nameof(T)) is not { } type) return default;
-    return Activator.CreateInstance(type) as T;
+  public static T LoadPlugin<T>(string path) {
+    var asm = Assembly.LoadFrom(path);
+    var targetType = typeof(T);
+    var pluginType = asm.GetTypes().FirstOrDefault(targetType.IsAssignableFrom);
+
+    return pluginType == null ? default : (T)Activator.CreateInstance(pluginType);
   }
 
-  private static Type GetFirstTypeWithInterface(IEnumerable<Type> types, string name) =>
-    types.FirstOrDefault(type => type.GetInterface(name) is not null);
-
-  public static Assembly LoadAssembly(string path) {
-    var loadContext = new PluginLoadContext(path);
-    return loadContext.LoadFromAssemblyName(AssemblyName.GetAssemblyName(path));
-  }
+  // TODO I can't get this to work
+  public static Assembly LoadAssembly(string path) =>
+    new PluginLoadContext(path).LoadFromAssemblyName(AssemblyName.GetAssemblyName(path));
 }
 
 public class PluginLoadContext(string pluginPath) : AssemblyLoadContext {
