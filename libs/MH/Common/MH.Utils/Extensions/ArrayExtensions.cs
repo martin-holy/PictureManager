331c46f9242524e4115565e7fa7604821eace61a
martin-holy <martin.holy@hotmail.com>
2024-04-04 12:17:59 +0200

    [N] ArrayExtensions ForEach

--- libs/MH/Common/MH.Utils/Extensions/ArrayExtensions.cs
+++ libs/MH/Common/MH.Utils/Extensions/ArrayExtensions.cs
@@ -1,6 +1,13 @@
-﻿namespace MH.Utils.Extensions;
+﻿using System;
+
+namespace MH.Utils.Extensions;
 
 public static class ArrayExtensions {
   public static T[] NullIfEmpty<T>(this T[] self) =>
     self?.Length > 0 ? self : null;
+
+  public static void ForEach<T>(this T[] self, Action<T> action) {
+    for (int i = 0; i < self.Length; i++)
+      action(self[i]);
+  }
 }
