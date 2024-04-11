7e176f8d521ff0360d5fd6276ebea5eecd6f85f6
martin-holy <martin.holy@hotmail.com>
2024-04-11 11:54:13 +0200

    [U] ToggleDialog

--- src/PictureManager.Plugins.Common/Interfaces/ViewModels/IPluginHostCoreVM.cs
+++ src/PictureManager.Plugins.Common/Interfaces/ViewModels/IPluginHostCoreVM.cs
@@ -1,8 +1,10 @@
 ﻿using MH.UI.Controls;
+using MH.UI.Dialogs;
 
 namespace PictureManager.Plugins.Common.Interfaces.ViewModels;
 
 public interface IPluginHostCoreVM {
   public TabControl MainTabs { get; }
   public TabControl ToolsTabs { get; }
+  public ToggleDialog ToggleDialog { get; }
 }
