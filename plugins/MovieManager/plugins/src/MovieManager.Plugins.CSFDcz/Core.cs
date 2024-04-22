d4dc4779da911f69ec45ef5f7a6adf248459bd16
martin-holy <martin.holy@hotmail.com>
2024-04-22 19:49:36 +0200

    [U] MovieManager CSFDcz

---
+++ plugins/MovieManager/plugins/src/MovieManager.Plugins.CSFDcz/Core.cs
@@ -0,0 +1,35 @@
+﻿using MH.Utils;
+using MovieManager.Plugins.Common.Interfaces;
+using MovieManager.Plugins.Common.Models;
+using System;
+using System.Threading.Tasks;
+
+namespace MovieManager.Plugins.CSFDcz;
+
+public class Core : IPluginCore, IMovieSearchPlugin, IActorSearchPlugin, IMovieDetailPlugin {
+  public static readonly string IdName = "CSFD";
+
+  public async Task<SearchResult[]> SearchMovie(string query) {
+    var url = $"https://www.csfd.cz/hledat/?q={query.Replace(' ', '+')}&creators=0&users=0"; // TODO url
+    var content = await Common.Core.GetWebPageContent(url);
+    return content == null ? [] : Parser.ParseSearch(content);
+  }
+
+  public IActorSearchResult[] SearchActor(string query) => throw new System.NotImplementedException();
+
+  public async Task<MovieDetail> GetMovieDetail(DetailId id) {
+    if (!id.Name.Equals(IdName)) return null;
+    var url = $"https://www.imdb.com/title/{id.Id}"; // TODO url
+    var content = await Common.Core.GetWebPageContent(url);
+    if (content == null) return null;
+    
+    try {
+      return Parser.ParseMovie(content);
+    }
+    catch (Exception ex) {
+      Log.Error(ex);
+      return null;
+    }
+  }
+}
+
---
+++ plugins/MovieManager/plugins/src/MovieManager.Plugins.CSFDcz/Parser.cs
@@ -0,0 +1,31 @@
+﻿using MovieManager.Plugins.Common.Models;
+using System;
+
+namespace MovieManager.Plugins.CSFDcz;
+
+public static class Parser {
+  public static SearchResult[] ParseSearch(string text) {
+    var movies = ExtractText(text, "<section class=\"box main-movies\">", "</section>");
+    var serials = ExtractText(text, "<section class=\"box main-series\">", "</section>", movies.Item3);
+
+    return [];
+  }
+
+  public static MovieDetail ParseMovie(string text) {
+    return null;
+  }
+
+  private static Tuple<string, int, int> ExtractText(string text, string from, string to, int start = 0) {
+    var startIndex = text.IndexOf(from, start, StringComparison.Ordinal);
+    if (startIndex == -1) return null;
+
+    var endIndex = text.IndexOf(to, startIndex, StringComparison.Ordinal);
+    if (endIndex == -1) return null;
+
+    startIndex += from.Length;
+    var partLength = endIndex - startIndex;
+    var substring = text.Substring(startIndex, partLength);
+
+    return new(substring, startIndex, endIndex);
+  }
+}
