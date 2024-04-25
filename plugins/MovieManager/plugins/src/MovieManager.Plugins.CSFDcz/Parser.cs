8c7d4a78cc5adc265c21526add651e7bcb36921d
martin-holy <martin.holy@hotmail.com>
2024-04-25 12:10:01 +0200

    [U] MovieManager CSFD

--- plugins/MovieManager/plugins/src/MovieManager.Plugins.CSFDcz/Parser.cs
+++ plugins/MovieManager/plugins/src/MovieManager.Plugins.CSFDcz/Parser.cs
@@ -22,9 +22,9 @@ public static class Parser {
     var actors = ExtractUrlAndText(text.GetFromRange(new(infoRange.Item2, article.Item2)));
 
     return new() {
-      DetailId = new(ExtractId(text.GetFromRange(urlRange)), Core.IdName),
+      DetailId = new(ExtractFirstInt(text.GetFromRange(urlRange)), Core.IdName),
       Name = text.GetFromRange(nameRange),
-      Year = ExtractYear(text, yearRange),
+      Year = ExtractSearchYear(text, yearRange),
       Type = text.GetFromRange(infoRange),
       Desc = string.Join(", ", actors.Select(x => x.Item2))
     };
@@ -39,34 +39,61 @@ public static class Parser {
       DetailId = detailId
     };
 
+    // title
     if (text.GetRangeBetween("film-header-name", "<h1>", "</h1>", idx) is { } titleRange) {
       idx = titleRange.Item2;
       md.Title = text.GetFromRange(titleRange).Trim();
     }
 
+    // poster
+    if (text.GetRangeBetween("film-posters", "src=\"//", "\"", idx) is { } posterRange)
+      md.Poster = new() { Url = ExtractImage(text.GetFromRange(posterRange)) };
+
     if (!text.TryIndexOf("film-info-content", ref idx, idx)) return null;
 
-    // TODO film-posters
-
+    // rating
     if (text.GetRangeBetween("film-rating-average", ">", "</div>", idx) is { } ratingRange) {
       idx = ratingRange.Item2;
       md.Rating = ExtractRating(text, ratingRange);
     }
 
+    // genres
     if (text.GetRangeBetween("class=\"genres\"", ">", "</div>", idx) is { } genreRange) {
       idx = genreRange.Item2;
       md.Genres = ExtractGenres(text, genreRange);
     }
 
+    // year and length
+    if (text.GetRangeBetween("class=\"origin\"", "<span>", "</span>", idx) is { } yearRange) {
+      md.Year = int.TryParse(ExtractFirstInt(text.GetFromRange(yearRange)), out var yearInt) ? yearInt : 0;
+
+      if (text.GetRangeBetween(">", null, ">", yearRange.Item2) is { } lengthRange)
+        md.Runtime = int.TryParse(ExtractFirstInt(text.GetFromRange(lengthRange)), out var lengthInt) ? lengthInt : 0;
+    }
+
+    // cast
+    if (text.TryIndexOf("id=\"creators\"", ref idx, idx)) {
+      if (text.GetRangeBetween("Hrají:", ">", "</span>", idx) is { } actorsRange) {
+        var actorLinks = ExtractUrlAndText(text.GetFromRange(actorsRange));
+        md.Cast = actorLinks
+          .Select(x => new Actor {
+            DetailId = new(ExtractFirstInt(x.Item1), Core.IdName),
+            Name = x.Item2
+          })
+          .Select(x => new Cast { Actor = x, Characters = [] })
+          .ToArray();
+      }
+    }
+
     return null;
   }
 
-  private static string ExtractId(string text) {
+  private static string ExtractFirstInt(string text) {
     var match = Regex.Match(text, @"\d+");
     return match.Success ? match.Value : null;
   }
 
-  private static int ExtractYear(string text, Tuple<int, int> yearRange) =>
+  private static int ExtractSearchYear(string text, Tuple<int, int> yearRange) =>
     int.TryParse(text.GetFromRange(yearRange).Replace("(", string.Empty).Replace(")", string.Empty), out var yearInt) ? yearInt : 0;
 
   private static double ExtractRating(string text, Tuple<int, int> ratingRange) {
@@ -78,6 +105,14 @@ public static class Parser {
     var genres = ExtractUrlAndText(text.GetFromRange(genreRange));
     // TODO translate
     return genres.Select(x => x.Item2).ToArray();
+  }
+
+  /// <summary>
+  /// Removes the resized part of the image url
+  /// </summary>
+  private static string ExtractImage(string url) {
+    var parts = url.Split('/');
+    return string.Join("/", parts.Take(1).Concat(parts.Skip(4)));
   }
 
   private static Tuple<string, string>[] ExtractUrlAndText(string html) {
