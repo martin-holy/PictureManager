ef82c38f42517c8419323f52880500cfc64c1d3b
martin-holy <martin.holy@hotmail.com>
2024-05-07 18:29:16 +0200

    [N] CalendarExt control

---
+++ libs/MH/Windows/WPF/MH.UI.WPF/Controls/CalendarExt.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Input;
+
+namespace MH.UI.WPF.Controls;
+
+public class CalendarExt : Calendar {
+  protected override void OnPreviewMouseUp(MouseButtonEventArgs e) {
+    base.OnPreviewMouseUp(e);
+
+    if (Mouse.Captured is Calendar || Mouse.Captured is CalendarItem)
+      Mouse.Capture(null);
+  }
+}
