619e8269031290f120dd275d3db30b7e6705c995
martin-holy <martin.holy@hotmail.com>
2024-05-07 18:30:18 +0200

    [N] CultureAwareBinding

---
+++ libs/MH/Windows/WPF/MH.UI.WPF/Utils/CultureAwareBinding.cs
@@ -0,0 +1,10 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace MH.UI.WPF.Utils;
+
+public class CultureAwareBinding : Binding {
+  public CultureAwareBinding() {
+    ConverterCulture = CultureInfo.CurrentCulture;
+  }
+}
