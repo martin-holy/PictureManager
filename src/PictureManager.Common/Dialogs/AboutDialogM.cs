4ca04e89220576664a90f7040d1a69a375cb5693
martin-holy <martin.holy@hotmail.com>
2024-05-11 09:49:51 +0200

    [N] Link to Telegram channel in about dialog

--- src/PictureManager.Common/Dialogs/AboutDialogM.cs
+++ src/PictureManager.Common/Dialogs/AboutDialogM.cs
@@ -8,14 +8,17 @@ namespace PictureManager.Common.Dialogs {
     private FileVersionInfo _version;
 
     public FileVersionInfo Version { get => _version; set { _version = value; OnPropertyChanged(); } }
-    public string HomePageUrl { get; } = "https://github.com/martin-holy/PictureManager";
+    public string HomePageUrl => "https://github.com/martin-holy/PictureManager";
+    public string TelegramUrl => "https://t.me/PictureManager";
 
     public RelayCommand OpenHomePageCommand { get; }
+    public RelayCommand OpenTelegramCommand { get; }
     
     public AboutDialogM() : base("About", MH.UI.Res.IconImage) {
       Version = FileVersionInfo.GetVersionInfo(Assembly.GetExecutingAssembly().Location);
 
       OpenHomePageCommand = new(() => OpenUrl(HomePageUrl));
+      OpenTelegramCommand = new(() => OpenUrl(TelegramUrl));
     }
 
     private static void OpenUrl(string url) =>
