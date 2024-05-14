ff4b71336c3138a684cfe049e1d2fe185234d807
martin-holy <martin.holy@hotmail.com>
2024-05-14 16:43:57 +0200

    [C] MediaItem InterfaceTableDataAdapter

---
+++ src/PictureManager.Common/InterRepos/MediaItemIR.cs
@@ -0,0 +1,9 @@
+﻿using MH.Utils.BaseClasses;
+using PictureManager.Common.Models.MediaItems;
+using PictureManager.Interfaces.Models;
+
+namespace PictureManager.Common.InterRepos;
+
+public class MediaItemIR : InterfaceTableDataAdapter<MediaItemM, IMediaItemM> {
+  public MediaItemIR(TableDataAdapter<MediaItemM> tableDataAdapter) : base(tableDataAdapter) { }
+}
