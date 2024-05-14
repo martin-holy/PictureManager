bb07a065c34fb422b72af7595478afc806578bfb
martin-holy <martin.holy@hotmail.com>
2024-05-14 18:34:44 +0200

    [C] Segment InterfaceTableDataAdapter

---
+++ src/PictureManager.Common/InterRepos/SegmentIR.cs
@@ -0,0 +1,9 @@
+﻿using MH.Utils.BaseClasses;
+using PictureManager.Common.Models;
+using PictureManager.Interfaces.Models;
+
+namespace PictureManager.Common.InterRepos;
+
+public class SegmentIR : InterfaceTableDataAdapter<SegmentM, ISegmentM> {
+  public SegmentIR(TableDataAdapter<SegmentM> tableDataAdapter) : base(tableDataAdapter) { }
+}
