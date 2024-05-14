374fd17a1232e2a1754c65538985d4d0647e5f36
martin-holy <martin.holy@hotmail.com>
2024-05-14 18:44:36 +0200

    [C] Person InterfaceTableDataAdapter

--- src/PictureManager.Interfaces/Repositories/IPMCoreR.cs
+++ src/PictureManager.Interfaces/Repositories/IPMCoreR.cs
@@ -7,6 +7,6 @@ public interface IPMCoreR {
   public IFolderR Folder { get; }
   public IKeywordR Keyword { get; }
   public IInterfaceTableDataAdapter<IMediaItemM> MediaItem { get; }
-  public IRepository<IPersonM> Person { get; }
+  public IInterfaceTableDataAdapter<IPersonM> Person { get; }
   public IInterfaceTableDataAdapter<ISegmentM> Segment { get; }
 }
