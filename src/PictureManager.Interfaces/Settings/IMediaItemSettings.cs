2d39b1d81ba7c29bcd92215e1d6926e97b6021b3
martin-holy <martin.holy@hotmail.com>
2024-05-17 11:19:58 +0200

    PictureManager.Interfaces removed

--- src/PictureManager.Interfaces/Settings/IMediaItemSettings.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace PictureManager.Interfaces.Settings;
-
-public interface IMediaItemSettings {
-  public bool ScrollExactlyToMediaItem { get; }
-  public double MediaItemThumbScale { get; }
-  public double VideoItemThumbScale { get; }
-  public int ThumbSize { get; }
-}
