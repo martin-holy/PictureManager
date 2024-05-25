a1ee18d50d0467ee83026b090c758a0e57ae1cd6
martin-holy <martin.holy@hotmail.com>
2024-05-25 19:57:51 +0200

    [U] Buttons

--- libs/MH/Windows/WPF/MH.UI.WPF.Sample/ViewModels/Controls/ButtonsVM.cs
+++ libs/MH/Windows/WPF/MH.UI.WPF.Sample/ViewModels/Controls/ButtonsVM.cs
@@ -1,5 +1,14 @@
-﻿using MH.Utils.BaseClasses;
+﻿using MH.UI.WPF.Sample.Resources;
+using MH.Utils.BaseClasses;
 
 namespace MH.UI.WPF.Sample.ViewModels.Controls;
 
-public class ButtonsVM : ObservableObject;
+public class ButtonsVM : ObservableObject {
+  public RelayCommand DialogButtonWithIconCommand { get; }
+  public RelayCommand DialogButtonWithoutIconCommand { get; }
+
+  public ButtonsVM() {
+    DialogButtonWithIconCommand = new(() => { }, Icons.Image, "Sample");
+    DialogButtonWithoutIconCommand = new(() => { }, null, "Sample");
+  }
+}
