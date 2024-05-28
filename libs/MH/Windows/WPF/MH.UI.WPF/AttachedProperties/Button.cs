e0ca905eb732ee48c5ccba6cee1f094f6d1294aa
martin-holy <martin.holy@hotmail.com>
2024-05-28 11:38:24 +0200

    [U] Buttons styles

---
+++ libs/MH/Windows/WPF/MH.UI.WPF/AttachedProperties/Button.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+using System.Windows.Media;
+
+namespace MH.UI.WPF.AttachedProperties; 
+
+public static class Button {
+  public static readonly DependencyProperty OverLayerProperty = DependencyProperty.RegisterAttached(
+    "OverLayer", typeof(Brush), typeof(Button));
+
+  public static Brush GetOverLayer(DependencyObject d) => (Brush)d.GetValue(OverLayerProperty);
+  public static void SetOverLayer(DependencyObject d, Brush value) => d.SetValue(OverLayerProperty, value);
+}
