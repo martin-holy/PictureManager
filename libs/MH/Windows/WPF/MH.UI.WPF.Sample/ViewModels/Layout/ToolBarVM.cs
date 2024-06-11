79c22d5169d58e34412fc5b23876594215c620a0
martin-holy <martin.holy@hotmail.com>
2024-06-11 08:03:11 +0200

    [U] Menu style

--- libs/MH/Windows/WPF/MH.UI.WPF.Sample/ViewModels/Layout/ToolBarVM.cs
+++ libs/MH/Windows/WPF/MH.UI.WPF.Sample/ViewModels/Layout/ToolBarVM.cs
@@ -4,5 +4,10 @@ using MH.Utils.BaseClasses;
 namespace MH.UI.WPF.Sample.ViewModels.Layout;
 
 public sealed class ToolBarVM : ObservableObject {
+  private bool _checkedMenuItem = true;
+
   public SlidePanelPinButton SlidePanelPinButton { get; } = new();
+  public bool CheckedMenuItem { get => _checkedMenuItem; set { _checkedMenuItem = value; OnPropertyChanged(); } }
+  public RelayCommand SampleImageCommand { get; } = new(() => { }, Res.IconImage, "Sample image");
+  public RelayCommand SampleVideoCommand { get; } = new(() => { }, Res.IconMovieClapper, "Sample video");
 }
