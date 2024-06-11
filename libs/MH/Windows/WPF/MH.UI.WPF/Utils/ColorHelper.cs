4e4c5a505e073311972c2e5439bc6b4e5ac93faa
martin-holy <martin.holy@hotmail.com>
2024-06-11 20:02:40 +0200

    [N] ColorHelper to set Accent colors to app resources

--- libs/MH/Windows/WPF/MH.UI.WPF/Utils/ColorHelper.cs
+++ libs/MH/Windows/WPF/MH.UI.WPF/Utils/ColorHelper.cs
@@ -13,7 +13,15 @@ public static class ColorHelper {
       Application.Current.Resources[$"{name}{i}"] = Color.FromArgb((byte)(size * i), color.R, color.G, color.B);
   }
 
+  public static void AddVariants(string name, Color color) {
+    MH.Utils.Imaging.RgbToHsl(color.R, color.G, color.B, out var h, out var s, out var l);
+    l = 50;
+    MH.Utils.Imaging.HslToRgb(h, s, l, out var r, out var g, out var b);
+    Application.Current.Resources[$"{name}-Dark"] = Color.FromRgb(r, g, b);
+  }
+
   public static void AddColorsToResources() {
+    AddVariants("MH.Color.Accent", SystemParameters.WindowGlassColor);
     AddGradientColors("MH.Color.Accent", SystemParameters.WindowGlassColor);
     AddGradientColors("MH.Color.Black", Color.FromRgb(0, 0, 0));
     AddGradientColors("MH.Color.White", Color.FromRgb(255, 255, 255));
