e38a8b780e5064851a813fbc4ee7ee3d6d7ea24f
martin-holy <martin.holy@hotmail.com>
2024-06-23 20:42:56 +0200

    [U] IconTextBlock style

--- libs/MH/Windows/WPF/MH.UI.WPF/Controls/IconControls.cs
+++ libs/MH/Windows/WPF/MH.UI.WPF/Controls/IconControls.cs
@@ -4,16 +4,18 @@ using System.Windows.Controls.Primitives;
 
 namespace MH.UI.WPF.Controls;
 
-public enum IconTextBlockShadow { None, Icon, Text, Both };
+public enum IconTextBlockShadow { None, Icon, Text, Both }
 
 public class IconTextBlock : Control {
   public static readonly DependencyProperty CompactProperty = DependencyProperty.Register(
     nameof(Compact), typeof(bool), typeof(IconTextBlock));
-
+  public static readonly DependencyProperty TextBorderStyleProperty = DependencyProperty.Register(
+    nameof(TextBorderStyle), typeof(Style), typeof(IconTextBlock));
   public static readonly DependencyProperty ShadowProperty = DependencyProperty.Register(
     nameof(Shadow), typeof(IconTextBlockShadow), typeof(IconTextBlock));
 
   public bool Compact { get => (bool)GetValue(CompactProperty); set => SetValue(CompactProperty, value); }
+  public Style TextBorderStyle { get => (Style)GetValue(TextBorderStyleProperty); set => SetValue(TextBorderStyleProperty, value); }
   public IconTextBlockShadow Shadow { get => (IconTextBlockShadow)GetValue(ShadowProperty); set => SetValue(ShadowProperty, value); }
 }
 
