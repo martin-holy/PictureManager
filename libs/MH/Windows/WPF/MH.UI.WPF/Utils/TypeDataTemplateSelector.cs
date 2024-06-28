73a304e6e079489bc9972a309ea9ffe601df1f13
martin-holy <martin.holy@hotmail.com>
2024-06-28 19:13:29 +0200

    [N] TypeDataTemplateSelector

---
+++ libs/MH/Windows/WPF/MH.UI.WPF/Utils/TypeDataTemplateSelector.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace MH.UI.WPF.Utils;
+
+public class TypeDataTemplateSelector : DataTemplateSelector {
+  public TypeTemplateMapping[] TemplateMappings { get; set; }
+
+  public override DataTemplate SelectTemplate(object item, DependencyObject container) {
+    if (item == null || TemplateMappings == null || container is not FrameworkElement fe)
+      return base.SelectTemplate(item, container);
+
+    var itemType = item.GetType();
+    var tm = TemplateMappings.FirstOrDefault(x => x.Type.IsAssignableFrom(itemType));
+
+    if (!string.IsNullOrEmpty(tm?.TemplateKey) && fe.TryFindResource(tm.TemplateKey) is DataTemplate dt)
+      return dt;
+
+    return base.SelectTemplate(item, container);
+  }
+}
+
+public class TypeTemplateMapping {
+  public Type Type { get; set; }
+  public string TemplateKey { get; set; }
+}
