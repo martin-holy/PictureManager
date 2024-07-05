0a3f3d56b4a0ed4ed92178fd1bae5c60b6483a8d
martin-holy <martin.holy@hotmail.com>
2024-07-05 12:29:26 +0200

    [U] VisibilityConverter

--- libs/MH/Windows/WPF/MH.UI.WPF/Converters/VisibilityConverter.cs
+++ libs/MH/Windows/WPF/MH.UI.WPF/Converters/VisibilityConverter.cs
@@ -4,40 +4,28 @@ using System.Windows.Data;
 
 namespace MH.UI.WPF.Converters;
 
-public enum CheckFor { NotNull, Null, True, False, NotEmpty, NullOrEmpty, All }
+public enum CheckFor { NotNull, Null, True, False, NotEmpty, NullOrEmpty, Int, All }
 
 public class VisibilityConverter : BaseConverter {
-  private static VisibilityConverter _allToCollapsed;
-  private static VisibilityConverter _allToHidden;
   private static VisibilityConverter _allToVisible;
-  private static VisibilityConverter _nullToCollapsed;
-  private static VisibilityConverter _nullToHidden;
   private static VisibilityConverter _nullToVisible;
-  private static VisibilityConverter _notNullToCollapsed;
-  private static VisibilityConverter _notNullToHidden;
   private static VisibilityConverter _notNullToVisible;
   private static VisibilityConverter _trueToVisible;
   private static VisibilityConverter _falseToVisible;
   private static VisibilityConverter _trueToHidden;
-  private static VisibilityConverter _falseToHidden;
   private static VisibilityConverter _notEmptyToVisible;
   private static VisibilityConverter _nullOrEmptyToVisible;
+  private static VisibilityConverter _intToVisible;
 
-  public static VisibilityConverter AllToCollapsed => _allToCollapsed ??= new() { CheckFor = CheckFor.All, ToCollapsed = true };
-  public static VisibilityConverter AllToHidden => _allToHidden ??= new() { CheckFor = CheckFor.All, ToHidden = true };
   public static VisibilityConverter AllToVisible => _allToVisible ??= new() { CheckFor = CheckFor.All, ToVisible = true };
-  public static VisibilityConverter NullToCollapsed => _nullToCollapsed ??= new() { CheckFor = CheckFor.Null, ToCollapsed = true };
-  public static VisibilityConverter NullToHidden => _nullToHidden ??= new() { CheckFor = CheckFor.Null, ToHidden = true };
   public static VisibilityConverter NullToVisible => _nullToVisible ??= new() { CheckFor = CheckFor.Null, ToVisible = true };
-  public static VisibilityConverter NotNullToCollapsed => _notNullToCollapsed ??= new() { CheckFor = CheckFor.NotNull, ToCollapsed = true };
-  public static VisibilityConverter NotNullToHidden => _notNullToHidden ??= new() { CheckFor = CheckFor.NotNull, ToHidden = true };
   public static VisibilityConverter NotNullToVisible => _notNullToVisible ??= new() { CheckFor = CheckFor.NotNull, ToVisible = true };
   public static VisibilityConverter TrueToVisible => _trueToVisible ??= new() { CheckFor = CheckFor.True, ToVisible = true };
   public static VisibilityConverter FalseToVisible => _falseToVisible ??= new() { CheckFor = CheckFor.False, ToVisible = true };
   public static VisibilityConverter TrueToHidden => _trueToHidden ??= new() { CheckFor = CheckFor.True, ToHidden = true };
-  public static VisibilityConverter FalseToHidden => _falseToHidden ??= new() { CheckFor = CheckFor.False, ToHidden = true };
   public static VisibilityConverter NotEmptyToVisible => _notEmptyToVisible ??= new() { CheckFor = CheckFor.NotEmpty, ToVisible = true };
   public static VisibilityConverter NullOrEmptyToVisible => _nullOrEmptyToVisible ??= new() { CheckFor = CheckFor.NullOrEmpty, ToVisible = true };
+  public static VisibilityConverter IntToVisible => _intToVisible ??= new() { CheckFor = CheckFor.Int, ToVisible = true };
 
   public CheckFor CheckFor { get; init; }
 
@@ -53,6 +41,7 @@ public class VisibilityConverter : BaseConverter {
       CheckFor.False => GetFor(value is false),
       CheckFor.NotEmpty => GetFor(value is IList { Count: > 0 }),
       CheckFor.NullOrEmpty => GetFor(value is null or IList { Count: 0 }),
+      CheckFor.Int => GetFor(value is > 0),
       CheckFor.All => GetFor(AllToBoolConverter.AllToBool(value, parameter)),
       _ => Binding.DoNothing
     };
