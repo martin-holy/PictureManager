dba0a534e3f1b02fe2c8ee532bc48477a653eee5
martin-holy <martin.holy@hotmail.com>
2024-07-06 13:57:16 +0200

    [C] ObservableObject nullable

--- libs/MH/Common/MH.Utils/BaseClasses/ObservableObject.cs
+++ libs/MH/Common/MH.Utils/BaseClasses/ObservableObject.cs
@@ -5,11 +5,11 @@ using System.Runtime.CompilerServices;
 namespace MH.Utils.BaseClasses;
 
 public class ObservableObject : INotifyPropertyChanged {
-  public event PropertyChangedEventHandler PropertyChanged = delegate { };
-  public void OnPropertyChanged([CallerMemberName] string name = null) =>
-    PropertyChanged.Invoke(this, new(name));
+  public event PropertyChangedEventHandler? PropertyChanged = delegate { };
+  public void OnPropertyChanged([CallerMemberName] string? name = null) =>
+    PropertyChanged!(this, new(name));
 
-  protected bool SetIfVary<T>(ref T field, T value, [CallerMemberName] string name = null) {
+  protected bool SetIfVary<T>(ref T field, T value, [CallerMemberName] string? name = null) {
     if (EqualityComparer<T>.Default.Equals(field, value)) return false;
     field = value;
     OnPropertyChanged(name);
