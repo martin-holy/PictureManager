0c3ea4de731f7c7e4032f82b0c23298993326b34
martin-holy <martin.holy@hotmail.com>
2024-07-06 14:14:11 +0200

    [C] ListItem nullable

--- libs/MH/Common/MH.Utils/BaseClasses/ListItem.cs
+++ libs/MH/Common/MH.Utils/BaseClasses/ListItem.cs
@@ -9,20 +9,20 @@ namespace MH.Utils.BaseClasses;
 public class ListItem : ObservableObject, IListItem {
   protected BitVector32 Bits = new(0);
 
-  private string _icon;
-  private string _name;
+  private string? _icon;
+  private string? _name;
 
   public bool IsSelected { get => Bits[BitsMasks.IsSelected]; set { Bits[BitsMasks.IsSelected] = value; OnPropertyChanged(); } }
   public bool IsHidden { get => Bits[BitsMasks.IsHidden]; set { Bits[BitsMasks.IsHidden] = value; OnPropertyChanged(); } }
   public bool IsIconHidden { get => Bits[BitsMasks.IsIconHidden]; set { Bits[BitsMasks.IsIconHidden] = value; OnPropertyChanged(); } }
   public bool IsNameHidden { get => Bits[BitsMasks.IsNameHidden]; set { Bits[BitsMasks.IsNameHidden] = value; OnPropertyChanged(); } }
-  public string Icon { get => _icon; set { _icon = value; OnPropertyChanged(); } }
-  public string Name { get => _name; set { _name = value; OnPropertyChanged(); } }
-  public object Data { get; set; }
+  public string? Icon { get => _icon; set { _icon = value; OnPropertyChanged(); } }
+  public string? Name { get => _name; set { _name = value; OnPropertyChanged(); } }
+  public object? Data { get; set; }
 
   public ListItem() { }
 
-  public ListItem(string icon, string name) : this() {
+  public ListItem(string? icon, string? name) : this() {
     Icon = icon;
     Name = name;
   }
@@ -35,19 +35,26 @@ public class ListItem : ObservableObject, IListItem {
 public class ListItem<T> : ListItem {
   private T _content;
 
-  public T Content { get => _content; set { _content = value; OnPropertyChanged(); } }
-
-  public ListItem(T content) {
-    Content = content;
+  public T Content {
+    get => _content;
+    set {
+      if (EqualityComparer<T>.Default.Equals(_content, value)) return;
+      _content = value;
+      OnPropertyChanged();
+    }
   }
 
-  public ListItem(T content, string icon, string name) : base(icon, name) {
-    Content = content;
+  public ListItem(T content) {
+    _content = content;
+  }
+
+  public ListItem(T content, string? icon, string? name) : base(icon, name) {
+    _content = content;
   }
 }
 
 public static class ListItemExtensions {
-  public static T GetByName<T>(this IEnumerable<T> items, string name, StringComparison sc = StringComparison.Ordinal)
+  public static T? GetByName<T>(this IEnumerable<T> items, string name, StringComparison sc = StringComparison.Ordinal)
     where T : IListItem =>
-    items.SingleOrDefault(x => x.Name.Equals(name, sc));
+    items.SingleOrDefault(x => x.Name?.Equals(name, sc) == true);
 }
--- libs/MH/Common/MH.Utils/Interfaces/IListItem.cs
+++ libs/MH/Common/MH.Utils/Interfaces/IListItem.cs
@@ -1,10 +1,10 @@
-﻿namespace MH.Utils.Interfaces {
-  public interface IListItem : ISelectable {
-    public bool IsHidden { get; set; }
-    public bool IsIconHidden { get; set; }
-    public bool IsNameHidden { get; set; }
-    public string Icon { get; set; }
-    public string Name { get; set; }
-    public object Data { get; }
-  }
+﻿namespace MH.Utils.Interfaces;
+
+public interface IListItem : ISelectable {
+  public bool IsHidden { get; set; }
+  public bool IsIconHidden { get; set; }
+  public bool IsNameHidden { get; set; }
+  public string? Icon { get; set; }
+  public string? Name { get; set; }
+  public object? Data { get; }
 }
