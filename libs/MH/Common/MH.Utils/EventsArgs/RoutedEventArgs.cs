ad0367ffeafc098fee6bb45cbe1ed701e45cdd3a
martin-holy <martin.holy@hotmail.com>
2024-07-06 14:31:42 +0200

    [C] RoutedEventArgs nullable

--- libs/MH/Common/MH.Utils/EventsArgs/RoutedEventArgs.cs
+++ libs/MH/Common/MH.Utils/EventsArgs/RoutedEventArgs.cs
@@ -2,8 +2,8 @@
 
 namespace MH.Utils.EventsArgs {
   public class RoutedEventArgs : EventArgs {
-    public object OriginalSource { get; set; }
-    public object DataContext { get; set; }
+    public object? OriginalSource { get; set; }
+    public object? DataContext { get; set; }
     public bool IsCtrlOn { get; set; }
     public bool IsAltOn { get; set; }
     public bool IsShiftOn { get; set; }
