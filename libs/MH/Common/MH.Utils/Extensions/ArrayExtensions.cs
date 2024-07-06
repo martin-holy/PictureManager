5bc8a5b1cdc93721bb9744262f155696245bd213
martin-holy <martin.holy@hotmail.com>
2024-07-06 20:27:40 +0200

    [C] ArrayExtensions nullable

--- libs/MH/Common/MH.Utils/Extensions/ArrayExtensions.cs
+++ libs/MH/Common/MH.Utils/Extensions/ArrayExtensions.cs
@@ -3,7 +3,7 @@
 namespace MH.Utils.Extensions;
 
 public static class ArrayExtensions {
-  public static T[] NullIfEmpty<T>(this T[] self) =>
+  public static T[]? NullIfEmpty<T>(this T[]? self) =>
     self?.Length > 0 ? self : null;
 
   public static void ForEach<T>(this T[] self, Action<T> action) {
