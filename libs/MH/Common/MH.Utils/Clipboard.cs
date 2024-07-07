05c2aa79fb2567630a804057296c61af8f397ce3
martin-holy <martin.holy@hotmail.com>
2024-07-07 09:52:32 +0200

    [C] Clipboard nullable

--- libs/MH/Common/MH.Utils/Clipboard.cs
+++ libs/MH/Common/MH.Utils/Clipboard.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 
-namespace MH.Utils {
-  public static class Clipboard {
-    public static Action<string> SetText { get; set; }
-  }
+namespace MH.Utils;
+
+public static class Clipboard {
+  public static Action<string> SetText { get; set; } = null!;
 }
