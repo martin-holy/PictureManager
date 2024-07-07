d00d5debdcbf8c4eecdfac5771f3799dca674356
martin-holy <martin.holy@hotmail.com>
2024-07-07 09:32:44 +0200

    [C] EnumerableExtensions nullable

--- libs/MH/Common/MH.Utils/Extensions/EnumerableExtensions.cs
+++ libs/MH/Common/MH.Utils/Extensions/EnumerableExtensions.cs
@@ -4,14 +4,14 @@ using System.Linq;
 namespace MH.Utils.Extensions; 
 
 public static class EnumerableExtensions {
-  public static IEnumerable<T> EmptyIfNull<T>(this IEnumerable<T> items) =>
-    items ?? Enumerable.Empty<T>();
+  public static IEnumerable<T> EmptyIfNull<T>(this IEnumerable<T>? items) =>
+    items ?? [];
 
-  public static IEnumerable<int> ToHashCodes<T>(this IEnumerable<T> items) =>
-    items?.Select(x => x.GetHashCode());
+  public static IEnumerable<int>? ToHashCodes<T>(this IEnumerable<T>? items) =>
+    items?.Select(x => x!.GetHashCode());
 
-  public static string ToCsv<T>(this IEnumerable<T> items, string separator = ",") =>
+  public static string ToCsv<T>(this IEnumerable<T>? items, string separator = ",") =>
     items == null
       ? string.Empty
-      : string.Join(separator, items.Select(x => x.ToString()));
+      : string.Join(separator, items.Select(x => x!.ToString()));
 }
