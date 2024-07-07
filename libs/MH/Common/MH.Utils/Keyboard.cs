a719c330e8f7834021e4d45a3c4ede2bdc3ccb45
martin-holy <martin.holy@hotmail.com>
2024-07-07 10:16:29 +0200

    [C] Keyboard nullable

--- libs/MH/Common/MH.Utils/Keyboard.cs
+++ libs/MH/Common/MH.Utils/Keyboard.cs
@@ -1,9 +1,9 @@
 ﻿using System;
 
-namespace MH.Utils {
-  public static class Keyboard {
-    public static Func<bool> IsCtrlOn { get; set; }
-    public static Func<bool> IsAltOn { get; set; }
-    public static Func<bool> IsShiftOn { get; set; }
-  }
+namespace MH.Utils;
+
+public static class Keyboard {
+  public static Func<bool> IsCtrlOn { get; set; } = null!;
+  public static Func<bool> IsAltOn { get; set; } = null!;
+  public static Func<bool> IsShiftOn { get; set; } = null!;
 }
