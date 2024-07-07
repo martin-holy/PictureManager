9bf00291d66882ffc0e2d5be98bdd05e2ef6cf7e
martin-holy <martin.holy@hotmail.com>
2024-07-07 20:50:10 +0200

    [C] InputDialog nullable

--- libs/MH/Common/MH.UI/Dialogs/InputDialog.cs
+++ libs/MH/Common/MH.UI/Dialogs/InputDialog.cs
@@ -6,24 +6,24 @@ namespace MH.UI.Dialogs;
 public class InputDialog : Dialog {
   private string _message;
   private string _answer;
-  private string _errorMessage;
+  private string? _errorMessage;
   private bool _error;
-  private readonly Func<string, string> _validator;
+  private readonly Func<string, string?> _validator;
 
   public string Message { get => _message; set { _message = value; OnPropertyChanged(); } }
   public string Answer { get => _answer; set { _answer = value; OnPropertyChanged(); } }
-  public string ErrorMessage { get => _errorMessage; set { _errorMessage = value; OnPropertyChanged(); } }
+  public string? ErrorMessage { get => _errorMessage; set { _errorMessage = value; OnPropertyChanged(); } }
   public bool Error { get => _error; set { _error = value; OnPropertyChanged(); } }
 
-  public InputDialog(string title, string message, string icon, string answer, Func<string, string> validator) : base(title, icon) {
-    Message = message;
-    Answer = answer;
+  public InputDialog(string title, string message, string icon, string answer, Func<string, string?> validator) : base(title, icon) {
+    _message = message;
+    _answer = answer;
     _validator = validator;
 
-    Buttons = new DialogButton[] {
+    Buttons = [
       new(new(Validate, Res.IconCheckMark, "Ok"), true),
       new(CancelCommand, false, true)
-    };
+    ];
   }
 
   private void Validate() {
