47e7adfe5f0f9bb34ef2943446da580c16fd1a19
martin-holy <martin.holy@hotmail.com>
2024-07-10 15:12:20 +0200

    [C] DragDropHelper nullable

--- libs/MH/Common/MH.Utils/DragDropHelper.cs
+++ libs/MH/Common/MH.Utils/DragDropHelper.cs
@@ -9,7 +9,7 @@
   }
 
   public static class DragDropHelper {
-    public delegate object CanDragFunc(object source);
+    public delegate object? CanDragFunc(object source);
     public delegate DragDropEffects CanDropFunc(object target, object data, bool haveSameOrigin);
     public delegate void DoDropAction(object data, bool haveSameOrigin);
   }
