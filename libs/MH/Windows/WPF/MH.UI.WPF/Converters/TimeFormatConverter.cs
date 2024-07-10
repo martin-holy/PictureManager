343b715ace1bce4b9408131da1a8c16130242f7b
martin-holy <martin.holy@hotmail.com>
2024-07-10 11:31:35 +0200

    [C] TimeFormatConverter nullable

--- libs/MH/Windows/WPF/MH.UI.WPF/Converters/TimeFormatConverter.cs
+++ libs/MH/Windows/WPF/MH.UI.WPF/Converters/TimeFormatConverter.cs
@@ -5,13 +5,13 @@ namespace MH.UI.WPF.Converters;
 
 public sealed class TimeFormatConverter : BaseConverter {
   private static readonly object _lock = new();
-  private static TimeFormatConverter _inst;
+  private static TimeFormatConverter? _inst;
   public static TimeFormatConverter Inst { get { lock (_lock) { return _inst ??= new(); } } }
 
   private const string _position = "position";
   private const string _duration = "duration";
 
-  public override object Convert(object value, object parameter) =>
+  public override object? Convert(object? value, object? parameter) =>
     value is not int ms || parameter is null
       ? Binding.DoNothing
       : parameter switch {
