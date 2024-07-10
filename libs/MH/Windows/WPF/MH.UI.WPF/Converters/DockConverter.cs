787ebdb03d3d9742f2a86689d5ed07336f72fc11
martin-holy <martin.holy@hotmail.com>
2024-07-10 11:01:22 +0200

    [C] DockConverter nullable

--- libs/MH/Windows/WPF/MH.UI.WPF/Converters/DockConverter.cs
+++ libs/MH/Windows/WPF/MH.UI.WPF/Converters/DockConverter.cs
@@ -2,12 +2,12 @@
 
 public class DockConverter : BaseConverter {
   private static readonly object _lock = new();
-  private static DockConverter _inst;
+  private static DockConverter? _inst;
   public static DockConverter Inst { get { lock (_lock) { return _inst ??= new(); } } }
 
-  public override object Convert(object value, object parameter) =>
-    (System.Windows.Controls.Dock)(int)(MH.UI.Controls.Dock)value;
+  public override object? Convert(object? value, object? parameter) =>
+    value == null ? null : (System.Windows.Controls.Dock)(int)(MH.UI.Controls.Dock)value;
 
-  public override object ConvertBack(object value, object parameter) =>
-    (MH.UI.Controls.Dock)(int)(System.Windows.Controls.Dock)value;
+  public override object? ConvertBack(object? value, object? parameter) =>
+    value == null ? null : (MH.UI.Controls.Dock)(int)(System.Windows.Controls.Dock)value;
 }
