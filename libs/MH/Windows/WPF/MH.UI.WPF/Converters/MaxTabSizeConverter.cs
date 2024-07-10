4de2498db5575afc42c966ba6794c888dce05658
martin-holy <martin.holy@hotmail.com>
2024-07-10 11:36:34 +0200

    [C] MaxTabSizeConverter nullable

--- libs/MH/Windows/WPF/MH.UI.WPF/Converters/MaxTabSizeConverter.cs
+++ libs/MH/Windows/WPF/MH.UI.WPF/Converters/MaxTabSizeConverter.cs
@@ -7,7 +7,7 @@ public class MaxTabSizeConverter : BaseMultiConverter {
   private static MaxTabSizeConverter? _inst;
   public static MaxTabSizeConverter Inst { get { lock (_lock) { return _inst ??= new(); } } }
 
-  public override object Convert(object[]? values, object? parameter) {
+  public override object? Convert(object?[]? values, object? parameter) {
     if (values?.Length != 3) return Binding.DoNothing;
 
     (values[0] as MH.UI.Controls.TabControl)?.UpdateMaxTabSize(values[1] as double?, values[2] as double?);
