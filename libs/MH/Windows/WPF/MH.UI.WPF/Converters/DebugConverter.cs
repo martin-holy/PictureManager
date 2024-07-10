abc87e71b54655ac518ecdbd0d548c3ad3fcf438
martin-holy <martin.holy@hotmail.com>
2024-07-10 10:51:54 +0200

    [C] DebugConverter nullable

--- libs/MH/Windows/WPF/MH.UI.WPF/Converters/DebugConverter.cs
+++ libs/MH/Windows/WPF/MH.UI.WPF/Converters/DebugConverter.cs
@@ -6,10 +6,10 @@ namespace MH.UI.WPF.Converters;
 
 public class DebugConverter : BaseConverter {
   private static readonly object _lock = new();
-  private static DebugConverter _inst;
+  private static DebugConverter? _inst;
   public static DebugConverter Inst { get { lock (_lock) { return _inst ??= new(); } } }
 
-  public override object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+  public override object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture) {
     Debug.WriteLine($"DebugConverter value: {value}, parameter: {parameter}");
     return value;
   }
