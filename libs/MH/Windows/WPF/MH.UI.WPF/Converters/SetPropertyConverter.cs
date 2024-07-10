c29556c6dd74c8a802e6c26a6ee825f4359d3e86
martin-holy <martin.holy@hotmail.com>
2024-07-10 11:30:47 +0200

    [C] SetPropertyConverter nullable

--- libs/MH/Windows/WPF/MH.UI.WPF/Converters/SetPropertyConverter.cs
+++ libs/MH/Windows/WPF/MH.UI.WPF/Converters/SetPropertyConverter.cs
@@ -4,11 +4,11 @@ namespace MH.UI.WPF.Converters;
 
 public class SetPropertyConverter : BaseMultiConverter {
   private static readonly object _lock = new();
-  private static SetPropertyConverter _inst;
+  private static SetPropertyConverter? _inst;
   public static SetPropertyConverter Inst { get { lock (_lock) { return _inst ??= new(); } } }
 
-  public override object Convert(object[] values, object parameter) {
-    if (values.Length == 2 && parameter is string propName)
+  public override object? Convert(object?[]? values, object? parameter) {
+    if (values?.Length == 2 && parameter is string propName)
       values[0]?.GetType().GetProperty(propName)?.SetValue(values[0], values[1]);
 
     return Binding.DoNothing;
