7282042b2dd55787fa5ce423d103f208bdafab84
martin-holy <martin.holy@hotmail.com>
2024-07-10 11:36:52 +0200

    [C] BaseConverter nullable

--- libs/MH/Windows/WPF/MH.UI.WPF/Converters/BaseConverter.cs
+++ libs/MH/Windows/WPF/MH.UI.WPF/Converters/BaseConverter.cs
@@ -5,15 +5,15 @@ using System.Windows.Data;
 namespace MH.UI.WPF.Converters;
 
 public class BaseConverter : IValueConverter {
-  public virtual object Convert(object? value, Type targetType, object? parameter, CultureInfo culture) =>
+  public virtual object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture) =>
     Convert(value, parameter);
 
-  public virtual object Convert(object? value, object? parameter) =>
+  public virtual object? Convert(object? value, object? parameter) =>
     throw new NotImplementedException();
 
-  public virtual object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture) =>
+  public virtual object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture) =>
     ConvertBack(value, parameter);
 
-  public virtual object ConvertBack(object? value, object? parameter) =>
+  public virtual object? ConvertBack(object? value, object? parameter) =>
     throw new NotSupportedException();
 }
