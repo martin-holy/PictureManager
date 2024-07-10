3f4b65a7c1b0ba1779aab8a422a9f7d97d00b3ea
martin-holy <martin.holy@hotmail.com>
2024-07-10 16:33:27 +0200

    [C] DragDropHelper nullable

--- libs/MH/Common/MH.Utils/DragDropHelper.cs
+++ libs/MH/Common/MH.Utils/DragDropHelper.cs
@@ -1,16 +1,16 @@
-﻿namespace MH.Utils {
-  public enum DragDropEffects {
-    Scroll = int.MinValue,
-    All = -2147483645,
-    None = 0,
-    Copy = 1,
-    Move = 2,
-    Link = 4
-  }
+﻿namespace MH.Utils;
 
-  public static class DragDropHelper {
-    public delegate object? CanDragFunc(object source);
-    public delegate DragDropEffects CanDropFunc(object target, object data, bool haveSameOrigin);
-    public delegate void DoDropAction(object data, bool haveSameOrigin);
-  }
+public enum DragDropEffects {
+  Scroll = int.MinValue,
+  All = -2147483645,
+  None = 0,
+  Copy = 1,
+  Move = 2,
+  Link = 4
+}
+
+public static class DragDropHelper {
+  public delegate object? CanDragFunc(object source);
+  public delegate DragDropEffects CanDropFunc(object? target, object? data, bool haveSameOrigin);
+  public delegate void DoDropAction(object data, bool haveSameOrigin);
 }
