ba9ab8878114ad14db7fc13991279153fdc28c8e
martin-holy <martin.holy@hotmail.com>
2024-07-10 11:38:00 +0200

    [C] BaseMultiConverter nullable

--- libs/MH/Windows/WPF/MH.UI.WPF/Converters/BaseMultiConverter.cs
+++ libs/MH/Windows/WPF/MH.UI.WPF/Converters/BaseMultiConverter.cs
@@ -5,12 +5,12 @@ using System.Windows.Data;
 namespace MH.UI.WPF.Converters;
 
 public class BaseMultiConverter : IMultiValueConverter {
-  public virtual object Convert(object[] values, Type targetType, object parameter, CultureInfo culture) =>
+  public virtual object? Convert(object?[]? values, Type targetType, object? parameter, CultureInfo culture) =>
     Convert(values, parameter);
 
-  public virtual object Convert(object[] values, object parameter) =>
+  public virtual object? Convert(object?[]? values, object? parameter) =>
     throw new NotImplementedException();
 
-  public virtual object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture) =>
+  public virtual object[] ConvertBack(object? value, Type[] targetTypes, object? parameter, CultureInfo culture) =>
     throw new NotSupportedException();
 }
