41308967c24ec33021dc4874e850fc9a6551dc20
martin-holy <martin.holy@hotmail.com>
2024-07-10 10:50:44 +0200

    [C] CollectionViewItemTemplateConverter nullable

--- libs/MH/Windows/WPF/MH.UI.WPF/Converters/CollectionViewItemTemplateConverter.cs
+++ libs/MH/Windows/WPF/MH.UI.WPF/Converters/CollectionViewItemTemplateConverter.cs
@@ -9,10 +9,10 @@ namespace MH.UI.WPF.Converters;
 
 public sealed class CollectionViewItemTemplateConverter : BaseConverter {
   private static readonly object _lock = new();
-  private static CollectionViewItemTemplateConverter _inst;
+  private static CollectionViewItemTemplateConverter? _inst;
   public static CollectionViewItemTemplateConverter Inst { get { lock (_lock) { return _inst ??= new(); } } }
 
-  public override object Convert(object value, object parameter) =>
+  public override object? Convert(object? value, object? parameter) =>
     value is FrameworkElement fe
     && fe.TryFindParent<StackPanel>() is
       { DataContext: ITreeItem { Parent: ICollectionViewGroup { UIView: { } view } } }
