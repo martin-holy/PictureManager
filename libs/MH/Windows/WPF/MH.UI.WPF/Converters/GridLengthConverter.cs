d000e20509dce359ea7e98f0ee958cf0f544afc6
martin-holy <martin.holy@hotmail.com>
2024-07-10 11:18:56 +0200

    [C] GridLengthConverter nullable

--- libs/MH/Windows/WPF/MH.UI.WPF/Converters/GridLengthConverter.cs
+++ libs/MH/Windows/WPF/MH.UI.WPF/Converters/GridLengthConverter.cs
@@ -4,12 +4,12 @@ namespace MH.UI.WPF.Converters;
 
 public class GridLengthConverter : BaseConverter {
   private static readonly object _lock = new();
-  private static GridLengthConverter _inst;
+  private static GridLengthConverter? _inst;
   public static GridLengthConverter Inst { get { lock (_lock) { return _inst ??= new(); } } }
 
-  public override object Convert(object value, object parameter) =>
-    new GridLength((double)value);
+  public override object? Convert(object? value, object? parameter) =>
+    value == null ? null : new GridLength((double)value);
 
-  public override object ConvertBack(object value, object parameter) =>
-    ((GridLength)value).Value;
+  public override object? ConvertBack(object? value, object? parameter) =>
+    ((GridLength?)value)?.Value;
 }
