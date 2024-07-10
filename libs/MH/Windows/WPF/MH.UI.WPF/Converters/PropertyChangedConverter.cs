6cbb328a7a4d1d6bfcefd51631b61329c1ac144b
martin-holy <martin.holy@hotmail.com>
2024-07-10 11:21:28 +0200

    [C] PropertyChangedConverter nullable

--- libs/MH/Windows/WPF/MH.UI.WPF/Converters/PropertyChangedConverter.cs
+++ libs/MH/Windows/WPF/MH.UI.WPF/Converters/PropertyChangedConverter.cs
@@ -5,10 +5,10 @@ namespace MH.UI.WPF.Converters;
 
 public class PropertyChangedConverter : BaseConverter {
   private static readonly object _lock = new();
-  private static PropertyChangedConverter _inst;
+  private static PropertyChangedConverter? _inst;
   public static PropertyChangedConverter Inst { get { lock (_lock) { return _inst ??= new(); } } }
 
-  public override object Convert(object value, object parameter) {
+  public override object? Convert(object? value, object? parameter) {
     if (value is not RoutedPropertyChangedEventArgs<double> e) return null;
 
     return new MH.Utils.EventsArgs.PropertyChangedEventArgs<double>(e.OldValue, e.NewValue) {
