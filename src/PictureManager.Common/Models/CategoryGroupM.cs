c5a9d0b1771f21e6846f57eb1486bac8c14a605d
martin-holy <martin.holy@hotmail.com>
2024-07-11 16:18:25 +0200

    [C] CategoryGroupM nullable

--- src/PictureManager.Common/Models/CategoryGroupM.cs
+++ src/PictureManager.Common/Models/CategoryGroupM.cs
@@ -3,7 +3,6 @@ using MH.Utils.BaseClasses;
 using MH.Utils.Interfaces;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace PictureManager.Common.Models;
 
@@ -13,11 +12,15 @@ namespace PictureManager.Common.Models;
 public class CategoryGroupM(int id, string name, Category category, string icon)
   : TreeItem(icon, name), IEquatable<CategoryGroupM>, ITreeGroup {
   #region IEquatable implementation
-  public bool Equals(CategoryGroupM other) => Id == other?.Id;
-  public override bool Equals(object obj) => Equals(obj as CategoryGroupM);
+  public bool Equals(CategoryGroupM? other) => Id == other?.Id;
+  public override bool Equals(object? obj) => Equals(obj as CategoryGroupM);
   public override int GetHashCode() => Id;
-  public static bool operator ==(CategoryGroupM a, CategoryGroupM b) => a?.Equals(b) ?? b is null;
-  public static bool operator !=(CategoryGroupM a, CategoryGroupM b) => !(a == b);
+  public static bool operator ==(CategoryGroupM? a, CategoryGroupM? b) {
+    if (ReferenceEquals(a, b)) return true;
+    if (a is null || b is null) return false;
+    return a.Equals(b);
+  }
+  public static bool operator !=(CategoryGroupM? a, CategoryGroupM? b) => !(a == b);
   #endregion
 
   public int Id { get; } = id;
@@ -26,7 +29,5 @@ public class CategoryGroupM(int id, string name, Category category, string icon)
 
 public static class CategoryGroupExtensions {
   public static IEnumerable<CategoryGroupM> GetCategoryGroups<T>(this T item) where T : ITreeItem =>
-    item.Parent is CategoryGroupM cg
-      ? cg.GetThisAndParents()
-      : Enumerable.Empty<CategoryGroupM>();
+    item.Parent is CategoryGroupM cg ? cg.GetThisAndParents() : [];
 }
