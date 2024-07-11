d38ef4ad91faed9fbfe31ec4593994e0ba84d785
martin-holy <martin.holy@hotmail.com>
2024-07-11 16:30:57 +0200

    [C] FavoriteFolderM nullable

--- src/PictureManager.Common/Models/FavoriteFolderM.cs
+++ src/PictureManager.Common/Models/FavoriteFolderM.cs
@@ -5,14 +5,18 @@ namespace PictureManager.Common.Models;
 
 public sealed class FavoriteFolderM(int id, string name) : TreeItem(Res.IconFolder, name), IEquatable<FavoriteFolderM> {
   #region IEquatable implementation
-  public bool Equals(FavoriteFolderM other) => Id == other?.Id;
-  public override bool Equals(object obj) => Equals(obj as FavoriteFolderM);
+  public bool Equals(FavoriteFolderM? other) => Id == other?.Id;
+  public override bool Equals(object? obj) => Equals(obj as FavoriteFolderM);
   public override int GetHashCode() => Id;
-  public static bool operator ==(FavoriteFolderM a, FavoriteFolderM b) => a?.Equals(b) ?? b is null;
-  public static bool operator !=(FavoriteFolderM a, FavoriteFolderM b) => !(a == b);
+  public static bool operator ==(FavoriteFolderM? a, FavoriteFolderM? b) {
+    if (ReferenceEquals(a, b)) return true;
+    if (a is null || b is null) return false;
+    return a.Equals(b);
+  }
+  public static bool operator !=(FavoriteFolderM? a, FavoriteFolderM? b) => !(a == b);
   #endregion
 
-  private FolderM _folder;
+  private FolderM _folder = null!;
 
   public int Id { get; } = id;
   public FolderM Folder { get => _folder; set { _folder = value; OnPropertyChanged(); } }
