6a74701c4c231b92fb84b987363ee5a9fa2db069
martin-holy <martin.holy@hotmail.com>
2024-07-12 19:43:15 +0200

    [C] DriveM nullable

--- src/PictureManager.Common/Models/DriveM.cs
+++ src/PictureManager.Common/Models/DriveM.cs
@@ -2,6 +2,6 @@
 
 namespace PictureManager.Common.Models; 
 
-public class DriveM(int id, string name, ITreeItem parent, string sn) : FolderM(id, name, parent) {
+public class DriveM(int id, string name, ITreeItem? parent, string sn) : FolderM(id, name, parent) {
   public string SerialNumber { get; set; } = sn;
 }
