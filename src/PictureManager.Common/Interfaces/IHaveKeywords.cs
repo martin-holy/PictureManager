cb465a1ea0ddda4445325b6c4f74c76ca15c5b52
martin-holy <martin.holy@hotmail.com>
2024-07-12 20:15:20 +0200

    [C] IHaveKeywords nullable

--- src/PictureManager.Common/Interfaces/IHaveKeywords.cs
+++ src/PictureManager.Common/Interfaces/IHaveKeywords.cs
@@ -7,19 +7,19 @@ using System.Linq;
 namespace PictureManager.Common.Interfaces;
 
 public interface IHaveKeywords {
-    public List<KeywordM> Keywords { get; set; }
-    public IEnumerable<KeywordM> GetKeywords() => Keywords.GetKeywords();
+  public List<KeywordM>? Keywords { get; set; }
+  public IEnumerable<KeywordM> GetKeywords() => Keywords!.GetKeywords();
 }
 
 public static class HaveKeywordsExtensions {
-  public static IEnumerable<KeywordM> GetKeywords(this IEnumerable<IHaveKeywords> items) =>
+  public static IEnumerable<KeywordM> GetKeywords(this IEnumerable<IHaveKeywords>? items) =>
     items
       .EmptyIfNull()
       .SelectMany(x => x.GetKeywords())
       .Distinct();
 
   public static IEnumerable<T> GetBy<T>(this IEnumerable<T> items, KeywordM keyword, bool recursive) where T : IHaveKeywords {
-    var arr = recursive ? keyword.Flatten().ToArray() : new[] { keyword };
+    var arr = recursive ? keyword.Flatten().ToArray() : [keyword];
     return items.Where(x => x.Keywords?.Any(k => arr.Any(ar => ReferenceEquals(ar, k))) == true);
   }
 }
