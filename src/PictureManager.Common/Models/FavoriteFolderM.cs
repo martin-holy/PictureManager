2438eedede4c2e63b49a575257e551862e943557
martin-holy <martin.holy@hotmail.com>
2024-07-12 19:41:27 +0200

    [C] FavoriteFolderM nullable

--- src/PictureManager.Common/Models/FavoriteFolderM.cs
+++ src/PictureManager.Common/Models/FavoriteFolderM.cs
@@ -3,7 +3,7 @@ using System;
 
 namespace PictureManager.Common.Models;
 
-public sealed class FavoriteFolderM(int id, string name) : TreeItem(Res.IconFolder, name), IEquatable<FavoriteFolderM> {
+public sealed class FavoriteFolderM(int id, string name, FolderM folder) : TreeItem(Res.IconFolder, name), IEquatable<FavoriteFolderM> {
   #region IEquatable implementation
   public bool Equals(FavoriteFolderM? other) => Id == other?.Id;
   public override bool Equals(object? obj) => Equals(obj as FavoriteFolderM);
@@ -16,8 +16,6 @@ public sealed class FavoriteFolderM(int id, string name) : TreeItem(Res.IconFold
   public static bool operator !=(FavoriteFolderM? a, FavoriteFolderM? b) => !(a == b);
   #endregion
 
-  private FolderM _folder = null!;
-
   public int Id { get; } = id;
-  public FolderM Folder { get => _folder; set { _folder = value; OnPropertyChanged(); } }
+  public FolderM Folder { get => folder; set { folder = value; OnPropertyChanged(); } }
 }
