4b537522b08b16438ec0bfcad9e6797fcc068c41
martin-holy <martin.holy@hotmail.com>
2024-07-13 12:30:11 +0200

    [C] ToolsTabsVM nullable

--- src/PictureManager.Common/ViewModels/ToolsTabsVM.cs
+++ src/PictureManager.Common/ViewModels/ToolsTabsVM.cs
@@ -6,24 +6,24 @@ using PictureManager.Common.ViewModels.Entities;
 namespace PictureManager.Common.ViewModels;
 
 public sealed class ToolsTabsVM : TabControl {
-  public PeopleToolsTabVM PeopleTab { get; private set; }
-  public PersonDetailVM PersonDetailTab { get; private set; }
+  public PeopleToolsTabVM? PeopleTab { get; private set; }
+  public PersonDetailVM? PersonDetailTab { get; private set; }
 
-  public static RelayCommand<PersonM> OpenPersonTabCommand { get; set; }
-  public static RelayCommand OpenPeopleTabCommand { get; set; }
+  public static RelayCommand<PersonM> OpenPersonTabCommand { get; set; } = null!;
+  public static RelayCommand OpenPeopleTabCommand { get; set; } = null!;
 
   public ToolsTabsVM() : base(new(Dock.Top, Dock.Right, new SlidePanelPinButton()) { JustifyTabSize = true }) {
     OpenPersonTabCommand = new(OpenPersonTab, Res.IconInformation, "Detail");
     OpenPeopleTabCommand = new(() => OpenPeopleTab(null), Res.IconPeopleMultiple, "People");
   }
 
-  public void OpenPeopleTab(PersonM[] people) {
+  public void OpenPeopleTab(PersonM[]? people) {
     PeopleTab ??= new();
     PeopleTab.Reload(people);
     Activate(Res.IconPeopleMultiple, "People", PeopleTab);
   }
 
-  private void OpenPersonTab(PersonM person) {
+  private void OpenPersonTab(PersonM? person) {
     PersonDetailTab ??= new(Core.S.Person, Core.S.Segment);
     PersonDetailTab.Reload(person);
     Activate(Res.IconPeople, "Person", PersonDetailTab);
