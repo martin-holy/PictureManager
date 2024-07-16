2dc2fa244d7adc9f272fa1baaaa9ca3e7c079fa2
martin-holy <martin.holy@hotmail.com>
2024-07-16 10:05:06 +0200

    [C] ErrorDialogM nullable

--- src/PictureManager.Common/Dialogs/ErrorDialogM.cs
+++ src/PictureManager.Common/Dialogs/ErrorDialogM.cs
@@ -4,18 +4,15 @@ using System;
 namespace PictureManager.Common.Dialogs;
 
 public sealed class ErrorDialogM : Dialog {
-  private string _message;
-  private string _detail;
-
-  public string Message { get => _message; set { _message = value; OnPropertyChanged(); } }
-  public string Detail { get => _detail; set { _detail = value; OnPropertyChanged(); } }
+  public string Message { get; }
+  public string Detail { get; }
 
   public ErrorDialogM(Exception ex, string title = "") : base(title, Res.IconBug) {
     Message = ex.Message;
     Detail = ex.InnerException == null
-      ? ex.StackTrace
+      ? ex.StackTrace ?? string.Empty
       : $"{ex.InnerException.Message}\n{ex.StackTrace}";
-    Buttons = new DialogButton[] { new(CloseCommand, false, true) };
+    Buttons = [new(CloseCommand, false, true)];
 
     if (string.IsNullOrEmpty(title))
       Title = "Error";
