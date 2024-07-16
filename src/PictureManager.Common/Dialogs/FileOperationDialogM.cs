39248248671bfa0ecf235d0789c43065c7ae8e12
martin-holy <martin.holy@hotmail.com>
2024-07-16 15:13:03 +0200

    [C] FileOperationDialogM nullable

--- src/PictureManager.Common/Dialogs/FileOperationDialogM.cs
+++ src/PictureManager.Common/Dialogs/FileOperationDialogM.cs
@@ -9,18 +9,18 @@ namespace PictureManager.Common.Dialogs;
 public sealed class FileOperationDialogM : Dialog {
   private int _progressValue;
   private bool _isIndeterminate;
-  private string _dirFrom;
-  private string _dirTo;
-  private string _fileName;
+  private string _dirFrom = string.Empty;
+  private string _dirTo = string.Empty;
+  private string _fileName = string.Empty;
 
   public int ProgressValue { get => _progressValue; set { _progressValue = value; OnPropertyChanged(); } }
   public bool IsIndeterminate { get => _isIndeterminate; set { _isIndeterminate = value; OnPropertyChanged(); } }
   public string DirFrom { get => _dirFrom; set { _dirFrom = value; OnPropertyChanged(); } }
   public string DirTo { get => _dirTo; set { _dirTo = value; OnPropertyChanged(); } }
   public string FileName { get => _fileName; set { _fileName = value; OnPropertyChanged(); } }
-  public CancellationTokenSource WorkCts { get; private set; } = new();
-  public object WorkResult { get; private set; }
-  public Task WorkTask { get; set; }
+  public CancellationTokenSource? WorkCts { get; private set; } = new();
+  public object? WorkResult { get; private set; }
+  public Task? WorkTask { get; set; }
   public IProgress<object[]> Progress { get; set; }
 
   public FileOperationDialogM(string title) : base(title, Res.IconImageMultiple) {
@@ -28,12 +28,12 @@ public sealed class FileOperationDialogM : Dialog {
 
     Progress = new Progress<object[]>(e => {
       ProgressValue = (int)e[0];
-      DirFrom = e[1].ToString();
-      DirTo = e[2].ToString();
-      FileName = e[3].ToString();
+      DirFrom = (string)e[1];
+      DirTo = (string)e[2];
+      FileName = (string)e[3];
     });
 
-    Buttons = new DialogButton[] { new(CancelCommand, false, true) };
+    Buttons = [new(CancelCommand, false, true)];
   }
 
   ~FileOperationDialogM() {
@@ -53,6 +53,6 @@ public sealed class FileOperationDialogM : Dialog {
   public override Task OnResultChanged(int result) {
     if (result != 0 || WorkCts == null) return Task.CompletedTask;
     WorkCts.Cancel();
-    return WorkTask;
+    return WorkTask ?? Task.CompletedTask;
   }
 }
