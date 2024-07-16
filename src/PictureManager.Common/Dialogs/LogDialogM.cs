dc20343c6296ff026156735cad53fe4dc5bf3293
martin-holy <martin.holy@hotmail.com>
2024-07-16 15:21:35 +0200

    [C] LogDialogM

--- src/PictureManager.Common/Dialogs/LogDialogM.cs
+++ src/PictureManager.Common/Dialogs/LogDialogM.cs
@@ -5,8 +5,9 @@ namespace PictureManager.Common.Dialogs;
 
 public sealed class LogDialogM : Dialog {
   public LogDialogM() : base("Log", Res.IconSort) {
-    Buttons = new DialogButton[] {
+    Buttons = [
       new(new(() => { Log.Items.Clear(); Result = 1; }, null, "Clear"), true),
-      new(CloseCommand, false, true) };
+      new(CloseCommand, false, true)
+    ];
   }
 }
