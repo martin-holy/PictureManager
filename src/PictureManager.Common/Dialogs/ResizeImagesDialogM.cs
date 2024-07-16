18b9535ff78eaad87f6cc362fa5d7176d4a965af
martin-holy <martin.holy@hotmail.com>
2024-07-16 15:41:11 +0200

    [C] ResizeImagesDialogM nullable

--- src/PictureManager.Common/Dialogs/ResizeImagesDialogM.cs
+++ src/PictureManager.Common/Dialogs/ResizeImagesDialogM.cs
@@ -10,118 +10,118 @@ using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace PictureManager.Common.Dialogs {
-  public sealed class ResizeImagesDialogM : Dialog {
-    private CancellationTokenSource _cts;
-    private readonly ImageM[] _items;
-    private bool _preserveThumbnail;
-    private bool _preserveMetadata;
-    private string _fileName;
-    private string _destDir;
-    private double _mpx;
-    private double _maxMpx;
-    private int _progressMax;
-    private int _progressValue;
-    private ObservableCollection<string> _dirPaths;
+namespace PictureManager.Common.Dialogs;
 
-    public bool PreserveThumbnail { get => _preserveThumbnail; set { _preserveThumbnail = value; OnPropertyChanged(); } }
-    public bool PreserveMetadata { get => _preserveMetadata; set { _preserveMetadata = value; OnPropertyChanged(); } }
-    public string FileName { get => _fileName; set { _fileName = value; OnPropertyChanged(); } }
-    public string DestDir { get => _destDir; set { _destDir = value; OnPropertyChanged(); } }
-    public double Mpx { get => _mpx; set { _mpx = value; OnPropertyChanged(); } }
-    public double MaxMpx { get => _maxMpx; set { _maxMpx = value; OnPropertyChanged(); } }
-    public int ProgressMax { get => _progressMax; set { _progressMax = value; OnPropertyChanged(); } }
-    public int ProgressValue { get => _progressValue; set { _progressValue = value; OnPropertyChanged(); } }
-    public ObservableCollection<string> DirPaths { get => _dirPaths; set { _dirPaths = value; OnPropertyChanged(); } }
+public sealed class ResizeImagesDialogM : Dialog {
+  private CancellationTokenSource? _cts;
+  private readonly ImageM[] _items;
+  private bool _preserveThumbnail;
+  private bool _preserveMetadata;
+  private string _fileName = string.Empty;
+  private string? _destDir;
+  private double _mpx;
+  private double _maxMpx;
+  private int _progressMax;
+  private int _progressValue;
 
-    public RelayCommand OpenFolderBrowserCommand { get; }
+  public bool PreserveThumbnail { get => _preserveThumbnail; set { _preserveThumbnail = value; OnPropertyChanged(); } }
+  public bool PreserveMetadata { get => _preserveMetadata; set { _preserveMetadata = value; OnPropertyChanged(); } }
+  public string FileName { get => _fileName; set { _fileName = value; OnPropertyChanged(); } }
+  public string? DestDir { get => _destDir; set { _destDir = value; OnPropertyChanged(); } }
+  public double Mpx { get => _mpx; set { _mpx = value; OnPropertyChanged(); } }
+  public double MaxMpx { get => _maxMpx; set { _maxMpx = value; OnPropertyChanged(); } }
+  public int ProgressMax { get => _progressMax; set { _progressMax = value; OnPropertyChanged(); } }
+  public int ProgressValue { get => _progressValue; set { _progressValue = value; OnPropertyChanged(); } }
+  public ObservableCollection<string> DirPaths { get; }
 
-    public ResizeImagesDialogM(ImageM[] items) : base("Resize Images", Res.IconImageMultiple) {
-      OpenFolderBrowserCommand = new(OpenFolderBrowser, Res.IconFolder, "Select folder");
-      Buttons = new DialogButton[] {
-        new(new(Resize, null, "Resize"), true),
-        new(CloseCommand, false, true) }; 
-      _items = items;
-      ProgressMax = _items.Length;
-      DirPaths = new(Core.Settings.Common.DirectorySelectFolders.EmptyIfNull());
-      SetMaxMpx();
+  public RelayCommand OpenFolderBrowserCommand { get; }
+
+  public ResizeImagesDialogM(ImageM[] items) : base("Resize Images", Res.IconImageMultiple) {
+    OpenFolderBrowserCommand = new(OpenFolderBrowser, Res.IconFolder, "Select folder");
+    Buttons = [
+      new(new(Resize, () => !string.IsNullOrEmpty(DestDir), null, "Resize"), true),
+      new(CloseCommand, false, true)
+    ]; 
+    _items = items;
+    ProgressMax = _items.Length;
+    DirPaths = new(Core.Settings.Common.DirectorySelectFolders.EmptyIfNull());
+    SetMaxMpx();
+  }
+
+  public override Task OnResultChanged(int result) {
+    if (result == 0 && _cts != null) _cts.Cancel();
+    return Task.CompletedTask;
+  }
+
+  private void SetMaxMpx() {
+    var maxPx = 0;
+    foreach (var mi in _items) {
+      var px = mi.Width * mi.Height;
+      if (px > maxPx) maxPx = px;
     }
 
-    public override Task OnResultChanged(int result) {
-      if (result == 0 && _cts != null) _cts.Cancel();
-      return Task.CompletedTask;
-    }
+    MaxMpx = Math.Round(maxPx / 1000000.0, 1);
+    Mpx = MaxMpx;
+  }
 
-    private void SetMaxMpx() {
-      var maxPx = 0;
-      foreach (var mi in _items) {
-        var px = mi.Width * mi.Height;
-        if (px > maxPx) maxPx = px;
+  private void Resize() {
+    if (!Directory.Exists(DestDir)) {
+      try {
+        Directory.CreateDirectory(DestDir!);
       }
-
-      MaxMpx = Math.Round(maxPx / 1000000.0, 1);
-      Mpx = MaxMpx;
-    }
-
-    private void Resize() {
-      if (!Directory.Exists(DestDir)) {
-        try {
-          Directory.CreateDirectory(DestDir);
-        }
-        catch (Exception ex) {
-          Log.Error(ex);
-          return;
-        }
+      catch (Exception ex) {
+        Log.Error(ex);
+        return;
       }
-
-      ResizeImages(DestDir, Convert.ToInt32(Mpx * 1000000), PreserveMetadata, PreserveThumbnail);
     }
 
-    private async void ResizeImages(string destination, int px, bool withMetadata, bool withThumbnail) {
-      _cts = new();
+    ResizeImages(DestDir!, Convert.ToInt32(Mpx * 1000000), PreserveMetadata, PreserveThumbnail);
+  }
 
-      await Task.Run(() => {
-        try {
-          var index = 0;
-          var po = new ParallelOptions { MaxDegreeOfParallelism = Environment.ProcessorCount, CancellationToken = _cts.Token };
+  private async void ResizeImages(string destination, int px, bool withMetadata, bool withThumbnail) {
+    _cts = new();
 
-          Parallel.ForEach(_items, po, mi => {
-            index++;
-            Tasks.RunOnUiThread(() => {
-              ProgressValue = index;
-              FileName = mi.FileName;
-            });
+    await Task.Run(() => {
+      try {
+        var index = 0;
+        var po = new ParallelOptions { MaxDegreeOfParallelism = Environment.ProcessorCount, CancellationToken = _cts.Token };
 
-            try {
-              var dest = Path.Combine(destination, mi.FileName);
-              Imaging.ResizeJpg(mi.FilePath, dest, px, withMetadata, withThumbnail, Core.Settings.Common.JpegQuality);
-            }
-            catch (Exception ex) {
-              Log.Error(ex, mi.FilePath);
-            }
+        Parallel.ForEach(_items, po, mi => {
+          index++;
+          Tasks.RunOnUiThread(() => {
+            ProgressValue = index;
+            FileName = mi.FileName;
           });
-        }
-        catch (OperationCanceledException) { }
-        finally {
-          _cts.Dispose();
-          _cts = null;
-          Tasks.RunOnUiThread(() => { Result = 1; });
-        }
-      });
-    }
 
-    private void OpenFolderBrowser() {
-      var dir = new FolderBrowserDialogM();
-
-      if (Show(dir) != 1) return;
-
-      if (!DirPaths.Contains(dir.SelectedFolder.FullPath)) {
-        DirPaths.Insert(0, dir.SelectedFolder.FullPath);
-        Core.Settings.Common.DirectorySelectFolders = DirPaths.ToArray();
-        Core.Settings.Save();
+          try {
+            var dest = Path.Combine(destination, mi.FileName);
+            Imaging.ResizeJpg(mi.FilePath, dest, px, withMetadata, withThumbnail, Core.Settings.Common.JpegQuality);
+          }
+          catch (Exception ex) {
+            Log.Error(ex, mi.FilePath);
+          }
+        });
       }
+      catch (OperationCanceledException) { }
+      finally {
+        _cts.Dispose();
+        _cts = null;
+        Tasks.RunOnUiThread(() => { Result = 1; });
+      }
+    });
+  }
 
-      DestDir = dir.SelectedFolder.FullPath;
+  private void OpenFolderBrowser() {
+    var dir = new FolderBrowserDialogM();
+
+    if (Show(dir) != 1 || dir.SelectedFolder == null) return;
+
+    if (!DirPaths.Contains(dir.SelectedFolder.FullPath)) {
+      DirPaths.Insert(0, dir.SelectedFolder.FullPath);
+      Core.Settings.Common.DirectorySelectFolders = DirPaths.ToArray();
+      Core.Settings.Save();
     }
+
+    DestDir = dir.SelectedFolder.FullPath;
   }
 }
