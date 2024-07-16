adebe1a2d32536d5df64e1ec9bbc59be6a735fd5
martin-holy <martin.holy@hotmail.com>
2024-07-16 09:31:24 +0200

    [C] AboutDialogM version prop

--- src/PictureManager.Common/Dialogs/AboutDialogM.cs
+++ src/PictureManager.Common/Dialogs/AboutDialogM.cs
@@ -3,27 +3,25 @@ using MH.Utils.BaseClasses;
 using System.Diagnostics;
 using System.Reflection;
 
-namespace PictureManager.Common.Dialogs {
-  public sealed class AboutDialogM : Dialog {
-    private FileVersionInfo _version;
+namespace PictureManager.Common.Dialogs;
 
-    public FileVersionInfo Version { get => _version; set { _version = value; OnPropertyChanged(); } }
-    public string HomePageUrl => "https://github.com/martin-holy/PictureManager";
-    public string TelegramUrl => "https://t.me/PictureManager";
+public sealed class AboutDialogM : Dialog {
+  public FileVersionInfo Version { get; }
+  public string HomePageUrl => "https://github.com/martin-holy/PictureManager";
+  public string TelegramUrl => "https://t.me/PictureManager";
 
-    public RelayCommand OpenHomePageCommand { get; }
-    public RelayCommand OpenTelegramCommand { get; }
+  public RelayCommand OpenHomePageCommand { get; }
+  public RelayCommand OpenTelegramCommand { get; }
     
-    public AboutDialogM() : base("About", MH.UI.Res.IconImage) {
-      Version = FileVersionInfo.GetVersionInfo(Assembly.GetExecutingAssembly().Location);
+  public AboutDialogM() : base("About", MH.UI.Res.IconImage) {
+    Version = FileVersionInfo.GetVersionInfo(Assembly.GetExecutingAssembly().Location);
 
-      OpenHomePageCommand = new(() => OpenUrl(HomePageUrl));
-      OpenTelegramCommand = new(() => OpenUrl(TelegramUrl));
-    }
-
-    private static void OpenUrl(string url) =>
-      Process.Start(new ProcessStartInfo(url) {
-        UseShellExecute = true
-      });
+    OpenHomePageCommand = new(() => OpenUrl(HomePageUrl));
+    OpenTelegramCommand = new(() => OpenUrl(TelegramUrl));
   }
+
+  private static void OpenUrl(string url) =>
+    Process.Start(new ProcessStartInfo(url) {
+      UseShellExecute = true
+    });
 }
