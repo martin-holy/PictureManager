5484c5646a13783530002927cb37a8970a8dca1c
martin-holy <martin.holy@hotmail.com>
2024-07-18 15:25:05 +0200

    [C] StringUtils nullable

--- src/PictureManager.Common/Utils/StringUtils.cs
+++ src/PictureManager.Common/Utils/StringUtils.cs
@@ -1,13 +1,13 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 
-namespace PictureManager.Common.Utils {
-  public static class StringUtils {
-    private static readonly HashSet<char> CommentAllowedCharacters = new("@#$€_&+-()*'.:;!?=<>% ");
+namespace PictureManager.Common.Utils;
 
-    public static string NormalizeComment(string comment) =>
-      string.IsNullOrEmpty(comment)
-        ? null
-        : new string(comment.Where(x => char.IsLetterOrDigit(x) || CommentAllowedCharacters.Contains(x)).ToArray());
-  }
+public static class StringUtils {
+  private static readonly HashSet<char> _commentAllowedCharacters = new("@#$€_&+-()*'.:;!?=<>% ");
+
+  public static string? NormalizeComment(string? comment) =>
+    string.IsNullOrEmpty(comment)
+      ? null
+      : new string(comment.Where(x => char.IsLetterOrDigit(x) || _commentAllowedCharacters.Contains(x)).ToArray());
 }
