b73218eb9d2294d290e9a6a3023f8ea68884a837
martin-holy <martin.holy@hotmail.com>
2024-07-19 09:13:50 +0200

    [C] SegmentsViewWidthConverter nullable

--- src/PictureManager.Windows.WPF/Converters/SegmentsViewWidthConverter.cs
+++ src/PictureManager.Windows.WPF/Converters/SegmentsViewWidthConverter.cs
@@ -5,10 +5,10 @@ namespace PictureManager.Windows.WPF.Converters;
 
 public class SegmentsViewWidthConverter : BaseConverter {
   private static readonly object _lock = new();
-  private static SegmentsViewWidthConverter _inst;
+  private static SegmentsViewWidthConverter? _inst;
   public static SegmentsViewWidthConverter Inst { get { lock (_lock) { return _inst ??= new(); } } }
 
-  public override object Convert(object value, object parameter) =>
+  public override object Convert(object? value, object? parameter) =>
     value is int size && int.TryParse(parameter as string, out var count) ?
       (size + 1) * count + 15 + 2 + 1 //(size + 1) * count + ScrollBar + Margin + ToBeSure
       : Binding.DoNothing;
