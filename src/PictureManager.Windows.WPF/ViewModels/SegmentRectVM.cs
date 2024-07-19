bd90d84beb79b02509257e98996064cee99a71ec
martin-holy <martin.holy@hotmail.com>
2024-07-19 13:28:53 +0200

    [C] SegmentRectVM nullable

--- src/PictureManager.Windows.WPF/ViewModels/SegmentRectVM.cs
+++ src/PictureManager.Windows.WPF/ViewModels/SegmentRectVM.cs
@@ -7,44 +7,44 @@ using System.Windows.Input;
 namespace PictureManager.Windows.WPF.ViewModels;
 
 public sealed class SegmentRectVM : ObservableObject {
-  private IInputElement _view;
+  private IInputElement _view = null!;
     
   public SegmentRectS SegmentRectS { get; }
     
   public RelayCommand<RoutedEventArgs> SetViewCommand { get; }
-  public static RelayCommand<MouseEventArgs> SetCurrentCommand { get; set; }
-  public static RelayCommand<MouseButtonEventArgs> CreateCommand { get; set; }
-  public static RelayCommand<MouseEventArgs> EditCommand { get; set; }
-  public static RelayCommand EndEditCommand { get; set; }
-  public static RelayCommand<SegmentRectM> DeleteCommand { get; set; }
+  public static RelayCommand<MouseEventArgs> SetCurrentCommand { get; set; } = null!;
+  public static RelayCommand<MouseButtonEventArgs> CreateCommand { get; set; } = null!;
+  public static RelayCommand<MouseEventArgs> EditCommand { get; set; } = null!;
+  public static RelayCommand EndEditCommand { get; set; } = null!;
+  public static RelayCommand<SegmentRectM> DeleteCommand { get; set; } = null!;
 
   public SegmentRectVM(SegmentRectS segmentRectS) {
     SegmentRectS = segmentRectS;
 
-    SetViewCommand = new(e => _view = (IInputElement)e.Source);
+    SetViewCommand = new(x => _view = (IInputElement)x!.Source, x => x != null);
     SetCurrentCommand = new(SetCurrent);
     CreateCommand = new(Create, () => SegmentRectS.AreVisible);
     EditCommand = new(Edit);
     EndEditCommand = new(SegmentRectS.EndEdit);
-    DeleteCommand = new(SegmentRectS.Delete, MH.UI.Res.IconXCross, "Delete");
+    DeleteCommand = new(x => SegmentRectS.Delete(x!), x => x != null, MH.UI.Res.IconXCross, "Delete");
   }
 
-  private void SetCurrent(MouseEventArgs e) {
-    if (e.Source is FrameworkElement fe && (fe.Name.Equals("PART_MovePoint") || fe.Name.Equals("PART_ResizeBorder"))) {
+  private void SetCurrent(MouseEventArgs? e) {
+    if (e?.Source is FrameworkElement fe && (fe.Name.Equals("PART_MovePoint") || fe.Name.Equals("PART_ResizeBorder"))) {
       var pos = e.GetPosition(_view);
       SegmentRectS.SetCurrent((SegmentRectM)fe.DataContext, pos.X, pos.Y);
     }
   }
 
-  private void Create(MouseButtonEventArgs e) {
-    if ((Keyboard.Modifiers & ModifierKeys.Control) > 0 || e.RightButton == MouseButtonState.Pressed) {
+  private void Create(MouseButtonEventArgs? e) {
+    if (e != null && ((Keyboard.Modifiers & ModifierKeys.Control) > 0 || e.RightButton == MouseButtonState.Pressed)) {
       var pos = e.GetPosition(_view);
       SegmentRectS.CreateNew(pos.X, pos.Y);
     }
   }
 
-  private void Edit(MouseEventArgs e) {
-    if (SegmentRectS.Current == null) return;
+  private void Edit(MouseEventArgs? e) {
+    if (e == null || SegmentRectS.Current == null) return;
 
     if (e.RightButton != MouseButtonState.Pressed && e.LeftButton != MouseButtonState.Pressed) {
       SegmentRectS.EndEdit();
