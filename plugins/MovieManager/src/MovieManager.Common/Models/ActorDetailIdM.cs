f93fc6692c3b7df83e716ed3a6766a7ac4030929
martin-holy <martin.holy@hotmail.com>
2024-07-19 14:04:42 +0200

    [C] ActorDetailIdM nullable

--- plugins/MovieManager/src/MovieManager.Common/Models/ActorDetailIdM.cs
+++ plugins/MovieManager/src/MovieManager.Common/Models/ActorDetailIdM.cs
@@ -1,5 +1,5 @@
 ﻿namespace MovieManager.Common.Models;
 
-public class ActorDetailIdM(int id, string detailId, string detailName) : BaseDetailIdM(id, detailId, detailName) {
-  public ActorM Actor { get; set; }
+public class ActorDetailIdM(int id, string detailId, string detailName, ActorM actor) : BaseDetailIdM(id, detailId, detailName) {
+  public ActorM Actor { get; set; } = actor;
 }
