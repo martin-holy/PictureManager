16455b95ec2ed07eb6e4cffe1e1907a84af53860
martin-holy <martin.holy@hotmail.com>
2024-07-19 09:12:32 +0200

    [C] RatingConverter nullable

--- src/PictureManager.Windows.WPF/Converters/RatingConverter.cs
+++ src/PictureManager.Windows.WPF/Converters/RatingConverter.cs
@@ -6,13 +6,13 @@ namespace PictureManager.Windows.WPF.Converters;
 
 public class RatingConverter : BaseConverter {
   private static readonly object _lock = new();
-  private static RatingConverter _inst;
+  private static RatingConverter? _inst;
   public static RatingConverter Inst { get { lock (_lock) { return _inst ??= new(); } } }
 
   private static readonly SolidColorBrush _white = new(Color.FromRgb(255, 255, 255));
   private static readonly SolidColorBrush _gray = new(Color.FromRgb(104, 104, 104));
 
-  public override object Convert(object value, object parameter) =>
+  public override object Convert(object? value, object? parameter) =>
     value is not int v || !int.TryParse(parameter as string, out int p)
       ? Binding.DoNothing
       : p < v
