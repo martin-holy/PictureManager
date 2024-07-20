c9cf84d69c43546736a48b29e2934efe538a0dda
martin-holy <martin.holy@hotmail.com>
2024-07-20 11:26:49 +0200

    [C] MovieM nullable

--- plugins/MovieManager/src/MovieManager.Common/Models/MovieM.cs
+++ plugins/MovieManager/src/MovieManager.Common/Models/MovieM.cs
@@ -1,6 +1,5 @@
 ﻿using MH.Utils;
 using MH.Utils.BaseClasses;
-using MH.Utils.Extensions;
 using MH.Utils.Interfaces;
 using PictureManager.Common.Interfaces;
 using PictureManager.Common.Models;
@@ -12,26 +11,26 @@ using System.Linq;
 
 namespace MovieManager.Common.Models;
 
-public sealed class MovieM : ObservableObject, ISelectable, IHaveKeywords {
+public sealed class MovieM(int id, string title) : ObservableObject, ISelectable, IHaveKeywords {
   private bool _isSelected;
   private double _myRating;
-  private MediaItemM _poster;
+  private MediaItemM? _poster;
 
-  public int Id { get; }
-  public string Title { get; set; }
+  public int Id { get; } = id;
+  public string Title { get; set; } = title;
   public int Year { get; set; }
   public int? YearEnd { get; set; }
   public int Length { get; set; }
   public double Rating { get; set; }
   public double MyRating { get => _myRating; set { _myRating = value; OnPropertyChanged(); } }
-  public List<GenreM> Genres { get; set; }
-  public List<KeywordM> Keywords { get; set; }
+  public List<GenreM>? Genres { get; set; }
+  public List<KeywordM>? Keywords { get; set; }
   public ObservableCollection<DateOnly> Seen { get; set; } = [];
-  public string MPAA { get; set; }
-  public string Plot { get; set; }
-  public MediaItemM Poster { get => _poster; set { _poster = value; OnPropertyChanged(); } }
-  public List<MediaItemM> MediaItems { get; set; }
-  public MovieDetailIdM DetailId { get; set; }
+  public string? MPAA { get; set; }
+  public string? Plot { get; set; }
+  public MediaItemM? Poster { get => _poster; set { _poster = value; OnPropertyChanged(); } }
+  public List<MediaItemM>? MediaItems { get; set; }
+  public MovieDetailIdM DetailId { get; set; } = null!;
 
   public bool IsSelected { get => _isSelected; set { _isSelected = value; OnPropertyChanged(); } }
   public KeywordM[] DisplayKeywords => GetDisplayKeywords();
@@ -39,18 +38,14 @@ public sealed class MovieM : ObservableObject, ISelectable, IHaveKeywords {
   public string FormatedLength =>
     TimeSpan.FromMinutes(Length).ToString(@"h\:mm");
 
-  public MovieM(int id, string title) {
-    Id = id;
-    Title = title;
-  }
-
   public override int GetHashCode() => Id;
 
   private KeywordM[] GetDisplayKeywords() =>
-    Keywords?
-      .EmptyIfNull()
-      .SelectMany(x => x.GetThisAndParents())
-      .Distinct()
-      .OrderBy(x => x.GetFullName("/", k => k.Name))
-      .ToArray();
+    Keywords == null
+      ? []
+      : Keywords
+        .SelectMany(x => x.GetThisAndParents())
+        .Distinct()
+        .OrderBy(x => x.GetFullName("/", k => k.Name))
+        .ToArray();
 }
