7d2731e58538bcdad9a2d5c6d9a4ad73ccea2985
martin-holy <martin.holy@hotmail.com>
2024-08-09 10:32:50 +0200

    [C] ImageResizeDialog based on ParallelProgressDialog

--- src/PictureManager.Common/Features/MediaItem/Image/ImageResizeDialog.cs
+++ src/PictureManager.Common/Features/MediaItem/Image/ImageResizeDialog.cs
@@ -1,102 +1,64 @@
-﻿using MH.UI.Controls;
+﻿using MH.UI.Dialogs;
 using MH.Utils;
 using MH.Utils.BaseClasses;
 using System;
 using System.IO;
-using System.Threading;
 using System.Threading.Tasks;
 
 namespace PictureManager.Common.Features.MediaItem.Image;
 
-public sealed class ImageResizeDialog : Dialog {
-  private readonly ImageM[] _items;
-  private readonly AsyncRelayCommand _resizeCommand;
+public sealed class ImageResizeDialog : ParallelProgressDialog<ImageM> {
   private bool _preserveThumbnail;
   private bool _preserveMetadata;
-  private string _fileName = string.Empty;
   private string? _destDir;
   private double _mpx;
   private double _maxMpx;
-  private int _progressMax;
-  private int _progressValue;
-  private readonly IProgress<(int, string)> _progress;
+  private int _px;
 
   public bool PreserveThumbnail { get => _preserveThumbnail; set { _preserveThumbnail = value; OnPropertyChanged(); } }
   public bool PreserveMetadata { get => _preserveMetadata; set { _preserveMetadata = value; OnPropertyChanged(); } }
-  public string FileName { get => _fileName; set { _fileName = value; OnPropertyChanged(); } }
   public string? DestDir { get => _destDir; set { _destDir = value; OnPropertyChanged(); } }
   public double Mpx { get => _mpx; set { _mpx = value; OnPropertyChanged(); } }
   public double MaxMpx { get => _maxMpx; set { _maxMpx = value; OnPropertyChanged(); } }
-  public int ProgressMax { get => _progressMax; set { _progressMax = value; OnPropertyChanged(); } }
-  public int ProgressValue { get => _progressValue; set { _progressValue = value; OnPropertyChanged(); } }
 
   public RelayCommand OpenFolderBrowserCommand { get; }
 
-  public ImageResizeDialog(ImageM[] items) : base("Resize Images", Res.IconImageMultiple) {
-    OpenFolderBrowserCommand = new(OpenFolderBrowser, Res.IconFolder, "Select folder");
-    _resizeCommand = new(Resize, () => !string.IsNullOrEmpty(_destDir), null, "Resize");
-    _items = items;
-    _progressMax = _items.Length;
-    _progress = new Progress<(int, string)>(x => {
-      ProgressValue = x.Item1;
-      FileName = x.Item2;
-    });
-
-    Buttons = [
-      new(_resizeCommand, true),
-      new(CloseCommand, false, true)
-    ]; 
-    
-    SetMaxMpx();
+  public ImageResizeDialog(ImageM[] items) : base("Resize Images", Res.IconImageMultiple, items, null, "Resize") {
+    OpenFolderBrowserCommand = new(() => DestDir = Core.VM.BrowseForFolder(), Res.IconFolder, "Select folder");
+    SetMaxMpx(items);
   }
 
-  protected override Task OnResultChanged(int result) {
-    if (result == 0) _resizeCommand.CancelCommand.Execute(null);
+  protected override bool CanAction() =>
+    !string.IsNullOrEmpty(_destDir);
+
+  protected override bool DoBefore() {
+    Directory.CreateDirectory(_destDir!);
+    _px = Convert.ToInt32(Mpx * 1000000);
+    return true;
+  }
+
+  protected override Task Do(ImageM image) {
+    ReportProgress(image.FileName);
+
+    try {
+      var dest = Path.Combine(_destDir!, image.FileName);
+      Imaging.ResizeJpg(image.FilePath, dest, _px, _preserveMetadata, _preserveThumbnail, Core.Settings.Common.JpegQuality);
+    }
+    catch (Exception ex) {
+      Log.Error(ex, image.FilePath);
+    }
+
     return Task.CompletedTask;
   }
 
-  private void SetMaxMpx() {
+  private void SetMaxMpx(ImageM[] items) {
     var maxPx = 0;
-    foreach (var mi in _items) {
+    foreach (var mi in items) {
       var px = mi.Width * mi.Height;
       if (px > maxPx) maxPx = px;
     }
 
     MaxMpx = Math.Round(maxPx / 1000000.0, 1);
     Mpx = MaxMpx;
-  }
-
-  private async Task Resize(CancellationToken token) {
-    var px = Convert.ToInt32(Mpx * 1000000);
-
-    try {
-      if (!Directory.Exists(_destDir))
-        Directory.CreateDirectory(_destDir!);
-
-      await Task.Run(() => {
-        var index = 0;
-        var po = new ParallelOptions { MaxDegreeOfParallelism = Environment.ProcessorCount, CancellationToken = token };
-
-        Parallel.ForEach(_items, po, mi => {
-          _progress.Report((index++, mi.FileName));
-
-          try {
-            var dest = Path.Combine(_destDir!, mi.FileName);
-            Imaging.ResizeJpg(mi.FilePath, dest, px, _preserveMetadata, _preserveThumbnail, Core.Settings.Common.JpegQuality);
-          }
-          catch (Exception ex) {
-            Log.Error(ex, mi.FilePath);
-          }
-        });
-      }, token);
-    }
-    finally {
-      Result = 1;
-    }
-  }
-
-  private void OpenFolderBrowser() {
-    if (Core.VM.BrowseForFolder() is { } dirPath)
-      _destDir = dirPath;
   }
 }
