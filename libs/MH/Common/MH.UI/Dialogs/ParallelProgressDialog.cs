17af09092687a5130ababb020f10716ef20e2d31
martin-holy <martin.holy@hotmail.com>
2024-08-16 12:10:27 +0200

    [C] ParallelProgressDialog: ForEachAsync

--- libs/MH/Common/MH.UI/Dialogs/ParallelProgressDialog.cs
+++ libs/MH/Common/MH.UI/Dialogs/ParallelProgressDialog.cs
@@ -1,16 +1,11 @@
-﻿using System;
-using System.Threading;
+﻿using System.Threading;
 using System.Threading.Tasks;
 
 namespace MH.UI.Dialogs;
 
 public class ParallelProgressDialog<T>(string title, string icon, T[] items, string? actionIcon, string? actionText, bool autoClose = true)
   : ProgressDialog<T>(title, icon, items, actionIcon, actionText, autoClose) {
-  public int MaxDegreeOfParallelism { get; set; } = Environment.ProcessorCount;
 
-  protected override Task Do(T[] items, CancellationToken token) {
-    var po = new ParallelOptions { MaxDegreeOfParallelism = MaxDegreeOfParallelism, CancellationToken = token };
-    Parallel.ForEach(items, po, item => Do(item));
-    return Task.CompletedTask;
-  }
+  protected override Task Do(T[] items, CancellationToken token) =>
+    Parallel.ForEachAsync(items, token, (item, _) => new(Do(item)));
 }
