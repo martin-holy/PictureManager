30ba72018697d67cd2d8e5293ff0d4d34681d206
martin-holy <martin.holy@hotmail.com>
2024-08-19 09:32:53 +0200

    [U] IPluginCore: Icon, Text and Settings props

--- src/PictureManager.Common/Interfaces/Plugin/IPluginCore.cs
+++ src/PictureManager.Common/Interfaces/Plugin/IPluginCore.cs
@@ -1,11 +1,15 @@
-﻿using System;
+﻿using MH.UI.BaseClasses;
+using System;
 using System.Threading.Tasks;
 
 namespace PictureManager.Common.Interfaces.Plugin;
 
 public interface IPluginCore {
+  public string Icon { get; }
+  public string Text { get; }
   public string Name { get; }
   public IPluginCoreVM VM { get; }
+  public UserSettings? Settings { get; }
   public Task InitAsync(Core pmCore, CoreR pmCoreR, IProgress<string> progress);
   public void AfterInit(CoreS pmCoreS, CoreVM pmCoreVM);
 }
