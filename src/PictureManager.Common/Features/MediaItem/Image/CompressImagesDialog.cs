849d8b895a3650a07f0b290ae094f8257147b050
martin-holy <martin.holy@hotmail.com>
2024-08-20 20:24:17 +0200

    [C] CompressImagesDialog rename

--- src/PictureManager.Common/Features/MediaItem/Image/CompressImagesDialog.cs
+++ src/PictureManager.Common/Features/MediaItem/Image/CompressImagesDialog.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace PictureManager.Common.Features.MediaItem.Image;
 
-public sealed class CompressDialog : ParallelProgressDialog<ImageM> {
+public sealed class CompressImagesDialog : ParallelProgressDialog<ImageM> {
   private readonly ImageS _imageS;
   private int _jpegQualityLevel;
   private long _totalSourceSize;
@@ -16,7 +16,7 @@ public sealed class CompressDialog : ParallelProgressDialog<ImageM> {
   public string TotalSourceSize => IOExtensions.FileSizeToString(_totalSourceSize);
   public string TotalCompressedSize => IOExtensions.FileSizeToString(_totalCompressedSize);
 
-  public CompressDialog(ImageM[] items, ImageS imageS, int quality) :
+  public CompressImagesDialog(ImageM[] items, ImageS imageS, int quality) :
     base("Compress images", MH.UI.Res.IconImage, items, MH.UI.Res.IconImage, "Compress", false) {
     _imageS = imageS;
     _jpegQualityLevel = quality;
