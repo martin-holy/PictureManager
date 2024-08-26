2b32a5adfaca3245b2ff7b0790c58510d76f791c
martin-holy <martin.holy@hotmail.com>
2024-08-26 19:54:12 +0200

    [C] ZoomAndPan: platform independent separation

---
+++ libs/MH/Common/MH.UI/Controls/ZoomAndPan.cs
@@ -0,0 +1,166 @@
+﻿using MH.Utils;
+using MH.Utils.BaseClasses;
+using System;
+
+namespace MH.UI.Controls;
+
+public interface IZoomAndPanHost {
+  public double Width { get; }
+  public double Height { get; }
+
+  public void StartAnimation(double toValue, double duration, bool horizontal, Action onCompleted);
+  public void StopAnimation();
+}
+
+public class ZoomAndPan : ObservableObject {
+  private double _startX;
+  private double _startY;
+  private double _originX;
+  private double _originY;
+  private bool _isZoomed;
+
+  private double _scaleX;
+  private double _scaleY;
+  private double _transformX;
+  private double _transformY;
+  private double _contentWidth;
+  private double _contentHeight;
+  private bool _isAnimationOn;
+
+  public IZoomAndPanHost? Host { get; set; }
+  public double ScaleX { get => _scaleX; set { _scaleX = value; OnPropertyChanged(); } }
+  public double ScaleY { get => _scaleY; set { _scaleY = value; OnPropertyChanged(); } }
+  public double TransformX { get => _transformX; set { _transformX = value; OnPropertyChanged(); } }
+  public double TransformY { get => _transformY; set { _transformY = value; OnPropertyChanged(); } }
+  public double ContentWidth { get => _contentWidth; set { _contentWidth = value; OnPropertyChanged(); } }
+  public double ContentHeight { get => _contentHeight; set { _contentHeight = value; OnPropertyChanged(); } }
+  public bool IsAnimationOn { get => _isAnimationOn; set { _isAnimationOn = value; OnPropertyChanged(); } }
+  public double ActualZoom => _scaleX * 100;
+
+  public event EventHandler AnimationEndedEvent = delegate { };
+
+  private void _raiseAnimationEnded() => AnimationEndedEvent(this, EventArgs.Empty);
+
+  private void _setScale(double scale, double relativeX, double relativeY) {
+    var absoluteX = (relativeX * _scaleX) + _transformX;
+    var absoluteY = (relativeY * _scaleY) + _transformY;
+    ScaleX = scale;
+    ScaleY = scale;
+    TransformX = absoluteX - (relativeX * _scaleX);
+    TransformY = absoluteY - (relativeY * _scaleY);
+    OnPropertyChanged(nameof(ActualZoom));
+  }
+
+  public void ScaleToFit() {
+    if (Host == null) return;
+    var scale = _getFitScale(_contentWidth, _contentHeight, Host.Width, Host.Height);
+    ScaleX = scale;
+    ScaleY = scale;
+    TransformX = (Host.Width - (_contentWidth * scale)) / 2;
+    TransformY = (Host.Height - (_contentHeight * scale)) / 2;
+    _isZoomed = false;
+    OnPropertyChanged(nameof(ActualZoom));
+  }
+
+  public void ScaleToFitContent(double width, double height) {
+    ContentWidth = width;
+    ContentHeight = height;
+    ScaleToFit();
+  }
+
+  private static double _getFitScale(double contentW, double contentH, double hostW, double hostH) {
+    var cw = contentW;
+    var ch = contentH;
+
+    if (cw > hostW) {
+      cw = hostW;
+      ch = contentH / (contentW / hostW);
+
+      if (ch > hostH) {
+        ch = hostH;
+        cw = contentW / (contentH / hostH);
+      }
+    }
+
+    if (ch > hostH) {
+      cw = contentW / (contentH / hostH);
+
+      if (cw > hostW)
+        cw = hostW;
+    }
+
+    return cw / contentW;
+  }
+
+  public void StartAnimation(int minDuration) {
+    if (Host == null) return;
+    var horizontal = Host.Height / _contentHeight * _contentWidth > Host.Width;
+    var scale = horizontal
+      ? Host.Height / _contentHeight
+      : Host.Width / _contentWidth;
+
+    if (scale > 1) scale = 1;
+
+    var toValue = horizontal
+      ? ((_contentWidth * scale) - Host.Width) * -1
+      : ((_contentHeight * scale) - Host.Height) * -1;
+
+    _setScale(scale, _contentWidth / 2, _contentHeight / 2);
+
+    var duration = toValue * 10 * -1 > minDuration
+      ? toValue * 10 * -1
+      : minDuration;
+
+    IsAnimationOn = true;
+    Host.StartAnimation(toValue, duration, horizontal, () => _onAnimationCompleted(toValue, horizontal));
+  }
+
+  private void _onAnimationCompleted(double toValue, bool horizontal) {
+    if (!_isAnimationOn) return;
+
+    if (horizontal)
+      TransformX = toValue;
+    else
+      TransformY = toValue;
+
+    IsAnimationOn = false;
+    Host?.StopAnimation();
+    _raiseAnimationEnded();
+  }
+
+  public void StopAnimation() {
+    if (_isAnimationOn)
+      Host?.StopAnimation();
+  }
+
+  public void OnHostSizeChanged() =>
+    ScaleToFit();
+
+  public void OnHostMouseMove(double hostPosX, double hostPosY) {
+    TransformX = _originX - (_startX - hostPosX);
+    TransformY = _originY - (_startY - hostPosY);
+  }
+
+  public void OnContentMouseDown(double hostPosX, double hostPosY, double contentPosX, double contentPosY) {
+    if (!_isZoomed)
+      _setScale(1, contentPosX, contentPosY);
+
+    _startX = hostPosX;
+    _startY = hostPosY;
+    _originX = _transformX;
+    _originY = _transformY;
+  }
+
+  public void OnContentMouseUp() {
+    if (!_isZoomed)
+      ScaleToFit();
+  }
+
+  public void OnContentMouseWheel(int delta, double contentPosX, double contentPosY) {
+    if (!Keyboard.IsCtrlOn() || (!(delta > 0) && (_scaleX < .2 || _scaleY < .2))) return;
+
+    _isZoomed = true;
+    var scale = _scaleX + (delta > 0 ? .1 : -.1);
+    _setScale(scale, contentPosX, contentPosY);
+  }
+}
---
+++ libs/MH/Windows/WPF/MH.UI.WPF/Controls/ZoomAndPanHost.cs
@@ -0,0 +1,85 @@
+﻿using MH.UI.Controls;
+using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+
+namespace MH.UI.WPF.Controls;
+
+public class ZoomAndPanHost : ContentControl, IZoomAndPanHost {
+  private Canvas _canvas = null!;
+  private UIElement _content = null!;
+  private TranslateTransform _contentTransform = null!;
+
+  public static readonly DependencyProperty ZoomAndPanProperty = DependencyProperty.Register(
+    nameof(ZoomAndPan), typeof(ZoomAndPan), typeof(ZoomAndPanHost), new(_onZoomAndPanChanged));
+
+  public ZoomAndPan ZoomAndPan {
+    get => (ZoomAndPan)GetValue(ZoomAndPanProperty);
+    set => SetValue(ZoomAndPanProperty, value);
+  }
+
+  double IZoomAndPanHost.Width => ActualWidth;
+  double IZoomAndPanHost.Height => ActualHeight;
+
+  public override void OnApplyTemplate() {
+    base.OnApplyTemplate();
+
+    _canvas = (Canvas)GetTemplateChild("PART_Canvas")!;
+    _canvas.SizeChanged += _onCanvasSizeChanged;
+    _canvas.MouseMove += _onCanvasMouseMove;
+
+    _content = (UIElement)GetTemplateChild("PART_Content")!;
+    _content.MouseLeftButtonDown += _onContentMouseLeftButtonDown;
+    _content.MouseLeftButtonUp += _onContentMouseLeftButtonUp;
+    _content.MouseWheel += _onContentMouseWheel;
+
+    _contentTransform = (TranslateTransform)GetTemplateChild("PART_ContentTransform")!;
+  }
+
+  public void StartAnimation(double toValue, double duration, bool horizontal, Action onCompleted) {
+    var animation = new DoubleAnimation(0, toValue, TimeSpan.FromMilliseconds(duration), FillBehavior.Stop);
+    animation.Completed += (_, _) => onCompleted();
+    _contentTransform.BeginAnimation(horizontal
+      ? TranslateTransform.XProperty
+      : TranslateTransform.YProperty, animation);
+  }
+
+  public void StopAnimation() =>
+    _contentTransform.BeginAnimation(TranslateTransform.XProperty, null);
+
+  private void _onCanvasSizeChanged(object sender, SizeChangedEventArgs e) =>
+    ZoomAndPan.OnHostSizeChanged();
+
+  private void _onCanvasMouseMove(object sender, MouseEventArgs e) {
+    if (!_content.IsMouseCaptured) return;
+    var hostPos = e.GetPosition(_canvas);
+    ZoomAndPan.OnHostMouseMove(hostPos.X, hostPos.Y);
+  }
+
+  private void _onContentMouseLeftButtonDown(object sender, MouseButtonEventArgs e) {
+    var hostPos = e.GetPosition(_canvas);
+    var contentPos = e.GetPosition(_content);
+    ZoomAndPan.OnContentMouseDown(hostPos.X, hostPos.Y, contentPos.X, contentPos.Y);
+    _canvas.Cursor = Cursors.Hand;
+    _content.CaptureMouse();
+  }
+
+  private void _onContentMouseLeftButtonUp(object sender, MouseButtonEventArgs e) {
+    _canvas.Cursor = Cursors.Arrow;
+    _content.ReleaseMouseCapture();
+    ZoomAndPan.OnContentMouseUp();
+  }
+
+  private void _onContentMouseWheel(object sender, MouseWheelEventArgs e) {
+    var contentPos = e.GetPosition(_content);
+    ZoomAndPan.OnContentMouseWheel(e.Delta, contentPos.X, contentPos.Y);
+  }
+
+  private static void _onZoomAndPanChanged(DependencyObject d, DependencyPropertyChangedEventArgs e) {
+    if (d is not ZoomAndPanHost host) return;
+    host.ZoomAndPan.Host = host;
+  }
+}
