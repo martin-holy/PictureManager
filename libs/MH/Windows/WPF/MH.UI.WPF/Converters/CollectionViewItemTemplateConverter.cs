1bee510ed2a809d2163b9c5fe7e3c597a835f5b1
martin-holy <martin.holy@hotmail.com>
2024-09-13 18:17:40 +0200

    [U] CollectionViewItemTemplateConverter

--- libs/MH/Windows/WPF/MH.UI.WPF/Converters/CollectionViewItemTemplateConverter.cs
+++ libs/MH/Windows/WPF/MH.UI.WPF/Converters/CollectionViewItemTemplateConverter.cs
@@ -12,9 +12,30 @@ public sealed class CollectionViewItemTemplateConverter : BaseConverter {
   private static CollectionViewItemTemplateConverter? _inst;
   public static CollectionViewItemTemplateConverter Inst { get { lock (_lock) { return _inst ??= new(); } } }
 
-  public override object? Convert(object? value, object? parameter) =>
+  // TODO new version
+  /*public override object? Convert(object? value, object? parameter) =>
+    value is FrameworkElement fe && fe.TryFindParent<StackPanel>() is
+      { DataContext: ITreeItem { Parent: ICollectionViewGroup g } }
+      ? ResourceConverter.Inst.Convert(g.GetItemTemplateName(), null) as DataTemplate
+      : null;*/
+
+  // TODO temporary version
+  public override object? Convert(object? value, object? parameter) {
+    if (value is not FrameworkElement fe || fe.TryFindParent<StackPanel>()
+          is not { DataContext: ITreeItem { Parent: ICollectionViewGroup g } })
+      return null;
+
+    var templateName = g.GetItemTemplateName();
+    if (string.IsNullOrEmpty(templateName))
+      return g.UIView is CollectionView cv ? cv.InnerItemTemplate : null;
+
+    return ResourceConverter.Inst.Convert(templateName, null) as DataTemplate;
+  }
+
+  // TODO old version
+  /*public override object? Convert(object? value, object? parameter) =>
     value is FrameworkElement fe && fe.TryFindParent<StackPanel>() is
       { DataContext: ITreeItem { Parent: ICollectionViewGroup { UIView: CollectionView cv } } }
       ? cv.InnerItemTemplate
-      : null;
+      : null;*/
 }
