2912483a58122369e6df5ee25b728df6ecb50716
martin-holy <martin.holy@hotmail.com>
2024-09-24 16:34:03 +0200

    [C] GeoNameVM refactoring

--- src/PictureManager.Common/Features/GeoName/GeoNameVM.cs
+++ src/PictureManager.Common/Features/GeoName/GeoNameVM.cs
@@ -1,20 +1,22 @@
 ﻿using MH.UI.Controls;
 using MH.UI.Dialogs;
 using MH.Utils.BaseClasses;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace PictureManager.Common.Features.GeoName;
 
 public sealed class GeoNameVM {
   private readonly GeoNameR _r;
 
-  public static RelayCommand NewGeoNameFromGpsCommand { get; set; } = null!;
+  public static AsyncRelayCommand NewGeoNameFromGpsCommand { get; private set; } = null!;
 
-  public GeoNameVM(GeoNameR r) {
+  internal GeoNameVM(GeoNameR r) {
     _r = r;
-    NewGeoNameFromGpsCommand = new(NewGeoNameFromGps, Res.IconLocationCheckin, "New GeoName from GPS");
+    NewGeoNameFromGpsCommand = new(_newGeoNameFromGps, Res.IconLocationCheckin, "New GeoName from GPS");
   }
 
-  public async void NewGeoNameFromGps() {
+  private async Task _newGeoNameFromGps(CancellationToken token) {
     var inputDialog = new InputDialog(
       "GeoName latitude and longitude",
       "Enter in format: N36.75847,W3.84609",
