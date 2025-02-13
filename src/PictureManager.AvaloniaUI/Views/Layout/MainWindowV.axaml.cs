a63ff4a521146d5c94df9184bce24e52cab099cc
martin-holy <martin.holy@hotmail.com>
2025-02-13 11:47:33 +0100

    PMX: MainWindowV

---
+++ src/PictureManager.AvaloniaUI/Views/Layout/MainWindowV.axaml.cs
@@ -0,0 +1,10 @@
+﻿using MH.UI.AvaloniaUI.Controls;
+
+namespace PictureManager.AvaloniaUI.Views.Layout;
+
+public partial class MainWindowV : CustomWindow {
+  public MainWindowV() {
+    InitializeComponent();
+    // TODO PORT HideMouseTimer for slideshow
+  }
+}
