995ab2eea033aa435800283fe9647c073a13c645
martin-holy <martin.holy@hotmail.com>
2025-02-13 11:28:34 +0100

    PMX: CoreUI

---
+++ src/PictureManager.AvaloniaUI/CoreUI.cs
@@ -0,0 +1,18 @@
+﻿using PictureManager.Common;
+using AUI = MH.UI.AvaloniaUI;
+
+namespace PictureManager.AvaloniaUI;
+
+public class CoreUI : ICoreP {
+  public CoreUI() {
+    AUI.Utils.Init.SetDelegates();
+    AUI.Resources.Dictionaries.IconToBrush = Res.IconToBrushDic;
+  }
+
+  public void AfterInit() {
+    // TODO PORT
+  }
+
+  public void CreateImageThumbnail(string srcPath, string destPath, int desiredSize, int quality) =>
+    throw new System.NotImplementedException();
+}
