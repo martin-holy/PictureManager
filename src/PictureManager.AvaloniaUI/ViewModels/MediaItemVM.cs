17f8a28608db809d759d7bf12c3a7b5bccd33775
martin-holy <martin.holy@hotmail.com>
2025-02-28 09:18:00 +0100

    PMX: ReadMetadata

--- src/PictureManager.AvaloniaUI/ViewModels/MediaItemVM.cs
+++ src/PictureManager.AvaloniaUI/ViewModels/MediaItemVM.cs
@@ -1,12 +1,59 @@
+using Avalonia.Media.Imaging;
+using MH.Utils;
 using PictureManager.Common.Features.MediaItem;
+using PictureManager.Common.Features.MediaItem.Image;
+using PictureManager.Common.Features.MediaItem.Video;
+using System;
+using System.IO;
 
 namespace PictureManager.AvaloniaUI.ViewModels;
 
 public static class MediaItemVM {
   public static void ReadMetadata(MediaItemMetadata mim, bool gpsOnly = false) {
-    // TODO PORT
-    mim.Width = 100;
-    mim.Height = 100;
-    mim.Success = true;
+    if (mim.MediaItem is VideoM) {
+      ReadVideoMetadata(mim);
+      return;
+    }
+
+    try {
+      if (mim.MediaItem is ImageM) {
+        using Stream srcFileStream = File.Open(mim.MediaItem.FilePath, FileMode.Open, FileAccess.Read, FileShare.Read);
+        using Bitmap bmp = new Bitmap(srcFileStream);
+
+        mim.Width = bmp.PixelSize.Width;
+        mim.Height = bmp.PixelSize.Height;
+
+        // TODO PORT read metadata
+
+        mim.Success = true;
+      }
+    }
+    catch (Exception ex) {
+      Log.Error(ex, mim.MediaItem.FilePath);
+
+      // true because only image dimensions are required
+      mim.Success = true;
+    }
+  }
+
+  private static void ReadVideoMetadata(MediaItemMetadata mim) {
+    try {
+      // TODO PORT read video size and orientation
+      var size = new[] { 300, 400, 1 };
+      mim.Height = size[0];
+      mim.Width = size[1];
+      mim.Orientation = size[2] switch {
+        90 => Orientation.Rotate90,
+        180 => Orientation.Rotate180,
+        270 => Orientation.Rotate270,
+        _ => Orientation.Normal,
+      };
+
+      mim.Success = true;
+    }
+    catch (Exception ex) {
+      Log.Error(ex, mim.MediaItem.FilePath);
+      mim.Success = false;
+    }
   }
 }
