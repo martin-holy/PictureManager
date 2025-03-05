de10cd2686479f4275fec9e9654d5d4f7c53f929
martin-holy <martin.holy@hotmail.com>
2025-03-05 11:27:23 +0100

    PMX: MarkedTagsItemsCountConverter

---
+++ src/PictureManager.AvaloniaUI/Converters/MarkedTagsItemsCountConverter.cs
@@ -0,0 +1,17 @@
+﻿using MH.UI.AvaloniaUI.Converters;
+using System.Collections.Generic;
+
+namespace PictureManager.AvaloniaUI.Converters;
+
+public class MarkedTagsItemsCountConverter : BaseMultiConverter {
+  private static readonly object _lock = new();
+  private static MarkedTagsItemsCountConverter? _inst;
+  public static MarkedTagsItemsCountConverter Inst { get { lock (_lock) { return _inst ??= new(); } } }
+
+  public override object Convert(IList<object?> values, object? parameter) =>
+    values is [Dictionary<object, int> tags, { } tag]
+      ? tags.ContainsKey(tag)
+        ? tags[tag].ToString()
+        : string.Empty
+      : string.Empty;
+}
