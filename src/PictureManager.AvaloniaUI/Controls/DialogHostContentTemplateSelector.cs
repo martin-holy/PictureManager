3639bdf67ffcf54a3a0344179efe31ec8d1cd217
martin-holy <martin.holy@hotmail.com>
2025-03-10 10:39:50 +0100

    PMX: DialogHostContentTemplateSelector

---
+++ src/PictureManager.AvaloniaUI/Controls/DialogHostContentTemplateSelector.cs
@@ -0,0 +1,19 @@
+﻿using MH.UI.AvaloniaUI.Controls;
+using MH.UI.Dialogs;
+using PictureManager.Common.Features.GeoLocation;
+using PictureManager.Common.Features.MediaItem;
+using PictureManager.Common.Features.MediaItem.Image;
+
+namespace PictureManager.AvaloniaUI.Controls;
+
+public class DialogHostContentTemplateSelector() : TypeDataTemplateSelector(_mappings) {
+  private static readonly TypeTemplateMapping[] _mappings = [
+    new(typeof(GetGeoNamesFromWebDialog), "MH.UI.Dialogs.ProgressDialog`1"),
+    new(typeof(ReadGeoLocationFromFilesDialog), "MH.UI.Dialogs.ProgressDialog`1"),
+    new(typeof(ComputeImageHashesDialog), "MH.UI.Dialogs.ProgressDialog`1"),
+    new(typeof(ReloadMetadataDialog), "MH.UI.Dialogs.ProgressDialog`1"),
+    new(typeof(SaveMetadataDialog), "MH.UI.Dialogs.ProgressDialog`1"),
+    new(typeof(GroupByDialog<>), "MH.UI.Dialogs.GroupByDialog`1"),
+    new(typeof(ProgressDialog<>), "MH.UI.Dialogs.ProgressDialog`1")
+  ];
+}
