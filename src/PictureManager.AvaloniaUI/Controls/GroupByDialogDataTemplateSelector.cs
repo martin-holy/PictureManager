f88a5549434c9b6ebeec991a480669ac5946cbdd
martin-holy <martin.holy@hotmail.com>
2025-03-11 12:04:20 +0100

    PMX: GroupByDialogDataTemplateSelector

---
+++ src/PictureManager.AvaloniaUI/Controls/GroupByDialogDataTemplateSelector.cs
@@ -0,0 +1,12 @@
+﻿using MH.UI.AvaloniaUI.Controls;
+using MH.Utils.Interfaces;
+using PictureManager.Common.Features.Person;
+
+namespace PictureManager.AvaloniaUI.Controls;
+
+public class GroupByDialogDataTemplateSelector() : TypeDataTemplateSelector(_mappings) {
+  private static readonly TypeTemplateMapping[] _mappings = [
+    new(typeof(PersonM), "PM.DT.Person.ListItem"),
+    new(typeof(IListItem), "MH.DT.IListItem")
+  ];
+}
