55a819513323a2259408dab4d9a772c4d3cb58c8
martin-holy <martin.holy@hotmail.com>
2025-03-14 12:47:16 +0100

    PMX: FileOperationDelete

--- src/PictureManager.AvaloniaUI/CoreUI.cs
+++ src/PictureManager.AvaloniaUI/CoreUI.cs
@@ -3,12 +3,17 @@ using Avalonia.Controls.ApplicationLifetimes;
 using PictureManager.AvaloniaUI.Controls;
 using PictureManager.Common;
 using PictureManager.Common.Features.MediaItem;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using AUI = MH.UI.AvaloniaUI;
 
 namespace PictureManager.AvaloniaUI;
 
 public class CoreUI : ICoreP {
   public CoreUI() {
+    CoreR.FileOperationDelete = FileOperationDelete;
+
     AUI.Utils.Init.SetDelegates();
     AUI.Resources.Dictionaries.IconToBrush = Res.IconToBrushDic;
     AUI.Controls.DialogHost.ContentTemplateSelector = new DialogHostContentTemplateSelector();
@@ -26,6 +31,12 @@ public class CoreUI : ICoreP {
     Application.Current?.ApplicationLifetime is IClassicDesktopStyleApplicationLifetime { MainWindow: { } window }
       ? window.DesktopScaling
       : 1.0;
+
+  public static Dictionary<string, string>? FileOperationDelete(List<string> items, bool recycle, bool silent) {
+    // TODO PORT recycle
+    items.Where(File.Exists).ToList().ForEach(File.Delete);
+    return null;
+  }
 
   public void CreateImageThumbnail(string srcPath, string destPath, int desiredSize, int quality) =>
     Utils.Imaging.CreateImageThumbnail(srcPath, destPath, desiredSize, quality);
