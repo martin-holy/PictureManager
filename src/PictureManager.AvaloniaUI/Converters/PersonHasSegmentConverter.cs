39a5838488f985258a9461f3e6b073daff3a1cee
martin-holy <martin.holy@hotmail.com>
2025-03-22 09:46:03 +0100

    PMX: PersonHasSegmentConverter

---
+++ src/PictureManager.AvaloniaUI/Converters/PersonHasSegmentConverter.cs
@@ -0,0 +1,13 @@
+﻿using MH.UI.AvaloniaUI.Converters;
+using PictureManager.Common.Features.Person;
+
+namespace PictureManager.AvaloniaUI.Converters;
+
+public sealed class PersonHasSegmentConverter : BaseConverter {
+  private static readonly object _lock = new();
+  private static PersonHasSegmentConverter? _inst;
+  public static PersonHasSegmentConverter Inst { get { lock (_lock) { return _inst ??= new(); } } }
+
+  public override object Convert(object? value, object? parameter) =>
+    value is PersonM { Segment: not null };
+}
