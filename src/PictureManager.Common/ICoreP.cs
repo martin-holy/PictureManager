de7eb86685aece43155c9ff5d1470f9329dcb8e4
martin-holy <martin.holy@hotmail.com>
2025-03-29 13:42:54 +0100

    [C] ICoreP: GetFolderPathCache

--- src/PictureManager.Common/ICoreP.cs
+++ src/PictureManager.Common/ICoreP.cs
@@ -5,4 +5,5 @@ namespace PictureManager.Common;
 public interface ICoreP {
   public void CreateImageThumbnail(string srcPath, string destPath, int desiredSize, int quality);
   public string GetFilePathCache(FolderM folder, string fileNameCache);
+  public string GetFolderPathCache(FolderM folder);
 }
