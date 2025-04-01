66cc5b67547c742a7d0a5bdfae1576729e5a787c
martin-holy <martin.holy@hotmail.com>
2025-04-01 18:17:44 +0200

    [C] SegmentVM code style

--- src/PictureManager.Common/Features/Segment/SegmentVM.cs
+++ src/PictureManager.Common/Features/Segment/SegmentVM.cs
@@ -35,28 +35,28 @@ public sealed class SegmentVM : ObservableObject {
     Views = new(_s);
     Rect = new(_s.Rect);
 
-    LoadByKeywordCommand = new(x => LoadBy(x!), x => x != null, Res.IconSegment, "Load Segments");
-    LoadByPersonCommand = new(x => LoadBy(x!), x => x != null, Res.IconSegment, "Load Segments");
-    SetSelectedAsSamePersonCommand = new(SetSelectedAsSamePerson, Res.IconEquals, "Set selected as same person");
-    SetSelectedAsUnknownCommand = new(SetSelectedAsUnknown, CanSetSelectedAsUnknown, Res.IconUnknownSegment, "Set selected as Unknown");
+    LoadByKeywordCommand = new(x => _loadBy(x!), x => x != null, Res.IconSegment, "Load Segments");
+    LoadByPersonCommand = new(x => _loadBy(x!), x => x != null, Res.IconSegment, "Load Segments");
+    SetSelectedAsSamePersonCommand = new(_setSelectedAsSamePerson, Res.IconEquals, "Set selected as same person");
+    SetSelectedAsUnknownCommand = new(_setSelectedAsUnknown, _canSetSelectedAsUnknown, Res.IconUnknownSegment, "Set selected as Unknown");
   }
 
-  private void LoadBy(KeywordM k) =>
+  private void _loadBy(KeywordM k) =>
     _coreVM.OpenSegmentsViews(_r.GetBy(k, Keyboard.IsShiftOn()).ToArray(), k.GetFullName("-", x => x.Name));
 
-  private void LoadBy(PersonM p) =>
+  private void _loadBy(PersonM p) =>
     _coreVM.OpenSegmentsViews(_r.GetBy(p).ToArray(), p.Name);
 
-  private void SetSelectedAsSamePerson() =>
+  private void _setSelectedAsSamePerson() =>
     _s.SetSelectedAsSamePerson(_s.Selected.Items.ToArray());
 
-  private void SetSelectedAsUnknown() =>
-    SetAsUnknown(_s.Selected.Items.ToArray(), _r);
+  private void _setSelectedAsUnknown() =>
+    _setAsUnknown(_s.Selected.Items.ToArray(), _r);
 
-  private bool CanSetSelectedAsUnknown() =>
+  private bool _canSetSelectedAsUnknown() =>
     _s.Selected.Items.Count > 0;
 
-  private static void SetAsUnknown(SegmentM[] segments, SegmentR r) {
+  private static void _setAsUnknown(SegmentM[] segments, SegmentR r) {
     var msg = "Do you want to set {0} segment{1} as unknown?".Plural(segments.Length);
     if (Dialog.Show(new MessageDialog("Set as unknown", msg, MH.UI.Res.IconQuestion, true)) != 1) return;
     r.ChangePerson(null, segments, segments.GetPeople().ToArray());
