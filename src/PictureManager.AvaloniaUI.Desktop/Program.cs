5c6d5173b03490b01b77ac59cdc47c9fd1bb9d98
martin-holy <martin.holy@hotmail.com>
2025-04-06 14:45:21 +0200

    PMX: Desktop IImagingP

--- src/PictureManager.AvaloniaUI.Desktop/Program.cs
+++ src/PictureManager.AvaloniaUI.Desktop/Program.cs
@@ -1,24 +1,28 @@
 ﻿using System;
-
 using Avalonia;
 using Avalonia.ReactiveUI;
+using PictureManager.AvaloniaUI.Utils;
+using Imaging = PictureManager.AvaloniaUI.Desktop.Utils.Imaging;
 
 namespace PictureManager.AvaloniaUI.Desktop;
 
-class Program
-{
-    // Initialization code. Don't use any Avalonia, third-party APIs or any
-    // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
-    // yet and stuff might break.
-    [STAThread]
-    public static void Main(string[] args) => BuildAvaloniaApp()
-        .StartWithClassicDesktopLifetime(args);
+class Program {
+  // Initialization code. Don't use any Avalonia, third-party APIs or any
+  // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
+  // yet and stuff might break.
+  [STAThread]
+  public static void Main(string[] args) {
+    ImagingPFactory.Register(() => new Imaging());
 
-    // Avalonia configuration, don't remove; also used by visual designer.
-    public static AppBuilder BuildAvaloniaApp()
-        => AppBuilder.Configure<App>()
-            .UsePlatformDetect()
-            .WithInterFont()
-            .LogToTrace()
-            .UseReactiveUI();
+    BuildAvaloniaApp()
+      .StartWithClassicDesktopLifetime(args);
+  }
+
+  // Avalonia configuration, don't remove; also used by visual designer.
+  public static AppBuilder BuildAvaloniaApp() =>
+    AppBuilder.Configure<App>()
+      .UsePlatformDetect()
+      .WithInterFont()
+      .LogToTrace()
+      .UseReactiveUI();
 }
---
+++ src/PictureManager.AvaloniaUI.Desktop/Utils/Imaging.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+using Avalonia.Media.Imaging;
+using PictureManager.AvaloniaUI.Utils;
+using PictureManager.Common;
+using PictureManager.Common.Features.MediaItem;
+
+namespace PictureManager.AvaloniaUI.Desktop.Utils;
+
+public class Imaging : IImagingP {
+  public void CreateImageThumbnail(MediaItemM mi) =>
+    AvaloniaUI.Utils.Imaging.CreateImageThumbnail(
+      mi.FilePath,
+      mi.FilePathCache,
+      Core.Settings.MediaItem.ThumbSize,
+      Core.Settings.Common.JpegQuality);
+
+  public Bitmap? GetImageThumbnail(MediaItemM mi) {
+    if (!File.Exists(mi.FilePathCache)) return null;
+    return new(mi.FilePathCache);
+  }
+}
