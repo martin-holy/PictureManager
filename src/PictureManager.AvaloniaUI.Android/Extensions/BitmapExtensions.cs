0736e89280ab8376176ca200320c89d55c754c38
martin-holy <martin.holy@hotmail.com>
2025-04-06 14:23:29 +0200

    PMX: ToAvaloniaBitmap

--- src/PictureManager.AvaloniaUI.Android/Extensions/BitmapExtensions.cs
+++ src/PictureManager.AvaloniaUI.Android/Extensions/BitmapExtensions.cs
@@ -1,5 +1,4 @@
-using SkiaSharp;
-using System.Runtime.InteropServices;
+using System.IO;
 using AndroidBitmap = Android.Graphics.Bitmap;
 using AvaloniaBitmap = Avalonia.Media.Imaging.Bitmap;
 
@@ -7,22 +6,9 @@ namespace PictureManager.AvaloniaUI.Android.Extensions;
 
 public static class BitmapExtensions {
   public static AvaloniaBitmap ToAvaloniaBitmap(this AndroidBitmap androidBitmap) {
-    var info = new SKImageInfo(androidBitmap.Width, androidBitmap.Height, SKColorType.Rgba8888);
-    var skBitmap = new SKBitmap(info);
-
-    // Copy pixels
-    var pixels = new byte[androidBitmap.ByteCount];
-    androidBitmap.CopyPixelsToBuffer(Java.Nio.ByteBuffer.Wrap(pixels));
-
-    // Pin array and set pixels
-    var handle = GCHandle.Alloc(pixels, GCHandleType.Pinned);
-    try {
-      skBitmap.InstallPixels(info, handle.AddrOfPinnedObject());
-    }
-    finally {
-      handle.Free();
-    }
-
-    return new(SKImage.FromBitmap(skBitmap).Encode().AsStream());
+    using var stream = new MemoryStream();
+    androidBitmap.Compress(AndroidBitmap.CompressFormat.Jpeg, 85, stream);
+    stream.Position = 0;
+    return new(stream);
   }
 }
