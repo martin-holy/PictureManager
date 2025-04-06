95c00e2a5eef3d51daa6c7dc4e145345e5678f38
martin-holy <martin.holy@hotmail.com>
2025-04-06 14:41:25 +0200

    PMX: IImagingP

---
+++ src/PictureManager.AvaloniaUI/Utils/IImagingP.cs
@@ -0,0 +1,9 @@
+﻿using Avalonia.Media.Imaging;
+using PictureManager.Common.Features.MediaItem;
+
+namespace PictureManager.AvaloniaUI.Utils;
+
+public interface IImagingP {
+  public void CreateImageThumbnail(MediaItemM mi);
+  public Bitmap? GetImageThumbnail(MediaItemM mi);
+}
