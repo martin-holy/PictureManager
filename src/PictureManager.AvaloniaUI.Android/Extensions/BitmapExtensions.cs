833da08b382c5e0ffe896731e6fed7378e6588c2
martin-holy <martin.holy@hotmail.com>
2025-04-09 11:17:53 +0200

    PMX: ToAvaloniaBitmap nullable

--- src/PictureManager.AvaloniaUI.Android/Extensions/BitmapExtensions.cs
+++ src/PictureManager.AvaloniaUI.Android/Extensions/BitmapExtensions.cs
@@ -5,7 +5,8 @@ using AvaloniaBitmap = Avalonia.Media.Imaging.Bitmap;
 namespace PictureManager.AvaloniaUI.Android.Extensions;
 
 public static class BitmapExtensions {
-  public static AvaloniaBitmap ToAvaloniaBitmap(this AndroidBitmap androidBitmap) {
+  public static AvaloniaBitmap? ToAvaloniaBitmap(this AndroidBitmap? androidBitmap) {
+    if (androidBitmap == null) return null;
     using var stream = new MemoryStream();
     androidBitmap.Compress(AndroidBitmap.CompressFormat.Jpeg, 85, stream);
     stream.Position = 0;
