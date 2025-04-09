deacdddb6b3dd8d0434fd8baf0cd95c92acc51db
martin-holy <martin.holy@hotmail.com>
2025-04-09 14:09:29 +0200

    PMX: Android image thumbnail

--- src/PictureManager.AvaloniaUI.Android/Utils/Imaging.cs
+++ src/PictureManager.AvaloniaUI.Android/Utils/Imaging.cs
@@ -1,21 +1,19 @@
-﻿using System.IO;
-using Android.App;
+﻿using Android.App;
 using Android.Content;
 using Android.Graphics;
 using Android.Provider;
 using PictureManager.AvaloniaUI.Android.Extensions;
 using PictureManager.AvaloniaUI.Utils;
 using PictureManager.Common.Features.MediaItem;
-using AvaloniaBitmap = Avalonia.Media.Imaging.Bitmap;
+using System.IO;
 using AndroidBitmap = Android.Graphics.Bitmap;
+using AvaloniaBitmap = Avalonia.Media.Imaging.Bitmap;
 
 namespace PictureManager.AvaloniaUI.Android.Utils;
 
 public class Imaging : IImagingP {
   public static long GetImageId(string filePath) {
-    var uri = filePath.StartsWith("/storage/emulated")
-      ? MediaStore.Images.Media.InternalContentUri
-      : MediaStore.Images.Media.ExternalContentUri;
+    if (MediaStore.Images.Media.ExternalContentUri is not { } uri) return -1;
 
     var cursor = Application.Context.ContentResolver?.Query(
       uri,
@@ -37,29 +35,45 @@ public class Imaging : IImagingP {
 
   public void CreateImageThumbnail(MediaItemM mi) {
     var filePath = mi.FilePath;
+
+    if (MediaStore.Images.Thumbnails.ExternalContentUri is not { } uri) return;
+
     var id = GetImageId(filePath);
-    if (id < 0) return;
+    if (id < 0) {
+      id = InsertNewImage(filePath);
+      if (id < 0) return;
+    }
 
     var options = new BitmapFactory.Options { InSampleSize = 2 };
     var bitmap = BitmapFactory.DecodeFile(filePath, options);
     if (bitmap == null) return;
 
+    using var stream = new MemoryStream();
     var thumb = AndroidBitmap.CreateScaledBitmap(bitmap, 512, 384, true);
-    bitmap.Recycle();
+    thumb.Compress(AndroidBitmap.CompressFormat.Jpeg, 85, stream);
 
-    var values = new ContentValues();
+    var values = new ContentValues(3);
     values.Put(MediaStore.Images.Thumbnails.ImageId, id);
     values.Put(MediaStore.Images.Thumbnails.Kind, (int)ThumbnailKind.MiniKind);
-
-    using var stream = new MemoryStream();
-    thumb.Compress(AndroidBitmap.CompressFormat.Jpeg, 80, stream);
     values.Put(MediaStore.Images.Thumbnails.Data, stream.ToArray());
 
-    var uri = filePath.StartsWith("/storage/emulated")
-      ? MediaStore.Images.Thumbnails.InternalContentUri
-      : MediaStore.Images.Thumbnails.ExternalContentUri;
     Application.Context.ContentResolver!.Insert(uri, values);
     thumb.Recycle();
+    bitmap.Recycle();
+  }
+
+  public static long InsertNewImage(string filePath) {
+    if (MediaStore.Images.Media.ExternalContentUri is not { } uri) return -1;
+
+    var values = new ContentValues(2);
+    values.Put(MediaStore.Images.Media.InterfaceConsts.Data, filePath);
+    values.Put(MediaStore.Images.Media.InterfaceConsts.MimeType, "image/jpeg");
+    
+    var imgUri = Application.Context.ContentResolver?.Insert(uri, values);
+    
+    return long.TryParse(imgUri?.LastPathSegment, out var id)
+      ? id
+      : GetImageId(filePath);
   }
 
   public AvaloniaBitmap? GetImageThumbnail(MediaItemM mi) {
