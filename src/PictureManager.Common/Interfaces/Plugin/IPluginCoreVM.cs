4e74ec352264e104ba3e08f2d93fb993da57f5a2
martin-holy <martin.holy@hotmail.com>
2025-04-12 10:38:45 +0200

    [C] IPluginCoreVM MainMenuCommands

--- src/PictureManager.Common/Interfaces/Plugin/IPluginCoreVM.cs
+++ src/PictureManager.Common/Interfaces/Plugin/IPluginCoreVM.cs
@@ -1,10 +1,10 @@
-﻿using MH.Utils.BaseClasses;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Windows.Input;
 
 namespace PictureManager.Common.Interfaces.Plugin;
 
 public interface IPluginCoreVM {
   public string PluginIcon { get; }
   public string PluginTitle { get; }
-  public List<RelayCommand> MainMenuCommands { get; }
+  public List<ICommand> MainMenuCommands { get; }
 }
