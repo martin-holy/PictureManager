eed2951a11ffc861a2494b107c009f8c8de2d6c3
martin-holy <martin.holy@hotmail.com>
2025-04-13 17:36:39 +0200

    [C] FolderKeywordsDialog: ShowAsync

--- src/PictureManager.Common/Features/FolderKeyword/FolderKeywordsDialog.cs
+++ src/PictureManager.Common/Features/FolderKeyword/FolderKeywordsDialog.cs
@@ -4,6 +4,7 @@ using MH.Utils.BaseClasses;
 using PictureManager.Common.Features.Folder;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace PictureManager.Common.Features.FolderKeyword;
 
@@ -14,13 +15,13 @@ public sealed class FolderKeywordsDialog : Dialog {
   public ObservableCollection<FolderM> Items { get; } = [];
   public RelayCommand<FolderM> SelectCommand { get; }
 
-  public static RelayCommand OpenCommand { get; } = new(
-    () => Show(new FolderKeywordsDialog()), null, "Folder Keywords list");
+  public static AsyncRelayCommand OpenCommand { get; } = new(
+    _ => ShowAsync(new FolderKeywordsDialog()), null, "Folder Keywords list");
 
   public FolderKeywordsDialog() : base("Folder Keywords", Res.IconFolderPuzzle) {
     SelectCommand = new(x => SelectedFolder = x);
     Buttons = [
-      new(new RelayCommand(() => Remove(SelectedFolder!), () => SelectedFolder != null, MH.UI.Res.IconXCross, "Remove")),
+      new(new AsyncRelayCommand(_ => Remove(SelectedFolder!), () => SelectedFolder != null, MH.UI.Res.IconXCross, "Remove")),
       new(CloseCommand, false, true)
     ];
 
@@ -28,8 +29,8 @@ public sealed class FolderKeywordsDialog : Dialog {
       Items.Add(folder);
   }
 
-  private void Remove(FolderM folder) {
-    if (Show(new MessageDialog("Remove Confirmation", "Are you sure?", MH.UI.Res.IconQuestion, true)) != 1) return;
+  private async Task Remove(FolderM folder) {
+    if (await ShowAsync(new MessageDialog("Remove Confirmation", "Are you sure?", MH.UI.Res.IconQuestion, true)) != 1) return;
 
     Core.R.FolderKeyword.ItemDelete(folder);
     Items.Remove(folder);
