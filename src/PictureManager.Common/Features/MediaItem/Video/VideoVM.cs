dca03667d0d9955cef69311ebf030e1afb842089
martin-holy <martin.holy@hotmail.com>
2025-05-05 12:13:53 +0200

    [C] VideoVM: code style

--- src/PictureManager.Common/Features/MediaItem/Video/VideoVM.cs
+++ src/PictureManager.Common/Features/MediaItem/Video/VideoVM.cs
@@ -19,17 +19,17 @@ public sealed class VideoVM : ObservableObject {
 
   public VideoVM() {
     MediaPlayer.SelectNextItemAction = CurrentVideoItems.SelectNextOrFirstItem;
-    MediaPlayer.GetNewClipFunc = GetNewClip;
-    MediaPlayer.GetNewImageFunc = GetNewImage;
-    MediaPlayer.OnItemDeleteAction = OnItemDelete;
-    MediaPlayer.MarkerSetEvent += OnMarkerSet;
+    MediaPlayer.GetNewClipFunc = _getNewClip;
+    MediaPlayer.GetNewImageFunc = _getNewImage;
+    MediaPlayer.OnItemDeleteAction = _onItemDelete;
+    MediaPlayer.MarkerSetEvent += _onMarkerSet;
   }
 
   public void SetCurrent(MediaItemM? item, bool setSource = false) {
     var vid = item as VideoM ?? (item as VideoItemM)?.Video;
 
     if (vid == null) {
-      SetVideoSource(null);
+      _setVideoSource(null);
       Current = null;
       CurrentVideoItems.Root.Clear();
       CurrentVideoItems.Selected.DeselectAll();
@@ -40,12 +40,12 @@ public sealed class VideoVM : ObservableObject {
 
     CurrentVideoItems.Selected.DeselectAll();
     Current = vid;
-    SetVideoSource(Current);
-    ReloadCurrentVideoItems();
+    _setVideoSource(Current);
+    _reloadCurrentVideoItems();
     Core.VM.MainWindow.ToolsTabs.Activate(MH.UI.Res.IconMovieClapper, "Video", this);
   }
 
-  private void ReloadCurrentVideoItems() {
+  private void _reloadCurrentVideoItems() {
     var items = Current == null
       ? []
       : Core.R.VideoItemsOrder.All.TryGetValue(Current, out var list)
@@ -55,18 +55,18 @@ public sealed class VideoVM : ObservableObject {
     CurrentVideoItems.Reload(items, GroupMode.ThenByRecursive, groupByItems, true);
   }
 
-  private IVideoClip? GetNewClip(int timeStart) =>
+  private IVideoClip? _getNewClip(int timeStart) =>
     Current == null ? null : Core.R.VideoClip.CustomItemCreate(Current, timeStart);
 
-  private IVideoImage? GetNewImage(int timeStart) =>
+  private IVideoImage? _getNewImage(int timeStart) =>
     Current == null ? null : Core.R.VideoImage.CustomItemCreate(Current, timeStart);
 
-  private async void OnItemDelete() {
+  private async void _onItemDelete() {
     if (await Core.VM.MediaItem.Delete(CurrentVideoItems.Selected.Items.Cast<MediaItemM>().ToArray()))
       MediaPlayer.SetCurrent(null);
   }
 
-  private void OnMarkerSet(object? sender, Tuple<IVideoItem, bool> e) {
+  private void _onMarkerSet(object? sender, Tuple<IVideoItem, bool> e) {
     var item = (VideoItemM)e.Item1;
     Core.R.MediaItem.Modify(item);
 
@@ -79,7 +79,7 @@ public sealed class VideoVM : ObservableObject {
     item.OnPropertyChanged(nameof(item.FilePathCache));
   }
 
-  private void SetVideoSource(VideoM? vid) {
+  private void _setVideoSource(VideoM? vid) {
     if (vid == null) {
       MediaPlayer.Source = string.Empty;
       return;
