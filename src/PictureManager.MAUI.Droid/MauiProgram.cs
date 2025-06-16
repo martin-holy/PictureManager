89d332e3699133ac2d2d1254e18a33474572d2d0
martin-holy <martin.holy@hotmail.com>
2025-06-16 10:05:26 +0200

    droid: MyShellHandler

--- src/PictureManager.MAUI.Droid/MauiProgram.cs
+++ src/PictureManager.MAUI.Droid/MauiProgram.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.Maui.Hosting;
+﻿using MH.UI.MAUI.Controls;
+using Microsoft.Maui.Hosting;
 using PictureManager.Common.Features.MediaItem;
+using PictureManager.MAUI.Droid.Handlers;
 
 namespace PictureManager.MAUI.Droid;
 
@@ -9,12 +11,17 @@ public static class MauiProgram {
 
     builder
       .UseSharedMauiApp()
-      .ConfigureMauiHandlers(MH.UI.MAUI.Droid.MauiProgram.ConfigureHandlers);
+      .ConfigureMauiHandlers(MH.UI.MAUI.Droid.MauiProgram.ConfigureHandlers)
+      .ConfigureMauiHandlers(ConfigureHandlers);
 
     MH.UI.Android.Utils.Init.SetDelegates();
     MH.UI.Android.Utils.Icons.IconNameToColor = Resources.Res.IconToColorDic;
     MediaItemS.ReadMetadata = ViewModels.MediaItemVM.ReadMetadata;
 
     return builder.Build();
+  }
+
+  public static void ConfigureHandlers(IMauiHandlersCollection handlers) {
+    handlers.AddHandler<MyShell, MyShellHandler>();
   }
 }
