afff593a6b5af55a6e7bdf4dd275e338da1fb75e
martin-holy <martin.holy@hotmail.com>
2025-06-16 10:05:26 +0200

    droid: MyShellHandler

---
+++ src/PictureManager.MAUI.Droid/Handlers/MyShellHandler.cs
@@ -0,0 +1,29 @@
+﻿using Android.Widget;
+using MH.UI.MAUI.Controls;
+using Microsoft.Maui;
+using Microsoft.Maui.Handlers;
+using PictureManager.Common.Layout;
+using PictureManager.MAUI.Droid.Views;
+
+namespace PictureManager.MAUI.Droid.Handlers;
+
+public class MyShellHandler : ViewHandler<MyShell, FrameLayout> {
+  public static IPropertyMapper<MyShell, MyShellHandler> PropertyMapper =
+    new PropertyMapper<MyShell, MyShellHandler>(ViewMapper);
+
+  public MyShellHandler() : base(PropertyMapper) { }
+
+  protected override FrameLayout CreatePlatformView() => new(Context);
+
+  protected override void ConnectHandler(FrameLayout platformView) {
+    base.ConnectHandler(platformView);
+
+    if (VirtualView.BindingContext is MainWindowVM viewModel)
+      platformView.AddView(new MainWindowV(Context) { ViewModel = viewModel });
+  }
+
+  protected override void DisconnectHandler(FrameLayout platformView) {
+    platformView.RemoveAllViews();
+    base.DisconnectHandler(platformView);
+  }
+}
