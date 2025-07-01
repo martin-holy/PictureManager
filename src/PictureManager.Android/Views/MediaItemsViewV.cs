8066eb000ff89de3b06721698cbc43495c733f4d
martin-holy <martin.holy@hotmail.com>
2025-07-01 10:03:21 +0200

    droid: MediaItemsViewV

---
+++ src/PictureManager.Android/Views/MediaItemsViewV.cs
@@ -0,0 +1,41 @@
+﻿using Android.Content;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+using MH.UI.Android.Controls;
+using MH.UI.Interfaces;
+using PictureManager.Common.Features.MediaItem;
+
+namespace PictureManager.Android.Views;
+
+public class MediaItemsViewV : LinearLayout {
+  private CollectionViewHost _host = null!;
+
+  public MediaItemsViewVM? DataContext { get; private set; }
+
+  public MediaItemsViewV(Context context) : base(context) => _initialize(context, null);
+  public MediaItemsViewV(Context context, IAttributeSet attrs) : base(context, attrs) => _initialize(context, attrs);
+  protected MediaItemsViewV(nint javaReference, JniHandleOwnership transfer) : base(javaReference, transfer) => _initialize(Context!, null);
+
+  private void _initialize(Context context, IAttributeSet? attrs) {
+    _host = new(context) { GetItemView = _getItemView };
+    AddView(_host);
+  }
+
+  private View? _getItemView(LinearLayout container, ICollectionViewGroup group, object? item) {
+    if (item is not MediaItemM mi) return null;
+
+    return group.GetItemTemplateName() switch {
+      "PM.DT.MediaItem.Thumb-Full" => new MediaItemThumbFullV(container.Context!).Bind(mi, DataContext!, group),
+      _ => null,
+    };
+  }
+
+  public MediaItemsViewV Bind(MediaItemsViewVM? dataContext) {
+    DataContext = dataContext;
+    if (DataContext == null) return this;
+    _host.ViewModel = DataContext;
+    return this;
+  }
+}
