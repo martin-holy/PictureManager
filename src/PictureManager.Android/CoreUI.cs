410721d6a00fc062349eb4404bb4685098f04af8
martin-holy <martin.holy@hotmail.com>
2025-07-06 09:27:00 +0200

    droid: switch to thumbs on folder select

--- src/PictureManager.Android/CoreUI.cs
+++ src/PictureManager.Android/CoreUI.cs
@@ -1,4 +1,5 @@
 ﻿using Android.App;
+using MH.Utils.Interfaces;
 using PictureManager.Android.Views;
 using PictureManager.Common;
 using PictureManager.Common.Features.Folder;
@@ -23,9 +24,18 @@ public class CoreUI: ICoreP {
   public void AfterInit() {
     Core.Settings.MediaItem.MediaItemThumbScale = 0.5;
     MainWindow = new MainWindowV(_mainActivity).Bind(Core.VM.MainWindow);
+    _attachEvents();
   }
 
   public void CreateImageThumbnail(string srcPath, string destPath, int desiredSize, int quality) => throw new System.NotImplementedException();
   public string GetFilePathCache(FolderM folder, string fileNameCache) => throw new System.NotImplementedException();
   public string GetFolderPathCache(FolderM folder) => throw new System.NotImplementedException();
+
+  private void _attachEvents() {
+    Core.R.Folder.Tree.ItemSelectedEvent += _onFolderTreeItemSelected;
+  }
+
+  private void _onFolderTreeItemSelected(object? sender, ITreeItem e) {
+    MainWindow.SlidePanels.ViewPager.SetCurrentItem(1, true);
+  }
 }
