069c3feecf5b0caa6628ec8bcc5834a8bfcd791b
martin-holy <martin.holy@hotmail.com>
2025-09-13 10:49:26 +0200

    droid: GetFolderPathCache and IsFolderPathCacheableInMediaStore

--- src/PictureManager.Android/CoreUI.cs
+++ src/PictureManager.Android/CoreUI.cs
@@ -1,15 +1,21 @@
 ﻿using Android.App;
 using Android.Content;
+using Android.OS;
+using MH.Utils;
 using MH.Utils.Interfaces;
 using PictureManager.Android.Utils;
 using PictureManager.Android.Views;
 using PictureManager.Common;
 using PictureManager.Common.Features.Folder;
 using PictureManager.Common.Features.MediaItem;
+using System.IO;
+using System.Linq;
 
 namespace PictureManager.Android;
 
 public class CoreUI : ICoreP {
+  private readonly string[] _mediaRoots = ["DCIM", "Pictures"];
+
   public MainWindowV MainWindow { get; private set; } = null!;
 
   public CoreUI(MainActivity mainActivity) {
@@ -30,7 +36,30 @@ public class CoreUI : ICoreP {
 
   public void CreateImageThumbnail(string srcPath, string destPath, int desiredSize, int quality) => throw new System.NotImplementedException();
   public string GetFilePathCache(FolderM folder, string fileNameCache) => throw new System.NotImplementedException();
-  public string GetFolderPathCache(FolderM folder) => string.Empty; // TODO PORT
+  
+  public string GetFolderPathCache(FolderM folder) {
+    var parts = folder.GetThisAndParents().Reverse().Select(x => x.Name).ToArray();
+    var driveRoot = parts[0];
+    var relativePath = string.Join(Path.DirectorySeparatorChar, parts.Skip(1));
+    var cacheSubPath = Core.Settings.Common.CachePath.TrimStart(':').Replace('\\', Path.DirectorySeparatorChar);
+
+    return Path.Combine(driveRoot, cacheSubPath, relativePath);
+  }
+
+  public bool IsFolderPathCacheableInMediaStore(FolderM folder) {
+    if (Build.VERSION.SdkInt < BuildVersionCodes.Q) return true;
+
+    var parts = folder.GetThisAndParents().Reverse().ToArray();    
+    if (parts.Length > 1) {
+      var driveRoot = parts[0].Name;
+      var rootFolder = parts[1].Name;
+      var deviceRoot = global::Android.OS.Environment.ExternalStorageDirectory?.AbsolutePath;
+      if (driveRoot.Equals(deviceRoot, System.StringComparison.OrdinalIgnoreCase) && _mediaRoots.Contains(rootFolder))
+        return true;
+    }
+
+    return false;
+  }
 
   private void _attachEvents() {
     Core.R.Folder.Tree.ItemSelectedEvent += _onFolderTreeItemSelected;
