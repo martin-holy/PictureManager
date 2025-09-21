cc7354b8e3f0fb5319710709411a1536556ed54d
martin-holy <martin.holy@hotmail.com>
2025-09-21 11:01:56 +0200

    droid: ToolBarV: Log

--- src/PictureManager.Android/Views/Layout/ToolBarV.cs
+++ src/PictureManager.Android/Views/Layout/ToolBarV.cs
@@ -1,13 +1,34 @@
 ﻿using Android.Content;
 using Android.Widget;
 using MH.UI.Android.Controls;
+using MH.UI.Android.Utils;
+using PictureManager.Common;
 using PictureManager.Common.Layout;
+using System.Collections.Generic;
 
 namespace PictureManager.Android.Views.Layout;
 
 public class ToolBarV : LinearLayout {
+  private readonly List<CommandBinding> _commandBindings = [];
+  private bool _disposed;
+
   public ToolBarV(Context context, MainWindowVM mainWindowVM) : base(context) {
     Orientation = Orientation.Horizontal;
     AddView(new ButtonMenu(Context!, mainWindowVM.MainMenu, mainWindowVM.MainMenu.Icon));
+
+    var logBtn = new IconButton(context);
+    logBtn.SetImageDrawable(Icons.GetIcon(context, CoreVM.OpenLogCommand.Icon));
+    _commandBindings.Add(new(logBtn, CoreVM.OpenLogCommand));
+    AddView(logBtn);
+  }
+
+  protected override void Dispose(bool disposing) {
+    if (_disposed) return;
+    if (disposing) {
+      foreach (var cb in _commandBindings) cb.Dispose();
+      _commandBindings.Clear();
+    }
+    _disposed = true;
+    base.Dispose(disposing);
   }
 }
