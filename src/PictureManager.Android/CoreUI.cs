553da051d670a1a810b7bdf2c15bab3d811208ef
martin-holy <martin.holy@hotmail.com>
2025-09-22 11:00:54 +0200

    droid: utils init

--- src/PictureManager.Android/CoreUI.cs
+++ src/PictureManager.Android/CoreUI.cs
@@ -21,9 +21,9 @@ public class CoreUI : ICoreP {
 
   public CoreUI(MainActivity mainActivity) {
     // TODO PORT
+    MH.UI.Android.Utils.Init.Utils(mainActivity);
     MH.UI.Android.Utils.Init.SetDelegates();
-    MH.UI.Android.Utils.Icons.IconNameToColor = Resources.Res.IconToColorDic;
-    MH.UI.Android.Utils.DisplayU.Init(Application.Context!.Resources!.DisplayMetrics!);
+    MH.UI.Android.Utils.Icons.IconNameToColor = Resources.Res.IconToColorDic;    
     MH.UI.Controls.CollectionView.ItemBorderSize = MH.UI.Android.Utils.DisplayU.DpToPx(3);
     MediaItemS.ReadMetadata = ViewModels.MediaItemVM.ReadMetadata;
     MH.UI.Android.Controls.DialogHost.Initialize(mainActivity, DialogFactory.GetDialog);
