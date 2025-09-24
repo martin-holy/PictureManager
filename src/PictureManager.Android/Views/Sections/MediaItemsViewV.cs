2a17c3d53683845197154d4b1d48712b61ac565a
martin-holy <martin.holy@hotmail.com>
2025-09-24 14:35:22 +0200

    droid: MediaItemsViewV

--- src/PictureManager.Android/Views/Sections/MediaItemsViewV.cs
+++ src/PictureManager.Android/Views/Sections/MediaItemsViewV.cs
@@ -5,11 +5,9 @@ using MH.UI.Android.Controls;
 using MH.UI.Android.Extensions;
 using MH.UI.Android.Utils;
 using MH.UI.Interfaces;
-using MH.Utils.Extensions;
 using PictureManager.Android.Views.Entities;
 using PictureManager.Common.Features.MediaItem;
 using System;
-using System.ComponentModel;
 
 namespace PictureManager.Android.Views.Sections;
 
@@ -27,83 +25,43 @@ public class MediaItemsViewV : LinearLayout, IDisposable {
   public MediaItemsViewV(Context context, MediaItemsViewVM dataContext) : base(context) {
     DataContext = dataContext;
     Orientation = Orientation.Vertical;
-    LayoutParameters = new(LayoutParams.MatchParent, LayoutParams.MatchParent);
     SetBackgroundResource(Resource.Color.c_static_ba);
 
-    _loadingText = new TextView(context) {
-      LayoutParameters = new LayoutParams(LayoutParams.MatchParent, LayoutParams.MatchParent) {
-        Gravity = GravityFlags.Center
-      },
-      Text = "Loading ...",
-      TextSize = 18
-    };
-    AddView(_loadingText);
+    _loadingText = new TextView(context) { Text = "Loading ...", TextSize = 18 };
+    AddView(_loadingText, new LayoutParams(LPU.Match, LPU.Match) { Gravity = GravityFlags.Center });
 
-    _importContainer = new LinearLayout(context) {
-      LayoutParameters = new(LayoutParams.MatchParent, LayoutParams.MatchParent),
-      Orientation = Orientation.Vertical
-    };
+    _importText = new TextView(context);
+    _importProgress = new ProgressBar(context);
+    _importCancelButton = new Button(new ContextThemeWrapper(context, Resource.Style.mh_DialogButton), null, 0);
+    BindingU.Bind(_importCancelButton, DataContext.Import.CancelCommand);
+    _importContainer = new LinearLayout(context) { Orientation = Orientation.Vertical };
     _importContainer.SetGravity(GravityFlags.Center);
-    _importContainer.SetPadding(context.Resources!.GetDimensionPixelSize(Resource.Dimension.general_padding));
-
-    _importText = new TextView(context) {
-      LayoutParameters = new LayoutParams(LayoutParams.WrapContent, LayoutParams.WrapContent) {
-        BottomMargin = DisplayU.DpToPx(6)
-      }
-    };
-
-    _importProgress = new ProgressBar(context) {
-      LayoutParameters = new LayoutParams(LayoutParams.MatchParent, LayoutParams.WrapContent) {
-        LeftMargin = DisplayU.DpToPx(6),
-        RightMargin = DisplayU.DpToPx(6)
-      }
-    };
-
-    _importCancelButton = new Button(new ContextThemeWrapper(context, Resource.Style.mh_DialogButton), null, 0) {
-      LayoutParameters = new LayoutParams(LayoutParams.WrapContent, LayoutParams.WrapContent) {
-        Gravity = GravityFlags.End,
-        TopMargin = DisplayU.DpToPx(6),
-        RightMargin = DisplayU.DpToPx(6)
-      },
-      Text = "Cancel"
-    };
-    _importCancelButton.Click += _onImportCancelButtonClick;
-
-    _importContainer.AddView(_importText);
-    _importContainer.AddView(_importProgress);
-    _importContainer.AddView(_importCancelButton);
-    AddView(_importContainer);
+    _importContainer.SetPadding(DimensU.Spacing);
+    _importContainer.AddView(_importText, new LayoutParams(LPU.Wrap, LPU.Wrap).WithDpMargin(0, 0, 0, 6));
+    _importContainer.AddView(_importProgress, new LayoutParams(LPU.Match, LPU.Wrap).WithDpMargin(6, 0, 6, 0));
+    _importContainer.AddView(_importCancelButton, new LayoutParams(LPU.Wrap, LPU.Wrap) { Gravity = GravityFlags.End }.WithDpMargin(0, 6, 6, 0));
+    AddView(_importContainer, new LayoutParams(LPU.Match, LPU.Match));
 
     _host = new CollectionViewHost(context, dataContext, _getItemView);
     AddView(_host);
 
-    dataContext.PropertyChanged += _onDataContextPropertyChanged;
-    dataContext.Import.PropertyChanged += _onImportPropertyChanged;
-
-    _updateVisibility();
+    BindingU.Bind(dataContext, x => x.IsLoading, _ => _updateVisibility(), this);
+    BindingU.Bind(dataContext.Import, x => x.IsImporting, _ => _updateVisibility(), this);
+    BindingU.Bind(dataContext.Import, x => x.Count, count => {
+      _importText.Text = $"Importing {count} new items ...";
+      _importProgress.Max = count;
+    }, this);
+    BindingU.Bind(dataContext.Import, x => x.DoneCount, doneCount => _importProgress.Progress = doneCount, this);
   }
 
   protected override void Dispose(bool disposing) {
     if (_disposed) return;
     if (disposing) {
-      _importCancelButton.Click -= _onImportCancelButtonClick;
-      DataContext.PropertyChanged -= _onDataContextPropertyChanged;
-      DataContext.Import.PropertyChanged -= _onImportPropertyChanged;
-
       _host.Dispose();
-      _loadingText.Dispose();      
-      _importText.Dispose();
-      _importProgress.Dispose();
-      _importCancelButton.Dispose();
-      _importContainer.RemoveAllViews();
-      _importContainer.Dispose();
     }
     _disposed = true;
     base.Dispose(disposing);
   }
-
-  private void _onImportCancelButtonClick(object? sender, EventArgs e) =>
-    DataContext.Import.CancelCommand.Execute(null);
 
   private View? _getItemView(LinearLayout container, ICollectionViewGroup group, object? item) {
     if (item is not MediaItemM mi) return null;
@@ -111,26 +69,6 @@ public class MediaItemsViewV : LinearLayout, IDisposable {
       "PM.DT.MediaItem.Thumb-Full" => new MediaItemThumbFullV(container.Context!).Bind(mi),
       _ => null
     };
-  }
-
-  private void _onDataContextPropertyChanged(object? sender, PropertyChangedEventArgs e) {
-    if (e.Is(nameof(MediaItemsViewVM.IsLoading)))
-      _updateVisibility();
-  }
-
-  private void _onImportPropertyChanged(object? sender, PropertyChangedEventArgs e) {
-    switch (e.PropertyName) {
-      case nameof(MediaItemsViewVM.Import.IsImporting):
-        _updateVisibility();
-        break;
-      case nameof(MediaItemsViewVM.Import.Count):
-        _importText.Text = $"Importing {DataContext!.Import.Count} new items ...";
-        _importProgress.Max = DataContext.Import.Count;    
-        break;
-      case nameof(MediaItemsViewVM.Import.DoneCount):
-        _importProgress.Progress = DataContext!.Import.DoneCount;
-        break;
-    }
   }
 
   private void _updateVisibility() {
