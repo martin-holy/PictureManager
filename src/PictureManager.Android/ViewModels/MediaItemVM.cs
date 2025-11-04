6a6a437f9ae337f139ee5164d7a57d09f275005d
martin-holy <martin.holy@hotmail.com>
2025-11-04 12:05:39 +0100

    droid: MediaItemVM: _readVideoMetadata

--- src/PictureManager.Android/ViewModels/MediaItemVM.cs
+++ src/PictureManager.Android/ViewModels/MediaItemVM.cs
@@ -1,4 +1,5 @@
 ﻿using Android.Graphics;
+using MH.UI.Android.Utils;
 using MH.Utils;
 using PictureManager.Common.Features.MediaItem;
 using PictureManager.Common.Features.MediaItem.Image;
@@ -12,7 +13,7 @@ namespace PictureManager.Android.ViewModels;
 public static class MediaItemVM {
   public static void ReadMetadata(MediaItemMetadata mim, bool gpsOnly = false) {
     if (mim.MediaItem is VideoM) {
-      ReadVideoMetadata(mim);
+      _readVideoMetadata(mim);
       return;
     }
 
@@ -38,24 +39,22 @@ public static class MediaItemVM {
     }
   }
 
-  private static void ReadVideoMetadata(MediaItemMetadata mim) {
-    try {
-      // TODO PORT read video size and orientation
-      var size = new[] { 300, 400, 1 };
-      mim.Height = size[0];
-      mim.Width = size[1];
-      mim.Orientation = size[2] switch {
-        90 => Orientation.Rotate90,
-        180 => Orientation.Rotate180,
-        270 => Orientation.Rotate270,
-        _ => Orientation.Normal,
-      };
-
-      mim.Success = true;
-    }
-    catch (Exception ex) {
-      Log.Error(ex, mim.MediaItem.FilePath);
+  private static void _readVideoMetadata(MediaItemMetadata mim) {
+    if (ImagingU.GetVideoMetadata(mim.MediaItem.Folder.FullPath, mim.MediaItem.FileName) is not { } data) {
       mim.Success = false;
+      Log.Error("Can't read video metadata", mim.MediaItem.FilePath);
+      return;
     }
+
+    mim.Height = (int)data[0];
+    mim.Width = (int)data[1];
+    mim.Orientation = (int)data[2] switch {
+      90 => Orientation.Rotate90,
+      180 => Orientation.Rotate180,
+      270 => Orientation.Rotate270,
+      _ => Orientation.Normal,
+    };
+
+    mim.Success = true;
   }
 }
