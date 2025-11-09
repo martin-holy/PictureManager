0877d53f1d15c0e7a48b8839e313d19b2cd15795
martin-holy <martin.holy@hotmail.com>
2025-11-09 10:56:10 +0100

    droid [C]: TreeViewCategoriesV

--- src/PictureManager.Android/Views/Layout/MainWindowV.cs
+++ src/PictureManager.Android/Views/Layout/MainWindowV.cs
@@ -1,26 +1,23 @@
 ﻿using Android.Content;
-using Android.Views;
 using Android.Widget;
 using MH.UI.Android.Controls;
 using MH.UI.Android.Utils;
-using MH.UI.Controls;
 using MH.Utils;
 using PictureManager.Common.Features.MediaItem;
 using PictureManager.Common.Layout;
-using PictureManager.Common.Utils;
 
 namespace PictureManager.Android.Views.Layout;
 
 public class MainWindowV : LinearLayout {
   public SlidePanelsGridHost SlidePanels { get; }
-  public TabControlHost TreeViewCategories { get; }
+  public TreeViewCategoriesV TreeViewCategories { get; }
   public MiddleContentV MiddleContent { get; }
   public MainWindowVM DataContext { get; }
 
   public MainWindowV(Context context, MainWindowVM dataContext) : base(context) {
     DataContext = dataContext;
     
-    TreeViewCategories = new(context, dataContext.TreeViewCategories, _getTreeViewCategoriesView);
+    TreeViewCategories = new(context, dataContext.TreeViewCategories);
     MiddleContent = new(context, Common.Core.VM);
     SlidePanels = new(
       context,
@@ -35,10 +32,5 @@ public class MainWindowV : LinearLayout {
 
     this.Bind(Common.Core.VM.MediaViewer, x => x.UserInputMode, (v, p) =>
       v.SlidePanels.ViewPager.UserInputEnabled = p == MediaViewerVM.UserInputModes.Disabled);
-  }
-
-  private View? _getTreeViewCategoriesView(LinearLayout container, object? item) {
-    if (item is not TreeView tv) return null;
-    return new TreeViewHost(container.Context!, tv, MenuFactory.GetMenu);
   }
 }
---
+++ src/PictureManager.Android/Views/Layout/TreeViewCategoriesV.cs
@@ -0,0 +1,15 @@
+﻿using Android.Content;
+using Android.Views;
+using Android.Widget;
+using MH.UI.Android.Controls;
+using MH.UI.Controls;
+using PictureManager.Common.Utils;
+
+namespace PictureManager.Android.Views.Layout;
+
+public sealed class TreeViewCategoriesV(Context context, TabControl dataContext) : TabControlHost(context, dataContext) {
+  protected override View? _getItemView(LinearLayout container, object? item) =>
+    item is TreeView tv
+      ? new TreeViewHost(container.Context!, tv, MenuFactory.GetMenu)
+      : null;
+}
