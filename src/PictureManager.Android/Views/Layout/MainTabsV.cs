0c014929de1685c4799a9dd353900308e2de4497
martin-holy <martin.holy@hotmail.com>
2025-11-09 10:34:06 +0100

    droid [C]: MainTabsV

---
+++ src/PictureManager.Android/Views/Layout/MainTabsV.cs
@@ -0,0 +1,23 @@
+﻿using Android.Content;
+using Android.Views;
+using Android.Widget;
+using MH.UI.Android.Controls;
+using MH.UI.Android.Views;
+using MH.UI.Controls;
+using MH.UI.ViewModels;
+using PictureManager.Android.Views.Sections;
+using PictureManager.Common.Features.Common;
+using PictureManager.Common.Features.MediaItem;
+
+namespace PictureManager.Android.Views.Layout;
+
+public sealed class MainTabsV(Context context, TabControl dataContext) : TabControlHost(context, dataContext) {
+  protected override View? _getItemView(LinearLayout container, object? item) =>
+    item switch {
+      AllSettings allSettings => new SettingsV(container.Context!, allSettings),
+      MediaItemsViewVM miv => new MediaItemsViewV(container.Context!, miv),
+      LogVM log => new LogV(container.Context!, log),
+      View view => view,
+      _ => null
+    };
+}
--- src/PictureManager.Android/Views/Layout/MiddleContentV.cs
+++ src/PictureManager.Android/Views/Layout/MiddleContentV.cs
@@ -1,24 +1,19 @@
 ﻿using Android.Content;
 using Android.Views;
 using Android.Widget;
-using MH.UI.Android.Controls;
 using MH.UI.Android.Utils;
-using MH.UI.Android.Views;
-using MH.UI.ViewModels;
 using MH.Utils;
 using PictureManager.Android.Views.Sections;
 using PictureManager.Common;
-using PictureManager.Common.Features.Common;
-using PictureManager.Common.Features.MediaItem;
 
 namespace PictureManager.Android.Views.Layout;
 
 public class MiddleContentV : LinearLayout {
-  private readonly TabControlHost _mainTabs;
+  private readonly MainTabsV _mainTabs;
   private readonly MediaViewerV _mediaViewer;
 
   public MiddleContentV(Context context, CoreVM coreVM) : base(context) {
-    _mainTabs = new(context, coreVM.MainTabs, _getMainTabsView);
+    _mainTabs = new(context, coreVM.MainTabs);
     _mediaViewer = new(context, coreVM.MediaViewer);
 
     AddView(_mainTabs, new LayoutParams(LPU.Match, LPU.Match));
@@ -26,15 +21,6 @@ public class MiddleContentV : LinearLayout {
 
     this.Bind(coreVM.MediaViewer, x => x.IsVisible, (t, p) => t._updateVisibility(p));
   }
-
-  private View? _getMainTabsView(LinearLayout container, object? item) =>
-    item switch {
-      AllSettings allSettings => new SettingsV(container.Context!, allSettings),
-      MediaItemsViewVM miv => new MediaItemsViewV(container.Context!, miv),
-      LogVM log => new LogV(container.Context!, log),
-      View view => view,
-      _ => null
-    };
 
   private void _updateVisibility(bool viewerIsVisible) {
     _mainTabs.Visibility = viewerIsVisible ? ViewStates.Gone : ViewStates.Visible;
