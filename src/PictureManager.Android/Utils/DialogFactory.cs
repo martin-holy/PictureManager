c4a90a6065ba9a60b660da0f1702893fcf176620
martin-holy <martin.holy@hotmail.com>
2025-11-11 09:22:10 +0100

    droid: DialogFactory

--- src/PictureManager.Android/Utils/DialogFactory.cs
+++ src/PictureManager.Android/Utils/DialogFactory.cs
@@ -1,14 +1,22 @@
 ﻿using Android.Content;
 using Android.Views;
+using MH.UI.Android.Dialogs;
 using MH.UI.Controls;
+using MH.UI.Dialogs;
+using PictureManager.Common.Features.GeoLocation;
+using PictureManager.Common.Features.MediaItem;
+using PictureManager.Common.Features.MediaItem.Image;
 
 namespace PictureManager.Android.Utils;
 
 public static class DialogFactory {
-
   public static View? GetDialog(Context context, Dialog dataContext) {
-    // TODO add app dialogs
     return dataContext switch {
+      GetGeoNamesFromWebDialog => new ProgressDialogV(context, (IProgressDialog)dataContext),
+      ReadGeoLocationFromFilesDialog => new ProgressDialogV(context, (IProgressDialog)dataContext),
+      ComputeImageHashesDialog => new ProgressDialogV(context, (IProgressDialog)dataContext),
+      ReloadMetadataDialog => new ProgressDialogV(context, (IProgressDialog)dataContext),
+      SaveMetadataDialog => new ProgressDialogV(context, (IProgressDialog)dataContext),
       _ => null
     };
   }
