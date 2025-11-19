b27a9372c6804d3da2cb248c9b94bb944d027292
martin-holy <martin.holy@hotmail.com>
2025-11-19 11:53:49 +0100

    droid: ToolsTabsV

---
+++ src/PictureManager.Android/Views/Layout/ToolsTabsV.cs
@@ -0,0 +1,17 @@
+﻿using Android.Content;
+using Android.Views;
+using Android.Widget;
+using MH.UI.Android.Controls;
+using MH.UI.Controls;
+using PictureManager.Android.Views.Sections;
+using PictureManager.Common.Features.Person;
+
+namespace PictureManager.Android.Views.Layout;
+
+public sealed class ToolsTabsV(Context context, TabControl dataContext) : TabControlHost(context, dataContext) {
+  protected override View? _getItemView(LinearLayout container, object? item) =>
+    item switch {
+      PersonDetailVM personDetail => new PersonDetailV(container.Context!, personDetail),
+      _ => null
+    };
+}
