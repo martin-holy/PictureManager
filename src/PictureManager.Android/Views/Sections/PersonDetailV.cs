d8f447e3a327cb67b8e7fa52770033b130200315
martin-holy <martin.holy@hotmail.com>
2025-11-20 11:32:11 +0100

    droid: PersonDetailV

---
+++ src/PictureManager.Android/Views/Sections/PersonDetailV.cs
@@ -0,0 +1,33 @@
+﻿using Android.Content;
+using Android.Views;
+using Android.Widget;
+using MH.UI.Android.Controls;
+using MH.UI.Android.Extensions;
+using MH.UI.Android.Utils;
+using MH.UI.Interfaces;
+using PictureManager.Android.Views.Entities;
+using PictureManager.Common.Features.Person;
+using PictureManager.Common.Features.Segment;
+
+namespace PictureManager.Android.Views.Sections;
+
+public sealed class PersonDetailV : LinearLayout {
+  public PersonDetailV(Context context, PersonDetailVM dataContext) : base(context) {
+    Orientation = Orientation.Vertical;
+
+    var name = new TextView(context) {
+      Text = dataContext.PersonM?.Name,
+      TextSize = DisplayU.DpToPx(12),
+      Background = BackgroundFactory.Dark(),
+      TextAlignment = TextAlignment.Center
+    };
+    name.SetPadding(DimensU.Spacing);
+
+    AddView(name, new LayoutParams(LPU.Match, LPU.Wrap).WithDpMargin(2, 0, 2, 0));
+
+    AddView(new CollectionViewHost(context, dataContext.AllSegments, _getSegmentView));
+  }
+
+  private SegmentV? _getSegmentView(LinearLayout container, ICollectionViewGroup group, object? item) =>
+    item is SegmentM segment ? new SegmentV(container.Context!, segment) : null;
+}
