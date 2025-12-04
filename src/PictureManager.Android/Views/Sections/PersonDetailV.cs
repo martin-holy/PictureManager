f07fb9a73f5bf630feb37a9270764424d9f94e62
martin-holy <martin.holy@hotmail.com>
2025-12-04 19:31:05 +0100

    droid: PersonDetailV: keywords

--- src/PictureManager.Android/Views/Sections/PersonDetailV.cs
+++ src/PictureManager.Android/Views/Sections/PersonDetailV.cs
@@ -7,11 +7,12 @@ using MH.UI.Android.Utils;
 using MH.UI.Controls;
 using MH.UI.Interfaces;
 using MH.Utils;
+using MH.Utils.Interfaces;
 using PictureManager.Android.Views.Entities;
 using PictureManager.Common;
+using PictureManager.Common.Features.Keyword;
 using PictureManager.Common.Features.Person;
 using PictureManager.Common.Features.Segment;
-using System;
 
 namespace PictureManager.Android.Views.Sections;
 
@@ -19,7 +20,7 @@ public sealed class PersonDetailV : LinearLayout {
   private readonly PersonDetailVM _dataContext;
   private readonly TreeMenu _menu;
   private readonly TextView _personName;
-  private IDisposable? _personBind;
+  private readonly IconItemsLayout _keywords;
 
   public PersonDetailV(Context context, PersonDetailVM dataContext) : base(context) {
     Orientation = Orientation.Vertical;
@@ -41,25 +42,29 @@ public sealed class PersonDetailV : LinearLayout {
     iconMenuAndName.AddView(_personName, new FrameLayout.LayoutParams(LPU.Match, LPU.Wrap));
     iconMenuAndName.AddView(iconMenu, new FrameLayout.LayoutParams(LPU.Wrap, LPU.Wrap) { Gravity = GravityFlags.CenterVertical | GravityFlags.Left }.WithDpMargin(2));
 
+    _keywords = new IconItemsLayout(context, Res.IconTag, _getKeywordView) {
+      MaxHeight = DisplayU.DpToPx((float)(DimensU.IconSize * 1.5))
+    };
+
     AddView(iconMenuAndName, new LayoutParams(LPU.Match, LPU.Wrap).WithDpMargin(2, 0, 2, 0));
+    AddView(_keywords, new LayoutParams(LPU.Match, LPU.Wrap).WithDpMargin(2, 2, 2, 0));
     AddView(new CollectionViewHost(context, dataContext.TopSegments, _getSegmentView), new LayoutParams(LPU.Match, _getTopSegmentsHeight()));
     AddView(new CollectionViewHost(context, dataContext.AllSegments, _getSegmentView), new LayoutParams(LPU.Match, LPU.Wrap));
-
-    // TODO Binding: review this. try to create nested binding. what if x.PersonM is null?
-    // maybe: if person is null, than bind to x.PersonM to know when to drop the bind to x.PersonM and bind to PersonM.Name
-    /*this.Bind(dataContext, x => x.PersonM, (pd, person) => {
-      pd._personName.Text = person?.Name;
-      _personBind?.Dispose();
-
-      if (person != null)
-        _personBind = pd.Bind(person, x => x.Name, (pd, name) => pd._personName.Text = name);
-    });*/
 
     this.Bind<PersonDetailV, PersonDetailVM, string>(
       dataContext,
       [nameof(PersonDetailVM.PersonM), nameof(PersonM.Name)],
       [s => (s as PersonDetailVM)?.PersonM, s => (s as PersonM)?.Name],
       (t, v) => t._personName.Text = v);
+
+    this.Bind<PersonDetailV, PersonDetailVM, KeywordM[]>(
+      dataContext,
+      [nameof(PersonDetailVM.PersonM), nameof(PersonM.DisplayKeywords)],
+      [s => (s as PersonDetailVM)?.PersonM, s => (s as PersonM)?.DisplayKeywords],
+      (t, v) => {
+        _keywords.Visibility = v.Length == 0 ? ViewStates.Gone : ViewStates.Visible;
+        _keywords.WrapLayout.Items = v.Length == 0 ? null : v;
+      });
   }
 
   private SegmentV? _getSegmentView(LinearLayout container, ICollectionViewGroup group, object? item) =>
@@ -69,4 +74,9 @@ public sealed class PersonDetailV : LinearLayout {
     SegmentVM.SegmentUiFullWidth + (CollectionView.ItemBorderSize * 2) + // segment with border
     DimensU.IconButtonSize + (DimensU.Spacing * 2) + // group height
     DimensU.Spacing; // spacing
+
+  private TextView? _getKeywordView(object item) =>
+    item is IListItem li
+      ? new TextView(Context) { Text = li.Name, Background = BackgroundFactory.RoundDarker() }
+      : null;
 }
