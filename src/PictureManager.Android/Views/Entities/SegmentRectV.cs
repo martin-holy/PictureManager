e7c074ae11a5426614180169cca07b620391c700
martin-holy <martin.holy@hotmail.com>
2025-12-27 20:45:44 +0100

    droid: SegmentRectV

--- src/PictureManager.Android/Views/Entities/SegmentRectV.cs
+++ src/PictureManager.Android/Views/Entities/SegmentRectV.cs
@@ -5,33 +5,37 @@ using Android.Widget;
 using MH.UI.Android.Utils;
 using MH.Utils;
 using PictureManager.Common.Features.Segment;
+using System;
 
 namespace PictureManager.Android.Views.Entities;
 
 public class SegmentRectV : View {
-  private readonly SegmentRectM _dataContext;
-  
+  private readonly IDisposable[] _bindings;
+
   private Drawable? _bgSelected;
   private Drawable? _bgWithPerson;
   private Drawable? _bgWithoutPerson;
 
+  public SegmentRectM DataContext { get; }
   public delegate bool OnTouchAction(SegmentRectM segmentRect, double x, double y);
 
   public SegmentRectV(Context context, SegmentRectM dataContext) : base(context) {
-    _dataContext = dataContext;
+    DataContext = dataContext;
 
-    this.Bind(dataContext, nameof(SegmentRectM.Size), x => x.Size,
-      (t, p) => t.LayoutParameters = new FrameLayout.LayoutParams((int)p, (int)p));
-    this.Bind(dataContext, nameof(SegmentRectM.X), x => x.X, (t, p) => t.SetX((float)p));
-    this.Bind(dataContext, nameof(SegmentRectM.Y), x => x.Y, (t, p) => t.SetY((float)p));
-    this.Bind(dataContext.Segment, nameof(SegmentM.Person), x => x.Person, (t, _) => t._setBackground());
-    this.Bind(dataContext.Segment, nameof(SegmentM.IsSelected), x => x.IsSelected, (t, _) => t._setBackground());
+    _bindings = [
+      this.Bind(dataContext, nameof(SegmentRectM.Size), x => x.Size,
+        (t, p) => t.LayoutParameters = new FrameLayout.LayoutParams((int)p, (int)p)),
+      this.Bind(dataContext, nameof(SegmentRectM.X), x => x.X, (t, p) => t.SetX((float)p)),
+      this.Bind(dataContext, nameof(SegmentRectM.Y), x => x.Y, (t, p) => t.SetY((float)p)),
+      this.Bind(dataContext.Segment, nameof(SegmentM.Person), x => x.Person, (t, _) => t._setBackground()),
+      this.Bind(dataContext.Segment, nameof(SegmentM.IsSelected), x => x.IsSelected, (t, _) => t._setBackground())
+    ];
   }
 
   private void _setBackground() {
-    Background = _dataContext.Segment.IsSelected
+    Background = DataContext.Segment.IsSelected
       ? _bgSelected ??= _createBackground(Resource.Color.segmentRectSelected)
-      : _dataContext.Segment.Person == null
+      : DataContext.Segment.Person == null
         ? _bgWithoutPerson ??= _createBackground(Resource.Color.segmentRectWithoutPerson)
         : _bgWithPerson ??= _createBackground(Resource.Color.segmentRectWithPerson);
   }
@@ -43,4 +47,11 @@ public class SegmentRectV : View {
       Resource.Dimension.border_stroke_width,
       -1,
       Resource.Color.c_black5);
+
+  protected override void Dispose(bool disposing) {
+    if (disposing)
+      foreach (var bind in _bindings) bind.Dispose();
+
+    base.Dispose(disposing);
+  }
 }
