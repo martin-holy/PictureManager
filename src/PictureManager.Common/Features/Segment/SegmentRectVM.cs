6a33b35c7f2869a8230222d25c2c9e7a8f77b057
martin-holy <martin.holy@hotmail.com>
2025-12-29 21:36:34 +0100

    [Core][N] SegmentRectVM: CanCreateNew prop

--- src/PictureManager.Common/Features/Segment/SegmentRectVM.cs
+++ src/PictureManager.Common/Features/Segment/SegmentRectVM.cs
@@ -7,9 +7,11 @@ namespace PictureManager.Common.Features.Segment;
 public sealed class SegmentRectVM : ObservableObject {
   private bool _showOverMediaItem;
   private bool _isEditEnabled;
+  private bool _canCreateNew;
 
   public bool ShowOverMediaItem { get => _showOverMediaItem; set { _showOverMediaItem = value; OnPropertyChanged(); } }
   public bool IsEditEnabled { get => _isEditEnabled; set { _isEditEnabled = value; OnPropertyChanged(); } }
+  public bool CanCreateNew { get => _canCreateNew; set { _setIfVary(ref _canCreateNew, value); } }
   public List<Tuple<int, int, int, bool>>? SegmentToolTipRects { get; private set; }
 
   public static RelayCommand<SegmentM> ReloadSegmentToolTipCommand { get; set; } = null!;
