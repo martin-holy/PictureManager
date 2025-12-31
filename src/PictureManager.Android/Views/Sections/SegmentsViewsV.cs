37fbe13e2475b781e7caf52842f52ee87a9291ba
martin-holy <martin.holy@hotmail.com>
2025-12-31 13:18:32 +0100

    droid: SegmentsViewsV

---
+++ src/PictureManager.Android/Views/Sections/SegmentsViewsV.cs
@@ -0,0 +1,13 @@
+﻿using Android.Content;
+using Android.Widget;
+using MH.UI.Android.Utils;
+using PictureManager.Common.Features.Segment;
+
+namespace PictureManager.Android.Views.Sections;
+
+public sealed class SegmentsViewsV : FrameLayout {
+  public SegmentsViewsV(Context context, SegmentsViewsVM dataContext) : base(context) {
+    SetBackgroundResource(Resource.Color.c_static_ba);
+    AddView(new SegmentsViewsTabsV(context, dataContext.Tabs), new LayoutParams(LPU.Match, LPU.Match));
+  }
+}
