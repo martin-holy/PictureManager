7e3bfb446c6d9a7f59d1ecb06b6e3e931ce83b55
martin-holy <martin.holy@hotmail.com>
2025-12-31 13:18:20 +0100

    droid: SegmentsViewsTabsV

---
+++ src/PictureManager.Android/Views/Sections/SegmentsViewsTabsV.cs
@@ -0,0 +1,13 @@
+﻿using Android.Content;
+using Android.Views;
+using Android.Widget;
+using MH.UI.Android.Controls;
+using MH.UI.Controls;
+using PictureManager.Common.Features.Segment;
+
+namespace PictureManager.Android.Views.Sections;
+
+public sealed class SegmentsViewsTabsV(Context context, TabControl dataContext) : TabControlHost(context, dataContext) {
+  protected override View? _getItemView(LinearLayout container, object? item) =>
+    item is SegmentsViewVM segmentsView ? new SegmentsViewV(container.Context!, segmentsView) : null;
+}
