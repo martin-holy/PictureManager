3ed49ab1e7332377fc7552f412495e45fd7737c4
martin-holy <martin.holy@hotmail.com>
2025-12-31 13:14:37 +0100

    droid: PersonThumbV

--- src/PictureManager.Android/Views/Entities/PersonThumbV.cs
+++ src/PictureManager.Android/Views/Entities/PersonThumbV.cs
@@ -1,20 +1,32 @@
 ﻿using Android.Content;
 using Android.Views;
 using Android.Widget;
+using MH.UI.Android.Controls;
+using MH.UI.Android.Extensions;
 using MH.UI.Android.Utils;
+using MH.Utils;
 using PictureManager.Common.Features.Person;
+using PictureManager.Common.Features.Segment;
 
 namespace PictureManager.Android.Views.Entities;
 
 public sealed class PersonThumbV : FrameLayout {
   public PersonThumbV(Context context, PersonM person) : base(context) {
-    if (person.Segment is { } segment) {
-      AddView(new SegmentV(context, segment), new LayoutParams(LPU.Match, LPU.Match));
+    this.Bind(person, nameof(PersonM.Segment), x => x.Segment, _onSegmentChanged);
+  }
+
+  private static void _onSegmentChanged(PersonThumbV self, SegmentM? segment) {
+    if (self.ChildCount > 0 && self.GetChildAt(0) is { } child) {
+      self.RemoveViewAt(0);
+      child.Dispose();
     }
-    else {
-      var noSegmentIcon = new ImageView(context);
-      noSegmentIcon.SetImageResource(Resource.Drawable.icon_people);
-      AddView(noSegmentIcon, new LayoutParams(LPU.Wrap, LPU.Wrap) { Gravity = GravityFlags.Center });
+
+    if (segment == null) {
+      self.AddView(new IconView(self.Context!)
+        .Bind(Resource.Drawable.icon_people, Resource.Color.gray5),
+        0, new LayoutParams(LPU.Match, LPU.Match) { Gravity = GravityFlags.Center }.WithDpMargin(DimensU.Spacing));
     }
+    else
+      self.AddView(new SegmentV(self.Context!, segment), 0, new LayoutParams(LPU.Match, LPU.Match));
   }
 }
