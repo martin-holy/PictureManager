a58c4b3b5b991ccb6e9c3788203492c2045ab48b
martin-holy <martin.holy@hotmail.com>
2026-01-01 20:50:07 +0100

    droid: PersonDetailV: custom menu replaced by TabItem menu

--- src/PictureManager.Android/Views/Sections/PersonDetailV.cs
+++ src/PictureManager.Android/Views/Sections/PersonDetailV.cs
@@ -5,10 +5,8 @@ using MH.UI.Android.Controls;
 using MH.UI.Android.Extensions;
 using MH.UI.Android.Utils;
 using MH.UI.Controls;
-using MH.UI.Interfaces;
 using MH.Utils;
 using MH.Utils.Interfaces;
-using PictureManager.Android.Views.Entities;
 using PictureManager.Common;
 using PictureManager.Common.Features.Keyword;
 using PictureManager.Common.Features.Person;
@@ -17,39 +15,27 @@ using PictureManager.Common.Features.Segment;
 namespace PictureManager.Android.Views.Sections;
 
 public sealed class PersonDetailV : LinearLayout {
-  private readonly PersonDetailVM _dataContext;
-  private readonly TreeMenu _menu;
   private readonly TextView _personName;
   private readonly IconItemsLayout _keywords;
 
   public PersonDetailV(Context context, PersonDetailVM dataContext) : base(context) {
     Orientation = Orientation.Vertical;
 
-    _dataContext = dataContext;
-    _menu = new(context, _ => dataContext.Menu);
-
-    var iconMenu = new IconButton(context)
-      .WithClickAction(this, (o, s) => o._menu.ShowItemMenu(s, o._dataContext.PersonM));
-    iconMenu.SetImageDrawable(Icons.GetIcon(context, Res.IconThreeBars));
-
     _personName = new TextView(context) {
       TextSize = 24f,
-      TextAlignment = TextAlignment.Center
+      TextAlignment = TextAlignment.Center,
+      Background = BackgroundFactory.Dark()
     };
     _personName.SetPadding(DimensU.Spacing);
-
-    var iconMenuAndName = new FrameLayout(context) { Background = BackgroundFactory.Dark() };
-    iconMenuAndName.AddView(_personName, new FrameLayout.LayoutParams(LPU.Match, LPU.Wrap) { Gravity = GravityFlags.Center });
-    iconMenuAndName.AddView(iconMenu, new FrameLayout.LayoutParams(LPU.Wrap, LPU.Wrap) { Gravity = GravityFlags.CenterVertical | GravityFlags.Left }.WithDpMargin(2));
 
     _keywords = new IconItemsLayout(context, Res.IconTag, _getKeywordView) {
       MaxHeight = DisplayU.DpToPx((float)(DimensU.IconSize * 1.5))
     };
 
-    AddView(iconMenuAndName, new LayoutParams(LPU.Match, LPU.Wrap).WithDpMargin(2, 0, 2, 0));
+    AddView(_personName, new LayoutParams(LPU.Match, LPU.Wrap) { Gravity = GravityFlags.Center }.WithDpMargin(2, 0, 2, 0));
     AddView(_keywords, new LayoutParams(LPU.Match, LPU.Wrap).WithDpMargin(2, 2, 2, 0));
-    AddView(new CollectionViewHost(context, dataContext.TopSegments, _getSegmentView), new LayoutParams(LPU.Match, _getTopSegmentsHeight()));
-    AddView(new CollectionViewHost(context, dataContext.AllSegments, _getSegmentView), new LayoutParams(LPU.Match, LPU.Wrap));
+    AddView(new CollectionViewHost(context, dataContext.TopSegments, SegmentsViewV.GetSegmentV), new LayoutParams(LPU.Match, _getTopSegmentsHeight()));
+    AddView(new CollectionViewHost(context, dataContext.AllSegments, SegmentsViewV.GetSegmentV), new LayoutParams(LPU.Match, LPU.Wrap));
 
     this.Bind<PersonDetailV, string>(
       dataContext,
@@ -67,9 +53,6 @@ public sealed class PersonDetailV : LinearLayout {
         _keywords.WrapLayout.Items = empty ? null : v;
       });
   }
-
-  private SegmentV? _getSegmentView(LinearLayout container, ICollectionViewGroup group, object? item) =>
-    item is SegmentM segment ? new SegmentV(container.Context!, segment) : null;
 
   private static int _getTopSegmentsHeight() =>
     SegmentVM.SegmentUiFullWidth + (CollectionView.ItemBorderSize * 2) + // segment with border
