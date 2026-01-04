8555840552d5bef63aea13e6633b94b98f69508e
martin-holy <martin.holy@hotmail.com>
2026-01-04 21:19:37 +0100

    droid: AboutDialogV

--- src/PictureManager.Android/Utils/DialogFactory.cs
+++ src/PictureManager.Android/Utils/DialogFactory.cs
@@ -3,6 +3,8 @@ using Android.Views;
 using MH.UI.Android.Dialogs;
 using MH.UI.Controls;
 using MH.UI.Dialogs;
+using PictureManager.Android.Views.Dialogs;
+using PictureManager.Common.Features.Common;
 using PictureManager.Common.Features.GeoLocation;
 using PictureManager.Common.Features.MediaItem;
 using PictureManager.Common.Features.MediaItem.Image;
@@ -12,9 +14,10 @@ namespace PictureManager.Android.Utils;
 public static class DialogFactory {
   public static View? GetDialog(Context context, Dialog dataContext) {
     return dataContext switch {
+      AboutDialog => new AboutDialogV(context, (AboutDialog)dataContext),
+      ComputeImageHashesDialog => new ProgressDialogV(context, (IProgressDialog)dataContext),
       GetGeoNamesFromWebDialog => new ProgressDialogV(context, (IProgressDialog)dataContext),
       ReadGeoLocationFromFilesDialog => new ProgressDialogV(context, (IProgressDialog)dataContext),
-      ComputeImageHashesDialog => new ProgressDialogV(context, (IProgressDialog)dataContext),
       ReloadMetadataDialog => new ProgressDialogV(context, (IProgressDialog)dataContext),
       SaveMetadataDialog => new ProgressDialogV(context, (IProgressDialog)dataContext),
       _ => null
---
+++ src/PictureManager.Android/Views/Dialogs/AboutDialogV.cs
@@ -0,0 +1,33 @@
+﻿using Android.Content;
+using Android.Widget;
+using MH.UI.Android.Extensions;
+using MH.UI.Android.Utils;
+using PictureManager.Common.Features.Common;
+
+namespace PictureManager.Android.Views.Dialogs;
+
+public sealed class AboutDialogV : LinearLayout {
+  public AboutDialogV(Context context, AboutDialog dataContext) : base(context) {
+    Orientation = Orientation.Vertical;
+
+    AddView(new TextView(context) { Text = Resources!.GetString(Resource.String.app_name, null), TextSize = 26 },
+      new LayoutParams(LPU.Wrap, LPU.Wrap).WithMargin(DimensU.Spacing));
+    
+    AddView(new TextView(context) { Text = dataContext.Version },
+      new LayoutParams(LPU.Wrap, LPU.Wrap).WithMargin(DimensU.Spacing));
+    
+    // TODO decide what is the best multiplatform way to get Copyright :)
+    AddView(new TextView(context) { Text = "Martin Holý (2015 - 2026)" },
+      new LayoutParams(LPU.Wrap, LPU.Wrap).WithMargin(DimensU.Spacing));
+
+    AddView(new TextView(context) { Text = dataContext.HomePageUrl }
+      .WithTextColor(Resource.Color.c_accent)
+      .WithCommand(dataContext.OpenHomePageCommand),
+      new LayoutParams(LPU.Wrap, LPU.Wrap).WithMargin(DimensU.Spacing));
+
+    AddView(new TextView(context) { Text = dataContext.TelegramUrl }
+      .WithTextColor(Resource.Color.c_accent)
+      .WithCommand(dataContext.OpenTelegramCommand),
+      new LayoutParams(LPU.Wrap, LPU.Wrap).WithMargin(DimensU.Spacing));
+  }
+}
