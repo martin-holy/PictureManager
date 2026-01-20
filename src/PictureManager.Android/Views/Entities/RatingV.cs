36729e4b515208530827443af02bb829920f31f3
martin-holy <martin.holy@hotmail.com>
2026-01-20 21:52:42 +0100

    droid: RatingV

---
+++ src/PictureManager.Android/Views/Entities/RatingV.cs
@@ -0,0 +1,24 @@
+﻿using Android.Content;
+using Android.Widget;
+using MH.UI.Android.Controls;
+using PictureManager.Common;
+using PictureManager.Common.Features.Rating;
+
+namespace PictureManager.Android.Views.Entities;
+
+public sealed class RatingV : LinearLayout {
+  public RatingV(Context context, RatingM dataContext) : base(context) {
+    Orientation = Orientation.Horizontal;
+
+    AddView(new IconView(context, Res.IconStar, _getColor(0, dataContext)));
+    AddView(new IconView(context, Res.IconStar, _getColor(1, dataContext)));
+    AddView(new IconView(context, Res.IconStar, _getColor(2, dataContext)));
+    AddView(new IconView(context, Res.IconStar, _getColor(3, dataContext)));
+    AddView(new IconView(context, Res.IconStar, _getColor(4, dataContext)));
+  }
+
+  private static int _getColor(int position, RatingM rating) =>
+    position < rating.Value
+      ? Resource.Color.c_white
+      : Resource.Color.gray4;
+}
