9bc2562d39710de03431ffbe9bd81aaa32e6987c
martin-holy <martin.holy@hotmail.com>
2026-01-24 15:09:57 +0100

    droid: TreeViewCategoriesV: rating

--- src/PictureManager.Android/Views/Layout/TreeViewCategoriesV.cs
+++ src/PictureManager.Android/Views/Layout/TreeViewCategoriesV.cs
@@ -3,13 +3,20 @@ using Android.Views;
 using Android.Widget;
 using MH.UI.Android.Controls;
 using MH.UI.Controls;
+using PictureManager.Android.Views.Entities;
+using PictureManager.Common.Features.Rating;
 using PictureManager.Common.Utils;
+using System.Linq;
 
 namespace PictureManager.Android.Views.Layout;
 
 public sealed class TreeViewCategoriesV(Context context, TabControl dataContext) : TabControlHost(context, dataContext) {
-  protected override View? _getItemView(LinearLayout container, object? item) =>
-    item is TreeView tv
-      ? new TreeViewHost(container.Context!, tv, MenuFactory.GetMenu)
-      : null;
+  protected override View? _getItemView(LinearLayout container, object? item) {
+    if (item is not TreeView tv) return null;
+
+    if (tv.RootHolder.FirstOrDefault() is RatingTreeCategory rating)
+      return new TreeViewHost(container.Context!, tv, MenuFactory.GetMenu, new RatingTreeItemViewHolderFactory());
+
+    return new TreeViewHost(container.Context!, tv, MenuFactory.GetMenu);
+  }
 }
