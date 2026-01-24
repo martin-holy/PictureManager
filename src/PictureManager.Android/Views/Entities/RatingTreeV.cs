2b9f5713024936dde0d25351e16926f0f6f58f98
martin-holy <martin.holy@hotmail.com>
2026-01-24 15:05:45 +0100

    droid: RatingTreeV

---
+++ src/PictureManager.Android/Views/Entities/RatingTreeV.cs
@@ -0,0 +1,25 @@
+﻿using Android.Content;
+using Android.Widget;
+using MH.UI.Android.Controls;
+using MH.UI.Android.Extensions;
+using MH.UI.Android.Utils;
+using MH.Utils.BaseClasses;
+using PictureManager.Common.Features.Rating;
+
+namespace PictureManager.Android.Views.Entities;
+
+public sealed class RatingTreeV : FlatTreeItemViewHolder {
+  private readonly RatingV _ratingV;
+  protected override bool _showName => false;
+
+  public RatingTreeV(Context context, TreeViewHost treeViewHost) : base(context, treeViewHost) {
+    _ratingV = new RatingV(context);
+    _container.AddView(_ratingV, new LinearLayout.LayoutParams(LPU.Wrap, LPU.Wrap).WithMargin(DimensU.Spacing, 0,0,0));
+  }
+
+  public override void Bind(FlatTreeItem? item) {
+    base.Bind(item);
+    if (item?.TreeItem is RatingTreeM rt)
+      _ratingV.Bind(rt.Rating);
+  }
+}
