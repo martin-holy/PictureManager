dfc18668c51effff49bccc40d9c932e3041a702c
martin-holy <martin.holy@hotmail.com>
2026-02-05 20:31:43 +0100

    droid: MediaItemThumbFullV

--- src/PictureManager.Android/Views/Entities/MediaItemThumbFullV.cs
+++ src/PictureManager.Android/Views/Entities/MediaItemThumbFullV.cs
@@ -1,21 +1,24 @@
 ﻿using Android.Content;
 using Android.Views;
 using Android.Widget;
+using MH.UI.Android.Controls;
 using MH.UI.Android.Utils;
 using PictureManager.Common;
 using PictureManager.Common.Features.MediaItem;
 using PictureManager.Common.Features.MediaItem.Image;
 using PictureManager.Common.Features.MediaItem.Video;
 using System;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace PictureManager.Android.Views.Entities;
 
-public class MediaItemThumbFullV : FrameLayout {
+public class MediaItemThumbFullV : FrameLayout, ICollectionViewItemContent {
   private readonly ImageView _image;
   private readonly ImageView _videoOverlayer;
+  private CancellationTokenSource? _cts;
 
-  public MediaItemM? DataContext { get; private set; }
+  public View View => this;
 
   public MediaItemThumbFullV(Context context) : base(context) {
     _image = new(context);
@@ -26,36 +29,60 @@ public class MediaItemThumbFullV : FrameLayout {
     AddView(_videoOverlayer, new LayoutParams(LPU.Wrap, LPU.Wrap) { Gravity = GravityFlags.Center });
   }
 
-  public MediaItemThumbFullV Bind(MediaItemM? mi) {
-    DataContext = mi;
+  public void Bind(object item) {
+    _cts?.Cancel();
+    if (item is not MediaItemM mi) return;
     _videoOverlayer.Visibility = mi is VideoM ? ViewStates.Visible : ViewStates.Gone;
-
-    if (mi == null) {
-      _image.SetImageBitmap(null);
-      return this;
-    }
-    
-    _loadThumbnailAsync(mi, _image, Context!);
-    return this;
+    _cts = new CancellationTokenSource();
+    _ = _loadThumbnailAsync(mi, _image, Context!, _cts.Token);
   }
 
-  private static async void _loadThumbnailAsync(MediaItemM mi, ImageView imageView, Context context) {
-    var thumbnail = await Task.Run(async () => {
-      try {
-        return mi switch {
-          ImageM => await MediaStoreU.GetImageThumbnail(mi.FilePath, context, 512)
-                      ?? ImagingU.CreateImageThumbnail(mi.FilePath, Core.Settings.MediaItem.ThumbSize),
-          VideoM => await MediaStoreU.GetVideoThumbnail(mi.FilePath, context, 512)
-                      ?? ImagingU.CreateVideoThumbnail(mi.FilePath, Core.Settings.MediaItem.ThumbSize),
-          _ => null,
-        };
-      }
-      catch (Exception ex) {
-        MH.Utils.Log.Error(ex);
-        return null;
-      }
-    });
+  public void Unbind() {
+    _cts?.Cancel();
+    _cts?.Dispose();
+    _cts = null;
+    _image.SetImageBitmap(null);
+  }
 
-    MH.Utils.Tasks.Dispatch(() => imageView.SetImageBitmap(thumbnail?.ApplyOrientation(mi.Orientation)));
+  private static async Task _loadThumbnailAsync(MediaItemM mi, ImageView imageView, Context context, CancellationToken token) {
+    try {
+      var bitmap = await Task.Run(async () => {
+        try {
+          token.ThrowIfCancellationRequested();
+
+          var filePath = mi.FilePath;
+          var thumbSize = Core.Settings.MediaItem.ThumbSize;
+
+          var thumb = mi switch {
+            ImageM => await MediaStoreU.GetImageThumbnail(filePath, context, 512)
+                      ?? ImagingU.CreateImageThumbnail(filePath, thumbSize),
+            VideoM => await MediaStoreU.GetVideoThumbnail(filePath, context, 512)
+                      ?? ImagingU.CreateVideoThumbnail(filePath, thumbSize),
+            _ => null
+          };
+
+          token.ThrowIfCancellationRequested();
+
+          return thumb?.ApplyOrientation(mi.Orientation);
+        }
+        catch (OperationCanceledException) {
+          throw;
+        }
+        catch (Exception ex) {
+          MH.Utils.Log.Error(ex);
+          return null;
+        }
+      }, token);
+
+      if (token.IsCancellationRequested) return;
+
+      imageView.Post(() => {
+        if (!token.IsCancellationRequested)
+          imageView.SetImageBitmap(bitmap);
+      });
+    }
+    catch (OperationCanceledException) {
+      // ignored
+    }
   }
 }
