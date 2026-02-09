5aee69eeb64153a395fd2f21ca3cd54f9567299c
martin-holy <martin.holy@hotmail.com>
2026-02-09 09:49:52 +0100

    droid: MediaItemsToolBarPanel

--- src/PictureManager.Android/Views/Sections/ToolBarPanels/MediaItemsToolBarPanel.cs
+++ src/PictureManager.Android/Views/Sections/ToolBarPanels/MediaItemsToolBarPanel.cs
@@ -2,6 +2,8 @@
 using Android.Widget;
 using MH.UI.Android.Controls;
 using MH.UI.Android.Utils;
+using PictureManager.Common;
+using PictureManager.Common.Features.MediaItem;
 
 namespace PictureManager.Android.Views.Sections.ToolBarPanels;
 
@@ -9,5 +11,11 @@ public sealed class MediaItemsToolBarPanel : LinearLayout {
   public MediaItemsToolBarPanel(Context context) : base(context) {
     Orientation = Orientation.Horizontal;
     AddView(new IconButton(context).WithCommand(CoreUI.ShareMediaItemsCommand));
+    AddView(new IconButton(context).WithCommand(MediaItemVM.CommentCommand));
+    AddView(new IconButton(context).WithCommand(CoreVM.RotateMediaItemsCommand));
+    AddView(new IconButton(context).WithCommand(MediaItemVM.DeleteCommand));
+    AddView(new IconButton(context).WithCommand(MediaItemsViewsVM.AddViewCommand));
+    AddView(new IconButton(context).WithCommand(CoreVM.OpenSegmentsViewsCommand));
+    // TODO add thumbs scale
   }
 }
