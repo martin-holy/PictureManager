1f17d42e98fab12002345fd3554ad1f5ca2341a6
martin-holy <martin.holy@hotmail.com>
2026-03-18 13:48:36 +0100

    [U][Core] MainMenu: Removed Left, Middle and Right Tabs from menu

--- src/PictureManager.Common/Layout/MainMenuVM.cs
+++ src/PictureManager.Common/Layout/MainMenuVM.cs
@@ -9,17 +9,12 @@ using PictureManager.Common.Features.Segment;
 using PictureManager.Common.Features.WhatIsNew;
 using System;
 using System.Collections.Generic;
-using System.Collections.Specialized;
 using System.Linq;
 using System.Windows.Input;
 
 namespace PictureManager.Common.Layout;
 
 public class MainMenuVM : TreeView {
-  private readonly MenuItem _leftTabs = new(Res.IconTabLeft, "Left tabs");
-  private readonly MenuItem _middleTabs = new(Res.IconTabMiddle, "Middle tabs");
-  private readonly MenuItem _rightTabs = new(Res.IconTabRight, "Right tabs");
-
   public string Icon { get; } = Res.IconThreeBars;
   public Func<IEnumerable<ITreeItem>> BuildMenu => () => RootHolder;
 
@@ -59,17 +54,9 @@ public class MainMenuVM : TreeView {
       new MenuItem(SegmentsDrawerVM.RemoveSelectedCommand)
     ]);
 
-    _initTabs(coreVM.MainWindow.TreeViewCategories, _leftTabs);
-    _initTabs(coreVM.MainTabs, _middleTabs);
-    _initTabs(coreVM.ToolsTabs, _rightTabs);
-
     RootHolder.Add(geoLocation);
     RootHolder.Add(mediaItem);
     RootHolder.Add(segments);
-    RootHolder.Add(new MenuItemSeparator());
-    RootHolder.Add(_leftTabs);
-    RootHolder.Add(_middleTabs);
-    RootHolder.Add(_rightTabs);
     RootHolder.Add(new MenuItemSeparator());
     RootHolder.Add(new MenuItem(CoreVM.SaveDbCommand));
     RootHolder.Add(new MenuItem(CoreVM.OpenSettingsCommand));
@@ -81,48 +68,5 @@ public class MainMenuVM : TreeView {
     foreach (var menuItem in RootHolder.Flatten().OfType<MenuItem>())
       if (commands.Contains(menuItem.Command))
         menuItem.IsHidden = true;
-  }
-
-  private void _initTabs(TabControl tabControl, MenuItem root) {
-    foreach (var tab in tabControl.Tabs)
-      _addTab(tabControl, tab, root);
-
-    tabControl.Tabs.CollectionChanged += (_, e) => _onTabsChanged(tabControl, root, e);
-  }
-
-  private void _addTab(TabControl tabControl, IListItem tab, MenuItem root) {
-    tab.PropertyChanged += (_, e) => {
-      if (!e.Is(nameof(IListItem.Name))) return;
-      root.ReplaceWithData(tab, _createTabMenuItem(tabControl, tab));
-    };
-
-    root.Add(_createTabMenuItem(tabControl, tab));
-  }
-
-  private MenuItem _createTabMenuItem(TabControl tabControl, IListItem tab) =>
-    new(new RelayCommand<IListItem>(
-      x => tabControl.Activate(x!.Data!),
-      x => x is { Data : { } },
-      tab.Icon,
-      tab.Name), tab) { Data = tab };
-
-  private void _onTabsChanged(TabControl tabControl, MenuItem root, NotifyCollectionChangedEventArgs e) {
-    switch (e.Action) {
-      case NotifyCollectionChangedAction.Add:
-        if (e.NewItems == null) return;
-        foreach (var tab in e.NewItems)
-          _addTab(tabControl, (IListItem)tab, root);
-        break;
-      case NotifyCollectionChangedAction.Remove:
-        if (e.OldItems == null) return;
-        foreach (var tab in e.OldItems)
-          root.RemoveWithData(tab);
-        break;
-      default:
-        root.Items.Clear();
-        foreach (var tab in tabControl.Tabs)
-          _addTab(tabControl, tab, root);
-        break;
-    }
   }
 }
