b9319952163319ab0838d17a80402f051c118e30
martin-holy <martin.holy@hotmail.com>
2026-03-21 12:58:36 +0100

    droid: FileOperationDialogV

--- src/PictureManager.Android/Views/Dialogs/FileOperationDialogV.cs
+++ src/PictureManager.Android/Views/Dialogs/FileOperationDialogV.cs
@@ -18,24 +18,34 @@ public sealed class FileOperationDialogV : LinearLayout {
     var fromText = new TextView(context) { Text = "From:" };
     var fromPath = new TextView(context)
       .BindText(dataContext, nameof(FileOperationDialog.DirFrom), x => x.DirFrom, x => x, bindings);
-    
+
     var toText = new TextView(context) { Text = "To:" };
     var toPath = new TextView(context)
       .BindText(dataContext, nameof(FileOperationDialog.DirTo), x => x.DirTo, x => x, bindings);
-    
+
     var fileText = new TextView(context) { Text = "File:" };
     var fileName = new TextView(context)
       .BindText(dataContext, nameof(FileOperationDialog.FileName), x => x.FileName, x => x, bindings);
-    
+
     var progress = new ProgressBar(Context) { Max = 100 };
     dataContext.Bind(nameof(FileOperationDialog.ProgressValue), x => x.ProgressValue, v => progress.Progress = v).DisposeWith(bindings);
     dataContext.Bind(nameof(FileOperationDialog.IsIndeterminate), x => x.IsIndeterminate, v => progress.Indeterminate = v).DisposeWith(bindings);
 
-    _addViews(this, [fromText, fromPath, toText, toPath, fileText, fileName, progress]);
-  }
+    var labels = LayoutU.Vertical(context)
+      .Add(fromText, LPU.LinearWrap().WithMargin(DimensU.Spacing))
+      .Add(toText, LPU.LinearWrap().WithMargin(DimensU.Spacing))
+      .Add(fileText, LPU.LinearWrap().WithMargin(DimensU.Spacing));
 
-  private static void _addViews(LinearLayout layout, View[] views) {
-    foreach (View view in views)
-      layout.AddView(view, LPU.LinearWrap().WithMargin(DimensU.Spacing));
+    var data = LayoutU.Vertical(context)
+      .Add(fromPath, LPU.LinearWrap().WithMargin(DimensU.Spacing))
+      .Add(toPath, LPU.LinearWrap().WithMargin(DimensU.Spacing))
+      .Add(fileName, LPU.LinearWrap().WithMargin(DimensU.Spacing));
+
+    var labelsAndData = LayoutU.Horizontal(context)
+      .Add(labels, LPU.LinearWrap())
+      .Add(data, LPU.LinearWrap());
+
+    AddView(labelsAndData, LPU.LinearWrap());
+    AddView(progress, LPU.Linear(LPU.Match, LPU.Wrap).WithMargin(DimensU.Spacing));
   }
 }
