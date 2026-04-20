596067f683159973240219be5fc16169e7a7ffeb
martin-holy <martin.holy@hotmail.com>
2026-04-20 16:35:18 +0200

    [Droid] MainWindowV: FullVideo <-> DetailVideo switching

--- src/PictureManager.Android/Views/Layout/MainWindowV.cs
+++ src/PictureManager.Android/Views/Layout/MainWindowV.cs
@@ -1,13 +1,19 @@
 ﻿using Android.Content;
+using Android.Views;
 using Android.Widget;
 using MH.UI.Android.Controls.Hosts.SlidePanelsGridHost;
 using MH.UI.Android.Utils;
+using MH.UI.Controls;
+using MH.Utils;
 using MH.Utils.Disposables;
+using MH.Utils.Interfaces;
+using PictureManager.Android.Views.Sections;
 using PictureManager.Common;
 
 namespace PictureManager.Android.Views.Layout;
 
 public class MainWindowV : FrameLayout {
+  private readonly CoreVM _coreVM;
   private readonly TreeViewCategoriesV _treeViewCategories;
   private readonly ToolBarV _toolBarV;
   private readonly ToolsTabsV _toolsTabsV;
@@ -17,6 +23,7 @@ public class MainWindowV : FrameLayout {
   public SlidePanelsGridHost SlidePanels { get; }
 
   public MainWindowV(Context context, CoreVM coreVM) : base(context) {
+    _coreVM = coreVM;
     _treeViewCategories = new(context, coreVM.MainWindow.TreeViewCategories, _bindings);
     _toolBarV = new(context, coreVM);
     _toolsTabsV = new(context, coreVM.MainWindow.ToolsTabs);
@@ -35,6 +42,27 @@ public class MainWindowV : FrameLayout {
     AddView(SlidePanels, LPU.FrameMatch());
 
     _toolBarV.Init(SlidePanels.ViewPager, _bindings);
+    SlidePanels.ViewPager.PageChangedEvent += _onPanelChanged;
+    coreVM.MainWindow.ToolsTabs
+      .Bind(nameof(TabControl.Selected), x => x.Selected, _onToolsTabChange, false)
+      .DisposeWith(_bindings);
+  }
+
+  private void _onPanelChanged(object? sender, View? view) {
+    if (view is MiddleContentV && _coreVM.MainWindow.IsInViewMode)
+      _middleContent.MediaViewer.ActivateCurrentPage();
+    else if (view is ToolsTabsV toolsTabsV)
+      _onToolsTabChange(toolsTabsV.DataContext.Selected);
+    else
+      _coreVM.Video.Stop();
+  }
+
+  private void _onToolsTabChange(IListItem? tab) {
+    var toolsTabsV = SlidePanels.ViewPager.GetCurrentItem() as ToolsTabsV;
+    if (toolsTabsV?.GetTabView(tab) is VideoDetailV videoDetailV)
+      videoDetailV.Activate();
+    else
+      _coreVM.Video.Stop();
   }
 
   protected override void Dispose(bool disposing) {
