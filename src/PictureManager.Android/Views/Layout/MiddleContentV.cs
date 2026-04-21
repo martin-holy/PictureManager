1a469c259aba3d311f5bbebea9423d78fe382314
martin-holy <martin.holy@hotmail.com>
2026-04-21 11:39:36 +0200

    [Droid] MiddleContentV

--- src/PictureManager.Android/Views/Layout/MiddleContentV.cs
+++ src/PictureManager.Android/Views/Layout/MiddleContentV.cs
@@ -11,29 +11,24 @@ using PictureManager.Common.Layout;
 namespace PictureManager.Android.Views.Layout;
 
 public class MiddleContentV : FrameLayout {
-  private readonly CoreVM _coreVM;
-
   public MainTabsV MainTabs { get; }
   public MediaViewerV MediaViewer { get; }
 
   public MiddleContentV(Context context, CoreVM coreVM, BindingScope bindings) : base(context) {
-    _coreVM = coreVM;
-
     MainTabs = new(context, coreVM.MainTabs);
     MediaViewer = new(context, coreVM.MediaViewer, bindings);
 
     AddView(MainTabs, LPU.FrameMatch());
     AddView(MediaViewer, LPU.FrameMatch());
 
-    coreVM.MainWindow.Bind(nameof(MainWindowVM.IsInViewMode), x => x.IsInViewMode, _onMainWindowIsInViewModeChanged);
+    coreVM.MainWindow.Bind(nameof(MainWindowVM.IsInViewMode), x => x.IsInViewMode, _updateVisibility);
   }
 
-  private void _onMainWindowIsInViewModeChanged(bool isInViewMode) {
+  private void _updateVisibility(bool isInViewMode) {
     if (isInViewMode) {
       MainTabs.Visibility = ViewStates.Gone;
       MediaViewer.Visibility = ViewStates.Visible;
     } else {
-      _coreVM.Video.MediaPlayer.IsPlaying = false;
       MediaViewer.Visibility = ViewStates.Gone;
       MainTabs.Visibility = ViewStates.Visible;
     }
