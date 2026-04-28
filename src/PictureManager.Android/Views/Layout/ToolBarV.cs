588a57f07aa9e16d1d705ed29c931501f487e77d
martin-holy <martin.holy@hotmail.com>
2026-04-28 18:05:39 +0200

    [Droid] ToolBarV: only in the middle panel

--- src/PictureManager.Android/Views/Layout/ToolBarV.cs
+++ src/PictureManager.Android/Views/Layout/ToolBarV.cs
@@ -1,10 +1,8 @@
 ﻿using Android.Content;
-using Android.Views;
 using MH.UI.Android.Controls;
 using MH.UI.Controls;
 using MH.Utils;
 using MH.Utils.Disposables;
-using MH.Utils.Interfaces;
 using PictureManager.Android.Views.Sections.ToolBarPanels;
 using PictureManager.Common;
 using PictureManager.Common.Features.MediaItem;
@@ -12,53 +10,41 @@ using PictureManager.Common.Layout;
 
 namespace PictureManager.Android.Views.Layout;
 
-public sealed class ToolBarV : ToolBar {
-  private readonly CoreVM _coreVM;
+public sealed class ToolBarV(Context context, CoreVM coreVM) : ToolBar(context) {
   private LoopPager? _loopPager;
-
-  public ToolBarV(Context context, CoreVM coreVM) : base(context) {
-    _coreVM = coreVM;
-  }
 
   public void Init(LoopPager loopPager, BindingScope bindings) {
     _loopPager = loopPager;
     DefaultPanelsKeys = ["common"];
 
-    RegisterPanel("common", () => new CommonToolBarPanel(Context!, _coreVM.MainWindow, bindings));
+    RegisterPanel("common", () => new CommonToolBarPanel(Context!, coreVM.MainWindow, bindings));
     RegisterPanel("mediaItems", () => new MediaItemsToolBarPanel(Context!, bindings));
-    RegisterPanel("mediaItemThumbs", () => new MediaItemThumbsToolBarPanel(Context!, _coreVM.MediaItem.Views, bindings));
-    RegisterPanel("mediaViewer", () => new MediaViewerToolBarPanel(Context!, _coreVM, bindings));
+    RegisterPanel("mediaItemThumbs", () => new MediaItemThumbsToolBarPanel(Context!, coreVM.MediaItem.Views, bindings));
+    RegisterPanel("mediaViewer", () => new MediaViewerToolBarPanel(Context!, coreVM, bindings));
 
     RegisterType(typeof(MediaViewerVM), ["common", "mediaItems", "mediaViewer"]);
     RegisterType(typeof(MediaItemsViewVM), ["common", "mediaItems", "mediaItemThumbs"]);
 
-    loopPager.PageChangedEvent += (_, page) => _updateToolBar(null, page);
+    loopPager.PageChanged += _updateToolBar;
     bindings.AddRange([
-      _coreVM.MainWindow.TreeViewCategories.Bind(nameof(TabControl.Selected), x => x.Selected, x => _updateToolBar(x, null), false),
-      _coreVM.MainTabs.Bind(nameof(TabControl.Selected), x => x.Selected, x => _updateToolBar(x, null), false),
-      _coreVM.MainWindow.ToolsTabs.Bind(nameof(TabControl.Selected), x => x.Selected, x => _updateToolBar(x, null), false),
-      _coreVM.MainWindow.Bind(nameof(MainWindowVM.IsInViewMode), x => x.IsInViewMode, _ => _updateToolBar(null, null), false)]);
+      coreVM.MainTabs.Bind(nameof(TabControl.Selected), x => x.Selected, x => _updateToolBar(-1), false),
+      coreVM.MainWindow.Bind(nameof(MainWindowVM.IsInViewMode), x => x.IsInViewMode, _ => _updateToolBar(-1), false)]);
     
-    _updateToolBar(null, null);
+    _updateToolBar(-1);
   }
 
-  private void _updateToolBar(IListItem? tab, View? page) {
-    page ??= _loopPager?.GetCurrentItem();
+  private void _updateToolBar(int index) {
+    if (index == -1)
+      index = _loopPager!.GetCurrentIndex();
 
-    if (_coreVM.MainWindow.IsInViewMode && page is MiddleContentV) {
-      Activate(typeof(MediaViewerVM));
+    if (index != 1) {
+      Activate(null);
       return;
     }
 
-    tab ??= _pageToSelectedTab(page);
-    Activate(tab?.Data?.GetType());
+    if (coreVM.MainWindow.IsInViewMode)
+      Activate(typeof(MediaViewerVM));
+    else
+      Activate(coreVM.MainTabs.Selected?.Data?.GetType());
   }
-
-  private IListItem? _pageToSelectedTab(View? page) =>
-    page switch {
-      TreeViewCategoriesV => _coreVM.MainWindow.TreeViewCategories.Selected,
-      MiddleContentV => _coreVM.MainTabs.Selected,
-      ToolsTabsV => _coreVM.ToolsTabs.Selected,
-      _ => null
-    };
 }
