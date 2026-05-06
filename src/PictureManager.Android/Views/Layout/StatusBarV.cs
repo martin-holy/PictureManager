bc892e49b3ea61a0622ff3a110a0ac7d0e4ad909
martin-holy <martin.holy@hotmail.com>
2026-05-06 12:17:07 +0200

    [Droid] StatusBarV

--- src/PictureManager.Android/Views/Layout/StatusBarV.cs
+++ src/PictureManager.Android/Views/Layout/StatusBarV.cs
@@ -15,6 +15,8 @@ namespace PictureManager.Android.Views.Layout;
 
 public sealed class StatusBarV : WrapLayout {
   private readonly StatusBarVM _statusBarVM;
+  private readonly BindingScope _currentBindings = new();
+  private MediaItemM? _current;
 
   private readonly IconItemsView _people;
   private readonly IconItemsView _keywords;
@@ -28,21 +30,23 @@ public sealed class StatusBarV : WrapLayout {
     AddView(_people, LPU.ViewGroupWrap());
     AddView(_keywords, LPU.ViewGroupWrap());
 
-    _statusBarVM.Bind<PersonM[]>(
-      [nameof(StatusBarVM.Current), nameof(MediaItemM.DisplayPeople)],
-      [x => (x as StatusBarVM)?.Current, x => (x as MediaItemM)?.DisplayPeople],
-      people => {
-        _people.Items = people;
-        _people.Visibility = people?.Length > 0 ? ViewStates.Visible : ViewStates.Gone;
-      });
+    _statusBarVM.Bind(nameof(StatusBarVM.Current), x => x.Current, _onCurrentChanged).DisposeWith(bindings);
+  }
 
-    _statusBarVM.Bind<string[]>(
-      [nameof(StatusBarVM.Current), nameof(MediaItemM.DisplayKeywords)],
-      [x => (x as StatusBarVM)?.Current, x => (x as MediaItemM)?.DisplayKeywords],
-      keywords => {
-        _keywords.Items = keywords;
-        _keywords.Visibility = keywords?.Length > 0 ? ViewStates.Visible : ViewStates.Gone;
-      });
+  private void _onCurrentChanged(MediaItemM? current) {
+    if (ReferenceEquals(_current, current)) return;
+    _current = current;
+    _currentBindings.Dispose();
+
+    _current?.Bind(nameof(MediaItemM.DisplayPeople), x => x.DisplayPeople, people => {
+      _people.Items = people;
+      _people.Visibility = people?.Length > 0 ? ViewStates.Visible : ViewStates.Gone;
+    });
+
+    _current?.Bind(nameof(MediaItemM.DisplayKeywords), x => x.DisplayKeywords, keywords => {
+      _keywords.Items = keywords;
+      _keywords.Visibility = keywords?.Length > 0 ? ViewStates.Visible : ViewStates.Gone;
+    });
   }
 
   private TextView? _getPersonView(object item) =>
