f961119f409bd2ea1bae6808b7f1a59abd35aaf4
martin-holy <martin.holy@hotmail.com>
2026-05-20 08:38:16 +0200

    Avalonia version deleted

--- src/PictureManager.AvaloniaUI/Utils/IImagingP.cs
+++
@@ -1,9 +0,0 @@
-﻿using Avalonia.Media.Imaging;
-using PictureManager.Common.Features.MediaItem;
-
-namespace PictureManager.AvaloniaUI.Utils;
-
-public interface IImagingP {
-  public void CreateImageThumbnail(MediaItemM mi);
-  public Bitmap? GetImageThumbnail(MediaItemM mi);
-}
